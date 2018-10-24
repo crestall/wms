0e80de95ed83349e5f70cc910e15c6e2f08cf7b3
marxolly <mark@solly.com.au>
2018-10-24 14:36:53 +1100

    Update app/models/Item.php

--- app/models/Item.php
+++ app/models/Item.php
@@ -550,7 +550,7 @@ class Item extends Model{
             $row_array['per_pallet'] = $row['per_pallet'];
             $row_array['total_available'] = $row['available'];
             $row_array['max_values'] = $row['choices'];
-            $row_array['select_choices'] = $row['select_choices'];
+            $row_array['select_values'] = $row['select_choices'];
             array_push($return_array,$row_array);
         }
         return $return_array;
