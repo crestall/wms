1b3aa6c607657570fa8222cdcbe7adf6063f576d
marxolly <mark@solly.com.au>
2018-10-29 16:42:52 +1100

    Update app/views/products/viewProducts.php

--- app/views/products/viewProducts.php
+++ app/views/products/viewProducts.php
@@ -54,7 +54,7 @@
                         </thead>
                         <tbody>
                             <?php foreach($products as $p):
-                                //$ppl = !empty($p['preferred_pick_location_id'])? $this->controller->location->getLocationName($p['preferred_pick_location_id']): "";
+                                $ppl = ($p['preferred_pick_location_id'] > 0)? $this->controller->location->getLocationName($p['preferred_pick_location_id']): "";
                                 echo "<pre>",print_r($p),"</pre>";
                                 ?>
                                 <tr>
@@ -66,7 +66,7 @@
                                     <td data-label="Pallet Item" class='text-center'><?php if($p['palletized'] > 0) echo "Yes"; else echo "No";?></td>
                                     <td data-label="Double Bay" class='text-center'><?php if($p['double_bay'] > 0) echo "Yes"; else echo "No";?></td>
                                     <td data-label="Requires Bubblewrap" class='text-center'><?php if($p['requires_bubblewrap'] > 0) echo "Yes"; else echo "No";?></td>
-                                    <td data-label="Preferred pick Location"><?php //echo $ppl;?></td>
+                                    <td data-label="Preferred pick Location"><?php echo $ppl;?></td>
                                     <td></td>
                                 </tr>
                             <?php endforeach;?>
