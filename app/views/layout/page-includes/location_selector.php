95c3148bcbf90778545ce0428e47dc3d4f999ab3
marxolly <mark@solly.com.au>
2018-10-30 09:51:15 +1100

    Update app/views/layout/page-includes/location_selector.php

--- app/views/layout/page-includes/location_selector.php
+++ app/views/layout/page-includes/location_selector.php
@@ -12,7 +12,7 @@ $pallet_multiplier = empty(Form::value('pallet_multiplier'))? 1 : Form::value('p
 </div>
 <div class="form-group row">
     <div class="form-check">
-        <label class="<?php if(isset($label_class)) echo $label_class; else echo "col-md-3";?> col-form-label" for="b2b">Add To Receiving</label>
+        <label class="<?php if(isset($label_class)) echo $label_class; else echo "col-md-3";?> col-form-label" for="to_receiving">Add To Receiving</label>
         <div class="<?php if(isset($div_class)) echo $div_class; else echo "col-md-4";?> checkbox checkbox-default">
             <input class="form-check-input styled" type="checkbox" id="to_receiving" name="to_receiving" <?php if(!empty(Form::value('to_receiving'))) echo 'checked';?> />
             <label for="to_receiving"></label>
