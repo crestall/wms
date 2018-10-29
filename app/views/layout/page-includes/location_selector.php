dc501aebf5c44467de6a43130af13479243c6a7e
marxolly <mark@solly.com.au>
2018-10-29 11:59:41 +1100

    Update app/views/layout/page-includes/location_selector.php

--- app/views/layout/page-includes/location_selector.php
+++ app/views/layout/page-includes/location_selector.php
@@ -2,8 +2,8 @@
 
 ?>
 <div class="form-group row">
-    <label class="col-md-5 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Location</label>
-    <div class="col-md-7">
+    <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Location</label>
+    <div class="col-md-4">
         <select id="add_to_location" name="add_to_location" class="form-control selectpicker" data-live-search="true"><option value="0">--Select One--</option><?php echo $this->controller->location->getSelectLocations($add_to_location, $item_id);?></select>
         <?php echo Form::displayError('add_to_location');?>
     </div>
