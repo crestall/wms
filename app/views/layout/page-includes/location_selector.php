ce9c0dda76092b712d6ffa308b5cd43b991958b8
marxolly <mark@solly.com.au>
2018-10-30 10:04:51 +1100

    Update app/views/layout/page-includes/location_selector.php

--- app/views/layout/page-includes/location_selector.php
+++ app/views/layout/page-includes/location_selector.php
@@ -11,13 +11,13 @@ $pallet_multiplier = empty(Form::value('pallet_multiplier'))? 1 : Form::value('p
     </div>
 </div>
 <div class="form-group row form-check">
-        <label class="<?php if(isset($label_class)) echo $label_class; else echo "col-md-3";?> col-form-label" for="to_receiving">Add To Receiving</label>
-        <div class="<?php if(isset($div_class)) echo $div_class; else echo "col-md-4";?> checkbox checkbox-default">
-            <input class="form-check-input styled" type="checkbox" id="to_receiving" name="to_receiving" <?php if(!empty(Form::value('to_receiving'))) echo 'checked';?> />
-            <label for="to_receiving"></label>
-        </div>
+    <label class="<?php if(isset($label_class)) echo $label_class; else echo "col-md-3";?> col-form-label" for="to_receiving">Add To Receiving</label>
+    <div class="<?php if(isset($div_class)) echo $div_class; else echo "col-md-4";?> checkbox checkbox-default">
+        <input class="form-check-input styled" type="checkbox" id="to_receiving" name="to_receiving" <?php if(!empty(Form::value('to_receiving'))) echo 'checked';?> />
+        <label for="to_receiving"></label>
+    </div>
 </div>
-<div class="form-group-row" id="pallet_count_holder" style="display: <?php echo $display;?>;">
+<div class="form-group row" id="pallet_count_holder" style="display: <?php echo $display;?>;">
     <label class="<?php if(isset($label_class)) echo $label_class; else echo "col-md-3";?> col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Pallet Count</label>
     <div class="<?php if(isset($div_class)) echo $div_class; else echo "col-md-4";?>">
         <input type="text" class="form-control required digits" name="pallet_multiplier" id="pallet_multiplier" value="<?php echo $pallet_multiplier;?>" />
