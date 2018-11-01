f805e545a97c0553f30175fc049491c7e0663729
marxolly <mark@solly.com.au>
2018-11-01 15:56:52 +1100

    Update app/views/inventory/registerNewStock.php

--- app/views/inventory/registerNewStock.php
+++ app/views/inventory/registerNewStock.php
@@ -1,6 +1,18 @@
 <div id="page-wrapper">
     <input type="hidden" id="client_id" value="<?php echo $client_id;?>" />
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-    
-
+    <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
+    <div class="row">
+        <div class="col-md-12">
+            <form method="post" action="/form/procRegisterNewStock" id="register_new_stock" autocomplete="off">
+                <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+                <div class="form-group row">
+                    <label class="col-md-3 col-form-label">&nbsp;</label>
+                    <div class="col-md-4">
+                        <button type="submit" class="btn btn-primary">Record Info</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
