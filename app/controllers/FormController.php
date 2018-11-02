27bceb3049d028d15e01348467fe26ddb9612fcf
marxolly <mark@solly.com.au>
2018-11-02 12:51:35 +1100

    Update app/controllers/FormController.php

--- app/controllers/FormController.php
+++ app/controllers/FormController.php
@@ -110,7 +110,7 @@ class FormController extends Controller {
 
     public function procRegisterNewStock()
     {
-        echo "<pre>",print_r($this->request->data),"</pre>"; die();
+        //echo "<pre>",print_r($this->request->data),"</pre>"; die();
         $post_data = array();
         foreach($this->request->data as $field => $value)
         {
