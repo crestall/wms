6eae754a11e822f0780ead253ca28cacd96e4f63
marxolly <mark@solly.com.au>
2018-11-30 15:11:07 +1100

    Update footer.php

--- app/views/layout/products/footer.php
+++ app/views/layout/products/footer.php
@@ -138,6 +138,57 @@
                             }
                         });
                     }
+                },
+                'collections-edit': {
+                    init: function(){
+                        $('#product_selector').change(function(e){
+                            if($(this).val() > 0)
+                            {
+                                $.blockUI({ message: '<div style="height:140px; padding-top:20px;"><h2>Collecting Details...</h2></div>' });
+                                window.location.href = "/products/collections-edit/product=" + $(this).val();
+                            }
+                        });
+                        autoCompleter.itemAutoComplete($('#item_searcher'), selectCallback, changeCallback, false);
+                        function selectCallback(event, ui)
+                        {
+                            var appendage = "<div class='row form-group'><div class='item_holder'><label class='col-md-3 col-form-label'>"+ui.item.value+"</label><div class='col-md-4'><input type='text' class='required number form-control item-group count' placeholder='qty' name='items["+ui.item.item_id+"][qty]' id='item_"+ui.item.item_id+"' data-itemid='"+ui.item.item_id+"' /></div>";
+                            appendage += "<div class='col-md-1 delete-image-holder'>";
+                            appendage += "<a class='delete' data-itemid='"+ui.item.item_id+"' title='remove this item'><i class='fas fa-backspace fa-2x text-danger'></i></a></div>";
+                            appendage += "</div></div>";
+                            $('div#the_items').append(appendage);
+                            if($('#selected_items').val() == '')
+                            {
+                                $('#selected_items').val(ui.item.item_id);
+                            }
+                            else
+                            {
+                                $('#selected_items').val($('#selected_items').val()+ ','+ui.item.item_id);
+                            }
+                            $(event.target).val('');
+                            ui.item.value="";
+                            itemsUpdater.itemDelete();
+                            return false;
+                        }
+                        function changeCallback(event, ui)
+                        {
+                            if (!ui.item)
+                	        {
+                                $('#item_searcher').val("");
+                                return false;
+                            }
+                        }
+                        itemsUpdater.itemDelete();
+                        $("form#pack_item_edit").submit(function(e){
+                            if($(this).valid())
+                            {
+                                $.blockUI({ message: '<div style="height:140px; padding-top:20px;"><h2>Updating Details...</h2></div>' });
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        });
+                    }
                 }
             }
 
