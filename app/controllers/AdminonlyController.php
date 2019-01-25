cd99783e26b8a68e89ce402148dea3a20d3edef7
marxolly <mark@solly.com.au>
2019-01-25 15:11:49 +1100

    dispatchedordersupdate

--- app/controllers/AdminonlyController.php
+++ app/controllers/AdminonlyController.php
@@ -16,7 +16,48 @@ class adminonlyController extends Controller
 
     public function dispatchedOrdersUpdater()
     {
-        
+        //echo "<pre>",print_r($this->request->params['args']),"</pre>";die();
+        $client_name = "All Clients";
+        $courier_id = -1;
+        $client_id = 0;
+        $fulfilled = 0;
+        $state = "";
+        $ff = "Unfulfilled";
+        if(!empty($this->request->params['args']))
+        {
+            if(isset($this->request->params['args']['client']))
+            {
+                $client_id = $this->request->params['args']['client'];
+                $client_name = $this->client->getClientName($client_id);
+            }
+            if(isset($this->request->params['args']['courier']))
+            {
+                $courier_id = $this->request->params['args']['courier'];
+            }
+            if(isset($this->request->params['args']['fulfilled']))
+            {
+                $fulfilled = $this->request->params['args']['fulfilled'];
+                $ff = "Fulfilled";
+            }
+            if(isset($this->request->params['args']['state']))
+            {
+                $state = $this->request->params['args']['state'];
+            }
+        }
+        $page_title = "$ff Orders For $client_name";
+        //$orders = $this->order->getUnfulfilledOrders($client_id, $courier_id, 0);     getAllOrders($client_id, $courier_id = -1, $fulfilled = 0, $store_order = -1)
+        $orders = $this->order->getAllOrders($client_id, $courier_id, $fulfilled, 0, $state);
+        //render the page
+        Config::setJsConfig('curPage', "view-orders");
+        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/orders/", Config::get('VIEWS_PATH') . 'adminOnly/viewOrders.php', [
+            'page_title'    =>  $page_title,
+            'client_name'   =>  $client_name,
+            'client_id'     =>  $client_id,
+            'courier_id'    =>  $courier_id,
+            'orders'        =>  $orders,
+            'fulfilled'     =>  $fulfilled,
+            'state'         =>  $state
+        ]);
     }
 
     public function eparcelShipmentDeleter()
