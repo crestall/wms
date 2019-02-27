f22f2e8986e4e36ca685da08b783db01804018e2
marxolly <mark@solly.com.au>
2019-02-27 14:21:25 +1100

    Update addoriginorder.php

--- app/views/forms/addoriginorder.php
+++ app/views/forms/addoriginorder.php
@@ -33,7 +33,8 @@ $client_id = (!empty(Form::value('client_id')))? (int)Form::value('client_id') :
             </div>
             <?php include(Config::get('VIEWS_PATH')."forms/address.php");?>
             <input type="hidden" name="selected_items" id="selected_items" />
-            <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" /> 
+            <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+            <input type="hidden" name="client_id" id="client_id" value="67" />
             <div class="form-group row">
                 <label class="col-md-3 col-form-label">&nbsp;</label>
                 <div class="col-md-4">
