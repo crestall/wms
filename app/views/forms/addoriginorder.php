f94b4a6ff87a66b9587ccde89995367ae121f25d
marxolly <mark@solly.com.au>
2019-02-27 14:14:38 +1100

    Update addoriginorder.php

--- app/views/forms/addoriginorder.php
+++ app/views/forms/addoriginorder.php
@@ -11,6 +11,7 @@ if(!empty(Form::value('items')))
     $idisp = "block";
 if($user_role == "client")
     $idisp = "block";
+$client_id = (!empty(Form::value('client_id')))? (int)Form::value('client_id') : 0;
 ?>
 <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
 <?php echo Form::displayError('general');?>
@@ -32,8 +33,7 @@ if($user_role == "client")
             </div>
             <?php include(Config::get('VIEWS_PATH')."forms/address.php");?>
             <input type="hidden" name="selected_items" id="selected_items" />
-            <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
-            <input type="hidden" name="client_id" value="<?php echo $client_id; ?>" />
+            <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" /> 
             <div class="form-group row">
                 <label class="col-md-3 col-form-label">&nbsp;</label>
                 <div class="col-md-4">
