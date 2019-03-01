46b26652e50bb51dd673423b919c9d30d9eef1c8
marxolly <mark@solly.com.au>
2019-03-01 14:44:06 +1100

    Update addoriginorder.php

--- app/views/forms/addoriginorder.php
+++ app/views/forms/addoriginorder.php
@@ -62,12 +62,12 @@ $inverter_qty = empty(Form::value('inverter_qty'))? 1 : Form::value('inverter_qt
             <div class="form-group row">
                 <div class="form-check">
                     <label class="form-check-label col-md-3">Roof Type</label>
-                    <div class="col-md-4 checkbox checkbox-default">
-                        <input class="form-check-input styled" type="checkbox" id="tin" name="roof_type" <?php if(Form::value('roof_type') == "tin") echo 'checked';?> value="tin" />
+                    <div class="col-md-1 checkbox checkbox-default">
+                        <input class="form-check-input styled" type="radio" id="tin" name="roof_type" <?php if(Form::value('roof_type') == "tin") echo 'checked';?> value="tin" />
                         <label for="tin"> Tin</label>
                     </div>
-                    <div class="col-md-4 checkbox checkbox-default">
-                        <input class="form-check-input styled" type="checkbox" id="tile" name="roof_type" <?php if(Form::value('roof_type') == "tile") echo 'checked';?> value="tin" />
+                    <div class="col-md-1 checkbox checkbox-default">
+                        <input class="form-check-input styled" type="radio" id="tile" name="roof_type" <?php if(Form::value('roof_type') == "tile") echo 'checked';?> value="tin" />
                         <label for="tile"> Tile</label>
                     </div>
                 </div>
