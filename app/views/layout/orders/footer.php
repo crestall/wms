48e9905b4869eff552de6305e253dcee32572cb9
marxolly <mark@solly.com.au>
2019-03-04 11:47:02 +1100

    Update footer.php

--- app/views/layout/orders/footer.php
+++ app/views/layout/orders/footer.php
@@ -470,15 +470,15 @@
                     },
                     openCalcButton: function(){
                         var lock = false;
-                        /*var validator = $( "#add_origin_order" ).validate();
+                        //var validator = $( "#add_origin_order" ).validate();
                         //validator.element( "#myselect" );
                         $('input.banks').each(function(i,e){
-                            if(!validator.check($(this)))
+                            if( !(isNaN($(this).val())) )
                             {
                                 lock = true;
                             }
                         });
-                        $("button#calc_items").prop("disabled", lock); */
+                        $("button#calc_items").prop("disabled", lock);
                     },
                     deleteBank: function(){
                         $('a.delete')
