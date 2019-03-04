24cfb52829485ad3c75f66a2aef6b8ca85ac042f
marxolly <mark@solly.com.au>
2019-03-04 14:11:02 +1100

    Update AjaxfunctionsController.php

--- app/controllers/AjaxfunctionsController.php
+++ app/controllers/AjaxfunctionsController.php
@@ -85,12 +85,12 @@ class ajaxfunctionsController extends Controller
         $interclamps = 2 * $joins + 4;
         $earth_lugs = 2 * $bank_count + 2;
 
-        $railsd = array(
+        $rails = array(
             'id'    => 1165,
-            'qty'   => $rails;
+            'qty'   => $rails
         );
 
-        echo "<pre>",print_r($railsd),"</pre>";
+        echo "<pre>",print_r($rails),"</pre>";
         /*
         echo "<p>Rails: $rails</p>";;
         echo "<p>End Clamps: $end_clamps</p>";
