e871298ae1ff23fe91bb47c109424424be32b34d
marxolly <mark@solly.com.au>
2019-03-04 14:23:21 +1100

    Update origin_order_parts.php

--- app/views/forms/origin_order_parts.php
+++ app/views/forms/origin_order_parts.php
@@ -4,5 +4,5 @@
 <div class="form-group row">
     <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Rails</label>
     <div class="col-md-1"><input type="text" name="items[0][qty]" class="form-control required number" value="<?php echo $rails['qty'];?>" /></div>
-    <input type="hidden" name="items[0][id]" value="<?pgp echo $rails['id'];?>" />
+    <input type="hidden" name="items[0][id]" value="<?php echo $rails['id'];?>" />
 </div>
