cd9a894b9070d5cb099907b66ca92a3335ed7176
marxolly <mark@solly.com.au>
2019-03-07 16:23:56 +1100

    Update index.php

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -114,6 +114,52 @@ $c = 1;
             <!-- store orders -->
         </div><!-- end 1st row -->
         <div class="row"><!-- second row -->
+            <!-- Solar orders -->
+            <div class="col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-10 text-center">
+                                <h2>Latest Unfulfilled Solar Order Counts </h2>
+                            </div>
+                            <div class="col-xs-2 text-right">
+                                 <a id="toggle_solarorders" data-toggle="collapse" href="#new_solarorders"><span class="fa arrow huge"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <div class="collapse in" id="new_solarorders">
+                            <div class="row">
+                               <?php foreach($solar_orders as $so):
+                                    $s = ($so['order_count'] > 1)? "s" : ""; ?>
+                                    <div class="col-lg-6">
+                                        <div class="panel panel-<?php echo $panel_classes[$c % count($panel_classes)];?>">
+                                            <div class="panel-heading order-panel">
+                                                <h3 class="text-center"><?php echo ucwords($so['name']);?></h3>
+                                            </div>
+                                            <div class="panel-footer">
+                                                <div class="row">
+                                                    <div class="col-xs-8">
+                                                        <div><span class="huge"><?php echo $so['order_count'];?></span> Order<?php echo $s;?></div>
+                                                        <div><a class="btn btn-<?php echo $panel_classes[$c % count($panel_classes)];?>" href="/orders/view-solarorders/type=<?php echo $so['type_id'];?>">Manage Orders</a></div>
+                                                    </div>
+                                                    <div class="col-xs-4">
+                                                        <i class="fas fa-truck fa-3x"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <?php if($c % 2 == 0):?>
+                                        </div><div class="row">
+                                    <?php endif;++$c;?>
+                                <?php endforeach;?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- solar orders -->
             <?php if($user_role == "admin"):?>
                 <div class="col-md-6">
                     <div class="panel panel-default">
@@ -160,52 +206,6 @@ $c = 1;
                     </div>
                 </div>
             <?php endif;?>
-            <!-- origin orders -->
-            <div class="col-md-6">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <div class="row">
-                            <div class="col-xs-10 text-center">
-                                <h2>Latest Unfulfilled Solar Order Counts </h2>
-                            </div>
-                            <div class="col-xs-2 text-right">
-                                 <a id="toggle_solarorders" data-toggle="collapse" href="#new_solarorders"><span class="fa arrow huge"></span></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="panel-body">
-                        <div class="collapse in" id="new_solarorders">
-                            <div class="row">
-                               <?php foreach($solar_orders as $so):
-                                    $s = ($so['order_count'] > 1)? "s" : ""; ?>
-                                    <div class="col-lg-6">
-                                        <div class="panel panel-<?php echo $panel_classes[$c % count($panel_classes)];?>">
-                                            <div class="panel-heading order-panel">
-                                                <h3 class="text-center"><?php echo ucwords($so['name']);?></h3>
-                                            </div>
-                                            <div class="panel-footer">
-                                                <div class="row">
-                                                    <div class="col-xs-8">
-                                                        <div><span class="huge"><?php echo $so['order_count'];?></span> Order<?php echo $s;?></div>
-                                                        <div><a class="btn btn-<?php echo $panel_classes[$c % count($panel_classes)];?>" href="/orders/view-solarorders/type=<?php echo $so['type_id'];?>">Manage Orders</a></div>
-                                                    </div>
-                                                    <div class="col-xs-4">
-                                                        <i class="fas fa-truck fa-3x"></i>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <?php if($c % 2 == 0):?>
-                                        </div><div class="row">
-                                    <?php endif;++$c;?>
-                                <?php endforeach;?>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- solar orders -->
         </div> <!-- end 2nd row -->
         <div class="row">
             <div class="col-md-12">
