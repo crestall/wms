a4141afddd84cbd139b29e9f5e2461a0283e77e5
marxolly <mark@solly.com.au>
2019-03-07 15:37:56 +1100

    Update FormController.php

--- app/controllers/FormController.php
+++ app/controllers/FormController.php
@@ -135,7 +135,7 @@ class FormController extends Controller {
         {
             $items = array_merge($items, $this->request->data['items']);
         }
-        echo "<pre>",print_r($items),"</pre>"; //die();
+        //echo "<pre>",print_r($items),"</pre>"; //die();
         $orders_items = array();
         foreach($items as $item)
         {
@@ -153,7 +153,7 @@ class FormController extends Controller {
         $the_items = array(
             0 => $orders_items
         );
-        echo "<pre>",print_r($the_items),"</pre>"; die();
+        //echo "<pre>",print_r($the_items),"</pre>"; die();
         $oitems = $this->allocations->createOrderItemsArray($the_items, 0, false);
         foreach($oitems[0] as $item)//there is only one order
         {
