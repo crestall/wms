59ab1664b3c80b330eba308f995b198473b4cda9
Mark Solly <msolly@contactpoint.com.au>
2019-03-20 16:00:01 +1100

    solarlabels

--- app/views/pdf/solarlabels.php
+++ app/views/pdf/solarlabels.php
@@ -10,12 +10,21 @@ foreach($orders_ids as $id):
     <table width="100%" style="font-size:16px; line-height:1.5">
         <tr>
             <td colspan="2" align="center">
-                <h1 style="font-size:50px"><?php echo $od['suburb'];?></h1>
+                <h1 style="font-size:50px"><?php echo $type;?></h1>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center">
+                <h1 style="font-size:40px"><?php echo $od['suburb'];?></h1>
             </td>
         </tr>
         <tr>
             <td>Work Order</td>
             <td><?php echo $od['work_order'];?></td>
+        </tr>
+        <tr>
+            <td>Address</td>
+            <td><?php echo $delivery_address;?></td>
         </tr>
     </table>
     <?php if ($c < count($orders_ids)):?>
