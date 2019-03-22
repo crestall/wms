55e90b6d5d484989328226319ffaf1a17779c069
marxolly <mark@solly.com.au>
2019-03-22 13:12:17 +1100

    Update Item.php

--- app/models/Item.php
+++ app/models/Item.php
@@ -166,7 +166,7 @@ class Item extends Model{
                 SELECT
                     COALESCE(SUM(oi.qty),0) AS allocated, oi.item_id, oi.location_id
                 FROM
-                    $items_table oi JOIN orders o ON oi.order_id = o.id Join items i ON oi.item_id = i.id
+                    $items_table oi JOIN $orders_table o ON oi.order_id = o.id Join items i ON oi.item_id = i.id
                 WHERE
                     o.status_id != 4
                 GROUP BY
