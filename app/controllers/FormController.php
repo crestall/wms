a0d5f93b415e59b2445bbc787f55b8edd3ceefff
marxolly <mark@solly.com.au>
2019-03-22 11:51:48 +1100

    Update FormController.php

--- app/controllers/FormController.php
+++ app/controllers/FormController.php
@@ -111,7 +111,7 @@ class FormController extends Controller {
 
     public function procOriginOrderAdd()
     {
-        //echo "<pre>",print_r($this->request->data),"</pre>"; die();
+        echo "<pre>",print_r($this->request->data),"</pre>"; die();
         $post_data = array();
         foreach($this->request->data as $field => $value)
         {
