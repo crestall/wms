b520c65e9be869bbaabc33a1473a3204edd71bd8
marxolly <mark@solly.com.au>
2019-03-26 16:23:09 +1100

    Update inventoryReport.php

--- app/views/reports/inventoryReport.php
+++ app/views/reports/inventoryReport.php
@@ -40,7 +40,23 @@
                         </thead>
                         <tbody>
                             <?php foreach($products as $p):
-                                $available = $p['onhand'] - $p['qc_count'] - $p['allocated'];?>
+                                $available = $p['onhand'] - $p['qc_count'] - $p['allocated'];
+                                $ls = "";
+                                foreach($p['locations'] as $l)
+                                {
+                                    $ls .= $l['name']." (".$l['onhand'].")";
+                                    if($l['allocated'] > 0)
+                                    {
+                                        $ls .= " Allocated (".$l['allocated'].")";
+                                    }
+                                    if($l['qc_count'] > 0)
+                                    {
+                                        $ls .= " Under Quality Control (".$l['qc_count'].")";
+                                    }
+                                    $ls .= "<br/>";
+                                }
+                                $ls = rtrim($ls, "<br/>");
+                                ?>
                                 <tr>
                                     <td data-label="Name"><?php echo $p['name'];?></td>
                                     <td data-label="SKU"><?php echo $p['sku'];?></td>
@@ -48,7 +64,7 @@
                                     <td data-label="Currently Allocated" class="number"><?php echo $p['allocated'];?></td>
                                     <td data-label="Under Quality Control" class="number"><?php echo $p['qc_count'];?></td>
                                     <td data-label="Total Available" class="number"><?php echo $available;?></td>
-                                    <td></td>
+                                    <td data-label="Locations"><?php echo $ls;?></td>
                                 </tr>
                             <?php endforeach;?>
                         </tbody>
