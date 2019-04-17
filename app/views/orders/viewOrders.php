01007011a3cf7018560e21e49cb6619f03149dc0
marxolly <mark@solly.com.au>
2019-04-17 13:24:25 +1000

    Update viewOrders.php

--- app/views/orders/viewOrders.php
+++ app/views/orders/viewOrders.php
@@ -23,6 +23,9 @@
                     <p><button class="btn btn-primary" id="show_unfulfilled">Show Only Unfulfilled Orders</button></p>
                 <?php endif;?>
             </div>
+            <div class="col-lg-3 text-center">
+                <p><button class="btn btn-info export_csv"><i class="fas fa-file-csv"></i> Export Selected To CSV</button></p>
+            </div>
         </div>
         <div class="row">
             <div class="col-lg-3 text-center">
