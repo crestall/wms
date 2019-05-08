180fb6e072cc5f8b55f8145fb237d5a96f9ab0c4
marxolly <mark@solly.com.au>
2019-05-08 16:20:44 +1000

    Update csvUpload.php

--- app/views/orders/csvUpload.php
+++ app/views/orders/csvUpload.php
@@ -5,7 +5,7 @@
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
     <div class="row">
-        <form id="order-csv-upload" method="post" action="/form/procOrderCsvUpload">
+        <form id="order-csv-upload" method="post" action="/form/procOrderCsvUpload" enctype="multipart/form-data">
             <div class="form-group row">
                 <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> CSV File</label>
                 <div class="col-md-4">
