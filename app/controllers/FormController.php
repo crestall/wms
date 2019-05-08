df24fc06d9e6f2d457515f0704cca83cafeecd75
marxolly <mark@solly.com.au>
2019-05-08 16:25:36 +1000

    Update FormController.php

--- app/controllers/FormController.php
+++ app/controllers/FormController.php
@@ -115,7 +115,7 @@ class FormController extends Controller {
     public function procOrderCsvUpload()
     {
         //echo "<pre>",print_r($this->request->data),"</pre>"; //die();
-        echo "<pre>",print_r($_FILES),"</pre>";die();
+        //echo "<pre>",print_r($_FILES),"</pre>";die();
         $post_data = array();
         foreach($this->request->data as $field => $value)
         {
@@ -125,7 +125,7 @@ class FormController extends Controller {
                 $post_data[$field] = $value;
             }
         }
-        if($_FILES['csv_file']["size"][0] > 0)
+        if($_FILES['csv_file']["size"] > 0)
         {
 
         }
