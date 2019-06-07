c2b827264285114084432d6e5e6d7bac5b803cb3
marxolly <mark@solly.com.au>
2019-06-07 14:15:52 +1000

    Update transferLocation.php

--- app/views/inventory/transferLocation.php
+++ app/views/inventory/transferLocation.php
@@ -1,8 +1,7 @@
 <div id="page-wrapper">
-    <input type="hidden" id="client_id" value="<?php echo $client_id;?>" />
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
     <div class="row">
-        
+
     </div>
 </div>
