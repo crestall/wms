a48bd64fd4a2b4dd96009fc1f98015de37169c06
marxolly <mark@solly.com.au>
2019-06-07 11:10:13 +1000

    Update Response.php

--- app/core/Response.php
+++ app/core/Response.php
@@ -248,11 +248,49 @@ class Response {
     }
 
 
-    private function fputcsv_eol($fp, $array, $delimiter = ',', $enclosure = '"', $eol = "\n") {
-      fputcsv($fp, $array, $delimiter, $enclosure);
-      if("\n" != $eol && 0 === fseek($fp, -1, SEEK_CUR)) {
-        fwrite($fp, $eol);
-      }
+    private function fputcsv_eol($fp, $fields, $delimiter = ',', $enclosure = '"', $eol = "\n") {
+        $str = '';
+        $escape_char = '\\';
+        foreach ($fields as $value)
+        {
+            if (    strpos($value, $delimiter) !== false ||
+                    strpos($value, $enclosure) !== false ||
+                    strpos($value, "\n") !== false ||
+                    strpos($value, "\r") !== false ||
+                    strpos($value, "\t") !== false ||
+                    strpos($value, ' ') !== false
+                )
+            {
+                $str2 = $enclosure;
+                $escaped = 0;
+                $len = strlen($value);
+                for ($i=0;$i<$len;$i++)
+                {
+                    if ($value[$i] == $escape_char)
+                    {
+                        $escaped = 1;
+                    }
+                    else if (!$escaped && $value[$i] == $enclosure)
+                    {
+                        $str2 .= $enclosure;
+                    }
+                    else
+                    {
+                        $escaped = 0;
+                    }
+                    $str2 .= $value[$i];
+                }
+                $str2 .= $enclosure;
+                $str .= $str2.$delimiter;
+            }
+            else
+            {
+                $str .= $value.$delimiter;
+            }
+        }
+        $str = substr($str,0,-1);
+        $str .= $eol;
+        return fwrite($handle, $str);
     }
 
     /**
