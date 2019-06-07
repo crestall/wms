87ba4fad547d9cfb4b495438366512ef98268b5e
marxolly <mark@solly.com.au>
2019-06-07 15:46:04 +1000

    Update Location.php

--- app/models/Location.php
+++ app/models/Location.php
@@ -475,6 +475,7 @@ class Location extends Model{
             $label = $l['location'];
             $value = $l['id'];
             $count = $db->queryData("SELECT count(*) AS count FROM orders_items oi JOIN orders o ON oi.order_id = o.id WHERE o.status_id != 4 AND oi.location_id = $value");
+            echo "SELECT count(*) AS count FROM orders_items oi JOIN orders o ON oi.order_id = o.id WHERE o.status_id != 4 AND oi.location_id = $value";
             if($count == 0)
             {
                 if($selected)
