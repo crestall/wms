d25c1c61fee69423562abc1e9c4d43bd3f7e7520
marxolly <mark@solly.com.au>
2019-06-07 10:50:20 +1000

    Update Response.php

--- app/core/Response.php
+++ app/core/Response.php
@@ -230,7 +230,7 @@ class Response {
         $cols = $this->csv["cols"];
         $rows = $this->csv["rows"];
 
-        $out = fopen("php://memory", 'r+');
+        $out = fopen("php://temp", 'r+');
         if($this->header_row)
         {
             //fputcsv($out, $cols, ',', '"');
