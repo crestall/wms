23a6f78447b472a1f34ba71e7ae9cc7d5f5e5417
marxolly <mark@solly.com.au>
2019-06-07 15:41:31 +1000

    location transfer

--- app/views/inventory/transferLocation.php
+++ app/views/inventory/transferLocation.php
@@ -7,7 +7,7 @@
                 <div class="form-group row">
                     <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Location to Move</label>
                     <div class="col-md-4">
-                        <select id="move_from_location" name="move_from_location" class="form-control selectpicker" data-live-search="true"><option value="0">--Select One--</option><?php echo $this->controller->location->getSelectItemInLocations($product_id, Form::value('move_from_location'));?></select>
+                        <select id="move_from_location" name="move_from_location" class="form-control selectpicker" data-live-search="true"><option value="0">--Select One--</option><?php echo $this->controller->location->getSelectNonEmptyUnallocatedLocations(Form::value('move_from_location'));?></select>
                         <?php echo Form::displayError('move_from_location');?>
                     </div>
                 </div>
