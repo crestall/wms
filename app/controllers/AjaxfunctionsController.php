828b1cac21bccc1f3b6b3ffbc6f6a73c2393a7c8
marxolly <mark@solly.com.au>
2019-06-11 15:56:38 +1000

    Update AjaxfunctionsController.php

--- app/controllers/AjaxfunctionsController.php
+++ app/controllers/AjaxfunctionsController.php
@@ -660,7 +660,7 @@ class ajaxfunctionsController extends Controller
     public function addPackageForm()
     {
         //echo "<pre>",print_r($this->request),"</pre>"; //die();
-        $order_ids = implode(",", $this->request['data']['order_ids']) ;
+        $order_ids = implode(",", $this->request->data['order_ids']) ;
         $this->view->render(Config::get('VIEWS_PATH') . 'dashboard/add_package.php', [
             'order_ids' =>  $order_ids
         ]);
