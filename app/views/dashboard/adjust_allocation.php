8da5e5b1749b3aec48d2dad3ea4acaf17142d51e
marxolly <mark@solly.com.au>
2019-06-12 14:06:36 +1000

    Update adjust_allocation.php

--- app/views/dashboard/adjust_allocation.php
+++ app/views/dashboard/adjust_allocation.php
@@ -3,8 +3,8 @@
         <form id="adjust-allocation" method="post" action="/form/procAdjustAllocations">
             <?php foreach($items as $item):?>
                 <div class="form-group row">
-                    <label class="col-md-4 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> <?php echo $item['name']." (".$item['qty'].")";?></label>
-                    <div class="col-md-8">
+                    <label class="col-md-6 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> <?php echo $item['name']." (".$item['qty'].")";?></label>
+                    <div class="col-md-6">
                         <select id="allocation_id_<?php echo $item['item_id'];?>" name="allocation_id_<?php echo $item['item_id'];?>" class="form-control selectpicker" data-live-search="true"><option value="0">--Select One--</option><?php echo $this->controller->location->getSelectItemInLocations($item['item_id'], $item['location_id'], false, $item['qty']) ;?></select>
                         <?php echo Form::displayError("allocation_id_{$item['item_id']}");?>
                     </div>
@@ -13,8 +13,8 @@
             <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
             <input type="hidden" name="order_id" value="<?php echo $order_id;?>" />
             <div class="form-group row">
-                <label class="col-md-4 col-form-label">&nbsp;</label>
-                <div class="col-md-8">
+                <label class="col-md-6 col-form-label">&nbsp;</label>
+                <div class="col-md-6">
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
             </div>
