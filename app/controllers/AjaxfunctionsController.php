846fa50aa40725cda24deba1351f077a7a65a8ec
marxolly <mark@solly.com.au>
2019-06-12 11:55:50 +1000

    Update AjaxfunctionsController.php

--- app/controllers/AjaxfunctionsController.php
+++ app/controllers/AjaxfunctionsController.php
@@ -660,9 +660,7 @@ class ajaxfunctionsController extends Controller
     public function addPackageForm()
     {
         //echo "<pre>",print_r($this->request),"</pre>"; //die();
-        $od = $this->order->getOrderDetail($this->request->data['order_id']);
-        $items = $this->order->getItemsForOrder($od['id']);
-        echo "<pre>",print_r($items),"</pre>"; die();
+        $order_ids = implode(",", $this->request->data['order_ids']) ;
         $this->view->render(Config::get('VIEWS_PATH') . 'dashboard/add_package.php', [
             'order_ids' =>  $order_ids
         ]);
@@ -671,7 +669,9 @@ class ajaxfunctionsController extends Controller
     public function adjustAllocationForm()
     {
         echo "<pre>",print_r($this->request),"</pre>"; //die();
-        $order_id = $this->request->data['order_ids'] ;
+        $od = $this->order->getOrderDetail($this->request->data['order_id']);
+        $items = $this->order->getItemsForOrder($od['id']);
+        echo "<pre>",print_r($items),"</pre>"; //die();
     }
 
     public function deletePackage()
