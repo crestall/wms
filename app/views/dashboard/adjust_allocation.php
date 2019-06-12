16fc26b9ed201504ce39d59c08247be2c6f284ed
marxolly <mark@solly.com.au>
2019-06-12 15:38:39 +1000

    Update adjust_allocation.php

--- app/views/dashboard/adjust_allocation.php
+++ app/views/dashboard/adjust_allocation.php
@@ -5,7 +5,7 @@
                 <div class="form-group row">
                     <label class="col-md-8 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> <?php echo $item['name']." (".$item['qty'].")";?></label>
                     <div class="col-md-4">
-                        <select id="allocation_id_<?php echo $item['item_id'];?>" name="allocation_id[<?php echo $item['item_id']];?>" class="form-control selectpicker" data-live-search="true" required><option value="">--Select One--</option><?php echo $this->controller->location->getSelectItemInLocations($item['item_id'], $item['location_id'], false, $item['qty']) ;?></select>
+                        <select id="allocation_id_<?php echo $item['item_id'];?>" name="allocation_id[<?php echo $item['item_id'];?>]" class="form-control selectpicker" data-live-search="true" required><option value="">--Select One--</option><?php echo $this->controller->location->getSelectItemInLocations($item['item_id'], $item['location_id'], false, $item['qty']) ;?></select>
                         <?php echo Form::displayError("allocation_id_{$item['item_id']}");?>
                     </div>
                 </div>
