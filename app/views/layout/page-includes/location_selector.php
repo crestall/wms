011aa16608926ef65e4c4f989d40913919c60af3
marxolly <mark@solly.com.au>
2019-06-18 14:16:51 +1000

    Update location_selector.php

--- app/views/layout/page-includes/location_selector.php
+++ app/views/layout/page-includes/location_selector.php
@@ -7,7 +7,15 @@ $double_bay = ( (isset($product_info) && $product_info['double_bay'] == 1) || (i
 <div class="form-group row">
     <label class="<?php if(isset($label_class)) echo $label_class; else echo "col-md-3";?> col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Location</label>
     <div class="<?php if(isset($div_class)) echo $div_class; else echo "col-md-4";?>">
-        <select id="add_to_location" name="add_to_location" class="form-control selectpicker" data-live-search="true"><option value="0">--Select One--</option><?php echo $this->controller->location->getSelectLocations($add_to_location, $item_id);?></select>
+        <select id="add_to_location" name="add_to_location" class="form-control selectpicker" data-live-search="true"><option value="0">--Select One--</option>
+        <?php
+        if($double_bay):
+            echo $this->controller->location->getSelectDBLocations($add_to_location, $item_id);
+        else:
+            echo $this->controller->location->getSelectLocations($add_to_location, $item_id);
+        endif;
+        ?>
+        </select>
         <?php echo Form::displayError('add_to_location');?>
     </div>
 </div>
