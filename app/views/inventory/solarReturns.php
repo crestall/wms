9543e7a4c60f2f693d1d13a8a8972fa9d29ca6db
marxolly <mark@solly.com.au>
2019-06-21 16:16:24 +1000

    Update solarReturns.php

--- app/views/inventory/solarReturns.php
+++ app/views/inventory/solarReturns.php
@@ -1,3 +1,8 @@
+<?php
+$idisp = "none";
+if(!empty(Form::value('csrf_token')))
+    $idisp = "block";
+?>
 <div id="page-wrapper">
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
@@ -11,7 +16,25 @@
                         <?php echo Form::displayError('order_type_id');?>
                     </div>
                 </div>
+                <div id="item_holder" style="display:<?php echo $idsip;?>">
+                    <div class="form-group row">
+                        <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Name</label>
+                        <div class="col-md-4">
+                            <input type="text" class="form-control item-searcher required" name="item_name" value="<?php echo Form::value('item_name');?>" />
+                            <?php echo Form::displayError('item_name');?>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Serial Number</label>
+                        <div class="col-md-4">
+                            <input type="text" class="form-control required" name="serial_number" value="<?php echo Form::value('serial_number');?>" />
+                            <?php echo Form::displayError('serial_number');?>
+                        </div>
+                    </div>
+                </div>
+
                 <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+                <input type="hidden" name="item_id" id="item_id" />
                 <div class="form-group row">
                     <label class="col-md-3 col-form-label">&nbsp;</label>
                     <div class="col-md-4">
