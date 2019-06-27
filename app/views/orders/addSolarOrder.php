ae2867900468c4064ab8a4c8601ab85f3cf26b01
marxolly <mark@solly.com.au>
2019-06-27 12:17:29 +1000

    Update addSolarOrder.php

--- app/views/orders/addSolarOrder.php
+++ app/views/orders/addSolarOrder.php
@@ -1,3 +1,9 @@
 <div id="page-wrapper">
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+    <div class="row form-group">
+        <label class="col-md-3">Select an Order Type</label>
+        <div class="col-md-4">
+            <p><select id="type_selector" class="form-control selectpicker"><option value="0">Select</option><?php echo $this->controller->solarordertype->getSelectSolarOrderTypes();?></select></p>
+        </div>
+    </div>
 </div>
