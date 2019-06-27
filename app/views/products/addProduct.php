34423698a57d18d5cbf882991cd8779042657c5e
marxolly <mark@solly.com.au>
2019-06-27 11:35:29 +1000

    Update addProduct.php

--- app/views/products/addProduct.php
+++ app/views/products/addProduct.php
@@ -31,7 +31,7 @@ $client_id = (Session::getUserRole() == "solar admin")? $this->controller->clien
             <div class="form-group row">
                 <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Client</label>
                 <div class="col-md-4">
-                    <select id="client_id" name="client_id" class="form-control selectpicker" <?php if(Session::getUserRole() == "solar admin") echo "disabled";?>><option value="0">--Select One--</option><?php echo $this->controller->client->getSelectClients($client_id);?></select>
+                    <select id="client_id" name="client_id" class="form-control selectpicker" <?php if(Session::getUserRole() == "solar admin") echo "readonly";?>><option value="0">--Select One--</option><?php echo $this->controller->client->getSelectClients($client_id);?></select>
                     <?php echo Form::displayError('client_id');?>
                 </div>
             </div>
