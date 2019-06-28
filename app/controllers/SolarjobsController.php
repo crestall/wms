9a4c9c18575e60369669678aed30fcb9fd48a6b7
marxolly <mark@solly.com.au>
2019-06-28 13:14:47 +1000

    Update SolarjobsController.php

--- app/controllers/SolarjobsController.php
+++ app/controllers/SolarjobsController.php
@@ -66,7 +66,7 @@ class SolarjobsController extends Controller
         //render the page
         Config::setJsConfig('curPage', "add-origin-service-job");
         $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/solarjobs/", Config::get('VIEWS_PATH') . 'solarjobs/addOriginServiceJob.php', [
-            'page_title'    =>  "Add TLJ Services Install",
+            'page_title'    =>  "Add Origin Service Job",
             'client_id'     =>  67,
             'order_type_id' =>  1
         ]);
