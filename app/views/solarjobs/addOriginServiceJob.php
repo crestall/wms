ca2cfbf7e2dfa1e0e22a0fbc92cf2597ed76337a
marxolly <mark@solly.com.au>
2019-07-03 12:30:22 +1000

    origin service job add

--- app/views/solarjobs/addOriginServiceJob.php
+++ app/views/solarjobs/addOriginServiceJob.php
@@ -1,6 +1,32 @@
+<?php
+$address = Form::value('address');
+$address2 = Form::value('address2');
+$suburb = Form::value('suburb');
+$state = Form::value('state');
+$postcode = Form::value('postcode');
+$country = Form::value('country');
+?>
 <div id="page-wrapper">
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-    <div class="col-lg-12">
-        <h2>Need to get some info for this</h2>
+    <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
+    <?php echo Form::displayError('general');?>
+    <div class="row">
+        <div class="col-lg-12">
+            <form id="origin-service-job" method="post" action="/form/procAddOriginServiceJob" autocomplete="off">
+
+
+                <?php include(Config::get('VIEWS_PATH')."forms/address.php");?>
+                <input type="hidden" name="selected_items" id="selected_items" />
+                <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+                <input type="hidden" name="client_id" id="client_id" value="<?php echo $client_id; ?>" />
+                <input type="hidden" name="type_id" id="type_id" value="<?php echo $type_id; ?>" />
+                <div class="form-group row">
+                    <label class="col-md-3 col-form-label">&nbsp;</label>
+                    <div class="col-md-4">
+                        <button type="submit" class="btn btn-primary" id="add_origin_service_submitter" disabled>Submit</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
