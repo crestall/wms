67dab64f4c1cd2660022efdf112bfe9616d2a08f
marxolly <mark@solly.com.au>
2019-07-05 16:21:22 +1000

    Update editOriginInstall.php

--- app/views/solarjobs/editOriginInstall.php
+++ app/views/solarjobs/editOriginInstall.php
@@ -28,9 +28,56 @@
         </div>
         <div class="row">
             <div class="col-md-12">
-                <h3>Instal For <?php echo $order_type;?></h3>
+                <h3>Install For <?php echo $order_type;?></h3>
             </div>
         </div>
         <?php echo "<pre>",print_r($details),"</pre>";?>
+        <div class="row">
+            <div class="col-md-12">
+                <?php if(isset($_SESSION['feedback'])) :?>
+                   <div class='feedbackbox'><?php echo Session::getAndDestroy('feedback');?></div>
+                <?php endif; ?>
+                <?php if(isset($_SESSION['errorfeedback'])) :?>
+                   <div class='errorbox'><?php echo Session::getAndDestroy('errorfeedback');?></div>
+                <?php endif; ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <h3>Job Details</h3>
+            </div>
+        </div>
+        <div class="bs-callout bs-callout-primary bs-callout-more">
+            <div class="row ">
+                <div class="col-md-8">
+                    <dl class="dl-horizontal order-details">
+                        <dt>Work Order</dt>
+                        <dd><?php echo $details['work_order'];?></dd>
+                        <dt>Customer Name</dt>
+                        <dd><?php echo $details['customer_name'];?></dd>
+                        <dt>Address</dt>
+                        <dd><?php echo $details['address'];?></dd>
+                        <?php if(!empty($details['address_2'])):?>
+                            <dt>&nbsp;</dt>
+                            <dd><?php echo $details['address_2'];?></dd>
+                        <?php endif;?>
+                        <dt>&nbsp;</dt>
+                        <dd><?php echo $details['suburb'];?></dd>
+                        <dt>&nbsp;</dt>
+                        <dd><?php echo $details['state'];?></dd>
+                        <dt>&nbsp;</dt>
+                        <dd><?php echo $details['postcode'];?></dd>
+                        <dt>&nbsp;</dt>
+                        <dd><?php echo $details['country'];?></dd>
+                    </dl>
+                </div>
+            </div>
+            <div class='row'>
+                <div class="col-md-4"></div>
+                <div class="col-md-4">
+                    <a class="btn btn-primary" href="/orders/address-update/order=<?php echo $id;?>">Update Address Details</a>
+                </div>
+            </div>
+        </div>
     <?php endif;?>
 </div>
