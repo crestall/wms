2cbb36492e439230ceed9002ed3977d4c86140a6
marxolly <mark@solly.com.au>
2019-07-08 15:19:31 +1000

    Update footer.php

--- app/views/layout/solarjobs/footer.php
+++ app/views/layout/solarjobs/footer.php
@@ -100,7 +100,8 @@
                                 $.blockUI({ message: '<div style="height:160px; padding-top:20px;"><h2>Finding Correct Form...</h2></div>' });
                                 var urls = {
                                     1 : "/solar-jobs/add-origin-job",
-                                    2 : "/solar-jobs/add-tlj-job"
+                                    2 : "/solar-jobs/add-tlj-job",
+                                    3 : "/solar-jobs/add-solargain-job"
                                 }
                                 window.location.href = urls[$(this).val()];
                             }
@@ -115,7 +116,8 @@
                                 $.blockUI({ message: '<div style="height:160px; padding-top:20px;"><h2>Finding Correct Form...</h2></div>' });
                                 var urls = {
                                     1 : "/solar-jobs/add-origin-service-job",
-                                    2 : "/solar-jobs/add-tlj-service-job"
+                                    2 : "/solar-jobs/add-tlj-service-job",
+                                    3 : "solar-jobs/add-solargain-service-job"
                                 }
                                 window.location.href = urls[$(this).val()];
                             }
