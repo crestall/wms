6a88edf778ea6051e96e4ea939fc196e5e03e615
marxolly <mark@solly.com.au>
2019-07-08 16:32:13 +1000

    Update FormController.php

--- app/controllers/FormController.php
+++ app/controllers/FormController.php
@@ -122,7 +122,7 @@ class FormController extends Controller {
 
     public function procAddSolargainServiceJob()
     {
-        echo "<pre>",print_r($this->request->data),"</pre>"; die();
+        echo "<pre>",print_r($this->request->data),"</pre>"; //die();
         $post_data = array();
         foreach($this->request->data as $field => $value)
         {
