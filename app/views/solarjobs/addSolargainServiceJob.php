923b2f21fe03ee11816b9d0c34076f4fc30e0437
marxolly <mark@solly.com.au>
2019-07-08 15:30:03 +1000

    Update addSolargainServiceJob.php

--- app/views/solarjobs/addSolargainServiceJob.php
+++ app/views/solarjobs/addSolargainServiceJob.php
@@ -1,6 +1,28 @@
+<?php
+
+?>
 <div id="page-wrapper">
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+    <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
+    <?php echo Form::displayError('general');?>
     <div class="col-lg-12">
-        <h2>Creat the form</h2>
+        <form id="add-solargain-service-job" method="post" action="/form/procAddSolargainServiceJob" autocomplete="off">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h3>Address Details</h3>
+                </div>
+            </div>
+            <?php include(Config::get('VIEWS_PATH')."forms/address.php");?>
+            <input type="hidden" name="selected_items" id="selected_items" />
+            <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+            <input type="hidden" name="client_id" id="client_id" value="67" />
+            <input type="hidden" name="type_id" id="type_id" value="<?php echo $type_id; ?>" />
+            <div class="form-group row">
+                <label class="col-md-3 col-form-label">&nbsp;</label>
+                <div class="col-md-4">
+                    <button type="submit" class="btn btn-primary" id="add_origin_order_submitter" disabled>Add Job</button>
+                </div>
+            </div>
+        </form>
     </div>
 </div>
