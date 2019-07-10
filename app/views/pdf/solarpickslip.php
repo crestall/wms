e2f4bfeca1039c5509da925e14c68366e43164b2
marxolly <mark@solly.com.au>
2019-07-10 10:43:47 +1000

    Update solarpickslip.php

--- app/views/pdf/solarpickslip.php
+++ app/views/pdf/solarpickslip.php
@@ -55,8 +55,8 @@ foreach($orders_ids as $id):
                     <td class='centre'><span class='check_box'></span></td>
                 </tr>
             <?php endforeach;?>
-            <tr style="height:130px">
-                <td colspan="6" style="text-align:right; font-weight:bold">Signed</td>
+            <tr>
+                <td colspan="6" style="text-align:right; font-weight:bold;height:130px">Signed</td>
                 <td class='centre'><span class='check_box'></span></td>
             </tr>
         </table>
