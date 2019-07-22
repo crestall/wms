15d4512e56501bab039252cf16ba31d2cf8d7a94
marxolly <mark@solly.com.au>
2019-07-22 15:18:43 +1000

    Update orderConsumables.php

--- app/views/ordering/orderConsumables.php
+++ app/views/ordering/orderConsumables.php
@@ -8,6 +8,8 @@
     <div class="row">
         <form id="order-consumables" method="post" action="/form/procSolarTeamOrderConsumables">
             <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+            <?php include(Config::get('VIEWS_PATH')."forms/item_adder.php");?> 
+            <input type="hidden" name="client_id" id="client_id" value="67" />
             <div class="form-group row">
                 <label class="col-md-3 col-form-label">&nbsp;</label>
                 <div class="col-md-4">
