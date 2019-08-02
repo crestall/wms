11620b983c031eaa52bff2d2696d55398be1cc55
marxolly <mark@solly.com.au>
2019-08-02 16:18:49 +1000

    Update FormController.php

--- app/controllers/FormController.php
+++ app/controllers/FormController.php
@@ -493,7 +493,7 @@ class FormController extends Controller {
 
     public function procAddTljOrder()
     {
-        echo "POST<pre>",print_r($this->request->data),"</pre>"; die();
+        echo "POST<pre>",print_r($this->request->data),"</pre>"; //die();
         $post_data = array();
         foreach($this->request->data as $field => $value)
         {
@@ -504,6 +504,14 @@ class FormController extends Controller {
             }
         }
         $items = array();
+        $items[] = array(
+            'id'    => $panel_id,
+            'qty'   => $panel_qty
+        );
+        $items[] = array(
+            'id'    => $inverter_id,
+            'qty'   => $inverter_qty
+        );
         /* */
         foreach($this->request->data['items'] as $i)
         {
@@ -513,7 +521,7 @@ class FormController extends Controller {
             );
         }
 
-        //echo "Items<pre>",print_r($items),"</pre>"; //die();
+        echo "Items<pre>",print_r($items),"</pre>"; die();
         $orders_items = array();
         foreach($items as $item)
         {
