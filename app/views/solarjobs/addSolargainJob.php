24c72673136ddde7956b74647b0bc0e31dbbba1f
marxolly <mark@solly.com.au>
2019-08-02 12:14:26 +1000

    Update addSolargainJob.php

--- app/views/solarjobs/addSolargainJob.php
+++ app/views/solarjobs/addSolargainJob.php
@@ -1,4 +1,10 @@
 <div id="page-wrapper">
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+    <div class="row form-group">
+        <label class="col-md-3">Select an Order Type</label>
+        <div class="col-md-4">
+            <p><select id="type_selector" class="form-control selectpicker"><option value="0">--Choose One--</option><?php echo $this->controller->solarordertype->getSelectSolarOrderTypes($order_type_id);?></select></p>
+        </div>
+    </div>
     <?php echo $form;?>
 </div>
