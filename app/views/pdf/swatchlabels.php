9d5025841ea23b2f966f63e2c9dff0115ff53895
marxolly <mark@solly.com.au>
2019-08-06 16:01:57 +1000

    Update swatchlabels.php

--- app/views/pdf/swatchlabels.php
+++ app/views/pdf/swatchlabels.php
@@ -1,4 +1,4 @@
-<table width="100%" style="font-size:16px; margin: 0 auto;">
+<table width="100%" style="font-size:16px; margin: 0 auto; border: thin solid black">
     <tr>
     <?php
     $this_order = 1;
