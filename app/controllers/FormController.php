84e7f7125fa33dc459b8cf4d008613bf7d548811
marxolly <mark@solly.com.au>
2019-08-06 10:57:04 +1000

    Update FormController.php

--- app/controllers/FormController.php
+++ app/controllers/FormController.php
@@ -2804,7 +2804,7 @@ class FormController extends Controller {
 
     public function procAddressUpdate()
     {
-        //echo "<pre>",print_r($this->request->data),"</pre>"; //die();
+        echo "<pre>",print_r($this->request->data),"</pre>"; //die();
         $post_data = array();
         foreach($this->request->data as $field => $value)
         {
