d7981b2dd86e89976482e8c7d46c783497257bd6
marxolly <mark@solly.com.au>
2019-08-06 12:59:31 +1000

    Update swatchlabels.php

--- app/views/pdf/swatchlabels.php
+++ app/views/pdf/swatchlabels.php
@@ -17,7 +17,7 @@
         elseif($this_order % 2 == 0)
             echo "</tr><tr>";
         ?>
-        <td style="margin:3mm; text-align:right; width:92mm; border:thin solid black">
+        <td style="margin:3mm; padding:3mm; text-align:right; width:92mm; border:thin solid black">
             <?php echo $address_string;?>
         </td>
         <?php ++$this_order;
