450c24467ea2f2153258f921319d677f60c1dd78
marxolly <mark@solly.com.au>
2019-08-07 15:32:27 +1000

    Update PdfController.php

--- app/controllers/PdfController.php
+++ app/controllers/PdfController.php
@@ -113,7 +113,7 @@ class pdfController extends Controller
             'SpaceY'    =>0,
             'width'     =>99,
             'height'    =>33,
-            'font-size' =>12
+            'font-size' =>10
         );
         $pdf = new AddressLabels($config);
         $pdf->AddPage();
