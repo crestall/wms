fd7e55e80c8ebc204b8a291f759388457a399e4d
marxolly <mark@solly.com.au>
2019-08-08 10:45:09 +1000

    solargain installs

--- app/views/solarjobs/addSolargainJob.php
+++ app/views/solarjobs/addSolargainJob.php
@@ -18,7 +18,7 @@ $date = (empty(Form::value('date_value')))? time() : Form::value('date_value');
 <div id="page-wrapper">
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
     <div class="row form-group">
-        <label class="col-md-3">Choose Another Type SOLARGAIN</label>
+        <label class="col-md-3">Choose Another Type</label>
         <div class="col-md-4">
             <p><select id="type_selector" class="form-control selectpicker"><option value="0">--Choose One--</option><?php echo $this->controller->solarordertype->getSelectSolarOrderTypes($order_type_id);?></select></p>
         </div>
