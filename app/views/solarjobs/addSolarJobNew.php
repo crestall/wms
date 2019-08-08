2fb74757797ee2975f9a863f54d62ad568b1b372
marxolly <mark@solly.com.au>
2019-08-08 16:27:28 +1000

    Update addSolarJobNew.php

--- app/views/solarjobs/addSolarJobNew.php
+++ app/views/solarjobs/addSolarJobNew.php
@@ -1,10 +1,41 @@
+<?php
+$address = Form::value('address');
+$address2 = Form::value('address2');
+$suburb = Form::value('suburb');
+$state =  (empty(Form::value('state')))? "VIC" : Form::value('state');
+$postcode = Form::value('postcode');
+$country = (empty(Form::value('country')))? "AU" : Form::value('country');
+$date_filter = "Install Date";
+$date = (empty(Form::value('date_value')))? time() : Form::value('date_value');
+?>
 <div id="page-wrapper">
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-    <div class="row form-group">
-        <label class="col-md-3">Select an Order Type</label>
-        <div class="col-md-4">
-            <p><select id="type_selector" class="form-control selectpicker"><option value="0">--Choose One--</option><?php echo $this->controller->solarordertype->getSelectSolarOrderTypes();?></select></p>
+    <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
+    <?php echo Form::displayError('general');?>
+    <div class="row">
+        <div class="col-lg-12">
+            <form id="add-service-job" method="post" action="/form/procAddSolarInstall" autocomplete="off">
+                <div class="row form-group">
+                    <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Select a Job Type</label>
+                    <div class="col-md-4">
+                        <p><select id="job_type" name="job_type" class="form-control selectpicker"><option value="0">--Choose One--</option><?php echo $this->controller->solarordertype->getSelectSolarOrderTypes(Form::value('job_type'));?></select></p>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Team</label>
+                    <div class="col-md-4">
+                        <select id="team_id" name="team_id" class="form-control selectpicker"><option value="0">--Select One--</option><?php echo $this->controller->solarteam->getSelectTeam(Form::value('team_id'));?></select>
+                        <?php echo Form::displayError('team_id');?>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Work Order</label>
+                    <div class="col-md-4">
+                        <input type="text" class="form-control required" name="work_order" id="work_order" value="<?php echo Form::value('work_order');?>" />
+                        <?php echo Form::displayError('work_order');?>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
-    <div class="row" id="form_holder"></div>
 </div>
