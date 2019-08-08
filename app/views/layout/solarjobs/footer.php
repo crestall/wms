f64313d7353c8f5c4d00f6730034fb1726589bd2
marxolly <mark@solly.com.au>
2019-08-08 16:40:26 +1000

    Update footer.php

--- app/views/layout/solarjobs/footer.php
+++ app/views/layout/solarjobs/footer.php
@@ -127,6 +127,10 @@
                             {
                                 $.blockUI({ message: '<div style="height:160px; padding-top:20px;"><h2>Adding Job...</h2></div>' });
                             }
+                            else
+                            {
+                                return false;
+                            }
                         });
                         $('select#team_id, select#job_type, #address, #suburb, #postcode, #country').change(function(e){
                             $(this).valid();
