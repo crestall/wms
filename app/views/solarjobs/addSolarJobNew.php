c215d0412b220d93f5621897ebc635fcb134e1f3
marxolly <mark@solly.com.au>
2019-08-09 12:35:21 +1000

    Update addSolarJobNew.php

--- app/views/solarjobs/addSolarJobNew.php
+++ app/views/solarjobs/addSolarJobNew.php
@@ -16,6 +16,11 @@ $odisp = (Form::value('type_id') == $origin_id)? "block":"none";
     <div class="row">
         <div class="col-lg-12">
             <form id="add-solar-install" method="post" action="/form/procAddSolarInstall" autocomplete="off">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h3>Job Details</h3>
+                    </div>
+                </div>
                 <div class="row form-group">
                     <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Select an Install Type</label>
                     <div class="col-md-4">
@@ -44,12 +49,73 @@ $odisp = (Form::value('type_id') == $origin_id)? "block":"none";
                     </div>
                 </div>
                 <?php include(Config::get('VIEWS_PATH')."layout/page-includes/select_date.php");?>
-                <?php include(Config::get('VIEWS_PATH')."forms/address.php");?>
-                <div id="origin_calc_holder" style="display:<?php echo $odisp;?>" >
-                    <p>This should slide down</p>
-                    <p>There will be calcs in here</p>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h3>Address Details</h3>
+                    </div>
                 </div>
-                <?php include(Config::get('VIEWS_PATH')."forms/item_adder.php");?> 
+                <?php include(Config::get('VIEWS_PATH')."forms/address.php");?>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h3>Install Details</h3>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-3 col-form-label">Panel</label>
+                    <div class="col-md-4">
+                        <input type="text" class="form-control origin-item-searcher" name="panel" id="panel" value="<?php echo Form::value('panel');?>" />
+                        <?php echo Form::displayError('panel');?>
+                    </div>
+                    <div class="col-md-4">
+                        <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Qty</label>
+                        <div class="col-md-3">
+                            <input type="text" class="form-control required number" name="panel_qty" id="panel_qty" value="<?php echo Form::value('panel_qty');?>" />
+                            <?php echo Form::displayError('panel_qty');?>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Inverter</label>
+                    <div class="col-md-4">
+                        <input type="text" class="form-control required origin-item-searcher" name="inverter" id="inverter" value="<?php echo Form::value('inverter');?>" />
+                        <?php echo Form::displayError('inverter');?>
+                    </div>
+                    <div class="col-md-4">
+                        <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Qty</label>
+                        <div class="col-md-3">
+                            <input type="text" class="form-control required number" name="inverter_qty" id="inverter_qty" value="<?php echo $inverter_qty;?>" />
+                            <?php echo Form::displayError('inverter_qty');?>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="form-radio">
+                        <label class="form-check-label col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Roof Type</label>
+                        <div class="col-md-2 checkbox checkbox-default">
+                            <input class="form-check-input styled required" type="radio" id="tin" name="roof_type" <?php if(Form::value('roof_type') == "tin") echo 'checked';?> value="tin" />
+                            <label for="tin"> Tin</label>
+                        </div>
+                        <div class="col-md-2 checkbox checkbox-default">
+                            <input type="radio" class="form-check-input styled" id="tile" name="roof_type" <?php if(Form::value('roof_type') == "tile") echo 'checked';?> value="tile" />
+                            <label for="tile"> Tile</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="form-check">
+                        <label class="form-check-label col-md-3" for="battery">Battery Install</label>
+                        <div class="col-md-4 checkbox checkbox-default">
+                            <input class="form-check-input styled" type="checkbox" id="battery" name="battery" <?php if(!empty(Form::value('battery'))) echo 'checked';?> />
+                            <label for="battery"></label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h3>Additional Items</h3>
+                    </div>
+                </div>
+                <?php include(Config::get('VIEWS_PATH')."forms/item_adder.php");?>
                 <input type="hidden" name="selected_items" id="selected_items" />
                 <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
                 <input type="hidden" name="client_id" id="client_id" value="<?php echo $client_id; ?>" />
