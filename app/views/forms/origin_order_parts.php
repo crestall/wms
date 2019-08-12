d2bc33203c7a2ad461e84f6b736eabf4ee283906
marxolly <mark@solly.com.au>
2019-08-12 13:29:16 +1000

    Update origin_order_parts.php

--- app/views/forms/origin_order_parts.php
+++ app/views/forms/origin_order_parts.php
@@ -10,30 +10,3 @@ $i = 0;
         <input type="hidden" name="consumables[<?php echo $i;?>][id]" value="<?php echo $details['id'];?>" />
     </div>
 <?php ++$i; endforeach;?>
-<div class="row">
-    <div class="col-lg-12">
-        <h4>Additional Items</h4>
-    </div>
-</div>
-<div id="item_selector" class="form-group row">
-    <div class="bs-callout bs-callout-primary bs-callout-more col-md-11">
-        <label class="col-md-3 col-form-label">Additional Items</label>
-        <div class="col-md-9" id="items_holder">
-            <div class="row item_holder">
-                <div class="col-sm-1 add-image-holder">
-                    <a class="add" style="cursor:pointer" title="Add Another Item">
-                        <i class="fas fa-plus-circle fa-2x text-success"></i>
-                    </a>
-                </div>
-                <div class="col-sm-4">
-                    <p><input type="text" class="form-control item-searcher" name="items[0][name]" placeholder="Item Name" /></p>
-                </div>
-                <div class="col-sm-4 qty-holder">
-
-                </div>
-                <div class="col-sm-3 qty-location"></div>
-                <input type="hidden" name="items[0][id]" class="item_id"  />
-            </div>
-        </div>
-    </div>
-</div>
