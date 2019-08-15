bac7567283c038b01d980c1af1ec55a36bfcbe4e
marxolly <mark@solly.com.au>
2019-08-15 15:10:36 +1000

    Update adjust_allocation.php

--- app/views/dashboard/adjust_allocation.php
+++ app/views/dashboard/adjust_allocation.php
@@ -2,15 +2,15 @@
     <div class="row" id="feedback_holder" style="display:none"></div>
     <div class="row">
         <form id="adjust-allocation" method="post" action="/form/procAdjustAllocations">
-            <?php foreach($items as $item):?>
+            <?php $c = 0; foreach($items as $item):?>
                 <div class="form-group row">
                     <label class="col-md-8 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> <?php echo $item['name']." (".$item['qty'].")";?></label>
                     <div class="col-md-4">
-                        <select id="allocation_id_<?php echo $item['item_id'];?>" name="allocation[<?php echo $item['item_id'];?>][location_id]" class="form-control selectpicker" data-live-search="true" required><option value="">--Select One--</option><?php echo $this->controller->location->getSelectItemInLocations($item['item_id'], $item['location_id'], false, $item['qty']) ;?></select>
+                        <select id="allocation_id_<?php echo $c;?>" name="allocation[<?php echo $item['item_id'];?>][<?php echo $c;?>][location_id]" class="form-control selectpicker" data-live-search="true" required><option value="">--Select One--</option><?php echo $this->controller->location->getSelectItemInLocations($item['item_id'], $item['location_id'], false, $item['qty']) ;?></select>
                     </div>
                 </div>
-                <input type="hidden" name="allocation[<?php echo $item['item_id'];?>][qty]" value="<?php echo $item['qty'];?>" />
-            <?php endforeach;?>
+                <input type="hidden" name="allocation[<?php echo $item['item_id'];?>][<?php echo $c;?>][qty]" value="<?php echo $item['qty'];?>" />
+            <?php ++$c; endforeach;?>
             <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
             <input type="hidden" name="order_id" value="<?php echo $order_id;?>" />
             <div class="form-group row">
