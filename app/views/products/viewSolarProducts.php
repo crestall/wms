8a23b492470f486bb1ee5febb7273d08fb0c33e4
marxolly <mark@solly.com.au>
2019-09-05 14:56:39 +1000

    Update viewSolarProducts.php

--- app/views/products/viewSolarProducts.php
+++ app/views/products/viewSolarProducts.php
@@ -39,7 +39,12 @@
                                 <td data-label="Supplier"><?php echo $p['supplier'];?></td>
                                 <td data-label="Owner"><?php echo $owner;?></td>
                                 <td data-label="Barcode" class="number"><?php echo $p['barcode'];?></td>
-                                <td></td>
+                                <td>
+                                    <p><input type="text" class="form-control number" id="lowstock_<?php echo $p['id'];?>" name="lowstock_<?php echo $p['id'];?>" value="<?php echo $p['low_stock_warning'];?>" /></p>
+                					<p><button class="btn btn-primary btn-sm update_product" data-productid="<?php echo $p['id'];?>">Update</button> </p>
+                                    <div class="errorbox" style="display:none;" id="error_<?php echo$p['id'];?>">Only input whole, positive numbers please</div>
+                                	<div class="feedbackbox" style="display:none;" id="feedback_<?php echo$p['id'];?>">Product warning level updated</div>
+                                </td>
                             </tr>
                         <?php endforeach;?>
                     </tbody>
