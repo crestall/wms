4e6daebe741beebe4b91a29ed2b7ef279df234fa
marxolly <mark@solly.com.au>
2019-09-13 15:34:41 +1000

    sydney comet courier add

--- app/core/OrderFulfiller.php
+++ app/core/OrderFulfiller.php
@@ -285,6 +285,42 @@
         $this->recordOutput("order_fulfillment/cometlocal");
     }
 
+    public function fulfillSydneyCometOrder($order_ids)
+    {
+        $this->output = "=========================================================================================================".PHP_EOL;
+        $this->output .= "FULFILLING SYDNEY COMET ORDERS ON ".date("jS M Y (D), g:i a (T)").PHP_EOL;
+        $this->output .= "=========================================================================================================".PHP_EOL;
+        $db = Database::openConnection();
+        $emails_to_send = array();
+        foreach($order_ids as $id)
+        {
+            $od = $this->controller->order->getOrderDetail($id);
+            if($od['status_id'] == $this->controller->order->picked_id || $od['status_id'] == $this->controller->order->packed_id)
+            {
+                $this->output .= "----------------------------------------------------------------------------------------------------".PHP_EOL;
+                $this->output .= "Doing Order Number: ".$od['order_number']." Using ".$this->controller->courier->getCourierName($od['courier_id']).PHP_EOL;
+                $db->updateDatabaseFields('orders', array('status_id' => $this->controller->order->fulfilled_id, 'date_fulfilled' => time()), $id);
+                //order is now fulfilled, reduce stock
+                $items = $this->controller->order->getItemsForOrder($id);
+                $this->output .= "Reducing Stock and recording movement for order id: $id".PHP_EOL;
+                $this->removeStock($items, $id);
+                if($od['client_id'] == 59)
+                {
+                    $this->output .= "Sending Noa Sleep confirmation".PHP_EOL;
+                    Email::sendNoaSydneyConfirmEmail($od['id']);
+                }
+                Session::set('showfeedback', true);
+                $_SESSION['feedback'] .= "<p>{$od['order_number']} has been successfully fulfilled</p>";
+            }
+            else
+            {
+                Session::set('showerrorfeedback', true);
+        	    $_SESSION['errorfeedback'] .= "<h3>{$od['order_number']} has not had the labels or pickslip printed</h3><p>Please do at least one and try again</p>";
+            }
+        }
+        $this->recordOutput("order_fulfillment/sydneycomet");
+    }
+
     public function fulfillSolarOrder($order_ids)
     {
         $this->output = "=========================================================================================================".PHP_EOL;
