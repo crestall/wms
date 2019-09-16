4e5836257aaa32c4459c0f7bfeeea0e87610d8d2
marxolly <mark@solly.com.au>
2019-09-16 12:26:22 +1000

    sydney comet email

--- app/core/Email.php
+++ app/core/Email.php
@@ -507,6 +507,94 @@
 
     }
 
+    public static function sendNoaSydneyConfirmEmail($order_id)
+    {
+        $db = Database::openConnection();
+		$mail = new PHPMailer(); // defaults to using php "mail()"
+		$body = file_get_contents(Config::get('EMAIL_TEMPLATES_PATH')."noa_shipped_syd_commet_email.html");
+        $od = $db->queryRow("SELECT * FROM orders WHERE id = $order_id");
+        $items = $db->queryData("SELECT i.*, oi.qty, oi.location_id
+            FROM orders_items oi JOIN items i ON oi.item_id = i.id
+            WHERE oi.order_id = $order_id");
+
+        $client_name = "Noa Home";
+        //$cd = $db->queryRow("SELECT * FROM customers WHERE id = {$od['customer_id']}");
+
+        $email_link = "<a href='mailto:{$od['tracking_email']}'>{$od['tracking_email']}</a>";
+        $sad =  $bad = array(
+            'address'   =>  $od['address'],
+            'address_2' =>  $od['address_2'],
+            'suburb'    =>  $od['suburb'],
+            'state'     =>  $od['state'],
+            'postcode'  =>  $od['postcode'],
+            'country'   =>  $od['country']
+        );
+
+        $billing_string = "<p>{$od['ship_to']}</br/>";
+        $billing_string .= $bad['address']."<br/>";
+        if(!empty($bad['address_2']))
+        {
+            $billing_string .= $bad['address_2']."<br/>";
+        }
+        $billing_string .= $bad['suburb'].", ".$bad['state']." ".$bad['postcode']."<br/>";
+        $billing_string .= $db->queryValue('countries', array('iso_code_2' => $bad['country']), "name")."</p>";
+        /*
+        $shipping_string = "<p>{$cd['name']}</br/>";
+        $shipping_string .= $sad['address']."<br/>";
+        if(!empty($sad['address_2']))
+        {
+            $shipping_string .= $sad['address_2']."<br/>";
+        }
+        $shipping_string .= $sad['suburb'].", ".$sad['state']." ".$sad['postcode']."<br/>";
+        $shipping_string .= $db->queryValue('countries', array('iso_code_2' => $sad['country']), 'name')."</p>";
+        */
+        $shipping_string = $billing_string;
+        $items_string = "";
+        foreach($items as $i)
+        {
+            $item_price = "$".number_format($i['price'], 2);
+            $items_string .= "<tr><td><a href='#'>{$i['name']}</a></td><td>{$i['qty']}</td><td>$item_price <span class='extax'>(ex.tax)</span></td></tr>";
+        }
+        $total_price = "$".number_format($od['order_total'], 2);
+        $items_string .= "<tr><td colspan='2' class='bold'>Subtotal:</td><td>$total_price <span class='extax'>(ex.tax)</span></td></tr>";
+        $items_string .= "<tr><td colspan='2' class='bold'>Shipping:</td><td>Free shipping</td></tr>";
+
+        $tracking_info = 'Please visit <a href="https://startrack.com.au/">startrack.com.au</a> and click the "Track & Trace" link at the top of the page. Enter '.$od['consignment_id'].' as the Tracking / reference number(s) for the latest on your delivery. In the event of your absence at the time of delivery, the driver may occasionally, based on careful judgment and inspection of the premises, leave your order in a safe place on your property without your signature ("Authority To Leave"). The driver is not responsible for in home or room placement of your order.<br/>';
+        if($od['courier_id'] == $db->queryValue("couriers", array('name' => 'Direct Freight')))
+        {
+            $tracking_info = 'Please visit <a href="https://directfreight.com.au/">Direct Freight</a> and enter '.$od['consignment_id'].' in the Track & Trace form and click "Track" for the latest on your delivery. In the event of your absence at the time of delivery, the driver may occasionally, based on careful judgment and inspection of the premises, leave your order in a safe place on your property without your signature ("Authority To Leave"). The driver is not responsible for in home or room placement of your order.<br/>';
+        }
+
+        $replace_array = array("{NAME}", "{ORDER_NUMBER}", "{EMAIL_LINK}", "{PHONE_NUMBER}", "{BILLING_STRING}", "{SHIPPING_STRING}", "{TRACKING_INFO}", "{ITEM_BODY}");
+		$replace_with_array = array($od['ship_to'], $od['client_order_id'], $email_link, $od['contact_phone'], $billing_string, $shipping_string, $tracking_info, $items_string);
+		$body = str_replace($replace_array, $replace_with_array, $body);
+
+		$mail->SetFrom(Config::get('EMAIL_FROM'), Config::get('EMAIL_FROM_NAME'));
+		$mail->Subject = "Your Order With Noa Home Has Been Dispatched";
+		$mail->MsgHTML($body);
+		//$mail->AddEmbeddedImage("$root/images/3pl_logo.png", "emailfoot", "3pl_logo.png");
+        $mail->AddAddress('mark.solly@3plplus.com.au', 'Mark Solly');
+		//$mail->AddAddress($od['tracking_email'], $od['ship_to']);
+        /*
+        $mail->AddBCC('jc@noasleep.com');
+        $mail->AddBCC('jeremykopek@noasleep.com');
+        $mail->AddBCC('customersupport@3plplus.com.au');
+        $mail->AddBCC('viviannenguyen@noasleep.com', 'Vivianne');
+        $mail->AddBCC('janikakopek@noahome.com', 'Janika');
+        //$mail->AddBCC('mark.solly@3plplus.com.au', 'Mark Solly');
+        */
+        if(!$mail->Send())
+		{
+			die($mail->ErrorInfo);
+		}
+		else
+		{
+		    $db->updateDatabaseField('orders', 'customer_emailed', 1, $order_id);
+			return true;
+		}
+
+    }
+
     public static function sendNoaLocalConfirmEmail($order_ids)
     {
         $db = Database::openConnection();
