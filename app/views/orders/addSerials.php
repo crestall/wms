c2852696c01bd6616167f392fd53e7d6b4cbd92c
marxolly <mark@solly.com.au>
2019-10-14 15:40:32 +1100

    Update addSerials.php

--- app/views/orders/addSerials.php
+++ app/views/orders/addSerials.php
@@ -1,4 +1,15 @@
 <div id="page-wrapper">
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-    
+    <div class="form-group row">
+        <label class="col-form-label col-md-3">Consignment ID</label>
+        <div class="col-md-4">
+            <input type="text" class="form-control" name="order_number" id="order_number" placeholder="Order Number" value="<?php echo Form::value('order_number');?>" />
+        </div>
+        <div class="col-md-2">
+            <button id="find_order" class="btn btn-sm btn-success">Locate Order Items</button>
+        </div>
+        <div class="col-md-2">
+            <button style="display:none" class="btn btn-sm btn-warning" id="clear_order">Clear Order Items</button>
+        </div>
+    </div>
 </div>
