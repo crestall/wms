09d05b5040a6cfd122d8e7ca5c925727fb194628
marxolly <mark@solly.com.au>
2019-10-15 13:20:05 +1100

    Update add_serials.php

--- app/views/forms/add_serials.php
+++ app/views/forms/add_serials.php
@@ -4,6 +4,6 @@ echo "<pre>",print_r($items),"</pre>";
 <?php foreach($items as $item): ?>
     <div class="form-group row">
         <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> <?php echo $item['name']."(".$item['sku'].")";?></label>
-        <div class="col-md-3"><input type="text" name="serial[<?php echo $item;?>][line_id]" class="form-control required" placeholder="Serial Number" /></div>
+        <div class="col-md-3"><input type="text" name="serial[<?php echo $item[line_id];?>]" class="form-control required" placeholder="Serial Number" /></div>
     </div>
 <?php endforeach;?>
