9286cd11765662e6f3f15747ca66c3f874811dd3
marxolly <mark@solly.com.au>
2019-10-16 14:48:42 +1100

    Update add_serials.php

--- app/views/forms/add_serials.php
+++ app/views/forms/add_serials.php
@@ -6,13 +6,19 @@
         $serials = $this->controller->orderitemserials->getRecordedSerials($item['order_id'], $item['item_id']);
         echo "<pre>",print_r($serials),"</pre>";
         $c = 1;
-        while($c <= $item['qty']){?>
+        while($c <= $item['qty']){
+            foreach($serials as $s):?>
+                <div class="form-group row">
+                    <label class="col-md-5 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> serial<?php echo $item['name']." (".$item['sku'].")";?></label>
+                    <div class="col-md-3"><input type="text" name="serial[<?php echo $c;?>][<?php echo $item['item_id'];?>]" class="form-control required" placeholder="Serial Number" /></div>
+                </div>
+                <?php ++$c;?>
+            <?php endforeach;?>
             <div class="form-group row">
                 <label class="col-md-5 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> <?php echo $item['name']." (".$item['sku'].")";?></label>
                 <div class="col-md-3"><input type="text" name="serial[<?php echo $c;?>][<?php echo $item['item_id'];?>]" class="form-control required" placeholder="Serial Number" /></div>
             </div>
-        <?php ++$c; };?>
-    <?php endforeach;?>
+            <?php ++$c;?>
     <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
     <input type="hidden" name="order_id" id="order_id" value="<?php echo $order_id;?>" />
     <div class="form-group row">
