2548c45111e30b5d3d59e52c36ca3b79ef91dcfd
marxolly <mark@solly.com.au>
2019-10-16 12:55:19 +1100

    Update add_serials.php

--- app/views/forms/add_serials.php
+++ app/views/forms/add_serials.php
@@ -6,12 +6,12 @@
         $serials = $this->controller->orderitemserials->getRecordedSerials($item['order_id'], $item['item_id']);
         //echo "<pre>",print_r($serials),"</pre>";
         $c = 1;
-        while($c <= $item['qty']);?>
+        while($c <= $item['qty']);{?>
             <div class="form-group row">
-                <label class="col-md-5 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> <?php echo $item['name']."(".$item['sku'].")";?></label>
+                <label class="col-md-5 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> <?php echo $item['name']." (".$item['sku'].")";?></label>
                 <div class="col-md-3"><input type="text" name="serial[<?php echo $item['line_id'];?>]" class="form-control required" placeholder="Serial Number" /></div>
             </div>
-        <?php ++$c; endwhile;?>
+        <?php ++$c; };?>
     <?php endforeach;?>
     <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
     <input type="hidden" name="order_id" id="order_id" value="<?php echo $order_id;?>" />
