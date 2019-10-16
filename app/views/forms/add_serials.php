5a5be96d46097930ddafca05e1afc44bace66a33
marxolly <mark@solly.com.au>
2019-10-16 13:01:49 +1100

    Update add_serials.php

--- app/views/forms/add_serials.php
+++ app/views/forms/add_serials.php
@@ -4,7 +4,7 @@
 <form id="add_serials" method="post" action="/form/procAddSerials">
     <?php foreach($items as $item):
         $serials = $this->controller->orderitemserials->getRecordedSerials($item['order_id'], $item['item_id']);
-        //echo "<pre>",print_r($serials),"</pre>";
+        echo "<pre>",print_r($serials),"</pre>";
         $c = 1;
         while($c <= $item['qty']){?>
             <div class="form-group row">
