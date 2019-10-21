c9d2a890bb5a07e39f3d6f2e2f5bfa003ce4fa12
marxolly <mark@solly.com.au>
2019-10-21 11:23:40 +1100

    Update FormController.php

--- app/controllers/FormController.php
+++ app/controllers/FormController.php
@@ -728,7 +728,7 @@ class FormController extends Controller {
             {
                 $tmp_name = $_FILES['csv_file']['tmp_name'];
                 $csv_array = array_map('str_getcsv', file($tmp_name));
-                //echo "<pre>",print_r($csv_array),"</pre>"; die();
+                echo "<pre>",print_r($csv_array),"</pre>"; die();
                 Session::set('feedback',"<h2><i class='far fa-check-circle'></i>Orders have been fulfilled</h2>");
                 foreach($csv_array as $r)
                 {
