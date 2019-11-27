09309c20ad43450b576d98d5b683438fb8c82562
marxolly <mark@solly.com.au>
2019-11-27 16:50:38 +1100

    Update Order.php

--- app/models/Order.php
+++ app/models/Order.php
@@ -965,7 +965,7 @@ class Order extends Model{
                 FROM
                     orders o
                 WHERE
-                    o.date_fulfilled >= $from) - (60*24*60*60)) AND o.date_fulfilled <= $to)";
+                    o.date_fulfilled >= (($from) - (60*24*60*60)) AND (o.date_fulfilled <= $to)";
 
                 if($client_id > 0)
                     $query1 .= " AND o.client_id = ".$client_id;
