8a58353952c47b104077f9eb05db42dd4a73fb47
marxolly <mark@solly.com.au>
2020-03-24 14:44:29 +1100

    Update Directfreight.php

--- app/core/Directfreight.php
+++ app/core/Directfreight.php
@@ -93,6 +93,8 @@
             ),
             CURLOPT_VERBOSE => true
         ));
+        $verbose = fopen('php://temp', 'w+');
+        curl_setopt($handle, CURLOPT_STDERR, $verbose);
         /*
         //curl_setopt_array ( $ch, $this->curl_options );
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -106,6 +108,19 @@
         */
 
         $result = curl_exec($ch);
+
+        if ($result === FALSE) {
+            printf("cUrl error (#%d): %s<br>\n", curl_errno($handle),
+                   htmlspecialchars(curl_error($handle)));
+        }
+
+        rewind($verbose);
+        $verboseLog = stream_get_contents($verbose);
+
+        echo "Verbose information:\n<pre>", htmlspecialchars($verboseLog), "</pre>\n";
+
+        die();
+
         $err = curl_error($ch);
         curl_close($ch);
         if ($err)
