a9738c55cebac408866552c5632abbc12f75354e
marxolly <mark@solly.com.au>
2020-04-14 10:58:21 +1000

    Update FormController.php

--- app/controllers/FormController.php
+++ app/controllers/FormController.php
@@ -466,6 +466,7 @@ class FormController extends Controller {
                         'postcode'      => trim($r[10]),
                         'contact_phone' => trim($r[5]),
                         'date'          => time(),
+                        'country'       => 'AU',
                         'signature_req' => 0,
                         'errors'        => 0,
                         'error_string'  => '',
