e704a05ec9e28133a82058d579c81f578e6face9
marxolly <mark@solly.com.au>
2020-04-16 12:56:06 +1000

    Update locations.php

--- app/views/sitesettings/locations.php
+++ app/views/sitesettings/locations.php
@@ -1,8 +1,21 @@
 <?php
-
+    if($active == 0)
+    {
+            $link_text = "<a href='/clients/view-clients' class='btn btn-primary'>View Active Locations</a>";
+    }
+    else
+    {
+            $link_text = "<a href='/clients/view-clients/active=0' class='btn btn-warning'>View Inactive Locations</a>";
+    }
+    $i = 1;
 ?>
 <div id="page-wrapper">
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+    <div class="row">
+        <div class="col-lg-12">
+            <p class="text-right"><?php echo $link_text;?></p>
+        </div>
+    </div>
     <div class="row">
         <div class="col-lg-12">
             <h2>Add a New Location</h2>
