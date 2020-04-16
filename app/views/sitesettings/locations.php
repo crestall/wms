1203c62c1011f6bdf36db528aedd1ab805c88dc2
marxolly <mark@solly.com.au>
2020-04-16 12:59:45 +1000

    Update locations.php

--- app/views/sitesettings/locations.php
+++ app/views/sitesettings/locations.php
@@ -1,11 +1,11 @@
 <?php
     if($active == 0)
     {
-            $link_text = "<a href='/clients/view-clients' class='btn btn-primary'>View Active Locations</a>";
+            $link_text = "<a href='/site-settings/locations' class='btn btn-primary'>View Active Locations</a>";
     }
     else
     {
-            $link_text = "<a href='/clients/view-clients/active=0' class='btn btn-warning'>View Inactive Locations</a>";
+            $link_text = "<a href='/site-settings/locations/active=0' class='btn btn-warning'>View Inactive Locations</a>";
     }
     $i = 1;
 ?>
