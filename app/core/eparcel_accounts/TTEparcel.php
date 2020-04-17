aa34e2e3b2421805b7bbd40781f2ebf68624cce6
marxolly <mark@solly.com.au>
2020-04-17 13:52:11 +1000

    Create TTEparcel.php

---
+++ app/core/eparcel_accounts/TTEparcel.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Twin Towers Location for the Eparcel class.
+ *
+ *
+ * @author     Mark Solly <mark.solly@3plplus.com.au>
+ */
+
+class TTEparcel extends Eparcel
+{
+    private $client_id = 5;
+    //const    API_BASE_URL = '/testbed/shipping/v1/';
+	public function init()
+	{
+    	$cd = $this->controller->client->getClientInfo($this->client_id);
+
+        if(!empty($cd['api_key']))
+        {
+            $this->API_KEY = $cd['api_key'];
+            $this->API_PWD = $cd['api_secret'];
+            $this->ACCOUNT_NO = str_pad($cd['charge_account'], 10, '0', STR_PAD_LEFT);
+        }
+	}
+}//end class
+
+?>
