4790c6a6e2bdab987a2aff69afdd491e1b533016
marxolly <mark@solly.com.au>
2020-04-21 14:41:34 +1000

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -56,9 +56,9 @@ return array(
      * Configuration for Email
      *
      */
-    "EMAIL_FROM"        => "no-reply@3plplus.com.au",
-    "EMAIL_FROM_NAME"   => "3PL Plus Warehouse Management System",
-    "EMAIL_REPLY_TO"    => "no-reply@3plplus.com.au",
+    "EMAIL_FROM"        => "no-reply@fsg.com.au",
+    "EMAIL_FROM_NAME"   => "Film Shot Graphics Warehouse Management System",
+    "EMAIL_REPLY_TO"    => "no-reply@fsg.com.au",
 
     "EMAIL_PASSWORD_RESET_URL" => PUBLIC_ROOT . "login/resetPassword",
 
