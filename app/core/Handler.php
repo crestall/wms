981286ff0f192b0ad484455a188d3b8803c07576
marxolly <mark@solly.com.au>
2020-04-21 10:36:37 +1000

    debug

--- app/core/Handler.php
+++ app/core/Handler.php
@@ -16,7 +16,9 @@ class Handler{
      * Constructor
      *
      */
-    private function __construct(){}
+    private function __construct(){
+        die('Handler Constructed');
+    }
 
     /**
      * Register the error and exception handlers.
