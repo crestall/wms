1895b0a263fb4a6681e8ea9fb81639ced7c9f59f
Mark Solly <msolly@contactpoint.com.au>
2020-05-05 09:06:49 +1000

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -59,7 +59,7 @@ return array(
     "EMAIL_FROM"        => "FSGWMS@fsg.com.au",
     "EMAIL_FROM_NAME"   => "Film Shot Graphics Warehouse Management System",
     "EMAIL_REPLY_TO"    => "FSGWMS@fsg.com.au",
-    "EMAIL_PWD"         => "noreply865!";
+    "EMAIL_PWD"         => "noreply865!",
     "EMAIL_UNAME"       => "FSGWMS@fsg.com.au",
     "EMAIl_HOST"        => "smtp.office365.com",
     "EMAIL_PORT"        => 587,
