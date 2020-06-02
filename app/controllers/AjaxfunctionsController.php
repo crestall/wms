23e869f6d4cd2f55e62e8b55eb58f70f50b42d15
Mark Solly <msolly@contactpoint.com.au>
2020-06-03 09:15:11 +1000

    Update AjaxfunctionsController.php

--- app/controllers/AjaxfunctionsController.php
+++ app/controllers/AjaxfunctionsController.php
@@ -71,7 +71,7 @@ class ajaxfunctionsController extends Controller
             }
         }
         $encrypted_value = Encryption::encrypt($string);
-        die('encrypted: '.$encrypted_value)
+        die('encrypted: '.$encrypted_value);
         $data = array(
             'error'             =>  false,
             'encryptedvalue'    =>  $encrypted_value,
