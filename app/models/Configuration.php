0277ec26dc00fddce80b9b352bc396eaa8ca6808
Mark Solly <msolly@contactpoint.com.au>
2020-06-05 09:45:20 +1000

    Create Configuration.php

---
+++ app/models/Configuration.php
@@ -0,0 +1,61 @@
+<?php
+class Configuration extends Model{
+    public $table = "configuration";
+
+    public function __construct(){ }
+
+    public function addConfiguration($data)
+    {
+        $db = Database::openConnection();
+        $vals = array(
+            'name'          => $data['name'],
+            'value'         => $data['value'],
+            'date_added'    => time();
+        );
+        return $db->insertQuery($this->table, $vals);
+    }
+
+    public function editConfiguration($data)
+    {
+        $db = Database::openConnection();
+        $vals = array(
+            'name'          => $data['name'],
+            'value'         => $data['value'],
+            'date_modified' => time();
+        );
+        $db->updateDatabaseFields($this->table, $vals, $data['id']);
+        return true;
+    }
+
+    public function getConfigurationName($id)
+    {
+        $db = Database::openConnection();
+        return $db->queryValue($this->table, array('id' => $id), 'name');
+    }
+
+    public function getConfigurationValue($id)
+    {
+        $db = Database::openConnection();
+        return $db->queryValue($this->table, array('id' => $id), 'value');
+    }
+
+    public function getConfigurationValueByName($name)
+    {
+        $db = Database::openConnection();
+        return $db->queryValue($this->table, array('name' => $name), 'value');
+    }
+
+    public function getConfigurations()
+    {
+        $db = Database::openConnection();
+        $q = "SELECT * FROM {$this->table} ORDER BY name";
+        return $db->queryData($q);
+    }
+
+    public function getConfigurationId($name)
+    {
+        $db = Database::openConnection();
+        return $db->queryValue($this->table, array('name' => $name));
+    }
+}
+?>
