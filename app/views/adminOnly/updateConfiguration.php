3aa4aa78c9fc67daf64d873f785df403de8d4dcb
Mark Solly <msolly@contactpoint.com.au>
2020-06-05 10:05:48 +1000

    Update updateConfiguration.php

--- app/views/adminOnly/updateConfiguration.php
+++ app/views/adminOnly/updateConfiguration.php
@@ -35,4 +35,46 @@
             </div>
         </form>
     </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <h2>Current Configuration</h2>
+        </div>
+    </div>
+        <?php if(count($configuraion)):?>
+        <div class="row">
+            <?php foreach($configuration as $c):?>
+                <form class="edit-config" action="/form/procConfigEdit" method="post">
+                    <div class="form-group row">
+                        <div class="col-md-3">
+                            <label class="col-form-label">Name</label>
+                            <input type="text" class="form-control required" name="name_<?php echo $c['id'];?>" id="name_<?php echo $c['id'];?>" value="<?php echo $c['name'];?>" />
+                            <?php echo Form::displayError("name_{$c['id']}");?>
+                        </div>
+                        <div class="col-md-3">
+                            <label class="col-form-label">Value</label>
+                            <input type="text" class="form-control" name="value_<?php echo $c['id'];?>" id="value<?php echo $c['id'];?>" value="<?php echo $c['value'];?>" />
+                            <?php echo Form::displayError("value{$c['id']}");?>
+                        </div>
+                        <div class="col-md-1">
+                            <label class="col-form-label">&nbsp;</label>
+                            <div class="input-group">
+                                <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+                                <input type="hidden" name="line_id" value="<?php echo $c['id'];?>" />
+                                <button type="submit" class="btn btn-primary">Update</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            <?php endforeach;?>
+        </div>
+    <?php else:?>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="errorbox">
+                    <h2><i class="fas fa-exclamation-triangle"></i> No Couriers Listed</h2>
+                    <p>You will need to add some first</p>
+                </div>
+            </div>
+        </div>
+    <?php endif;?>
 </div>
