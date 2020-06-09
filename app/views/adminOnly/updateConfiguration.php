2ff9f8f62afd7cb6356ec441474dca8b429c63f5
Mark Solly <msolly@contactpoint.com.au>
2020-06-09 14:47:48 +1000

    Update updateConfiguration.php

--- app/views/adminOnly/updateConfiguration.php
+++ app/views/adminOnly/updateConfiguration.php
@@ -6,7 +6,7 @@ $r = 1;
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php"); ?>
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
     <?php echo Form::displayError('general');?>
-    <?php echo "Something here ".Config::get('EPARCEL_ACCOUNT_NUMBER');?>
+    <?php echo "Something here ".Config::get('EPARCEL_ACCOUNT_NO');?>
     <div class="row">
         <form id="add-config-value"  method="post" action="/form/procConfigAdd">
             <div class="row">
