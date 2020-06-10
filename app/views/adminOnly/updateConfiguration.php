b08ff92af3bfd7bdc3f23e4ad72e26ed67eb6fd3
Mark Solly <msolly@contactpoint.com.au>
2020-06-11 09:46:35 +1000

    Update updateConfiguration.php

--- app/views/adminOnly/updateConfiguration.php
+++ app/views/adminOnly/updateConfiguration.php
@@ -6,7 +6,7 @@ $r = 1;
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php"); ?>
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
     <?php echo Form::displayError('general');?>
-    <?php echo "Something here ".Config::get('COOKIE_SECRET_KEY');?>
+    <?php echo "Something here ".Config::get('EMAIL_PWD');?>
     <div class="row">
         <form id="add-config-value"  method="post" action="/form/procConfigAdd">
             <div class="row">
