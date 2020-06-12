9175b3d6c4ace13b9cc1d96d02292bfede2c6a15
Mark Solly <msolly@contactpoint.com.au>
2020-06-12 11:47:04 +1000

    Update Encryption.php

--- app/core/Encryption.php
+++ app/core/Encryption.php
@@ -315,19 +315,4 @@ class Encryption{
 
         return $result === 0;
     }
-
-    private static function crypto_rand_secure($min, $max)
-    {
-        $range = $max - $min;
-        if ($range < 0) return $min; // not so random...
-        $log = log($range, 2);
-        $bytes = (int) ($log / 8) + 1; // length in bytes
-        $bits = (int) $log + 1; // length in bits
-        $filter = (int) (1 << $bits) - 1; // set all lower bits to 1
-        do {
-            $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
-            $rnd = $rnd & $filter; // discard irrelevant bits
-        } while ($rnd >= $range);
-        return $min + $rnd;
-    }
 }
