20acafe6679ee5b034cadf338391fe940dcde7f4
Mark Solly <msolly@contactpoint.com.au>
2020-06-12 10:51:09 +1000

    Update Encryption.php

--- app/core/Encryption.php
+++ app/core/Encryption.php
@@ -255,6 +255,27 @@ class Encryption{
     }
 
     /**
+     * Get a Random Token.
+     *
+     * @access public
+     * @static static method
+     * @param  number $length - default value = 32
+     * @return string
+     */
+    public static function getRandomToken($length=32)
+    {
+        $token = "";
+        $codeAlphabet  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $codeAlphabet .= "abcdefghijklmnopqrstuvwxyz";
+        $codeAlphabet .= "0123456789";
+        $codeAlphabet .= "!@#$%^&*()?/><";
+        for($i=0;$i<$length;$i++){
+            $token .= $codeAlphabet[self::crypto_rand_secure(0,strlen($codeAlphabet))];
+        }
+        return $token;
+    }
+
+    /**
      * A timing attack resistant comparison.
      *
      * @access private
@@ -286,5 +307,20 @@ class Encryption{
         }
 
         return $result === 0;
+    }
+
+    private static function crypto_rand_secure($min, $max)
+    {
+        $range = $max - $min;
+        if ($range < 0) return $min; // not so random...
+        $log = log($range, 2);
+        $bytes = (int) ($log / 8) + 1; // length in bytes
+        $bits = (int) $log + 1; // length in bits
+        $filter = (int) (1 << $bits) - 1; // set all lower bits to 1
+        do {
+            $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
+            $rnd = $rnd & $filter; // discard irrelevant bits
+        } while ($rnd >= $range);
+        return $min + $rnd;
     }
 }
