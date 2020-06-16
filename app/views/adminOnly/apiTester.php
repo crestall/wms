24edded0df05b3a19c7778ecc4edaa35a3b8248c
Mark Solly <msolly@contactpoint.com.au>
2020-06-16 11:06:07 +1000

    Update apiTester.php

--- app/views/adminOnly/apiTester.php
+++ app/views/adminOnly/apiTester.php
@@ -1,12 +1,17 @@
 <?php
-$r = 1;
-
+$encryptedData = $freedomMYOB->call('getMYOBOrders',array());
+$invoices =  json_decode(decryptData($encryptedData),true);
 ?>
 <div id="page-wrapper">
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php"); ?>
     <div class="row">
         <div class="col-lg-12">
             <h2>The Freedom MYOB API</h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <?php echo "<pre>",print_r($invoices),"</pre>";?>
         </div>
     </div>
 </div>
