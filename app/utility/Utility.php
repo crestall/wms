ef19bdc08b33a76809599c74b7e39fe3397817f9
Mark Solly <msolly@contactpoint.com.au>
2020-06-22 10:12:32 +1000

    Update Utility.php

--- app/utility/Utility.php
+++ app/utility/Utility.php
@@ -355,6 +355,7 @@ class Utility{
 
     public static function formatMobileString($string, $nz = false)
     {
+        echo "<pre>",var_dump($nz),"</pre>$string";
         if($nz)
         {
             if(  preg_match( '/^(\d{2})(\d{2,4})(\d{3})(\d{3})/', $string,  $matches ) )
