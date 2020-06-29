ac26048240ba3063d88d7898426b956bacbf8056
marxolly <mark@solly.com.au>
2020-06-29 14:02:51 +1000

    Update constants.php

--- app/config/constants.php
+++ app/config/constants.php
@@ -29,8 +29,8 @@ define('SITE_LIVE', false);
 * Database Configuration
 **************************************************************************/
 define('DB_HOST', "localhost");
-define('DB_NAME', "fsg_wms_dev");
-define('DB_USER', "dev_website");
-define('DB_PASS', "Cra233%d");
+define('DB_NAME', "fsg_wms");
+define('DB_USER', "website");
+define('DB_PASS', "66ihu#9J");
 define('DB_CHARSET', "utf8");
 ?>
