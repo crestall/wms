136d1cd00d4f236750eb6fc1f377af42306b746f
marxolly <mark@solly.com.au>
2020-06-29 12:35:39 +1000

    Update AjaxfunctionsController.php

--- app/controllers/AjaxfunctionsController.php
+++ app/controllers/AjaxfunctionsController.php
@@ -756,20 +756,7 @@ class ajaxfunctionsController extends Controller
                 }
             /*********** charge FREEDOM more *******************/
         }
-        if(3==3)
-        {
-            $df_charge = "$".number_format($df_response['TotalFreightCharge'] * 1.35 * 1.1 * DF_FUEL_SURCHARGE);
-            /*********** charge FREEDOM more *******************/
-                if($od['client_id'] == 7)
-                {
-                    $df_charge = "$".number_format($df_response['TotalFreightCharge'] * 1.4 * 1.1 * DF_FUEL_SURCHARGE);
-                }
-            /*********** charge FREEDOM more *******************/
-        }
-        else
-        {
-            $df_charge = "<div class='errorbox'><p>".$df_response['ResponseMessage']."</p></div>";
-        }
+        
 
         $this->view->render(Config::get('VIEWS_PATH') . 'dashboard/shipping_quotes.php', [
             'od'                        =>  $od,
