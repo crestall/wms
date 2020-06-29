72d5e124b257b6a636f42b5fbad2d87fc9a840a6
marxolly <mark@solly.com.au>
2020-06-29 11:05:57 +1000

    Update Config.php

--- app/core/Config.php
+++ app/core/Config.php
@@ -104,6 +104,7 @@ class Config{
             $db = Database::openConnection();
             if($sv = $db->queryValue('configuration', array('name' => $key), 'value'))
             {
+                echo"<p>$sv</p>";
                 self::$config[$source][$key] = Encryption::decryptStringBase64($sv);
                 echo"<p>$key :".self::$config[$source][$key]."</p>";
                 return self::$config[$source][$key] ;
