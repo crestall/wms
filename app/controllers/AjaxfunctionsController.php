1286825ee0ee6dc15096b248771c7de465407443
marxolly <mark@solly.com.au>
2020-06-29 12:25:02 +1000

    Update AjaxfunctionsController.php

--- app/controllers/AjaxfunctionsController.php
+++ app/controllers/AjaxfunctionsController.php
@@ -731,7 +731,7 @@ class ajaxfunctionsController extends Controller
         $eeparcel_shipments['shipments'][0] = $eparcel_express_details;
         /*  */ 
         $df_details = $this->directfreight->getDetails($od, $items);
-        //echo "<pre>",print_r(json_encode($df_details)),"</pre>"; //die();
+        echo "<pre>",print_r(json_encode($df_details)),"</pre>"; //die();
         $df_response = $this->directfreight->getQuote($df_details);
         //echo "<pre>",print_r($df_response),"</pre>"; //die();
 
