00d5c5f51062956a828fdbdc1d9e388949bcd4d6
Mark Solly <msolly@contactpoint.com.au>
2020-07-02 17:30:29 +1000

    Update AdminonlyController.php

--- app/controllers/AdminonlyController.php
+++ app/controllers/AdminonlyController.php
@@ -124,7 +124,7 @@ class adminonlyController extends Controller
         {
             //$address =str_replace("\r\n", "\n", $inv['ShipToAddress']);
             try{
-                list($name, $line1, $line2) = explode("<br />", $inv['ShipToAddress']));
+                list($name, $line1, $line2) = explode("<br />", $inv['ShipToAddress']);
                 echo "<p>Name: $name</p>";
                 echo "<p>Line1: $line1</p>";
                 echo "<p>Line2: $line2</p>";
