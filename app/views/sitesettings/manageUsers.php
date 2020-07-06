d9827618192f019ab46b70cb0ff787345537c359
marxolly <mark@solly.com.au>
2020-07-06 15:53:25 +1000

    Update manageUsers.php

--- app/views/sitesettings/manageUsers.php
+++ app/views/sitesettings/manageUsers.php
@@ -38,15 +38,11 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <?php $i = 1;
-                        foreach($this->controller->user->getAllUsersByRoleID($ur['id'], $active) as $user):?>
-                            <tr>
-                                <td><?php echo $i;?></td>
-                                <td><img src="/images/profile_pictures/<?php echo $user['profile_picture'];?>" alt="profile image" class="img-thumbnail" /> <?php echo $user['name'];?></td>
-                                <td><?php echo $this->controller->client->getClientName($user['client_id']);?></td>
-                                <td><?php echo $user['email'];?></td>
-                            </tr>
-                        <?php ++$i; endforeach;?>
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                        <td>5</td>
                     </tbody>
                 <?php endforeach;?>
             </table>
