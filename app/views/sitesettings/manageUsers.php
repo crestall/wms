43aed79096fb589fae17963f397caf2e9f7e8f43
marxolly <mark@solly.com.au>
2020-07-06 14:59:42 +1000

    Update manageUsers.php

--- app/views/sitesettings/manageUsers.php
+++ app/views/sitesettings/manageUsers.php
@@ -9,15 +9,37 @@
         <div class="col-lg-4"><p><a href="/site-settings/manage-users/active=0" class="btn btn-danger">View Only Inactive Users</a></p></div>
         <div class="col-lg-4"></div>
     </div>
-    <?php foreach($user_roles as $ur):
-        if(!$this->controller->user->canManageRole($ur['id']))
-            continue;
-        $name = ucwords($ur['name']);?>
-        <div class="row">
-            <div class="col-lg-12">
-                <h2><?php echo $name;?> Users</h2>
-                
-            </div>
+    <div id="waiting" class="row">
+        <div class="col-lg-12 text-center">
+            <h2>Drawing Table..</h2>
+            <p>May take a few moments</p>
+            <img class='loading' src='/images/preloader.gif' alt='loading...' />
         </div>
-    <?php endforeach;?>
+    </div>
+    <div class="row" id="table_holder" style="display:none">
+        <?php foreach($user_roles as $ur):
+            if(!$this->controller->user->canManageRole($ur['id']))
+                continue;
+            $name = ucwords($ur['name']);?>
+            <div class="row">
+                <div class="col-lg-12">
+                    <table id="user_list_table" class="table-striped table-hover" width="100%">
+                        <tr>
+                            <td>
+                                <h2><?php echo $name;?> Users</h2>
+                            </td>
+                        </tr>
+                    </table>
+
+                </div>
+            </div>
+        <?php endforeach;?>
+    </div>
+    <div class="row" id="table_holder" style="display:none">
+        <div class="col-lg-12">
+            <table id="client_list_table" class="table-striped table-hover" width="100%">
+
+            </table>
+        </div>
+    </div>
 </div>
