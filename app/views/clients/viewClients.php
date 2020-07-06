99cc4f46929e9dad49106037ee197a7e7d40bd16
marxolly <mark@solly.com.au>
2020-07-06 14:11:59 +1000

    Update viewClients.php

--- app/views/clients/viewClients.php
+++ app/views/clients/viewClients.php
@@ -34,7 +34,7 @@
                     	<tr>
                             <td><?php echo $i;?></td>
                             <td data-label="Client Name"><img src="/images/client_logos/tn_<?php echo $c['logo'];?>" alt="client logo" class="img-thumbnail" /> <?php echo $c['client_name'];?></td>
-                            <td data-label="Contact Name"><?php echo $c['contact_name'];?>></td>
+                            <td data-label="Contact Name"><?php echo $c['contact_name'];?></td>
                             <td data-label="Contact Email"><?php echo $c['billing_email'];?></td>
                             <td></td>
                         </tr>
