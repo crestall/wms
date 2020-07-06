ebb8d102254f38a779d198b01007eeec93e55b55
marxolly <mark@solly.com.au>
2020-07-06 15:56:50 +1000

    Update manageUsers.php

--- app/views/sitesettings/manageUsers.php
+++ app/views/sitesettings/manageUsers.php
@@ -45,7 +45,13 @@
                             <td>4</td>
                             <td>5</td>
                         </tr>
-
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>4</td>
+                            <td>5</td>
+                        </tr>
                     </tbody>
                 <?php endforeach;?>
             </table>
