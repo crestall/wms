2415935cea569dc8ff15d1da39a2247f83408001
marxolly <mark@solly.com.au>
2020-07-10 14:19:08 +1000

    Update Directfreight.php

--- app/core/Directfreight.php
+++ app/core/Directfreight.php
@@ -163,10 +163,10 @@
     public function createConsignment($details)
     {
         $response = $this->sendPostRequest('GetConsignmentPrice/', $details, "CONSIGNMENT");
-        echo $response; die();
+        //echo $response; die();
         //list($a_headers,$a_data) = $this->getResponse($response);
         //return json_decode($a_data[0], true);
-        //return $request;
+        return $response;
     }
 
     public function getDetails($od, $items)
