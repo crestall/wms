b8bbf6db91de681bf24a6595cd6e579716a4e789
marxolly <mark@solly.com.au>
2020-07-21 14:11:20 +1000

    layout

--- app/views/layout/page-includes/footer_common.php
+++ app/views/layout/page-includes/footer_common.php
@@ -1,8 +1,5 @@
-    <footer class="panel-footer clearfix" id="the_footer">
-        <div class="pull-left">
-            <?php echo date('Y');?> &copy; All rights reserved
-        </div>
-    </footer>
-
+<footer class="panel-footer clearfix" id="the_footer">
+    <div class="pull-left">
+        <?php echo date('Y');?> &copy; All rights reserved
     </div>
-    <!-- /#wrapper -->
+</footer>
