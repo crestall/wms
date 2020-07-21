8172eebb0b380821e1a29d8dd11804a0c0568a2c
marxolly <mark@solly.com.au>
2020-07-21 10:52:36 +1000

    fontawsome

--- app/views/layout/page-includes/header_common.php
+++ app/views/layout/page-includes/header_common.php
@@ -21,7 +21,7 @@
         <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/redmond/jquery-ui.css">
         <link rel="stylesheet" href="//cdn.datatables.net/1.10.18/css/dataTables.bootstrap.min.css" />
         <link rel="stylesheet" href="//cdn.datatables.net/fixedheader/3.1.5/css/fixedHeader.dataTables.min.css" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        <!--link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"-->
         <!--link rel="stylesheet" href="/styles/wms-style.css?t=<?php echo time();?>"-->
         <link rel="stylesheet" href="/styles/website.css?t=<?php echo time();?>">
 </head>
