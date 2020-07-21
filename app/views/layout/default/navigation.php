4074c4d99d06276267fd960e089972639f88a7fb
marxolly <mark@solly.com.au>
2020-07-21 14:57:01 +1000

    Update navigation.php

--- app/views/layout/default/navigation.php
+++ app/views/layout/default/navigation.php
@@ -42,8 +42,18 @@ endif;
                 <?php endforeach;?>
             <?php endif;?>
         </ul>
-
-
+        <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown link
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+        </ul>
     </div>
 </nav>
 <!-- End Navigation -->
