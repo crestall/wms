bf08480f546fd818b06a3edcee5fd8321da14af0
marxolly <mark@solly.com.au>
2020-07-22 09:56:02 +1000

    Update navigation.php

--- app/views/layout/default/navigation.php
+++ app/views/layout/default/navigation.php
@@ -42,7 +42,7 @@ endif;
             <?php endif;?>
         </ul>
     </div>
-    <ul class="user-info">
+    <ul class="navbar user-info">
         <li class="nav-item dropdown">
             <a class="dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Hello,<strong> <?php echo Session::getUsersName(); ?></strong> <img class="img-user" src="<?php echo $image;?>" />
