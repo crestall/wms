91fdd6689d93e46b94ab1c7ad578ff291d7c2426
Mark Solly <msolly@contactpoint.com.au>
2020-07-23 11:40:49 +1000

    Update controllerIndex.php

--- app/views/index/controllerIndex.php
+++ app/views/index/controllerIndex.php
@@ -8,7 +8,7 @@ $sections = $pages[strtolower($page_name)];
             <?php foreach($sections as $section_name => $details):
                 if(!$details['display']) continue; ?>
                 <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-                    <div class="card text-white bg-info">
+                    <div class="card text-white bg-info h-100">
                         <div class="card-body">
                             <h5 class="card-title"><?php echo $details['icon']." ".$section_name;?></h5>
                         </div>
