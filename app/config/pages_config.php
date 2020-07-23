500b11414df596117dde70ea79a1bb328bc3dc69
Mark Solly <msolly@contactpoint.com.au>
2020-07-23 15:05:01 +1000

    Update pages_config.php

--- app/config/pages_config.php
+++ app/config/pages_config.php
@@ -261,7 +261,7 @@ return array(
                 'icon'      => '<i class="fad fa-truck-pickup"></i>'
             )
 		),
-        'dataentry'    =>  array(
+        'data-entry'    =>  array(
             'default-icon'  => array(
                 'display'   => false,
                 'icon'      => '<i class="fad fa-indent"></i>'
