b2641d94d845c3ecb891e9b4a92c53abd73516a3
Mark Solly <msolly@contactpoint.com.au>
2020-07-23 11:05:53 +1000

    Update page_top.php

--- app/views/layout/page-includes/page_top.php
+++ app/views/layout/page-includes/page_top.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-12">
-            <h2 class="page-header"><?php echo $page_title;?></h2>
+            <h2 class="page-header"><?php echo $sections['default-icon']['icon'] $page_title;?></h2>
         </div>
     </div>
 </div>
