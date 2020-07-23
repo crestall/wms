9909fc5f4f7657d83d1d4d983466abfebf5ae913
Mark Solly <msolly@contactpoint.com.au>
2020-07-23 10:23:24 +1000

    Update page_top.php

--- app/views/layout/page-includes/page_top.php
+++ app/views/layout/page-includes/page_top.php
@@ -1,6 +1,7 @@
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header"><?php echo $page_title;?></h1>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <h2 class="page-header"><?php echo $page_title;?></h2>
+        </div>
+    </div>
+</div>
