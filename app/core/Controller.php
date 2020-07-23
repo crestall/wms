f594c14b885ed590fd46db513b2d328b34fcc6bb
Mark Solly <msolly@contactpoint.com.au>
2020-07-23 15:36:21 +1000

    Update Controller.php

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -350,9 +350,6 @@ class Controller {
     {
         $app = new App();
         $names = Utility::splitCamelCase($child);
-        array_pop($names);
-        var_dump($names);
-        //die();
         if(!$child || empty($child) || !$app::isControllerValid($child))
         {
             return (new ErrorsController())->error(404)->send();
