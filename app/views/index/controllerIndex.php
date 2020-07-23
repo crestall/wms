89830ad5422b827fb64601f4262d7a5d53ea7850
Mark Solly <msolly@contactpoint.com.au>
2020-07-23 11:24:32 +1000

    Update controllerIndex.php

--- app/views/index/controllerIndex.php
+++ app/views/index/controllerIndex.php
@@ -3,6 +3,17 @@ $sections = $pages[strtolower($page_name)];
 ?>
 <div id="page-wrapper">
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+    <div class="container">
+        <div class="row">
+            <?php foreach($sections as $section_name => $details):
+                if(!$details['display']) continue; ?>
+                <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+                    <p><?php echo $details['icon']." ".$section_name;?></p>    
+                </div>
+            <?php endforeach;?>
+        </div>
+
+    </div>
 </div>
 
 
