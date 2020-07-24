522cbbd624cd0b03cdf5e293d531effb0675406f
marxolly <mark@solly.com.au>
2020-07-24 11:55:12 +1000

    Update addorder.php

--- app/views/forms/addorder.php
+++ app/views/forms/addorder.php
@@ -132,8 +132,8 @@ $client_id = (!empty(Form::value('client_id')))? (int)Form::value('client_id') :
 
 
             <div class="custom-control custom-checkbox">
-                <input type="checkbox" class="custom-control-input" id="customCheck1">
                 <label class="custom-control-label" for="customCheck1">Check this custom checkbox</label>
+                <input type="checkbox" class="custom-control-input" id="customCheck1">
             </div>
 
 
