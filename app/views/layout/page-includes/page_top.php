d8becc95e99d881f6c4641428f35307300e6f2a7
marxolly <mark@solly.com.au>
2020-07-24 13:39:50 +1000

    Update page_top.php

--- app/views/layout/page-includes/page_top.php
+++ app/views/layout/page-includes/page_top.php
@@ -1,10 +1,8 @@
 <?php
 
 ?>
-<div class="container">
-    <div class="row">
-        <div class="col-lg-12">
-            <h2 class="page-header"><?php echo $page_title;?></h2>
-        </div>
+<div class="row">
+    <div class="col-lg-12">
+        <h2 class="page-header"><?php echo $page_title;?></h2>
     </div>
 </div>
