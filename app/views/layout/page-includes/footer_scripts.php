27117c07d3c03fd00e2727fdc34883a3d804e227
marxolly <mark@solly.com.au>
2020-07-27 09:44:44 +1000

    Update footer_scripts.php

--- app/views/layout/page-includes/footer_scripts.php
+++ app/views/layout/page-includes/footer_scripts.php
@@ -50,9 +50,7 @@
         if(Session::getIsLoggedIn()):
             $user_role = (Session::isAdminUser())? 'admin' : Session::getUserRole();
             $user_role = str_replace(" ","_", $user_role);
-            echo strtoupper($user_role."_PAGES");
             $pages = Config::getPages(strtoupper($user_role."_PAGES"));
-            echo "<pre>",print_r($pages),"</pre>";
             Config::setJsConfig('allPages', $pages);
         else:
             Config::setJsConfig('allPages', '');
