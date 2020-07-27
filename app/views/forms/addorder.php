e2d3b8267048f4e004c2a7d18b2934972042db99
marxolly <mark@solly.com.au>
2020-07-27 11:38:10 +1000

    Update addorder.php

--- app/views/forms/addorder.php
+++ app/views/forms/addorder.php
@@ -36,15 +36,15 @@ $client_id = (!empty(Form::value('client_id')))? (int)Form::value('client_id') :
                 </div>
             <?php endif;?>
 
-            <div id="item_selector" class="p-3 mb-2 bg-secondary text-white row">
+            <div id="item_selector" class="p-3 mb-2 rounded-top row mid-grey">
                 <div class="col-md-4">
                     <h4>Line Items</h4>
                 </div>
                 <div class="col-md-4">
-                    <a id="add-item" style="cursor:pointer" title="Add Another Item"><h4><i class="fad fa-plus-square fa-2x text-success"></i> Add another</a></h4>
+                    <a id="add-item" style="cursor:pointer" title="Add Another Item"><h4><i class="fad fa-plus-square text-success"></i> Add another</a></h4>
                 </div>
                 <div class="col-md-4">
-                    <a id="remove-all-items" style="cursor:pointer" title="Remove All Items"><h4><i class="fad fa-times-square fa-2x text-danger"></i> Remove all</a></h4>
+                    <a id="remove-all-items" style="cursor:pointer" title="Remove All Items"><h4><i class="fad fa-times-square text-danger"></i> Remove all</a></h4>
                 </div>
             </div>
 
