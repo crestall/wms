be0e8af11f818f5c72e2ab10f636843a272ff185
marxolly <mark@solly.com.au>
2020-07-27 12:03:30 +1000

    Update addorder.php

--- app/views/forms/addorder.php
+++ app/views/forms/addorder.php
@@ -49,7 +49,24 @@ $client_id = (!empty(Form::value('client_id')))? (int)Form::value('client_id') :
                     </div>
                 </div>
                 <div id="items_holder" class="row light-grey">
+                    <?php if(is_array(Form::value('items'))):?>
+                    <?php else:?>
+                        <div class="row item_holder">
+                            <div class="col-sm-1 add-image-holder">
+                                <a class="add" style="cursor:pointer" title="Add Another Item">
+                                    <i class="fas fa-plus-circle fa-2x text-success"></i>
+                                </a>
+                            </div>
+                            <div class="col-sm-6">
+                                <p><input type="text" class="form-control item-searcher" name="items[0][name]" placeholder="Item Name" /></p>
+                            </div>
+                            <div class="col-sm-2 qty-holder">
 
+                            </div>
+                            <div class="col-sm-3 qty-location"></div>
+                            <input type="hidden" name="items[0][id]" class="item_id"  />
+                        </div>
+                    <?php endif;?>
                 </div>
             </div>
 
