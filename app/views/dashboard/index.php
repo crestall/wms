9a34002b1afb54cc16d87513229fce29096e6880
marxolly <mark@solly.com.au>
2020-07-30 09:10:20 +1000

    Update index.php

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -18,23 +18,22 @@ $c = 1;
                 <div class="col-md-12 text-center">
                     <h2>Latest Unfulfilled Order Counts</h2>
                 </div>
-                    <div class="card-deck homepagedeck">
-                        <?php foreach($orders as $o):
-                            $s = ($o['order_count'] > 1)? "s" : ""; ?>
-                            <div class="card homepagecard">
-                                <div class="card-header">
-                                    <h4><?php echo $o['client_name'];?></h4>
-                                </div>
-                                <div class="card-body">
-                                	<i class="fas fa-truck fa-3x fa-flip-horizontal" style="vertical-align: middle;"></i>&nbsp;<span style="font-size:larger"><?php echo $o['order_count'];?> New Order<?php echo $s;?></span>
-                                </div>
-                                <div class="card-footer text-right">
-                                    <a class="btn btn-lg btn-outline-fsg" href="/orders/view-orders/client=<?php echo $o['client_id'];?>">Manage Orders</a>
-                                </div>
+                <div class="card-deck homepagedeck">
+                    <?php foreach($orders as $o):
+                        $s = ($o['order_count'] > 1)? "s" : ""; ?>
+                        <div class="card homepagecard">
+                            <div class="card-header">
+                                <h4><?php echo $o['client_name'];?></h4>
                             </div>
-                        <?php ++$c; endforeach;?>
-                    </div>
-
+                            <div class="card-body">
+                            	<i class="fad fa-truck fa-3x fa-flip-horizontal" style="vertical-align: middle;"></i>&nbsp;<span style="font-size:larger"><?php echo $o['order_count'];?> New Order<?php echo $s;?></span>
+                            </div>
+                            <div class="card-footer text-right">
+                                <a class="btn btn-lg btn-outline-fsg" href="/orders/view-orders/client=<?php echo $o['client_id'];?>">Manage Orders</a>
+                            </div>
+                        </div>
+                    <?php ++$c; endforeach;?>
+                </div>
             <?php else:?>
                 <div class="col-md-12">
                     <div class="errorbox">
@@ -53,6 +52,16 @@ $c = 1;
         <?php elseif($user_role == "client"):?>
             <div class="col-md-12 text-center">
                 <h2>Client User</h2>
+            </div>
+            <div class="card-deck homepagedeck">
+                <div class="card homepagecard">
+                    <div class="card-header">
+                        <h4>Create An Order</h4>
+                    </div>
+                    <div class="card-body">
+                    	<a class="btn btn-lg btn-outline-fsg" href="/orders/add-order"><i class="fad fa-truck fa-3x fa-flip-horizontal" style="vertical-align: middle;"></i>&nbsp;<span style="font-size:larger">Go!</span></a>
+                    </div>
+                </div>
             </div>
         <?php else:?>
             <div class="row">
