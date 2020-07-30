1752c7acfb85773e2e0ea144cbccb6be54e6fb18
marxolly <mark@solly.com.au>
2020-07-30 12:28:08 +1000

    Update controllerIndex.php

--- app/views/index/controllerIndex.php
+++ app/views/index/controllerIndex.php
@@ -6,17 +6,19 @@ $page_title = $sections['default-icon']['icon']." ".$page_title;
 <div id="page-wrapper">
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
     <div class="container">
-        <?php foreach($sections as $section_name => $details):
-            if(!$details['display']) continue;
-            $SectionName = ucwords(str_replace("-", " ", $section_name));?>
-            <div class="card homepagecard">
-                <div class="card-header">
-                    <h4><?php echo $SectionName;?></h4>
+        <div class="card-deck homepagedeck">
+            <?php foreach($sections as $section_name => $details):
+                if(!$details['display']) continue;
+                $SectionName = ucwords(str_replace("-", " ", $section_name));?>
+                <div class="card homepagecard">
+                    <div class="card-header">
+                        <h4><?php echo $SectionName;?></h4>
+                    </div>
+                    <div class="card-body text-center">
+                    	<a class="btn btn-lg btn-outline-fsg" href="<?php echo "$section_name";?>"><?php echo $details['icon'];?></a>
+                    </div>
                 </div>
-                <div class="card-body text-center">
-                	<a class="btn btn-lg btn-outline-fsg" href="<?php echo "$section_name";?>"><?php echo $details['icon'];?></a>
-                </div>
-            </div>
-        <?php endforeach;?>
+            <?php endforeach;?>
+        </div>
     </div>
 </div>
