52d60d6fa8a180ceb44b6c21042de178a7c33ff5
marxolly <mark@solly.com.au>
2020-07-31 15:31:16 +1000

    Update ordersearch.php

--- app/views/forms/ordersearch.php
+++ app/views/forms/ordersearch.php
@@ -12,7 +12,7 @@ $user_role = (Session::isAdminUser())? 'admin' : Session::getUserRole();
 <div class="col-lg-12">
     <form id="order_search" method="get" action="/orders/order-search-results">
         <div class="form-group row">
-            <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Search Term</label>
+            <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Search Term</label>
             <div class="col-md-4">
                 <input type="text" class="form-control required" name="term" id="term" value="<?php echo $term;?>" />
                 <!--span class="inst">Only required if searching all orders</span-->
@@ -23,7 +23,7 @@ $user_role = (Session::isAdminUser())? 'admin' : Session::getUserRole();
             <input type="hidden" name="client_id" id="client_id" value="<?php echo Session::get("client_id");?>" />
         <?php else:?>
             <div class="form-group row">
-                <label class="col-md-3 col-form-label">Client</label>
+                <label class="col-md-3">Client</label>
                 <div class="col-md-4">
                     <select id="client_id" name="client_id" class="form-control selectpicker"><option value="0">--Select One--</option><?php echo $this->controller->client->getSelectClients($client_id);?></select>
                     <?php echo Form::displayError('client_id');?>
@@ -31,13 +31,13 @@ $user_role = (Session::isAdminUser())? 'admin' : Session::getUserRole();
             </div>
         <?php endif;?>
         <div class="row form-group">
-            <label class="col-md-3 col-form-label">Filter By Courier</label>
+            <label class="col-md-3">Filter By Courier</label>
             <div class="col-md-4">
                 <select id="courier_id" name="courier_id" class="form-control selectpicker"><option value="0">--Select One--</option><?php echo $this->controller->courier->getSelectCouriers($courier_id, false, false);?></select>
             </div>
         </div>
         <div class="row form-group">
-            <label class="col-md-3 col-form-label">Filter By Date Dispatched</label>
+            <label class="col-md-3">Filter By Date Dispatched</label>
             <div class="col-md-1">
                 <label>From</label>
             </div>
@@ -61,9 +61,9 @@ $user_role = (Session::isAdminUser())? 'admin' : Session::getUserRole();
         <input type="hidden" id="date_to_value" name="date_to_value" value="<?php echo $date_to_value;?>" />
         <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
         <div class="form-group row">
-            <label class="col-md-3 col-form-label">&nbsp;</label>
+            <label class="col-md-3">&nbsp;</label>
             <div class="col-md-4">
-                <button type="submit" class="btn btn-primary">Search</button>
+                <button type="submit" class="btn btn-outline-fsg">Submit Search</button>
             </div>
         </div>
     </form>
