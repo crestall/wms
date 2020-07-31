2359b6bb695d270ae5bd143e436f2b652ce6a177
marxolly <mark@solly.com.au>
2020-07-31 13:59:54 +1000

    Update addBulkOrders.php

--- app/views/orders/addBulkOrders.php
+++ app/views/orders/addBulkOrders.php
@@ -15,6 +15,13 @@
                         <?php echo Form::displayError('client_id');?>
                     </div>
                 </div>
+                <div class="form-group row">
+                    <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> CSV File</label>
+                    <div class="col-md-4">
+                        <input type="file" name="csv_file" id="csv_file" class="required" />
+                        <?php echo Form::displayError('csv_file');?>
+                    </div>
+                </div>
 
 
 
@@ -28,20 +35,8 @@
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
     <div class="row">
         <form id="bulk-order-csv-upload" method="post" action="/form/procBulkOrderAdd" enctype="multipart/form-data">
-            <div class="form-group row">
-                <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Client</label>
-                <div class="col-md-4">
-                    <select id="client_id" name="client_id" class="form-control selectpicker"><option value="0">--Select One--</option><?php echo $this->controller->client->getSelectClients(Form::value('client_id'));?></select>
-                    <?php echo Form::displayError('client_id');?>
-                </div>
-            </div>
-            <div class="form-group row">
-                <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> CSV File</label>
-                <div class="col-md-4">
-                    <input type="file" name="csv_file" id="csv_file" class="required" />
-                    <?php echo Form::displayError('csv_file');?>
-                </div>
-            </div>
+
+            
             <div class="form-group row">
                 <div class="form-check">
                     <label class="form-check-label col-md-3" for="header_row">My CSV has a header row</label>
