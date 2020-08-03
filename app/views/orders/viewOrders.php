7d4129c8bb57533fa9af263be95b5a19d6eca2fc
marxolly <mark@solly.com.au>
2020-08-03 16:14:38 +1000

    Update viewOrders.php

--- app/views/orders/viewOrders.php
+++ app/views/orders/viewOrders.php
@@ -98,7 +98,7 @@
     </div>
     <?php if(count($orders)):?>
     <div class="table-responsive-xl">
-            <table class="table-striped table-hover" id="client_orders_table" width="100%">
+            <table class="table table-striped table-hover" id="client_orders_table" width="100%">
                 <thead>
         	    	<tr>
                         <th></th>
