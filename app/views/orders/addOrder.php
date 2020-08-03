c9bd1a2ef2fd4f3fd7a618d46302e0ed8cbcc483
marxolly <mark@solly.com.au>
2020-08-03 11:38:00 +1000

    Update addOrder.php

--- app/views/orders/addOrder.php
+++ app/views/orders/addOrder.php
@@ -1,5 +1,5 @@
 <div id="page-wrapper">
-    <div id="page_container" class="container-fluid">
+    <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <?php echo $form;?>
     </div>
