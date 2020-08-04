28eee5fea94b3c648e4ef7364f2ebf27576ecb76
marxolly <mark@solly.com.au>
2020-08-04 12:26:50 +1000

    Update footer.php

--- app/views/layout/orders/footer.php
+++ app/views/layout/orders/footer.php
@@ -906,21 +906,21 @@
                             	    $(this).val(c).change();
                             });
                         });
-
+                        /*
                         dataTable.init($('table#client_orders_table'), {
                             "columnDefs": [
                                 { "orderable": false, "targets": [0,3,5,7,8,9,10,11,12,13] }
                             ],
                             "order": []
                         } );
+                        */
 
-                        /*
                         $('table#client_orders_table').filterTable({
                             inputSelector: '#table_searcher',
                             minRows: 2,
                             ignoreColumns: [3,6,7,8,9,10,11,12,13]
                         });
-                        */
+
                         //$('table#client_orders_table').stickyTableHeaders();
 
                         $('a.select-courier').click(function(e){
