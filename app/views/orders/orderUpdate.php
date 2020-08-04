a034ee1b36ec343fa8a076ccd3f3ed5e2759ba8e
marxolly <mark@solly.com.au>
2020-08-04 16:04:55 +1000

    Update orderUpdate.php

--- app/views/orders/orderUpdate.php
+++ app/views/orders/orderUpdate.php
@@ -18,13 +18,13 @@ if(!$error)
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <?php if($error):?>
             <div class="row">
-                <div class="col-md-12">
+                <div class="col">
                     <div class="errorbox">
                         <div class="row">
-                            <div class="col-md-2" style="font-size:96px">
+                            <div class="col-2" style="font-size:96px">
                                 <i class="fas fa-exclamation-triangle"></i>
                             </div>
-                            <div class="col-md-6">
+                            <div class="col-10">
                                 <h2>No Order ID Supplied</h2>
                                 <p>No order was supplied to update</p>
                                 <p><a href="/orders/view-orders">Please click here to view all orders to choose from</a></p>
