04202ad71b020f1beeb2aa8a955ff3a2b200c013
marxolly <mark@solly.com.au>
2020-08-04 11:22:22 +1000

    Update 500.php

--- app/views/errors/500.php
+++ app/views/errors/500.php
@@ -8,7 +8,7 @@
                 <div class="error_page internal_error text-center">
                     <h1 class="page-header" style="font-size:80px;">500</h1>
                     <h2 class="page-header">Oops, we are sorry but our system encountered an internal error</h2>
-                    <p class="text-muted">If you wish to report this error, please include the URL (show in the address bar) and time of the error</p>
+                    <p class="text-muted">If you wish to report this error, please include the URL (shown in the address bar) and time of the error</p>
                     <p><a href="/dashboard" class="btn btn-info">Back to home</a></p>
                 </div>
             </div>
