ea2db1b33aa781d36fad4e281fb4357061649bf5
marxolly <mark@solly.com.au>
2020-08-04 16:16:12 +1000

    Update orderUpdate.php

--- app/views/orders/orderUpdate.php
+++ app/views/orders/orderUpdate.php
@@ -38,10 +38,10 @@ if(!$error)
                 <div class="col-md-12">
                     <div class="errorbox">
                         <div class="row">
-                            <div class="col-md-2" style="font-size:96px">
-                                <i class="fas fa-exclamation-triangle"></i>
+                            <div class="col-4 text-right">
+                                <i class="fad fa-exclamation-triangle fa-6x"></i>
                             </div>
-                            <div class="col-md-6">
+                            <div class="col-8">
                                 <h2>No Order Found</h2>
                                 <p>No order was found with that ID</p>
                                 <p><a href="/orders/view-orders">Please click here to view all orders to choose from</a></p>
