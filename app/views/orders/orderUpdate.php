49c5d5ea9ee351362bcc68c6f8bf8956dd98cfc3
marxolly <mark@solly.com.au>
2020-08-04 16:14:43 +1000

    Update orderUpdate.php

--- app/views/orders/orderUpdate.php
+++ app/views/orders/orderUpdate.php
@@ -21,10 +21,10 @@ if(!$error)
                 <div class="col">
                     <div class="errorbox">
                         <div class="row">
-                            <div class="col-3 text-right">
+                            <div class="col-4 text-right">
                                 <i class="fad fa-exclamation-triangle fa-6x"></i>
                             </div>
-                            <div class="col-9">
+                            <div class="col-8">
                                 <h2>No Order ID Supplied</h2>
                                 <p>No order was supplied to update</p>
                                 <p><a href="/orders/view-orders">Please click here to view all orders to choose from</a></p>
