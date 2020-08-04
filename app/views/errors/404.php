00d4514c2424301035079c8202a523c2a3e1df49
marxolly <mark@solly.com.au>
2020-08-04 10:38:15 +1000

    Update 404.php

--- app/views/errors/404.php
+++ app/views/errors/404.php
@@ -5,7 +5,7 @@
     <div id="page_container" class="container-fluid">
         <div class="row">
             <div class="col-lg-12">
-                <div class="broken_page text-center">
+                <div class="error_page not_found text-center">
                     <h1 class="page-header" style="font-size:80px;">404</h1>
                     <h2 class="page-header">That page was not found here</h2>
                     <p class="text-muted">Please use the menu bar at the left to find where it has gone</p>
