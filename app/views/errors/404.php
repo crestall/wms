f568fa1f44c6dffc67dcb663897c85b03709a033
marxolly <mark@solly.com.au>
2020-08-04 10:58:53 +1000

    Update 404.php

--- app/views/errors/404.php
+++ app/views/errors/404.php
@@ -8,7 +8,9 @@
                 <div class="error_page not_found text-center">
                     <h1 class="page-header" style="font-size:80px;">404</h1>
                     <h2 class="page-header">That page was not found here</h2>
-                    <p class="text-muted">Please use the menu bar at the left to find where it has gone</p>
+                    <p>We have probably moved it somewhere else and didn't update all the links</p>
+                    <p class="text-muted">Please use the menu above find where it might have gone</p>
+                    <p class="text-muted">If you wish to report this error, please include the URL (show in the address bar) and time of the error</p>
                     <p><a href="/dashboard" class="btn btn-info">Back to home</a></p>
                 </div>
             </div>
