0a6a8726504e3714045b028220efed0c084373f6
Mark Solly <msolly@contactpoint.com.au>
2020-08-05 16:39:00 +1000

    Update orderUpdate.php

--- app/views/orders/orderUpdate.php
+++ app/views/orders/orderUpdate.php
@@ -90,9 +90,10 @@ if(!$error)
                                 <label class="col-3">Deliver To:</label>
                                 <div class="col-9">Name</div>
                             </div>
-
-                            <label class="col-2">Address:</label>
-                            <div class="col-10">Line 1</div>
+                            <div class="row">
+                                <label class="col-3">Address:</label>
+                                <div class="col-9">Line 1</div>
+                            </div>
                             <label class="col-2">&nbsp;</label>
                             <div class="col-10">Line 2</div>
                             <label class="col-2">&nbsp;</label>
