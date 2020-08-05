21ba802a2fe96833a93f41a198e6ca3532bf4bd7
Mark Solly <msolly@contactpoint.com.au>
2020-08-05 11:31:25 +1000

    Update ErrorsController.php

--- app/controllers/ErrorsController.php
+++ app/controllers/ErrorsController.php
@@ -22,22 +22,22 @@ class ErrorsController extends Controller{
     }
 
     public function NotFound(){
-        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/errors/", Config::get('ERRORS_PATH') . "404.php");
+        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/errors/", Config::get('ERRORS_PATH') . "404.php",['pht' => ": Page Not Found"]);
     }
 
     public function Unauthenticated(){
-        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/errors/", Config::get('ERRORS_PATH') . "401.php");
+        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/errors/", Config::get('ERRORS_PATH') . "401.php",['pht' => ": Unauthorized"]);
     }
 
     public function Unauthorized(){
-        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/errors/", Config::get('ERRORS_PATH') . "403.php");
+        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/errors/", Config::get('ERRORS_PATH') . "403.php",['pht' => ": Forbidden"]);
     }
 
     public function BadRequest(){
-        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/errors/", Config::get('ERRORS_PATH') . "400.php");
+        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/errors/", Config::get('ERRORS_PATH') . "400.php",['pht' => ": Bad Request"]);
     }
 
     public function System(){
-        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/errors/", Config::get('ERRORS_PATH') . "500.php");
+        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/errors/", Config::get('ERRORS_PATH') . "500.php",['pht' => ": Internal Server Error"]);
     }
 }
