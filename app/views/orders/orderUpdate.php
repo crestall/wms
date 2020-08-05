d3ea964a190a416828c36a485401ab9d12cf4208
marxolly <mark@solly.com.au>
2020-08-06 09:49:40 +1000

    Update orderUpdate.php

--- app/views/orders/orderUpdate.php
+++ app/views/orders/orderUpdate.php
@@ -81,7 +81,7 @@ if(!$error)
             </div>
             <div class="row">
                 <div class="col-sm-12 col-md-6 mb-3">
-                    <div class="card h-100">
+                    <div class="card h-100 order-card">
                         <div class="card-header">
                             Delivery Details
                         </div>
@@ -125,7 +125,7 @@ if(!$error)
                     </div>
                 </div>
                 <div class="col-sm-12 col-md-6 mb-3">
-                    <div class="card h-100">
+                    <div class="card h-100 order-card">
                         <div class="card-header">
                             Order Details
                         </div>
@@ -183,7 +183,7 @@ if(!$error)
                     </div>
                 </div>
                 <div class="col-sm-12 col-md-6 mb-3">
-                    <div class="card h-100">
+                    <div class="card h-100 order-card">
                         <div class="card-header">
                             Order Items
                         </div>
@@ -198,7 +198,7 @@ if(!$error)
                     </div>
                 </div>
                 <div class="col-sm-12 col-md-6 mb-3">
-                    <div class="card h-100">
+                    <div class="card h-100 order-card">
                         <div class="card-header">
                             Add Packages/Pallets
                         </div>
@@ -211,7 +211,7 @@ if(!$error)
                     </div>
                 </div>
                 <div class="col-sm-12 col-md-6 mb-3">
-                    <div class="card">
+                    <div class="card h-100 order-card">
                         <div class="card-header">
                             Update Courier
                         </div>
