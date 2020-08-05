e356c6868cdd3c085bf8a2b66f9a7202a80d81d0
Mark Solly <msolly@contactpoint.com.au>
2020-08-05 11:57:29 +1000

    Update orderSearchResults.php

--- app/views/orders/orderSearchResults.php
+++ app/views/orders/orderSearchResults.php
@@ -1,5 +1,5 @@
 <div id="page-wrapper">
-    <div id="page_container" class="container-xl">
+    <div id="page_container" class="container-xxl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <?php echo $form;?>
         <div class="form-group row">
