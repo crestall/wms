2c799967585d0b9376c76c7116a5d63cee862255
Mark Solly <msolly@contactpoint.com.au>
2020-08-05 16:24:33 +1000

    Update orderUpdate.php

--- app/views/orders/orderUpdate.php
+++ app/views/orders/orderUpdate.php
@@ -79,9 +79,53 @@ if(!$error)
                     <?php endif; ?>
                 </div>
             </div>
-            <div class="card">
-                
+            <div class="row row-cols-1 row-cols-md-2">
+                <div class="col mb-3">
+                    <div class="card">
+                        <div class="card-header">
+                            Delivery Details
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-3">
+                    <div class="card">
+                        <div class="card-header">
+                            Order Details
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-3">
+                    <div class="card">
+                        <div class="card-header">
+                            Order Items
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-3">
+                    <div class="card">
+                        <div class="card-header">
+                            Add Packages/Pallets
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-3">
+                    <div class="card">
+                        <div class="card-header">
+                            Update Courier
+                        </div>
+                    </div>
+                </div>
             </div>
+
+
+
+
+            <div class="row">
+                <div class="col-sm-12 col-md-6 mb-3">
+
+                </div>
+            </div>
+
 
 
 
