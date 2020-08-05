4e2b6d51eacb6c232a90cd4a24a441530639f139
Mark Solly <msolly@contactpoint.com.au>
2020-08-05 12:30:32 +1000

    Update orderImporting.php

--- app/views/orders/orderImporting.php
+++ app/views/orders/orderImporting.php
@@ -2,139 +2,141 @@
 
 ?>
 <div id="page-wrapper">
-    <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-    <div class="row">
-        <div class="col-md-2">
-            <p><a class="btn btn-primary" href="#nuchev">Nuchev</a></p>
-        </div>
-        <div class="col-md-2">
-            <p><a class="btn btn-primary" href="#oneplate">One Plate</a></p>
-        </div>
-        <div class="col-md-2">
-            <p><a class="btn btn-primary" href="#freedom">Freedom</a></p>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-lg-12">
-            <?php if(isset($_SESSION['feedback'])) :?>
-                <?php if(isset($_SESSION['bberror']) && $_SESSION['bberror']):?>
-                    <div class='errorbox'><?php Session::destroy('bberror');?>
-                <?php else:?>
-                     <div class='feedbackbox'>&nbsp;
-                <?php endif;?>
-                <?php echo Session::getAndDestroy('feedback');?></div>
-            <?php endif; ?>
-        </div>
-    </div>
-    <div class="bs-callout bs-callout-primary bs-callout-more">
-        <a name="nuchev"></a>
+    <div id="page_container" class="container-xl">
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <div class="row">
-            <div class="col-md-12">
-                <h2>Nuchev Orders</h2>
+            <div class="col-md-2">
+                <p><a class="btn btn-primary" href="#nuchev">Nuchev</a></p>
+            </div>
+            <div class="col-md-2">
+                <p><a class="btn btn-primary" href="#oneplate">One Plate</a></p>
+            </div>
+            <div class="col-md-2">
+                <p><a class="btn btn-primary" href="#freedom">Freedom</a></p>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-3">&nbsp;</div>
-            <div class="col-md-4">
-                <h3>Import single Order</h3>
+            <div class="col-lg-12">
+                <?php if(isset($_SESSION['feedback'])) :?>
+                    <?php if(isset($_SESSION['bberror']) && $_SESSION['bberror']):?>
+                        <div class='errorbox'><?php Session::destroy('bberror');?>
+                    <?php else:?>
+                         <div class='feedbackbox'>&nbsp;
+                    <?php endif;?>
+                    <?php echo Session::getAndDestroy('feedback');?></div>
+                <?php endif; ?>
             </div>
         </div>
-        <div class="row">
-            <form id="nuchev_single_import" action="/orders/importNuchevOrder" method="post">
-                <div class="form-group row">
-                    <label class="col-md-3 col-form-label">WooCommerce Order ID</label>
-                    <div class="col-md-4">
-                        <input type="text" class="form-control required" name="nuchevwoocommerce_id" id="nuchevwoocommerce_id" value="<?php echo Form::value('nuchevwoocommerce_id');?>" />
-                        <?php echo Form::displayError('nuchevwoocommerce_id');?>
-                    </div>
+        <div class="bs-callout bs-callout-primary bs-callout-more">
+            <a name="nuchev"></a>
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Nuchev Orders</h2>
                 </div>
-                <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
-                <input type="hidden" name="client_id" value="<?php echo $nuchev_clientid; ?>" />
-                <div class="form-group row">
-                    <label class="col-md-3 col-form-label">&nbsp;</label>
-                    <div class="col-md-4">
-                        <button type="submit" class="btn btn-primary">Import It</button>
-                    </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3">&nbsp;</div>
+                <div class="col-md-4">
+                    <h3>Import single Order</h3>
                 </div>
-            </form>
-        </div>
-        <hr/>
-        <div class="row">
-            <div class="col-md-3">&nbsp;</div>
-            <div class="col-md-4">
-                <h3>Run Full Import</h3>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-3">&nbsp;</div>
-            <div class="col-md-4">
-                <p><button class="btn btn-primary" id="nuchev_full_import" data-function="importNuchevOrders">Run It</button></p>
-            </div>
-        </div>
-    </div>
-    <div class="bs-callout bs-callout-primary bs-callout-more">
-        <a name="oneplate"></a>
-        <div class="row">
-            <div class="col-md-12">
-                <h2>One Plate Orders</h2>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-3">&nbsp;</div>
-            <div class="col-md-4">
-                <h3>Import single Order</h3>
-            </div>
-        </div>
-        <div class="row">
-            <form id="oneplate_single_import" action="/orders/importOneplateOrder" method="post">
-                <div class="form-group row">
-                    <label class="col-md-3 col-form-label">WooCommerce Order ID</label>
-                    <div class="col-md-4">
-                        <input type="text" class="form-control required" name="oneplatewoocommerce_id" id="oneplatewoocommerce_id" value="<?php echo Form::value('oneplatewoocommerce_id');?>" />
-                        <?php echo Form::displayError('oneplatewoocommerce_id');?>
+            <div class="row">
+                <form id="nuchev_single_import" action="/orders/importNuchevOrder" method="post">
+                    <div class="form-group row">
+                        <label class="col-md-3 col-form-label">WooCommerce Order ID</label>
+                        <div class="col-md-4">
+                            <input type="text" class="form-control required" name="nuchevwoocommerce_id" id="nuchevwoocommerce_id" value="<?php echo Form::value('nuchevwoocommerce_id');?>" />
+                            <?php echo Form::displayError('nuchevwoocommerce_id');?>
+                        </div>
                     </div>
-                </div>
-                <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
-                <input type="hidden" name="client_id" value="<?php echo $oneplate_clientid; ?>" />
-                <div class="form-group row">
-                    <label class="col-md-3 col-form-label">&nbsp;</label>
-                    <div class="col-md-4">
-                        <button type="submit" class="btn btn-primary">Import It</button>
+                    <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+                    <input type="hidden" name="client_id" value="<?php echo $nuchev_clientid; ?>" />
+                    <div class="form-group row">
+                        <label class="col-md-3 col-form-label">&nbsp;</label>
+                        <div class="col-md-4">
+                            <button type="submit" class="btn btn-primary">Import It</button>
+                        </div>
                     </div>
+                </form>
+            </div>
+            <hr/>
+            <div class="row">
+                <div class="col-md-3">&nbsp;</div>
+                <div class="col-md-4">
+                    <h3>Run Full Import</h3>
                 </div>
-            </form>
-        </div>
-        <hr/>
-        <div class="row">
-            <div class="col-md-3">&nbsp;</div>
-            <div class="col-md-4">
-                <h3>Run Full Import</h3>
+            </div>
+            <div class="row">
+                <div class="col-md-3">&nbsp;</div>
+                <div class="col-md-4">
+                    <p><button class="btn btn-primary" id="nuchev_full_import" data-function="importNuchevOrders">Run It</button></p>
+                </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-3">&nbsp;</div>
-            <div class="col-md-4">
-                <p><button class="btn btn-primary" id="oneplate_full_import" data-function="importOnePlateOrders">Run It</button></p>
+        <div class="bs-callout bs-callout-primary bs-callout-more">
+            <a name="oneplate"></a>
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>One Plate Orders</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3">&nbsp;</div>
+                <div class="col-md-4">
+                    <h3>Import single Order</h3>
+                </div>
+            </div>
+            <div class="row">
+                <form id="oneplate_single_import" action="/orders/importOneplateOrder" method="post">
+                    <div class="form-group row">
+                        <label class="col-md-3 col-form-label">WooCommerce Order ID</label>
+                        <div class="col-md-4">
+                            <input type="text" class="form-control required" name="oneplatewoocommerce_id" id="oneplatewoocommerce_id" value="<?php echo Form::value('oneplatewoocommerce_id');?>" />
+                            <?php echo Form::displayError('oneplatewoocommerce_id');?>
+                        </div>
+                    </div>
+                    <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+                    <input type="hidden" name="client_id" value="<?php echo $oneplate_clientid; ?>" />
+                    <div class="form-group row">
+                        <label class="col-md-3 col-form-label">&nbsp;</label>
+                        <div class="col-md-4">
+                            <button type="submit" class="btn btn-primary">Import It</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <hr/>
+            <div class="row">
+                <div class="col-md-3">&nbsp;</div>
+                <div class="col-md-4">
+                    <h3>Run Full Import</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3">&nbsp;</div>
+                <div class="col-md-4">
+                    <p><button class="btn btn-primary" id="oneplate_full_import" data-function="importOnePlateOrders">Run It</button></p>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="bs-callout bs-callout-primary bs-callout-more">
-        <a name="freedom"></a>
-        <div class="row">
-            <div class="col-md-12">
-                <h2>Freedom Orders</h2>
+        <div class="bs-callout bs-callout-primary bs-callout-more">
+            <a name="freedom"></a>
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Freedom Orders</h2>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-3">&nbsp;</div>
-            <div class="col-md-4">
-                <h3>Run Full Import From MYOB</h3>
+            <div class="row">
+                <div class="col-md-3">&nbsp;</div>
+                <div class="col-md-4">
+                    <h3>Run Full Import From MYOB</h3>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-3">&nbsp;</div>
-            <div class="col-md-4">
-                <p><button class="btn btn-primary" id="freedom_full_import" data-function="importFreedomOrders">Run It</button></p>
+            <div class="row">
+                <div class="col-md-3">&nbsp;</div>
+                <div class="col-md-4">
+                    <p><button class="btn btn-primary" id="freedom_full_import" data-function="importFreedomOrders">Run It</button></p>
+                </div>
             </div>
         </div>
     </div>
