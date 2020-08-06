7cb636a50c891aae212f746c5663080010dc4b19
marxolly <mark@solly.com.au>
2020-08-06 14:30:05 +1000

    Update footer.php

--- app/views/layout/orders/footer.php
+++ app/views/layout/orders/footer.php
@@ -372,10 +372,10 @@
                                     if(textStatus == 'error') {
                                         $(this).html('<div class=\'errorbox\'><h2>There has been an error</h2></div>');
                                     }
-                                    $('form#orders-add-package').submit(function(e){
+                                    $('form#order-add-package').submit(function(e){
                                         if($(this).valid())
                                         {
-                                            e.preventDefault();
+                                            
                                         }
                                         else
                                         {
