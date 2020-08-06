3579da80172c29a4657e603f875546921f99cb89
marxolly <mark@solly.com.au>
2020-08-06 14:01:28 +1000

    Update add_package1.php

--- app/views/dashboard/add_package1.php
+++ app/views/dashboard/add_package1.php
@@ -2,8 +2,8 @@
     <div class="row" id="feedback_holder" style="display:none"></div>
         <form id="order-add-package" method="post" action="/form/procAddPackage">
             <div class="form-group row">
-                <label class="col"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Width</label>
-                <div class="col">
+                <label class="col-md-3 col-sm-6"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Width</label>
+                <div class="col-md-3 col-sm-6">
                     <div class="input-group">
                         <input type="text" class="form-control required number" name="width" id="width" />
                         <div class="input-group-append">
@@ -11,8 +11,8 @@
                         </div>
                     </div>
                 </div>
-                <label class="col"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Depth</label>
-                <div class="col">
+                <label class="col-md-3 col-sm-6"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Depth</label>
+                <div class="col-md-3 col-sm-6">
                     <div class="input-group">
                         <input type="text" class="form-control required number" name="depth" id="depth" />
                         <div class="input-group-append">
@@ -22,8 +22,8 @@
                 </div>
             </div>
             <div class="form-group row">
-                <label class="col"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Height</label>
-                <div class="col">
+                <label class="col-md-3 col-sm-6"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Height</label>
+                <div class="col-md-3 col-sm-6">
                     <div class="input-group">
                         <input type="text" class="form-control required number" name="height" id="height" />
                         <div class="input-group-append">
@@ -31,8 +31,8 @@
                         </div>
                     </div>
                 </div>
-                <label class="col"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Weight</label>
-                <div class="col">
+                <label class="col-md-3 col-sm-6"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Weight</label>
+                <div class="col-md-3 col-sm-6">
                     <div class="input-group">
                         <input type="text" class="form-control required number" name="weight" id="weight" />
                         <div class="input-group-append">
@@ -42,8 +42,8 @@
                 </div>
             </div>
             <div class="form-group row">
-                <label class="col-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> <span id='label_text'>Count</span></label>
-                <div class="col-3">
+                <label class="col-md-3 col-sm-6"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> <span id='label_text'>Count</span></label>
+                <div class="col-md-3 col-sm-4">
                     <input type="text" class="form-control required number" name="count" id="count" value="1" />
                 </div>
                 <div class="custom-control custom-checkbox mr-sm-2">
@@ -54,8 +54,8 @@
             <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
             <input type="hidden" name="order_id" value="<?php echo $order_id;?>" />
             <div class="form-group row">
-                <label class="col-md-6 col-form-label">&nbsp;</label>
-                <div class="col-md-4">
+                <label class="col-3 col-sm-6">&nbsp;</label>
+                <div class="col-4">
                     <button type="submit" class="btn btn-outline-secondary">Add Package</button>
                 </div>
             </div>
