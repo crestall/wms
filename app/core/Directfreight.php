f58f623037b4a844a9a5555e27c2ce1c319e4e58
marxolly <mark@solly.com.au>
2020-08-07 14:40:02 +1000

    Update Directfreight.php

--- app/core/Directfreight.php
+++ app/core/Directfreight.php
@@ -54,7 +54,7 @@
         $url = directfreight::API_SCHEME . directfreight::API_BASE_URL . $action;
         //die($url);
         $data_string = json_encode($data);
-        //die($data_string);
+        die($data_string);
         $key = $this->{$area."_KEY"};
         $headers = array(
             'Content-Type: application/json',
@@ -162,7 +162,7 @@
 
     public function createConsignment($details)
     {
-        echo "<pre>",print_r($details),"</pre>";die();
+        //echo "<pre>",print_r($details),"</pre>";die();
         $response = $this->sendPostRequest('CreateConsignment/', $details, "CONSIGNMENT");
         //echo $response; die();
         //list($a_headers,$a_data) = $this->getResponse($response);
