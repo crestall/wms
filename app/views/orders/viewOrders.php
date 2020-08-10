25ce2645227e1046692107108d39c47ebfdc56d9
marxolly <mark@solly.com.au>
2020-08-10 13:03:02 +1000

    Update viewOrders.php

--- app/views/orders/viewOrders.php
+++ app/views/orders/viewOrders.php
@@ -18,28 +18,28 @@
     <?php if($user_role == "admin" || $user_role == "super admin"):?>
         <div class="row view-orders-buttons" >
             <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
-                <p><a class="btn btn-sm btn-block btn-outline-info export-csv" style="white-space: normal; min-height: 55px"><i class="fas fa-file-csv"></i> Export Selected To CSV</a></p>
+                <p><a class="btn btn-sm btn-block btn-outline-info export-csv"><i class="fas fa-file-csv"></i> Export Selected To CSV</a></p>
             </div>
             <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
-                <p><a class="btn btn-sm btn-block btn-outline-secondary slip-print" style="white-space: normal;"><i class="fas fa-file-alt"></i> Print Picking Slips For Selected</a></p>
+                <p><a class="btn btn-sm btn-block btn-outline-secondary slip-print"<i class="fas fa-file-alt"></i> Print Picking Slips For Selected</a></p>
             </div>
             <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
-                <p><a class="btn btn-sm btn-block btn-outline-primary select-courier" style="white-space: normal;"><i class="fas fa-truck"></i> Update Courier For Selected</a></p>
+                <p><a class="btn btn-sm btn-block btn-outline-primary select-courier"<i class="fas fa-truck"></i> Update Courier For Selected</a></p>
             </div>
             <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
-                <p><a class="btn btn-sm btn-block btn-outline-success eparcel-fulfill" style="white-space: normal;"><i class="fas fa-clipboard-check"></i> Fulfill Selected eParcel Orders</a></p>
+                <p><a class="btn btn-sm btn-block btn-outline-success eparcel-fulfill"<i class="fas fa-clipboard-check"></i> Fulfill Selected eParcel Orders</a></p>
             </div>
             <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
-                <p><a class="btn btn-sm btn-block btn-outline-warning print-invoices" style="white-space: normal;"><i class="fas fa-file-invoice"></i> Print Invoices For Selected</a> </p>
+                <p><a class="btn btn-sm btn-block btn-outline-warning print-invoices"<i class="fas fa-file-invoice"></i> Print Invoices For Selected</a> </p>
             </div>
             <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
-                <p><a class="btn btn-sm btn-block btn-outline-dark add-package" style="white-space: normal;"><i class="fas fa-box-open"></i> Add Package For Selected</a> </p>
+                <p><a class="btn btn-sm btn-block btn-outline-dark add-package"<i class="fas fa-box-open"></i> Add Package For Selected</a> </p>
             </div>
             <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
-                <p><a class="btn btn-sm btn-block btn-outline-secondary eparcel-label-print" style="white-space: normal;"><i class="fas fa-tags"></i> Print eParcel Labels For Selected</a></p>
+                <p><a class="btn btn-sm btn-block btn-outline-secondary eparcel-label-print"<i class="fas fa-tags"></i> Print eParcel Labels For Selected</a></p>
             </div>
             <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
-                <p><a class="btn btn-sm btn-block btn-outline-danger cancel-order" style="white-space: normal;"><i class="fas fa-ban"></i> Cancel Selected Orders</a></p>
+                <p><a class="btn btn-sm btn-block btn-outline-danger cancel-order"<i class="fas fa-ban"></i> Cancel Selected Orders</a></p>
             </div>
         </div>
     <?php elseif($user_role == "warehouse"):?>
