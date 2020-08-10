9983a6269801d4f97e88dada0bab45b31e791d9c
marxolly <mark@solly.com.au>
2020-08-10 15:55:17 +1000

    Update directFreightLabels.php

--- app/views/labels/directFreightLabels.php
+++ app/views/labels/directFreightLabels.php
@@ -16,52 +16,36 @@
                 <div class="col-lg-12">
                     <div class="errorbox">
                         <h2><i class="far fa-times-circle"></i>There Was An Error Generating Labels</h2>
-                        <?php echo $error_string;?>
+                        <?php echo $error_message;?>
                     </div>
                 </div>
             </div>
         <?php else:?>
-            <?php foreach($request_ids as $request_id):?>
-                <div class="row">
-                    <div class="col-lg-12">
-                        <?php $response = $this->controller->{$eParcelClass}->GetLabel($request_id);
-                        while ($response['labels'][0]['status'] == "PENDING")
-                        {
-                            $response = $this->controller->{$eParcelClass}->GetLabel($request_id);
-                            //echo "<hr/>";
-                            //echo "<p>".$response['labels'][0]['status']."</p>";
-                            //echo "<p>".$request_id."</p>";
-                        }
-                        ?>
+            <?php if count($bad_orders):?>
+            <div class="row">
+                <div class="errorbox">
+                    <div class="col-12">
+                        <h3><i class="far fa-times-circle"></i>There Was An Error With Some Orders</h3>
                     </div>
+                    <?php foreach($bad_orders as $bo):?>
+                        <div class="col-lg-3 col-md-4 col-sm-6"><?php echo $bo['order_number'];?></div>
+                        <div class="col-lg-9 col-md-8 col-sm-6"><?php echo $bo['response_message'];?></div>
+                    <?php endforeach;?>
                 </div>
-                <div class="row">
-                    <div class="col-lg-12">
-                        <?php if(isset($response['errors']) || $response['labels'][0]['status'] == "ERROR"):?>
-                            <div class="errorbox">
-                                <h2><i class="far fa-times-circle"></i>There Was An Error Generating Labels</h2>
-                                <?php echo "<pre>",print_r($response),"</pre>";?>
-                            </div>
-                        <?php else:?>
-                            <div class='feedbackbox'>
-                                <h2><i class="far fa-check-circle"></i>Labels Have Been Generated</h2>
-                                <?php //echo "<pre>",print_r(json_encode($response)),"</pre>";?>
-                                <p>Click the button to download them</p>
-                                <p><a href="<?php echo $response['labels'][0]['url'];?>" class="btn btn-primary" target="_blank">Download</a></p>
-                            </div>
-                        <?php endif;?>
+            </div>
+            <?php endif;?>
+            <div class="row">
+                <div class="feedbackbox">
+                    <div class="col-12">
+                        <h3><i class="far fa-check-circle"></i>The following orders labels have been generated</h3>
                     </div>
-                </div>
-                <?php /*echo "<pre>",print_r($response['labels']),"</pre>";?>
-                <?php //echo "<pre>",print_r($order_ids),"</pre>";?>
-                <?php foreach($order_ids as $oi):?>
-                    <?php echo "<p>".$this->controller->order->getOrderDetail($oi)['ship_to']."</p>";?>
-                <?php endforeach;?>
-                <hr/>
-                <?php foreach($response['labels'][0]['shipment_ids'] as $si):?>
-                    <?php echo "<p>".$this->controller->order->getOrderByShipmentId($si)['ship_to']."</p>";?>
-                <?php endforeach;*/?>
-            <?php endforeach;?>
+                    <div class="col-md-6">
+                        <?php echo implode(", ", $godd_orders);?>
+                    </div>
+                    <div class="col-md-6">
+                        <a class="btn tn-outline-secondary" href="<?php echo $url;?>">Click to Download Them</a>
+                    </div>
+            </div>
         <?php endif;?>
     </div>
 </div>
