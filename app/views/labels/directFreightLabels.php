90a7269d5165fc5744b56437a30a953e5e424aed
marxolly <mark@solly.com.au>
2020-08-10 16:06:05 +1000

    Update directFreightLabels.php

--- app/views/labels/directFreightLabels.php
+++ app/views/labels/directFreightLabels.php
@@ -12,17 +12,14 @@
             <?php endif;?>
         </div>
         <?php if($error):?>
-            <div class="row">
                 <div class="col-lg-12">
                     <div class="errorbox">
                         <h2><i class="far fa-times-circle"></i>There Was An Error Generating Labels</h2>
                         <?php echo $error_message;?>
                     </div>
                 </div>
-            </div>
         <?php else:?>
             <?php if(count($bad_orders)):?>
-            <div class="row">
                 <div class="errorbox">
                     <div class="col-12">
                         <h3><i class="far fa-times-circle"></i>There Was An Error With Some Orders</h3>
@@ -32,9 +29,7 @@
                         <div class="col-lg-9 col-md-8 col-sm-6"><?php echo $bo['response_message'];?></div>
                     <?php endforeach;?>
                 </div>
-            </div>
             <?php endif;?>
-            <div class="row">
                 <div class="feedbackbox">
                     <div class="col-12">
                         <h3><i class="far fa-check-circle"></i>The following orders labels have been generated</h3>
@@ -43,9 +38,8 @@
                         <?php echo implode(", ", $good_orders);?>
                     </div>
                     <div class="col-md-6">
-                        <a class="btn tn-outline-secondary" href="<?php echo $url;?>">Click to Download Them</a>
+                        <a class="btn btn-outline-secondary" href="<?php echo $url;?>">Click to Download Them</a>
                     </div>
-            </div>
         <?php endif;?>
     </div>
 </div>
