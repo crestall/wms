b4e0ac4de8093b15c28f2fbca424f3f10dd77a70
marxolly <mark@solly.com.au>
2020-08-11 14:34:08 +1000

    Update orderDetail.php

--- app/views/orders/orderDetail.php
+++ app/views/orders/orderDetail.php
@@ -26,63 +26,68 @@
             </div>
         <?php else:?>
             <div id="print_this" class="container-fluid">
-                <div class="col-sm-12">
-                    <h2>Details For Order Number: <?php echo $order['order_number'];?></h2>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h2>Details For Order Number: <?php echo $order['order_number'];?></h2>
+                    </div>
                 </div>
-                <div class="col-sm-12 col-md-6 mb-3">
-                    <div class="card border-secondary h-100 order-card">
-                        <div class="card-header bg-secondary text-white">
-                            Delivery Details
-                        </div>
-                        <div class="card-body">
-                                    <label class="col-5">Client Order Number</label>
-                                    <div class="col-7"><?php echo $order['client_order_id'];?></div>
-                                    <label class="col-5">Deliver To</label>
-                                    <div class="col-7"><?php echo $order['ship_to'];?></div>
-                                    <label class="col-5">Company</label>
-                                    <div class="col-7"><?php echo $order['company_name'];?></div>
-                                    <label class="col-5">Address</label>
-                                    <div class="col-7"><?php echo $order['address'];?></div>
-                                <?php if(!empty($order['address_2'])):?>
+                <div class="row">
+                    <div class="col-sm-12 col-md-6 mb-3">
+                        <div class="card border-secondary h-100 order-card">
+                            <div class="card-header bg-secondary text-white">
+                                Delivery Details
+                            </div>
+                            <div class="card-body">
+                                        <label class="col-5">Client Order Number</label>
+                                        <div class="col-7"><?php echo $order['client_order_id'];?></div>
+                                        <label class="col-5">Deliver To</label>
+                                        <div class="col-7"><?php echo $order['ship_to'];?></div>
+                                        <label class="col-5">Company</label>
+                                        <div class="col-7"><?php echo $order['company_name'];?></div>
+                                        <label class="col-5">Address</label>
+                                        <div class="col-7"><?php echo $order['address'];?></div>
+                                    <?php if(!empty($order['address_2'])):?>
 
+                                            <label class="col-5">&nbsp;</label>
+                                            <div class="col-7"><?php echo $order['address_2'];?></div>
+
+                                    <?php endif;?>
                                         <label class="col-5">&nbsp;</label>
-                                        <div class="col-7"><?php echo $order['address_2'];?></div>
+                                        <div class="col-7"><?php echo $order['suburb'];?></div>
+                                        <label class="col-5">&nbsp;</label>
+                                        <div class="col-7"><?php echo $order['state'];?></div>
+                                        <label class="col-5">&nbsp;</label>
+                                        <div class="col-7"><?php echo $order['postcode'];?></div>
+                                        <label class="col-5">&nbsp;</label>
+                                        <div class="col-7"><?php echo $order['country'];?></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 mb-3">
+                        <div class="card border-secondary h-100 order-card">
+                            <div class="card-header bg-secondary text-white">
+                                Order Status
+                            </div>
+                            <div class="card-body">
+                                <label class="col-5">Order Status</label>
+                                <div class="col-7"><?php echo $order_status;?></div>
+                                <label class="col-5">Date Ordered</label>
+                                <div class="col-7"><?php echo date('d-m-Y', $order['date_ordered']);?></div>
+                                <label class="col-5">Date Fulfilled</label>
+                                <div class="col-7"><?php if($order['date_fulfilled'] > 0)echo date('d-m-Y', $order['date_fulfilled']);?></div>
+                                <label class="col-5">Courier Service</label>
+                                <div class="col-7"><?php echo $courier;?></div>
+                                <label class="col-5">Use Express</label>
+                                <div class="col-7"><?php if($order['eparcel_express'] > 0) echo "Yes"; else echo "No";?></div>
+                                <label class="col-5">Signature Required</label>
+                                <div class="col-7"><?php if($order['signature_req'] > 0) echo "Yes"; else echo "No";?></div>
+                                <label class="col-5">Consignment ID</label>
+                                <div class="col-7"><?php if($order['date_fulfilled'] > 0)echo $order['consignment_id'];?></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-                                <?php endif;?>
-                                    <label class="col-5">&nbsp;</label>
-                                    <div class="col-7"><?php echo $order['suburb'];?></div>
-                                    <label class="col-5">&nbsp;</label>
-                                    <div class="col-7"><?php echo $order['state'];?></div>
-                                    <label class="col-5">&nbsp;</label>
-                                    <div class="col-7"><?php echo $order['postcode'];?></div>
-                                    <label class="col-5">&nbsp;</label>
-                                    <div class="col-7"><?php echo $order['country'];?></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-12 col-md-6 mb-3">
-                    <div class="card border-secondary h-100 order-card">
-                        <div class="card-header bg-secondary text-white">
-                            Order Status
-                        </div>
-                        <div class="card-body">
-                            <label class="col-5">Order Status</label>
-                            <div class="col-7"><?php echo $order_status;?></div>
-                            <label class="col-5">Date Ordered</label>
-                            <div class="col-7"><?php echo date('d-m-Y', $order['date_ordered']);?></div>
-                            <label class="col-5">Date Fulfilled</label>
-                            <div class="col-7"><?php if($order['date_fulfilled'] > 0)echo date('d-m-Y', $order['date_fulfilled']);?></div>
-                            <label class="col-5">Courier Service</label>
-                            <div class="col-7"><?php echo $courier;?></div>
-                            <label class="col-5">Use Express</label>
-                            <div class="col-7"><?php if($order['eparcel_express'] > 0) echo "Yes"; else echo "No";?></div>
-                            <label class="col-5">Signature Required</label>
-                            <div class="col-7"><?php if($order['signature_req'] > 0) echo "Yes"; else echo "No";?></div>
-                            <label class="col-5">Consignment ID</label>
-                            <div class="col-7"><?php if($order['date_fulfilled'] > 0)echo $order['consignment_id'];?></div>
-                        </div>
-                    </div>
-                </div>
                 <div class="row">
                     <div class="col-xs-12">
                         <table class="table">
