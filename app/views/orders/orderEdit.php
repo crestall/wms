a094a0a3fe037b0fcb450680b9c37f1bd0462cf4
marxolly <mark@solly.com.au>
2020-08-12 12:13:14 +1000

    Update orderEdit.php

--- app/views/orders/orderEdit.php
+++ app/views/orders/orderEdit.php
@@ -32,111 +32,114 @@ $comments = empty(Form::value('tpl_comments'))? $comments : Form::value('tpl_com
             </div>
             <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
             <div class="row">
-                <form id="order-edit" enctype="multipart/form-data" action="/form/procOrderEdit" method="post">
-                    <div class="form-group row">
-                        <label class="col-md-3 col-form-label"><?php if(empty($order['uploaded_file'])) echo "Upload"; else echo "Replace";?> PDF Attachment</label>
-                        <div class="col-md-4">
-                            <input type="file" name="invoice[]" id="invoice" multiple="multiple" onChange="fileUpload.makeFileList();" />
-                            <span class="inst">(if required) - use ctrl click to select multiple files</span>
-                            <?php if(!empty($order['uploaded_file'])):?>
-                                <br/>
-                                <span class="inst"><a href='/client_uploads/<?php echo $order['client_id']."/".$order['uploaded_file'];?>' target='_blank'>View Current File</a> </span>
-                                <br/>
-                                <div class="checkbox checkbox-default">
-                                    <input class="form-check-input styled" type="checkbox" id="delete_file" name="delete_file" />
-                                    <label for="delete_file"><span class="inst">Delete Current File</span></label>
+                <div class="col-12">
+                    <form id="order-edit" enctype="multipart/form-data" action="/form/procOrderEdit" method="post">
+                        <div class="form-group row">
+                            <label class="col-md-3 col-form-label"><?php if(empty($order['uploaded_file'])) echo "Upload"; else echo "Replace";?> PDF Attachment</label>
+                            <div class="col-md-4">
+                                <input type="file" name="invoice[]" id="invoice" multiple="multiple" onChange="fileUpload.makeFileList();" />
+                                <span class="inst">(if required) - use ctrl click to select multiple files</span>
+                                <?php if(!empty($order['uploaded_file'])):?>
+                                    <br/>
+                                    <span class="inst"><a href='/client_uploads/<?php echo $order['client_id']."/".$order['uploaded_file'];?>' target='_blank'>View Current File</a> </span>
+                                    <br/>
+                                    <div class="checkbox checkbox-default">
+                                        <input class="form-check-input styled" type="checkbox" id="delete_file" name="delete_file" />
+                                        <label for="delete_file"><span class="inst">Delete Current File</span></label>
+                                    </div>
+                                <?php endif;?>
+                                <ul id="fileList"></ul>
+                                <?php echo Form::displayError('invoice');?>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-3 col-form-label">Client Order Number</label>
+                            <div class="col-md-4">
+                                <input type="text" class="form-control" name="client_order_id" id="client_order_id" value="<?php echo $client_order_id;?>" />
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Deliver To</label>
+                            <div class="col-md-4">
+                                <input type="text" class="form-control required" name="deliver_to" id="deliver_to" value="<?php echo $deliver_to;?>" />
+                                <?php echo Form::displayError('deliver_to');?>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-3 col-form-label">Company Name</label>
+                            <div class="col-md-4">
+                                <input type="text" class="form-control" name="company_name" id="company_name" value="<?php echo $company_name;?>" />
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="form-check">
+                                <label class="form-check-label col-md-3" for="signature_req">Signature Required</label>
+                                <div class="col-md-4 checkbox checkbox-default">
+                                    <input class="form-check-input styled" type="checkbox" id="signature_req" name="signature_req" <?php if($signature_req) echo "checked";?> />
+                                    <label for="signature_req"></label>
+                                    <span class="inst">Leaving unchecked will give an 'Authority to Leave'</span>
                                 </div>
-                            <?php endif;?>
-                            <ul id="fileList"></ul>
-                            <?php echo Form::displayError('invoice');?>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 col-form-label">Client Order Number</label>
-                        <div class="col-md-4">
-                            <input type="text" class="form-control" name="client_order_id" id="client_order_id" value="<?php echo $client_order_id;?>" />
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Deliver To</label>
-                        <div class="col-md-4">
-                            <input type="text" class="form-control required" name="deliver_to" id="deliver_to" value="<?php echo $deliver_to;?>" />
-                            <?php echo Form::displayError('deliver_to');?>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 col-form-label">Company Name</label>
-                        <div class="col-md-4">
-                            <input type="text" class="form-control" name="company_name" id="company_name" value="<?php echo $company_name;?>" />
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <div class="form-check">
-                            <label class="form-check-label col-md-3" for="signature_req">Signature Required</label>
-                            <div class="col-md-4 checkbox checkbox-default">
-                                <input class="form-check-input styled" type="checkbox" id="signature_req" name="signature_req" <?php if($signature_req) echo "checked";?> />
-                                <label for="signature_req"></label>
-                                <span class="inst">Leaving unchecked will give an 'Authority to Leave'</span>
                             </div>
                         </div>
-                    </div>
-                    <div class="form-group row">
-                        <div class="form-check">
-                            <label class="form-check-label col-md-3" for="express_post">Use Express Post</label>
-                            <div class="col-md-4 checkbox checkbox-default">
-                                <input class="form-check-input styled" type="checkbox" id="express_post" name="express_post" <?php if($express_post) echo "checked";?> />
-                                <label for="express_post"></label>
+                        <div class="form-group row">
+                            <div class="form-check">
+                                <label class="form-check-label col-md-3" for="express_post">Use Express Post</label>
+                                <div class="col-md-4 checkbox checkbox-default">
+                                    <input class="form-check-input styled" type="checkbox" id="express_post" name="express_post" <?php if($express_post) echo "checked";?> />
+                                    <label for="express_post"></label>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="form-group row">
-                        <div class="form-check">
-                            <label class="form-check-label col-md-3" for="store_order">Store Order</label>
-                            <div class="col-md-4 checkbox checkbox-default">
-                                <input class="form-check-input styled" type="checkbox" id="store_order" name="store_order" <?php if($store_order) echo "checked";?> />
-                                <label for="store_order"></label>
+                        <div class="form-group row">
+                            <div class="form-check">
+                                <label class="form-check-label col-md-3" for="store_order">Store Order</label>
+                                <div class="col-md-4 checkbox checkbox-default">
+                                    <input class="form-check-input styled" type="checkbox" id="store_order" name="store_order" <?php if($store_order) echo "checked";?> />
+                                    <label for="store_order"></label>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 col-form-label">Tracking Email</label>
-                        <div class="col-md-4">
-                            <input type="text" class="form-control email" name="tracking_email" id="tracking_email" value="<?php echo $tracking_email;?>" />
-                            <span class="inst">Required if you wish to receive tracking notifications</span>
-                            <?php echo Form::displayError('tracking_email');?>
+                        <div class="form-group row">
+                            <label class="col-md-3 col-form-label">Tracking Email</label>
+                            <div class="col-md-4">
+                                <input type="text" class="form-control email" name="tracking_email" id="tracking_email" value="<?php echo $tracking_email;?>" />
+                                <span class="inst">Required if you wish to receive tracking notifications</span>
+                                <?php echo Form::displayError('tracking_email');?>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 col-form-label">Phone</label>
-                        <div class="col-md-4">
-                            <input type="text" class="form-control" name="contact_phone" id="contact_phone" value="<?php echo $contact_phone;?>" />
-                            <?php echo Form::displayError('contact_phone');?>
+                        <div class="form-group row">
+                            <label class="col-md-3 col-form-label">Phone</label>
+                            <div class="col-md-4">
+                                <input type="text" class="form-control" name="contact_phone" id="contact_phone" value="<?php echo $contact_phone;?>" />
+                                <?php echo Form::displayError('contact_phone');?>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 col-form-label">Delivery Instructions</label>
-                        <div class="col-md-4">
-                            <textarea class="form-control" name="delivery_instructions" id="delivery_instructions" placeholder="Leave in a safe place out of the weather"><?php echo $instructions;?></textarea>
-                            <span class="inst">Appears on shipping label. Defaults to 'Leave in a safe place out of the weather' for orders with an Authority To Leave</span>
+                        <div class="form-group row">
+                            <label class="col-md-3 col-form-label">Delivery Instructions</label>
+                            <div class="col-md-4">
+                                <textarea class="form-control" name="delivery_instructions" id="delivery_instructions" placeholder="Leave in a safe place out of the weather"><?php echo $instructions;?></textarea>
+                                <span class="inst">Appears on shipping label. Defaults to 'Leave in a safe place out of the weather' for orders with an Authority To Leave</span>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 col-form-label">3PLPLUS Instructions</label>
-                        <div class="col-md-4">
-                            <textarea class="form-control" name="tpl_comments" id="tpl_comments"><?php echo $comments;?></textarea>
-                            <span class="inst">Instructions for the pickers and packers</span>
+                        <div class="form-group row">
+                            <label class="col-md-3 col-form-label">3PLPLUS Instructions</label>
+                            <div class="col-md-4">
+                                <textarea class="form-control" name="tpl_comments" id="tpl_comments"><?php echo $comments;?></textarea>
+                                <span class="inst">Instructions for the pickers and packers</span>
+                            </div>
                         </div>
-                    </div>
-                    <input type="hidden" name="order_id" id="order_id" value="<?php echo $order_id;?>" />
-                    <input type="hidden" name="client_id" id="client_id" value="<?php echo $order['client_id'];?>" />
-                    <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
-                    <div class="form-group row">
-                        <label class="col-md-3 col-form-label">&nbsp;</label>
-                        <div class="col-md-4">
-                            <button type="submit" class="btn btn-primary">Edit Order</button>
+                        <input type="hidden" name="order_id" id="order_id" value="<?php echo $order_id;?>" />
+                        <input type="hidden" name="client_id" id="client_id" value="<?php echo $order['client_id'];?>" />
+                        <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+                        <div class="form-group row">
+                            <label class="col-md-3 col-form-label">&nbsp;</label>
+                            <div class="col-md-4">
+                                <button type="submit" class="btn btn-primary">Edit Order</button>
+                            </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
+
             </div>
         <?php endif;?>
     </div>
