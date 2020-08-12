4ad608dba6ab0eca8193c23771212fdbc1066a22
marxolly <mark@solly.com.au>
2020-08-12 11:18:44 +1000

    Update no_order_id.php

--- app/views/layout/page-includes/no_order_id.php
+++ app/views/layout/page-includes/no_order_id.php
@@ -1,12 +1,14 @@
 <div class="row">
     <div class="errorbox">
-        <div class="col-4 text-right">
-            <i class="fad fa-exclamation-triangle fa-6x"></i>
-        </div>
-        <div class="col-8">
-            <h2>No Order Found</h2>
-            <p>No order was found with that ID</p>
-            <p><a href="/orders/view-orders">Please click here to view all orders to choose from</a></p>
+        <div class="row">
+            <div class="col-4 text-right">
+                <i class="fad fa-exclamation-triangle fa-6x"></i>
+            </div>
+            <div class="col-8">
+                <h2>No Order Found</h2>
+                <p>No order was found with that ID</p>
+                <p><a href="/orders/view-orders">Please click here to view all orders to choose from</a></p>
+            </div>
         </div>
     </div>
 </div>
