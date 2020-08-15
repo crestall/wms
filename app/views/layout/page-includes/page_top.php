28d997c495f0f2eac71f21d05387d59639622aea
Mark Solly <msolly@contactpoint.com.au>
2020-08-15 17:13:45 +1000

    Update page_top.php

--- app/views/layout/page-includes/page_top.php
+++ app/views/layout/page-includes/page_top.php
@@ -1,6 +1,7 @@
 <?php
 
 ?>
+<?php include(Config::get('VIEWS_PATH')."layout/page-includes/breadcrumb.php");?>
 <div class="row">
     <div class="col-lg-12">
         <h2 class="page-header"><?php echo $page_title;?></h2>
