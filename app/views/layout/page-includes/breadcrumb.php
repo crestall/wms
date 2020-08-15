70bedb5078c7d9cadb2406b9f4e101214afad7fa
Mark Solly <msolly@contactpoint.com.au>
2020-08-15 16:23:52 +1000

    Update breadcrumb.php

---
+++ app/views/layout/page-includes/breadcrumb.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+<div class="mr-auto">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb blue-grey lighten-4">
+            <li class="breadcrumb-item"><a class="black-text" href="#">Home</a><i class="fas fa-angle-double-right mx-2" aria-hidden="true"></i></li>
+            <li class="breadcrumb-item"><a class="black-text" href="#">Orders</a></li>
+            <li class="bredcrumb-item">and so on...</li>
+        </ol>
+    </nav>
+</div>
