975e990493bcd0961525767de0c4a1e6fd96de33
marxolly <mark@solly.com.au>
2020-08-17 14:14:04 +1000

    Update viewProducts.php

--- app/views/products/viewProducts.php
+++ app/views/products/viewProducts.php
@@ -2,12 +2,10 @@
     <div id="page_container" class="container-xxl">
         <input type="hidden" id="client_id" value="<?php echo $client_id;?>" />
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-        <div class="col-lg-12">
-            <div class="form-group">
-                <label class="col-md-3">Select a Client</label>
-                <div class="col-md-4">
-                    <select id="client_selector" class="form-control selectpicker"><option value="0">Select</option><?php echo $this->controller->client->getSelectClients($client_id);?></select>
-                </div>
+        <div class="form-group row">
+            <label class="col-md-3">Select a Client</label>
+            <div class="col-md-4">
+                <select id="client_selector" class="form-control selectpicker"><option value="0">Select</option><?php echo $this->controller->client->getSelectClients($client_id);?></select>
             </div>
         </div>
         <?php if($client_id > 0):?>
