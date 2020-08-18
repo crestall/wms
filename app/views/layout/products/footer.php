ad015a21625235f6e2496d3a6970791455a6998d
marxolly <mark@solly.com.au>
2020-08-18 14:30:13 +1000

    Update footer.php

--- app/views/layout/products/footer.php
+++ app/views/layout/products/footer.php
@@ -8,41 +8,7 @@
                 common: {
                     init: function(){
 
-                    },
-                    'remove-all-items': function(){
-                        $('a#remove-all-items').click(function(e){
-                            e.preventDefault();
-                            swal({
-                                title: "Really remove all items?",
-                                text: "This cannot be undone",
-                                icon: "warning",
-                                buttons: true,
-                                dangerMode: true,
-                            }).then( function(willRemove) {
-                                if (willRemove) {
-                                   $('div#items_holder div.item_holder').remove();
-                                   var item_count = $(":input.item-searcher").length;
-                                    //console.log('items: '+item_count);
-                                    var html = "<div class='row item_holder'>"
-                                    html += "<div class='col-md-1 delete-image-holder'>";
-                                    html += "<a class='delete' title='remove this item' style='display:none;'><i class='fad fa-times-square text-danger'></i><span class='inst'>Remove</span></a>";
-                                    html += "</div>";
-                                    html += "<div class='col-md-6'>";
-                                    html += "<p><input type='text' class='form-control item-searcher' name='items["+item_count+"][name]' placeholder='Item Name' /></p>";
-                                    html += "</div>";
-                                    html += "<div class='col-md-2 qty-holder'>";
-                                    html += "</div>";
-                                    html += "<div class='col-md-3 qty-location'></div>";
-                                    html += "<input type='hidden' name='items["+item_count+"][id]' class='item_id'  />";
-                                    html += "</div>";
-                                    $('div#items_holder').append(html).find('input.item-searcher').focus();
-
-                                    actions['item-searcher'].init();
-                                    itemsUpdater.itemDelete();
-                                }
-                            });
-                        });
-                    },
+                    }
                 },
                 'edit-product': {
                     init: function()
@@ -93,7 +59,38 @@
                 },
                 'collections-edit': {
                     init: function(){
-                        actions.common['remove-all-items']();
+                        $('a#remove-all-items').click(function(e){
+                            e.preventDefault();
+                            swal({
+                                title: "Really remove all items?",
+                                text: "This cannot be undone",
+                                icon: "warning",
+                                buttons: true,
+                                dangerMode: true,
+                            }).then( function(willRemove) {
+                                if (willRemove) {
+                                   $('div#items_holder div.item_holder').remove();
+                                   var item_count = $(":input.item-searcher").length;
+                                    //console.log('items: '+item_count);
+                                    var html = "<div class='row item_holder'>"
+                                    html += "<div class='col-md-1 delete-image-holder'>";
+                                    html += "<a class='delete' title='remove this item' style='display:none;'><i class='fad fa-times-square text-danger'></i><span class='inst'>Remove</span></a>";
+                                    html += "</div>";
+                                    html += "<div class='col-md-6'>";
+                                    html += "<p><input type='text' class='form-control item-searcher' name='items["+item_count+"][name]' placeholder='Item Name' /></p>";
+                                    html += "</div>";
+                                    html += "<div class='col-md-2 qty-holder'>";
+                                    html += "</div>";
+                                    html += "<div class='col-md-3 qty-location'></div>";
+                                    html += "<input type='hidden' name='items["+item_count+"][id]' class='item_id'  />";
+                                    html += "</div>";
+                                    $('div#items_holder').append(html).find('input.item-searcher').focus();
+
+                                    actions['item-searcher'].init();
+                                    itemsUpdater.itemDelete();
+                                }
+                            });
+                        });
                         $('#product_selector').change(function(e){
                             if($(this).val() > 0)
                             {
