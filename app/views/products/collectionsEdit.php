5b0e04474fd949621515e97630794c0ff9729092
marxolly <mark@solly.com.au>
2020-08-18 11:01:23 +1000

    Update collectionsEdit.php

--- app/views/products/collectionsEdit.php
+++ app/views/products/collectionsEdit.php
@@ -22,51 +22,53 @@
             <?php else:?>
                 <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
                 <div class="row">
-                    <form id="collection_edit" action="/form/procCollectionEdit" method="post">
-                        <div class="form-group row">
-                            <label class="col-md-3 col-form-label">Add Item</label>
-                            <div class="col-md-4">
-                                <input type="text" class="form-control" name="item_searcher" id="item_searcher" />
+                    <div class="col">
+                        <form id="collection_edit" action="/form/procCollectionEdit" method="post">
+                            <div class="form-group row">
+                                <label class="col-md-3 col-form-label">Add Item</label>
+                                <div class="col-md-4">
+                                    <input type="text" class="form-control" name="item_searcher" id="item_searcher" />
+                                </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-7">
-                                <h3>Listed Items</h3>
+                            <div class="row">
+                                <div class="col-md-7">
+                                    <h3>Listed Items</h3>
+                                </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-7">
-                                <p>A total of <?php echo count($items);?> items listed</p>
+                            <div class="row">
+                                <div class="col-md-7">
+                                    <p>A total of <?php echo count($items);?> items listed</p>
+                                </div>
                             </div>
-                        </div>
-                        <div class="bs-callout bs-callout-primary bs-callout-more">
-                            <div id="the_items">
-                                <?php foreach($items as $i):?>
-                                    <div class="form-group row">
-                                        <div class='item_holder'>
-                                            <label class="col-md-5 col-form-label"><?php echo $i['name']." (".$i['sku'].")";?></label>
-                                            <div class="col-md-2">
-                                                <input type="text" class="form-control required number" name="items[<?php echo $i['linked_item_id'];?>][qty]"  value="<?php echo $i['number'];?>" />
-                                            </div>
-                                            <div class='col-md-1 delete-image-holder'>
-                                                <a class="delete" data-itemid="<?php echo $i['linked_item_id'];?>" title="remove this item"><i class="fas fa-backspace fa-2x text-danger"></i></a>
+                            <div class="bs-callout bs-callout-primary bs-callout-more">
+                                <div id="the_items">
+                                    <?php foreach($items as $i):?>
+                                        <div class="form-group row">
+                                            <div class='item_holder'>
+                                                <label class="col-md-5 col-form-label"><?php echo $i['name']." (".$i['sku'].")";?></label>
+                                                <div class="col-md-2">
+                                                    <input type="text" class="form-control required number" name="items[<?php echo $i['linked_item_id'];?>][qty]"  value="<?php echo $i['number'];?>" />
+                                                </div>
+                                                <div class='col-md-1 delete-image-holder'>
+                                                    <a class="delete" data-itemid="<?php echo $i['linked_item_id'];?>" title="remove this item"><i class="fas fa-backspace fa-2x text-danger"></i></a>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                <?php endforeach;?>
-                                <?php echo Form::displayError('items');?>
+                                    <?php endforeach;?>
+                                    <?php echo Form::displayError('items');?>
+                                </div>
                             </div>
-                        </div>
-                        <input type="hidden" name="selected_items" id="selected_items" value="<?php echo $sis;?>" />
-                        <input type="hidden" name="item_id" id="item_id" value="<?php echo $item_id;?>" />
-                        <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
-                        <div class="form-group row">
-                            <label class="col-md-3 col-form-label">&nbsp;</label>
-                            <div class="col-md-4">
-                                <button type="submit" class="btn btn-primary">Save Changes</button>
+                            <input type="hidden" name="selected_items" id="selected_items" value="<?php echo $sis;?>" />
+                            <input type="hidden" name="item_id" id="item_id" value="<?php echo $item_id;?>" />
+                            <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+                            <div class="form-group row">
+                                <label class="col-md-3 col-form-label">&nbsp;</label>
+                                <div class="col-md-4">
+                                    <button type="submit" class="btn btn-primary">Save Changes</button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             <?php endif;?>
         <?php endif;?>
