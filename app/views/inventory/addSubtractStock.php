67ce71068a5de701c6515598a9af7548489a1afa
marxolly <mark@solly.com.au>
2020-08-18 16:06:08 +1000

    Update addSubtractStock.php

--- app/views/inventory/addSubtractStock.php
+++ app/views/inventory/addSubtractStock.php
@@ -1,36 +1,38 @@
 <div id="page-wrapper">
-    <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-    <div class="row">
-        <div class="col">
-            <p><a class="btn btn-outline-fsg" href="/inventory/view-inventory/client=<?php echo $product_info['client_id'];?>">Return to Clients Inventory</a></p>
-        </div>
-        <div class="col text-right">
-            <p><a class="btn btn-outline-fsg" href="/inventory/move-stock/product=<?php echo $product_id;?>">Move Stock for This Item</a></p>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-3 col-md-3 col-lg-3">
-            <div class="card h-100 border-secondary">
-                <div class="card-header bg-secondary text-white">
-                    Current Locations
-                </div>
-                The locations list will go in this box
+    <div id="page_container" class="container-xxl">
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+        <div class="row">
+            <div class="col">
+                <p><a class="btn btn-outline-fsg" href="/inventory/view-inventory/client=<?php echo $product_info['client_id'];?>">Return to Clients Inventory</a></p>
+            </div>
+            <div class="col text-right">
+                <p><a class="btn btn-outline-fsg" href="/inventory/move-stock/product=<?php echo $product_id;?>">Move Stock for This Item</a></p>
             </div>
         </div>
-        <div class="col-sm-9 col-md-4 col-lg-4">
-            <div class="card h-100 border-secondary">
-                <div class="card-header bg-secondary text-white">
-                    Add To Stock
+        <div class="row">
+            <div class="col-sm-3 col-md-3 col-lg-3">
+                <div class="card h-100 border-secondary">
+                    <div class="card-header bg-secondary text-white">
+                        Current Locations
+                    </div>
+                    The locations list will go in this box
                 </div>
-                The add to stock form goes in this box
             </div>
-        </div>
-        <div class="col-sm-9 col-md-4 col-lg-4 offset-sm-3">
-            <div class="card h-100 border-secondary">
-                <div class="card-header bg-secondary text-white">
-                    Subtract From Stock
+            <div class="col-sm-9 col-md-4 col-lg-4">
+                <div class="card h-100 border-secondary">
+                    <div class="card-header bg-secondary text-white">
+                        Add To Stock
+                    </div>
+                    The add to stock form goes in this box
                 </div>
-                The subtract from stock form goes in this box
+            </div>
+            <div class="col-sm-9 col-md-4 col-lg-4 offset-sm-3">
+                <div class="card h-100 border-secondary">
+                    <div class="card-header bg-secondary text-white">
+                        Subtract From Stock
+                    </div>
+                    The subtract from stock form goes in this box
+                </div>
             </div>
         </div>
     </div>
