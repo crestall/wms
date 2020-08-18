f9bd681737e71e73ceff00b1759b8c3124bee96f
marxolly <mark@solly.com.au>
2020-08-18 14:08:30 +1000

    Update footer.php

--- app/views/layout/products/footer.php
+++ app/views/layout/products/footer.php
@@ -7,42 +7,7 @@
             var actions = {
                 common: {
                     init: function(){
-                        $('#palletized').click(function(e){
-                            $("#per_pallet_holder").slideToggle('slow');
-                        });
 
-                        $('#collection').click(function(e){
-                            if(this.checked)
-                            {
-                                $('#pack_item').prop('checked', false);
-                            }
-
-                        });
-
-                        $('#pack_item').click(function(e){
-                            if(this.checked)
-                            {
-                                $('#collection').prop('checked', false);
-                            }
-
-                        });
-
-                        $('#package_type').change(function(e){
-                            var html = "";
-                            $("option:selected", this).each(function(){
-                                if($(this).data('multiples') == 1)
-                                {
-                                    var count = ($( "#pt_count_"+$(this).val() ).val())? $( "#pt_count_"+$(this).val() ).val(): "" ;
-                                    html += "<div class='form-group row'>";
-                                    html += "<label class='col-md-3 col-form-label'><sup><small><i class='fas fa-asterisk text-danger'></i></small></sup> Number in "+$(this).text()+"</label>";
-                                    html += "<div class='col-md-4'>";
-                                    html += "<input type='text' class='form-control required number' name='number_in_"+$(this).val()+"' id='number_in_"+$(this).val()+"' value='"+count+"' />";
-                                    html += "</div>";
-                                    html += "</div>";
-                                }
-                            });
-                            $("#type_holder").html(html);
-                        });
                     }
                 },
                 'edit-product': {
@@ -92,57 +57,6 @@
                         });
                     }
                 },
-                'pack-items-edit': {
-                    init: function(){
-                        $('#product_selector').change(function(e){
-                            if($(this).val() > 0)
-                            {
-                                $.blockUI({ message: '<div style="height:140px; padding-top:20px;"><h2>Collecting Details...</h2></div>' });
-                                window.location.href = "/products/pack-items-edit/product=" + $(this).val();
-                            }
-                        });
-                        autoCompleter.itemAutoComplete($('#item_searcher'), selectCallback, changeCallback, false);
-                        function selectCallback(event, ui)
-                        {
-                            var appendage = "<div class='row form-group'><div class='item_holder'><label class='col-md-3 col-form-label'>"+ui.item.value+"</label><div class='col-md-4'><input type='text' class='required number form-control item-group count' placeholder='qty' name='items["+ui.item.item_id+"][qty]' id='item_"+ui.item.item_id+"' data-itemid='"+ui.item.item_id+"' /></div>";
-                            appendage += "<div class='col-md-1 delete-image-holder'>";
-                            appendage += "<a class='delete' data-itemid='"+ui.item.item_id+"' title='remove this item'><i class='fas fa-backspace fa-2x text-danger'></i></a></div>";
-                            appendage += "</div></div>";
-                            $('div#the_items').append(appendage);
-                            if($('#selected_items').val() == '')
-                            {
-                                $('#selected_items').val(ui.item.item_id);
-                            }
-                            else
-                            {
-                                $('#selected_items').val($('#selected_items').val()+ ','+ui.item.item_id);
-                            }
-                            $(event.target).val('');
-                            ui.item.value="";
-                            itemsUpdater.itemDelete();
-                            return false;
-                        }
-                        function changeCallback(event, ui)
-                        {
-                            if (!ui.item)
-                	        {
-                                $('#item_searcher').val("");
-                                return false;
-                            }
-                        }
-                        itemsUpdater.itemDelete();
-                        $("form#pack_item_edit").submit(function(e){
-                            if($(this).valid())
-                            {
-                                $.blockUI({ message: '<div style="height:140px; padding-top:20px;"><h2>Updating Details...</h2></div>' });
-                            }
-                            else
-                            {
-                                return false;
-                            }
-                        });
-                    }
-                },
                 'collections-edit': {
                     init: function(){
                         $('#product_selector').change(function(e){
@@ -155,10 +69,16 @@
                         autoCompleter.itemAutoComplete($('#item_searcher'), selectCallback, changeCallback, false);
                         function selectCallback(event, ui)
                         {
-                            var appendage = "<div class='row form-group'><div class='item_holder'><label class='col-md-3 col-form-label'>"+ui.item.value+"</label><div class='col-md-4'><input type='text' class='required number form-control item-group count' placeholder='qty' name='items["+ui.item.item_id+"][qty]' id='item_"+ui.item.item_id+"' data-itemid='"+ui.item.item_id+"' /></div>";
-                            appendage += "<div class='col-md-1 delete-image-holder'>";
-                            appendage += "<a class='delete' data-itemid='"+ui.item.item_id+"' title='remove this item'><i class='fas fa-backspace fa-2x text-danger'></i></a></div>";
-                            appendage += "</div></div>";
+                            var appendage = "<div class='row item_holder mb-3'>";
+                            appendage += "<label class='col-md-7'>"+ui.item.value+"</label>";
+                            appendage += "<div class='col-md-1'>";
+                            appendage += "<input type='text' class='form-control required number' name='items["+ui.item.item_id+"][qty]' data-itemid='"+ui.item.item_id+" />";
+                            appendage += "</div>";
+                            appendage += "<div class='col-md-2 delete-image-holder'>";
+                            appendage += "<a class='delete' data-itemid="+ui.item.item_id+" title='remove this item'><i class='fad fa-times-square text-danger'></i> <span class='inst'>Remove</span></a>";
+                            appendage += "</div> ";
+                            appendage += "</div>";
+
                             $('div#the_items').append(appendage);
                             if($('#selected_items').val() == '')
                             {
