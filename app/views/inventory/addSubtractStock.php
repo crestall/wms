6b8951bb6dbde9e2c33fb875929f0d9468e0c436
marxolly <mark@solly.com.au>
2020-08-18 15:30:37 +1000

    Update addSubtractStock.php

--- app/views/inventory/addSubtractStock.php
+++ app/views/inventory/addSubtractStock.php
@@ -1,11 +1,11 @@
 <div id="page-wrapper">
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
     <div class="row">
-        <div class="col-lg-3">
-            <p><a class="btn btn-primary" href="/inventory/view-inventory/client=<?php echo $product_info['client_id'];?>">Return to Clients Inventory</a></p>
+        <div class="col">
+            <p><a class="btn btn-outline-fsg" href="/inventory/view-inventory/client=<?php echo $product_info['client_id'];?>">Return to Clients Inventory</a></p>
         </div>
-        <div class="col-lg-3">
-            <p><a class="btn btn-primary" href="/inventory/move-stock/product=<?php echo $product_id;?>">Move Stock for This Item</a></p>
+        <div class="col text-right">
+            <p><a class="btn btn-outline-fsg" href="/inventory/move-stock/product=<?php echo $product_id;?>">Move Stock for This Item</a></p>
         </div>
     </div>
     <?php echo $addform;?>
