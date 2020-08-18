f9a0d8dcc78aa8d3081c6db43811ae029950724b
marxolly <mark@solly.com.au>
2020-08-18 10:05:47 +1000

    Update editProduct.php

--- app/views/products/editProduct.php
+++ app/views/products/editProduct.php
@@ -164,69 +164,18 @@ $client_id = $product['client_id'];
                             <?php echo Form::displayError('price');?>
                         </div>
                     </div>
-                    <div class="form-group row">
-                        <div class="form-check">
-                            <label class="form-check-label col-md-3" for="requires_bubblewrap">Requires Bubblewrap</label>
-                            <div class="col-md-4 checkbox checkbox-default">
-                                <input class="form-check-input styled" type="checkbox" id="requires_bubblewrap" name="requires_bubblewrap" <?php if($product['requires_bubblewrap'] > 0) echo "checked";?> />
-                                <label for="requires_bubblewrap"></label>
-                            </div>
-                        </div>
+                    <div class="form-group row custom-control custom-checkbox custom-control-right">
+                        <input class="custom-control-input" type="checkbox" id="requires_bubblewrap" name="requires_bubblewrap" <?php if($product['requires_bubblewrap'] > 0) echo "checked";?> />
+                        <label class="custom-control-label col-md-3" for="requires_bubblewrap">Requires Bubblewrap</label>
                     </div>
-                    <div class="form-group row">
-                        <div class="form-check">
-                            <label class="form-check-label col-md-3" for="pack_item">Pack Item</label>
-                            <div class="col-md-4 checkbox checkbox-default">
-                                <input class="form-check-input styled" type="checkbox" id="pack_item" name="pack_item" <?php if($product['pack_item'] > 0) echo "checked";?> />
-                                <label for="pack_item"></label>
-                            </div>
-                        </div>
+                    <div class="form-group row custom-control custom-checkbox custom-control-right">
+                        <input class="custom-control-input" type="checkbox" id="collection" name="collection" <?php if($product['collection'] > 0) echo "checked";?> />
+                        <label class="custom-control-label col-md-3" for="collection">Collection</label>
                     </div>
-                    <div class="form-group row">
-                        <div class="form-check">
-                            <label class="form-check-label col-md-3" for="collection">Collection</label>
-                            <div class="col-md-4 checkbox checkbox-default">
-                                <input class="form-check-input styled" type="checkbox" id="collection" name="collection" <?php if($product['collection'] > 0) echo "checked";?> />
-                                <label for="collection"></label>
-                            </div>
-                        </div>
+                    <div class="form-group row custom-control custom-checkbox custom-control-right">
+                        <input class="custom-control-input" type="checkbox" id="palletized" name="palletized" <?php if(!empty($product['palletized'] > 0) echo "palletized";?> />
+                        <label class="custom-control-label col-md-3" for="palletized">Dispatch as Whole Pallets</label>
                     </div>
-                    <div class="form-group row">
-                        <div class="form-check">
-                            <label class="form-check-label col-md-3" for="double_bay">Double Bay Item</label>
-                            <div class="col-md-4 checkbox checkbox-default">
-                                <input class="form-check-input styled" type="checkbox" id="double_bay" name="double_bay" <?php if($product['double_bay'] > 0) echo "checked";?> />
-                                <label for="double_bay"></label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <div class="form-check">
-                            <label class="form-check-label col-md-3" for="palletized">Dispatch as Whole Pallets</label>
-                            <div class="col-md-4 checkbox checkbox-default">
-                                <input class="form-check-input styled" type="checkbox" id="palletized" name="palletized" <?php if($product['palletized'] > 0) echo "checked";?> />
-                                <label for="palletized"></label>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="per_pallet_holder" style="display: <?php echo $per_pallet_display;?>">
-                        <div class="form-group row">
-                            <label class="col-md-3 col-form-label">Number per pallet</label>
-                            <div class="col-md-4">
-                                <input type="text" class="form-control required" name="per_pallet" id="per_pallet" value="<?php echo $product['per_pallet'];?>" />
-                                <?php echo Form::displayError('per_pallet');?>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 col-form-label">Package Type(s)</label>
-                        <div class="col-md-4">
-                            <select id="package_type" name="package_type[]" class="form-control selectpicker" multiple><?php echo $this->controller->packingtype->getSelectPackingTypesMultiple(array_keys($packing_types));?></select>
-                            <span class="inst">Select all relevent types</span>
-                            <?php echo Form::displayError('package_type');?>
-                        </div>
-                    </div>
-                    <div id="type_holder"></div>
                     <div class="row">
                         <div class="col-lg-12">
                             <h3>Inventory</h3>
