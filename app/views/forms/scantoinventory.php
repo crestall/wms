6a99dadc8c6eb400b346625a33a8dab6759b4282
marxolly <mark@solly.com.au>
2020-08-20 09:49:49 +1000

    Update scantoinventory.php

--- app/views/forms/scantoinventory.php
+++ app/views/forms/scantoinventory.php
@@ -64,58 +64,24 @@
             </div>
         </div>
         <div class="form-group row">
-            <label class="col-md-3 col-form-label">Weight</label>
+            <label class="col-md-3">Weight</label>
             <div class="col-md-4">
                 <div class="input-group">
                     <input type="text" class="form-control number" name="weight" id="weight" value="<?php echo Form::value('weight');?>" />
-                    <span class="input-group-addon">Kg</span>
+                    <div class="input-group-append">
+                        <span class="input-group-text">Kg</span>
+                    </div>
                 </div>
                 <?php echo Form::displayError('weight');?>
             </div>
         </div>
-        <div class="form-group row">
-            <div class="form-check">
-                <label class="form-check-label col-md-3" for="pack_item">Pack Item</label>
-                <div class="col-md-4 checkbox checkbox-default">
-                    <input class="form-check-input styled" type="checkbox" id="pack_item" name="pack_item" <?php if(!empty(Form::value('pack_item'))) echo "checked";?> />
-                    <label for="pack_item"></label>
-                </div>
-            </div>
+        <div class="form-group row custom-control custom-checkbox custom-control-right">
+            <input class="custom-control-input" type="checkbox" id="collection" name="collection" <?php if(!empty(Form::value('collection'))) echo "checked";?> />
+            <label class="custom-control-label col-md-3" for="collection">Collection</label>
         </div>
-        <div class="form-group row">
-            <div class="form-check">
-                <label class="form-check-label col-md-3" for="double_bay">Double Bay Item</label>
-                <div class="col-md-4 checkbox checkbox-default">
-                    <input class="form-check-input styled" type="checkbox" id="double_bay" name="double_bay" <?php if(!empty(Form::value('double_bay'))) echo "checked";?> />
-                    <label for="double_bay"></label>
-                </div>
-            </div>
-        </div>
-        <div class="form-group row">
-            <label class="col-md-3 col-form-label">Package Type(s)</label>
-            <div class="col-md-4">
-                <select id="package_type" name="package_type[]" class="form-control selectpicker" multiple><?php echo $this->controller->packingtype->getSelectPackingTypesMultiple();?></select>
-                <span class="inst">Select all relevent types</span>
-            </div>
-        </div>
-        <div id="type_holder"></div>
-        <div class="form-group row">
-            <div class="form-check">
-                <label class="form-check-label col-md-3" for="palletized">Dispatch as Whole Pallets</label>
-                <div class="col-md-4 checkbox checkbox-default">
-                    <input class="form-check-input styled" type="checkbox" id="palletized" name="palletized" />
-                    <label for="palletized"></label>
-                </div>
-            </div>
-        </div>
-        <div id="per_pallet_holder" style="display: none">
-            <div class="form-group row">
-                <label class="col-md-3 col-form-label">Number per pallet</label>
-                <div class="col-md-4">
-                    <input type="text" class="form-control required number" name="per_pallet" id="per_pallet" value="" />
-                    <?php echo Form::displayError('per_pallet');?>
-                </div>
-            </div>
+        <div class="form-group row custom-control custom-checkbox custom-control-right">
+            <input class="custom-control-input" type="checkbox" id="palletized" name="palletized" <?php if(!empty(Form::value('palletized'))) echo "palletized";?> />
+            <label class="custom-control-label col-md-3" for="palletized">Dispatch as Whole Pallets</label>
         </div>
         <div class="row">
             <div class="col-lg-12">
