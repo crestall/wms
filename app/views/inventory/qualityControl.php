0eaee7b652fa32320824922013a22f7456a25b2c
marxolly <mark@solly.com.au>
2020-08-19 15:09:36 +1000

    Update qualityControl.php

--- app/views/inventory/qualityControl.php
+++ app/views/inventory/qualityControl.php
@@ -30,11 +30,52 @@
             <div class="col-sm-8 col-md-8 col-lg-8 col-xl-8 mb-3">
                 <div class="card h-100 border-secondary">
                     <div class="card-header bg-secondary text-white">
-                        Move Stock
+                        Manage Quality Control Status
                     </div>
                     <div class="card-body">
                         <div class="container-fluid">
-
+                            <form id="quality_control" method="post" action="/form/procQualityControl">
+                                <div class="form-group row">
+                                    <label class="col-md-5 col-form-label">Add To Quality Control</label>
+                                    <div class="col-md-7">
+                                        <input type="text" class="form-control number" name="qty_add" id="qty_add" value="<?php echo Form::value('qty_add');?>" />
+                                        <?php echo Form::displayError('qty_add');?>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-5 col-form-label">Location</label>
+                                    <div class="col-md-7">
+                                        <select id="add_to_location" name="add_to_location" class="form-control selectpicker" data-live-search="true" data-style="btn-outline-secondary"><option value="0">--Select One--</option><?php echo $this->controller->location->getSelectItemInLocations($product_id, Form::value('add_to_location'));?></select>
+                                        <?php echo Form::displayError('add_to_location');?>
+                                    </div>
+                                </div>
+                                <?php if($show_remove):?>
+                                    <div class="form-group row">
+                                        <label class="col-md-5 col-form-label">Remove From Quality Control</label>
+                                        <div class="col-md-7">
+                                            <input type="text" class="form-control number" name="qty_subtract" id="qty_subtract" value="<?php echo Form::value('qty_subtract');?>" />
+                                            <?php echo Form::displayError('qty_subtract');?>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-md-5 col-form-label">Location</label>
+                                        <div class="col-md-7">
+                                            <select id="subtract_from_location" name="subtract_from_location" class="form-control selectpicker" data-live-search="true" data-style="btn-outline-secondary"><option value="0">--Select One--</option><?php echo $this->controller->location->getSelectQCItemInLocations($product_id, Form::value('subtract_from_location'));?></select>
+                                            <?php echo Form::displayError('subtract_from_location');?>
+                                        </div>
+                                    </div>
+                                <?php endif;?>
+                                <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+                                <input type="hidden" name="product_id" value="<?php echo $product_id; ?>" />
+                                <input type="hidden" name="product_name" value="<?php echo $product_info['name']; ?>" />
+                                <div class="form-group row">
+                                    <label class="col-md-5 col-form-label">&nbsp;</label>
+                                    <div class="col-md-7">
+                                        <button type="submit" class="btn btn-primary">Update Quality Control Stock</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                         </div>
                     </div>
                     <div class="card-footer">
@@ -43,47 +84,6 @@
                 </div>
             </div>
             <div class="col-md-7">
-                <form id="quality_control" method="post" action="/form/procQualityControl">
-                    <div class="form-group row">
-                        <label class="col-md-5 col-form-label">Add To Quality Control</label>
-                        <div class="col-md-7">
-                            <input type="text" class="form-control number" name="qty_add" id="qty_add" value="<?php echo Form::value('qty_add');?>" />
-                            <?php echo Form::displayError('qty_add');?>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-5 col-form-label">Location</label>
-                        <div class="col-md-7">
-                            <select id="add_to_location" name="add_to_location" class="form-control selectpicker" data-live-search="true"><option value="0">--Select One--</option><?php echo $this->controller->location->getSelectItemInLocations($product_id, Form::value('add_to_location'));?></select>
-                            <?php echo Form::displayError('add_to_location');?>
-                        </div>
-                    </div>
-                    <?php if($show_remove):?>
-                        <div class="form-group row">
-                            <label class="col-md-5 col-form-label">Remove From Quality Control</label>
-                            <div class="col-md-7">
-                                <input type="text" class="form-control number" name="qty_subtract" id="qty_subtract" value="<?php echo Form::value('qty_subtract');?>" />
-                                <?php echo Form::displayError('qty_subtract');?>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-md-5 col-form-label">Location</label>
-                            <div class="col-md-7">
-                                <select id="subtract_from_location" name="subtract_from_location" class="form-control selectpicker" data-live-search="true"><option value="0">--Select One--</option><?php echo $this->controller->location->getSelectQCItemInLocations($product_id, Form::value('subtract_from_location'));?></select>
-                                <?php echo Form::displayError('subtract_from_location');?>
-                            </div>
-                        </div>
-                    <?php endif;?>
-                    <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
-                    <input type="hidden" name="product_id" value="<?php echo $product_id; ?>" />
-                    <input type="hidden" name="product_name" value="<?php echo $product_info['name']; ?>" />
-                    <div class="form-group row">
-                        <label class="col-md-5 col-form-label">&nbsp;</label>
-                        <div class="col-md-7">
-                            <button type="submit" class="btn btn-primary">Update Quality Control Stock</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
+
         </div>
 </div>
