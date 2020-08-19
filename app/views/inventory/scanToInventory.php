1c429c60e058147d4e33f4837927a273fc4333e5
marxolly <mark@solly.com.au>
2020-08-20 08:34:49 +1000

    Update scanToInventory.php

--- app/views/inventory/scanToInventory.php
+++ app/views/inventory/scanToInventory.php
@@ -2,12 +2,10 @@
     <div id="page_container" class="container-xl">
         <input type="hidden" id="client_id" value="<?php echo $client_id;?>" />
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-        <div class="row">
-            <div class="form-group">
-                <label class="col-md-3">Select a Client</label>
-                <div class="col-md-4">
-                    <p><select id="client_selector" class="form-control selectpicker"><option value="0">Select</option><?php echo $this->controller->client->getSelectClients($client_id);?></select></p>
-                </div>
+        <div class="row mb-3">
+            <label class="col-md-3">Select a Client</label>
+            <div class="col-md-4">
+                <select id="client_selector" class="form-control selectpicker" data-style="btn-outline-secondary" data-live-search="true"><option value="0">Select</option><?php echo $this->controller->client->getSelectClients($client_id);?></select>
             </div>
         </div>
         <?php if($client_id > 0):?>
