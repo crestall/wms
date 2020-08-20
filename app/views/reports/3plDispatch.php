da992557695b6f83de67405df2859b937fdc3063
marxolly <mark@solly.com.au>
2020-08-21 09:02:11 +1000

    Update 3plDispatch.php

--- app/views/reports/3plDispatch.php
+++ app/views/reports/3plDispatch.php
@@ -11,7 +11,7 @@
         <?php if($client_id > 0):?>
             <?php include(Config::get('VIEWS_PATH')."layout/page-includes/between_dates.php");?>
             <?php if(count($client_orders)):?>
-                <?php //echo "<pre>",print_r($client_orders),"</pre>"; die();?>
+                <?php echo "<pre>",print_r($client_orders),"</pre>"; die();?>
                 <div id="waiting" class="row">
                     <div class="col-lg-12 text-center">
                         <h2>Drawing Table..</h2>
