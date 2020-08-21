d7e161a861d4f367daa0b7474f7197841bbff3a0
marxolly <mark@solly.com.au>
2020-08-21 15:06:15 +1000

    Update stockMovementReasons.php

--- app/views/sitesettings/stockMovementReasons.php
+++ app/views/sitesettings/stockMovementReasons.php
@@ -20,14 +20,9 @@ $role = Session::getUserRole();
                 </div>
             </div>
             <?php if($role === "super admin"):?>
-                <div class="form-group row">
-                    <div class="form-check">
-                        <label class="form-check-label col-md-3" for="locked">Locked</label>
-                        <div class="col-md-4 checkbox checkbox-default">
-                            <input class="form-check-input styled" type="checkbox" id="locked" name="locked" />
-                            <label for="locked"></label>
-                        </div>
-                    </div>
+                <div class="form-group row custom-control custom-checkbox custom-control-right">
+                    <input class="custom-control-input" type="checkbox" id="locked" name="locked" <?php if(!empty(Form::value('locked'))) echo 'checked';?> />
+                    <label class="custom-control-label col-md-3" for="locked">Locked</label>
                 </div>
             <?php endif;?>
             <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
