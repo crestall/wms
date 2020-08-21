e35541f8204104b071a46f518601d5575639ca85
marxolly <mark@solly.com.au>
2020-08-21 15:07:40 +1000

    Update stockMovementReasons.php

--- app/views/sitesettings/stockMovementReasons.php
+++ app/views/sitesettings/stockMovementReasons.php
@@ -6,7 +6,7 @@ $role = Session::getUserRole();
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
         <?php echo Form::displayError('general');?>
-        <form id="add-movementreason"  method="post" enctype="multipart/form-data" action="/form/procMovementreasonAdd">
+        <form id="add-movementreason"  method="post" enctype="multipart/form-data" action="/form/procMovementreasonAdd" class="mb-3">
             <div class="row">
                 <div class="col-lg-12">
                     <h3>Add New Reason</h3>
