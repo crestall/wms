3351fc820b753dc7bf42b9bce5b9959cd128b882
marxolly <mark@solly.com.au>
2020-08-21 15:03:12 +1000

    Update stockMovementReasons.php

--- app/views/sitesettings/stockMovementReasons.php
+++ app/views/sitesettings/stockMovementReasons.php
@@ -6,40 +6,38 @@ $role = Session::getUserRole();
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
         <?php echo Form::displayError('general');?>
-        <div class="row">
-            <form id="add-movementreason"  method="post" enctype="multipart/form-data" action="/form/procMovementreasonAdd">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h3>Add New Reason</h3>
-                    </div>
+        <form id="add-movementreason"  method="post" enctype="multipart/form-data" action="/form/procMovementreasonAdd">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h3>Add New Reason</h3>
                 </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Reason</label>
+                <div class="col-md-4">
+                    <input type="text" class="form-control required" name="name" id="name" value="<?php echo Form::value('name');?>" />
+                    <?php echo Form::displayError('name');?>
+                </div>
+            </div>
+            <?php if($role === "super admin"):?>
                 <div class="form-group row">
-                    <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Reason</label>
-                    <div class="col-md-4">
-                        <input type="text" class="form-control required" name="name" id="name" value="<?php echo Form::value('name');?>" />
-                        <?php echo Form::displayError('name');?>
-                    </div>
-                </div>
-                <?php if($role === "super admin"):?>
-                    <div class="form-group row">
-                        <div class="form-check">
-                            <label class="form-check-label col-md-3" for="locked">Locked</label>
-                            <div class="col-md-4 checkbox checkbox-default">
-                                <input class="form-check-input styled" type="checkbox" id="locked" name="locked" />
-                                <label for="locked"></label>
-                            </div>
+                    <div class="form-check">
+                        <label class="form-check-label col-md-3" for="locked">Locked</label>
+                        <div class="col-md-4 checkbox checkbox-default">
+                            <input class="form-check-input styled" type="checkbox" id="locked" name="locked" />
+                            <label for="locked"></label>
                         </div>
                     </div>
-                <?php endif;?>
-                <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
-                <div class="form-group row">
-                    <label class="col-md-3 col-form-label">&nbsp;</label>
-                    <div class="col-md-4">
-                        <button type="submit" class="btn btn-primary">Add Reason</button>
-                    </div>
                 </div>
-            </form>
-        </div>
+            <?php endif;?>
+            <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+            <div class="form-group row">
+                <label class="col-md-3 col-form-label">&nbsp;</label>
+                <div class="col-md-4">
+                    <button type="submit" class="btn btn-primary">Add Reason</button>
+                </div>
+            </div>
+        </form>
         <div id="waiting" class="row">
             <div class="col-lg-12 text-center">
                 <h2>Drawing Table..</h2>
