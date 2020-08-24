a4bc1faa98b500d5c8b997a610eb8027002feace
marxolly <mark@solly.com.au>
2020-08-24 14:10:48 +1000

    Update clientOrders.php

--- app/views/orders/clientOrders.php
+++ app/views/orders/clientOrders.php
@@ -9,8 +9,11 @@
             <div class="col-md-2">
                 <div class="input-group">
                     <input type="text" class="form-control" name="date_from" id="date_from" value="<?php echo date("d/m/Y", $from);;?>" />
-                    <span class="input-group-addon"><i class="far fa-calendar-alt"></i></span>
+                    <div class="input-group-append">
+                        <span class="input-group-text"><i class="fad fa-calendar-alt"></i></span>
+                    </div>
                 </div>
+            </div>
             </div>
             <div class="col-md-1">
                 <label>To</label>
@@ -18,11 +21,13 @@
             <div class="col-md-2">
                 <div class="input-group">
                     <input type="text" class="form-control" name="date_to" id="date_to" value="<?php echo date("d/m/Y", $to);;?>" />
-                    <span class="input-group-addon"><i class="far fa-calendar-alt"></i></span>
+                    <div class="input-group-append">
+                        <span class="input-group-text"><i class="fad fa-calendar-alt"></i></span>
+                    </div>
                 </div>
             </div>
             <div class="col-md-3">
-                <button id="change_dates" class="btn btn-small btn-primary">Change Dates</button>
+                <button id="change_dates" class="btn btn-small btn-outline-secondary">Change Dates</button>
             </div>
             <input type="hidden" id="date_from_value" name="date_from_value" value="<?php echo $from;?>" />
             <input type="hidden" id="date_to_value" name="date_to_value" value="<?php echo $to;?>" />
