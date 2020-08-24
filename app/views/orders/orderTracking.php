41433287898115ed132d18671607245ac931db94
marxolly <mark@solly.com.au>
2020-08-24 15:42:22 +1000

    Update orderTracking.php

--- app/views/orders/orderTracking.php
+++ app/views/orders/orderTracking.php
@@ -118,6 +118,25 @@ if(empty($entered_by))
                                             </div>
                                         <?php endforeach;?>
                                     <?php endif;?>
+                                <?php elseif($courier == "Direct Freight"):?>
+                                    <div class="row">
+                                        <div class="col">
+                                            <div class="feedbackbox">
+                                                <h5 class="card-subtitle">Direct Freight Tracking Goes Here</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php else:?>
+                                    <div class="row">
+                                        <div class="col">
+                                            <div class="feedbackbox">
+                                                <h5 class="card-subtitle">Local Courier Used</h5>
+                                                <div class="ml-4">
+                                                    <p>There is no tracking information available for this courier</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 <?php endif;?>
                             <?php else:?>
                                 <div class="row">
@@ -138,112 +157,6 @@ if(empty($entered_by))
                     </div>
                 </div>
             </div>
-
-            <div class="row">
-                <div class="col-lg-12">
-                    <h2>Tracking Details</h2>
-                </div>
-            </div>
-            <?php if(!is_null($order['consignment_id'])):?>
-                <?php if($courier == "eParcel" || $courier == "eParcel Express"):?>
-                    <?php if(isset($tracking['tracking_results'][0]['errors'])):?>
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="errorbox">
-                                    <h2>There was an error collecting the tracking data</h2>
-                                    <p><?php echo $tracking['tracking_results'][0]['errors'][0]['code'].": ".$tracking['tracking_results'][0]['errors'][0]['message'];?></p>
-                                    <p>Please <a href="/orders/client-orders">click here</a> to go back to the list of orders.</p>
-                                </div>
-                            </div>
-                        </div>
-                    <?php else:?>
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                            <th>Detals</th>
-                                            <th>Date &amp; Time</th>
-                                            <th>Location</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php foreach($tracking['tracking_results'][0]['trackable_items'][0]['items'][0]['events'] as $event):?>
-                                            <tr>
-                                                <td><?php echo $event['description'];?></td>
-                                                <td><?php echo date("D F j, Y, g:i a", strtotime($event['date']));?></td>
-                                                <td><?php if(isset($event['location'])) echo $event['location'];?></td>
-                                            </tr>
-                                        <?php endforeach;?>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    <?php endif;?>
-                <?php elseif($courier == "Hunters" || $courier == "HuntersPLU"):?>
-                    <?php if(isset($tracking['errorCode'])): ?>
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="errorbox">
-                                    <h2>There was an error collecting the tracking data</h2>
-                                    <p><?php echo $tracking['errorCode'].": ".$tracking['errorMessage'];?></p>
-                                    <p>Please <a href="/orders/client-orders">click here</a> to go back to the list of orders.</p>
-                                </div>
-                            </div>
-                        </div>
-                    <?php elseif(!count($tracking['events'])):?>
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="feedbackbox">
-                                    <h2>Order Still Processing</h2>
-                                    <p>No tracking events registered for that order</p>
-                                    <p>Please check back later when the details have been updated</p>
-                                    <p>Please <a href="/orders/client-orders">click here</a> to go back to the list of orders.</p>
-                                </div>
-                            </div>
-                        </div>
-                    <?php else:?>
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>Date</th>
-                                    <th>Event</th>
-                                    <th>Description</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php foreach($tracking['events'] as $e):?>
-                                    <tr>
-                                        <td><?php echo $e['when'];?></td>
-                                        <td><?php echo $e['what'];?></td>
-                                        <td><?php echo $e['info'];?></td>
-                                    </tr>
-                                <?php endforeach;?>
-                            </tbody>
-                        </table>
-                    <?php endif;?>
-                <?php else:?>
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="feedbackbox">
-                                <h2>Local Courier Used</h2>
-                                <p>There is no tracking information available for this courier</p>
-                            </div>
-                        </div>
-                    </div>
-                <?php endif;?>
-            <?php else:?>
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="errorbox">
-                            <h2>No Consignment For Order</h2>
-                            <p>The order does not have a consignment ID yet</p>
-                            <p>Maybe it has not been dispatched yet</p>
-                            <p>Please <a href="/orders/client-orders">click here</a> to go back to the list of orders to select one to track.</p>
-                        </div>
-                    </div>
-                </div>
-            <?php endif;?>
         <?php endif;?>
     </div>
 </div>
