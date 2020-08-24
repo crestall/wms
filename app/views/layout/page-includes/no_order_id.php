3523c548370784c72f4e4a4f283bb8e370be321c
marxolly <mark@solly.com.au>
2020-08-24 16:12:25 +1000

    Update no_order_id.php

--- app/views/layout/page-includes/no_order_id.php
+++ app/views/layout/page-includes/no_order_id.php
@@ -9,7 +9,7 @@
                 <div class="col-8">
                     <h2>No Order ID Supplied</h2>
                     <p>No order was supplied to update</p>
-                    <?php if(Session::getUserRole() != "client"):?>
+                    <?php if(Session::getUserRole() == "client"):?>
                         <p>Please <a href="/orders/client-orders">click here</a> to go back to the list of orders to select one to track.</p>
                     <?php else:?>
                         <p><a href="/orders/view-orders">Please click here to view all orders to choose from</a></p>
