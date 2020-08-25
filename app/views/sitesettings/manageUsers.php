6e28e41e0397ecdf3298ffd5f4a3c30d14332c0e
marxolly <mark@solly.com.au>
2020-08-26 09:57:37 +1000

    Update manageUsers.php

--- app/views/sitesettings/manageUsers.php
+++ app/views/sitesettings/manageUsers.php
@@ -23,9 +23,9 @@
                             <?php echo $name;?> Users
                         </div>
                         <div class="card-body">
-                            <div class="row">
+                            <div class="row border-bottom border-secondary mb-3">
                                 <label class="col-3">Filter</label>
-                                <div class="col-9">
+                                <div class="col-9 mb-3">
                                     <input class="form-control" type="text" id="<?php echo $id_name;?>" placeholder="Filter By Name" />
                                 </div>
                             </div>
