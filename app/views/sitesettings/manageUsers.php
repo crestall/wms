f5afaf82e4c528a8115a9dd48f22732795a2c5cd
marxolly <mark@solly.com.au>
2020-08-26 08:53:42 +1000

    Update manageUsers.php

--- app/views/sitesettings/manageUsers.php
+++ app/views/sitesettings/manageUsers.php
@@ -5,10 +5,10 @@
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <div class="row">
-            <div class="col-md-3"><p><a href="/user/add-user" class="btn btn-outline-fsg">Add New User</a></p></div>
-            <div class="col-md-3"><p><a href="/site-settings/manage-users/" class="btn btn-outline-success">View Only Active Users</a></p></div>
-            <div class="col-md-3"><p><a href="/site-settings/manage-users/active=0" class="btn btn-outline-danger">View Only Inactive Users</a></p></div>
-            <div class="col-md-3"><p><a href="/site-settings/manage-users/active=-1" class="btn btn-outline-warning">View All Users</a></p></div>
+            <div class="col-md-3 col-sm-6"><p><a href="/user/add-user" class="btn btn-outline-fsg">Add New User</a></p></div>
+            <div class="col-md-3 col-sm-6"><p><a href="/site-settings/manage-users/" class="btn btn-outline-success">View Only Active Users</a></p></div>
+            <div class="col-md-3 col-sm-6"><p><a href="/site-settings/manage-users/active=0" class="btn btn-outline-danger">View Only Inactive Users</a></p></div>
+            <div class="col-md-3 col-sm-6"><p><a href="/site-settings/manage-users/active=-1" class="btn btn-outline-warning">View All Users</a></p></div>
         </div>
         <div id="waiting" class="row">
             <div class="col-lg-12 text-center">
