e1cb9509f835cf50a5debddccac38273c5a32725
marxolly <mark@solly.com.au>
2020-08-25 15:04:20 +1000

    Update controllerIndex.php

--- app/views/index/controllerIndex.php
+++ app/views/index/controllerIndex.php
@@ -13,6 +13,7 @@ $resource = strtolower(str_replace(" ", "", $page_name));
                 if(!$details['display']) continue;
                 $SectionName = ucwords(str_replace("-", " ", $section_name));
                 $action = Utility::toCamelCase($SectionName);
+                echo "<pre>",print_r(Permission::$perms),"</pre>";
                 echo "<p>$action - </p>";
                 if(Permission::check($role, $resource, $action)):?>
                     <div class="card homepagecard">
