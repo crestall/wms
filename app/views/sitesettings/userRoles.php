23c5f1e1e5811bd6881ea5d61818cda3e10363d3
marxolly <mark@solly.com.au>
2020-08-26 11:56:07 +1000

    Update userRoles.php

--- app/views/sitesettings/userRoles.php
+++ app/views/sitesettings/userRoles.php
@@ -6,29 +6,27 @@
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
         <?php echo Form::displayError('general');?>
-        <div class="row">
-            <form id="add-userrole"  method="post" enctype="multipart/form-data" action="/form/procUserRoleAdd">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h3>Add New Role</h3>
-                    </div>
+        <form id="add-userrole"  method="post" enctype="multipart/form-data" action="/form/procUserRoleAdd">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h3>Add New Role</h3>
                 </div>
-                <div class="form-group row">
-                    <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Name</label>
-                    <div class="col-md-4">
-                        <input type="text" class="form-control required" name="name" id="name" value="<?php echo Form::value('name');?>" />
-                        <?php echo Form::displayError('name');?>
-                    </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Name</label>
+                <div class="col-md-4">
+                    <input type="text" class="form-control required" name="name" id="name" value="<?php echo Form::value('name');?>" />
+                    <?php echo Form::displayError('name');?>
                 </div>
-                <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
-                <div class="form-group row">
-                    <label class="col-md-3 col-form-label">&nbsp;</label>
-                    <div class="col-md-4">
-                        <button type="submit" class="btn btn-primary">Add Role</button>
-                    </div>
+            </div>
+            <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+            <div class="form-group row">
+                <label class="col-md-3 col-form-label">&nbsp;</label>
+                <div class="col-md-4">
+                    <button type="submit" class="btn btn-outline-secondary">Add Role</button>
                 </div>
-            </form>
-        </div>
+            </div>
+        </form>
         <div class="row">
             <div class="col-lg-12">
                 <h2>Current User Roles</h2>
