3dee8e4d58b3c484561d694f9efb36baf71c0cff
marxolly <mark@solly.com.au>
2020-08-28 15:05:37 +1000

    Update importOrders.php

--- app/views/orders/importOrders.php
+++ app/views/orders/importOrders.php
@@ -36,14 +36,9 @@
                     <?php echo Form::displayError('csv_file');?>
                 </div>
             </div>
-            <div class="form-group row">
-                <div class="form-check">
-                    <label class="form-check-label col-md-3" for="header_row">My CSV has a header row</label>
-                    <div class="col-md-4 checkbox checkbox-default">
-                        <input class="form-check-input styled" type="checkbox" id="header_row" name="header_row" checked />
-                        <label for="header_row"></label>
-                    </div>
-                </div>
+            <div class="form-group row custom-control custom-checkbox custom-control-right">
+                <input class="custom-control-input" type="checkbox" id="header_row" name="header_row" checked />
+                <label class="custom-control-label col-md-3" for="header_row">My CSV has a header row</label>
             </div>
             <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
             <div class="form-group row">
