322e1a3eeb187002cc36bc968c430949b880c677
marxolly <mark@solly.com.au>
2020-08-28 11:01:28 +1000

    Update FormController.php

--- app/controllers/FormController.php
+++ app/controllers/FormController.php
@@ -136,7 +136,7 @@ class FormController extends Controller {
 
     public function procDFCollection()
     {
-        echo "<pre>",print_r($this->request->data),"</pre>"; die();
+        //echo "<pre>",print_r($this->request->data),"</pre>"; die();
         $post_data = array();
         $request = array();
         foreach($this->request->data as $field => $value)
