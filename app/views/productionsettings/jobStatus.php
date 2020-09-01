037d4870e0c8b06bf2ca322678c70722d8027edb
marxolly <mark@solly.com.au>
2020-09-01 12:36:23 +1000

    Update jobStatus.php

--- app/views/productionsettings/jobStatus.php
+++ app/views/productionsettings/jobStatus.php
@@ -1,7 +1,6 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-        <?php echo "<pre>",print_r($status),"</pre>";?>
         <?php echo Form::displayError('general');?>
         <form id="add-courier"  method="post" enctype="multipart/form-data" action="/form/procJobStatusAdd">
             <div class="row">
@@ -30,5 +29,18 @@
                 <h2>Currently Available Status</h2>
             </div>
         </div>
+        <?php if(count($status)):?>
+            <?php echo "<pre>",print_r($status),"</pre>";?>
+            <?php //foreach($couriers as $c):?>
+        <?php else:?>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="errorbox">
+                        <h2><i class="fas fa-exclamation-triangle"></i> No Status Listed</h2>
+                        <p>You will need to add some first</p>
+                    </div>
+                </div>
+            </div>
+        <?php endif;?>
     </div>
 </div>
