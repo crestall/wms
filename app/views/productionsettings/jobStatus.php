c03363ae7d7b0cd3bb15cf3a2d97984e12462a8a
marxolly <mark@solly.com.au>
2020-09-01 12:30:36 +1000

    Update jobStatus.php

--- app/views/productionsettings/jobStatus.php
+++ app/views/productionsettings/jobStatus.php
@@ -2,5 +2,33 @@
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <?php echo "<pre>",print_r($status),"</pre>";?>
+        <?php echo Form::displayError('general');?>
+        <form id="add-courier"  method="post" enctype="multipart/form-data" action="/form/procJobStatusAdd">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h3>Add Job Status</h3>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Name</label>
+                <div class="col-md-4">
+                    <input type="text" class="form-control required" name="name" id="name" value="<?php echo Form::value('name');?>" />
+                    <?php echo Form::displayError('name');?>
+                </div>
+                <span class="inst">Names <span class="font-italic font-weight-bold">must</span> be unique</span>
+            </div>
+            <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+            <div class="form-group row">
+                <label class="col-md-3 col-form-label">&nbsp;</label>
+                <div class="col-md-4">
+                    <button type="submit" class="btn btn-outline-secondary">Add Job Status</button>
+                </div>
+            </div>
+        </form>
+        <div class="row">
+            <div class="col-lg-12">
+                <h2>Current Status</h2>
+            </div>
+        </div>
     </div>
 </div>
