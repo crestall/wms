70030812ed85a5085acff73099145d37595a9be0
marxolly <mark@solly.com.au>
2020-09-01 13:50:01 +1000

    Update jobStatus.php

--- app/views/productionsettings/jobStatus.php
+++ app/views/productionsettings/jobStatus.php
@@ -1,6 +1,7 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?> 
         <?php echo Form::displayError('general');?>
         <form id="add-job-status"  method="post" enctype="multipart/form-data" action="/form/procJobStatusAdd">
             <div class="row">
