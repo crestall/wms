27bc101c6defdb4a82d1ca0e0157bd7be006ac96
marxolly <mark@solly.com.au>
2020-09-01 12:33:53 +1000

    Update jobStatus.php

--- app/views/productionsettings/jobStatus.php
+++ app/views/productionsettings/jobStatus.php
@@ -27,7 +27,7 @@
         </form>
         <div class="row">
             <div class="col-lg-12">
-                <h2>Current Status</h2>
+                <h2>Currently Available Status</h2>
             </div>
         </div>
     </div>
