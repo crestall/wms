c60959a9aef7ba13738ec22cddcb715b49ec7842
marxolly <mark@solly.com.au>
2020-09-03 12:33:11 +1000

    Update addSupplier.php

--- app/views/suppliers/addSupplier.php
+++ app/views/suppliers/addSupplier.php
@@ -2,5 +2,13 @@
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
+        <form id="add_production_supplier" method="post" action="/form/procAddProductionSupplier">
+            <div class="form-group row">
+                <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Name</label>
+                <div class="col-md-4">
+                    <input type="text" class="form-control required" name="name" id="name" value="<?php echo Form::value('name');?>" />
+                </div>
+            </div>
+        </form>
     </div>
 </div>
