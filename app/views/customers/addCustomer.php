7eb16df2ac70b4918a456215dd0fdc9fbc4df51b
marxolly <mark@solly.com.au>
2020-09-04 12:08:12 +1000

    Update addCustomer.php

--- app/views/customers/addCustomer.php
+++ app/views/customers/addCustomer.php
@@ -10,7 +10,7 @@ $country = Form::value('country');
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
-        <form id="add_production_supplier" method="post" action="/form/procAddProductionSupplier">
+        <form id="add_production_customer" method="post" action="/form/procAddProductionCustomer">
             <div class="form-group row">
                 <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Name</label>
                 <div class="col-md-4">
@@ -19,10 +19,9 @@ $country = Form::value('country');
                 </div>
             </div>
             <div class="form-group row">
-                <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Contact</label>
+                <label class="col-md-3">Contact</label>
                 <div class="col-md-4">
-                    <input type="text" class="form-control required" name="contact" id="contact" value="<?php echo Form::value('contact');?>" />
-                    <?php echo Form::displayError('contact');?>
+                    <input type="text" class="form-control" name="contact" id="contact" value="<?php echo Form::value('contact');?>" />
                 </div>
             </div>
             <div class="form-group row">
@@ -32,17 +31,17 @@ $country = Form::value('country');
                 </div>
             </div>
             <div class="form-group row">
-                <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Email</label>
+                <label class="col-md-3">Email</label>
                 <div class="col-md-4">
-                    <input type="text" class="form-control required email" name="email" id="email" value="<?php echo Form::value('email');?>" />
+                    <input type="text" class="form-control email" name="email" id="email" value="<?php echo Form::value('email');?>" />
                     <?php echo Form::displayError('email');?>
                 </div>
             </div>
-            <?php include(Config::get('VIEWS_PATH')."forms/address.php");?>
+            <?php include(Config::get('VIEWS_PATH')."forms/address_nr.php");?>
             <div class="form-group row">
                 <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
                 <div class="col-md-4 offset-md-3">
-                    <button type="submit" class="btn btn-outline-secondary">Add This Supplier</button>
+                    <button type="submit" class="btn btn-outline-secondary">Add This Customer</button>
                 </div>
             </div>
         </form>
