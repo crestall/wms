3635063fb92143649e057988493ac9b38653722b
marxolly <mark@solly.com.au>
2020-09-04 12:39:35 +1000

    Update FormController.php

--- app/controllers/FormController.php
+++ app/controllers/FormController.php
@@ -142,7 +142,7 @@ class FormController extends Controller {
 
     public function procAddProductionCustomer()
     {
-        echo "<pre>",print_r($this->request->data),"</pre>"; die();
+        //echo "<pre>",print_r($this->request->data),"</pre>"; die();
         $post_data = array();
         foreach($this->request->data as $field => $value)
         {
