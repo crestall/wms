fd99614db577c54f4a687531e92bdf6dbdb64f78
marxolly <mark@solly.com.au>
2020-09-04 15:50:38 +1000

    Update addJob.php

--- app/views/jobs/addJob.php
+++ app/views/jobs/addJob.php
@@ -50,7 +50,7 @@ $country = Form::value('country');
                 </div>
                 <div class="row">
                     <div class="col">
-                        <a class="btn btn-outline-fsg" data-toggle="collapse" href="#customer_address" role="button" aria-expanded="false" aria-controls="customer_address">Show Address</button>
+                        <a class="btn btn-outline-fsg" data-toggle="collapse" href="#customer_address" role="button" aria-expanded="false" aria-controls="customer_address">Show Address</a>
                     </div>
                 </div>
                 <div id="customer_address" class="collapse">
