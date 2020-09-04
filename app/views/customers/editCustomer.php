baeeb1eb2c55dcc21139efabc8da95d5fde91c73
marxolly <mark@solly.com.au>
2020-09-04 14:26:29 +1000

    Update editCustomer.php

--- app/views/customers/editCustomer.php
+++ app/views/customers/editCustomer.php
@@ -14,7 +14,7 @@ $country    = empty(Form::value('country'))?    $customer['country']      : Form
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
-        <form id="edit_production_supplier" method="post" action="/form/procEditProductionCustomer">
+        <form id="edit_production_customer" method="post" action="/form/procEditProductionCustomer">
             <div class="form-group row">
                 <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Name</label>
                 <div class="col-md-4">
