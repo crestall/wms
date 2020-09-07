3b6b0f013559139bf3c0ef0c732f47f402d9d893
marxolly <mark@solly.com.au>
2020-09-07 11:08:38 +1000

    Update AjaxfunctionsController.php

--- app/controllers/AjaxfunctionsController.php
+++ app/controllers/AjaxfunctionsController.php
@@ -919,7 +919,7 @@ class ajaxfunctionsController extends Controller
     public function getJobCustomer()
     {
         //echo "<pre>",print_r($this->request),"</pre>";
-        $data = $this->productioncustomer->getAutocompleteCustomer($this->request->query);
+        $data = $this->productioncustomer->getAutocompleteCustomer($this->request->query['customer_name']);
         $this->view->renderJson($data);
     }
 
