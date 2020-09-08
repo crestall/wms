7ccbd2ab2f2d965db59e4024736c84d1bf1762cc
marxolly <mark@solly.com.au>
2020-09-08 11:29:04 +1000

    Update addJob.php

--- app/views/jobs/addJob.php
+++ app/views/jobs/addJob.php
@@ -154,25 +154,24 @@ $date_ed = (empty(Form::value('date_ed_value')))? "" : date('d/m/Y', Form::value
                     <input type="hidden" name="date_ed_value" id="date_ed_value" value="<?php echo Form::value('date_ed_value');?>" />
                 </div>
                 <div class="form-group row mb-3">
-                    <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Customer Name</label>
+                    <label class="col-md-3">Supplier Name</label>
                     <div class="col-md-4">
-                        <input type="text" class="required form-control" name="customer_name" id="customer_name" value="<?php echo Form::value('customer_name');?>" />
-                        <input type="hidden" name="customer_id" id="customer_id" value="0" />
-                        <?php echo Form::displayError('customer_name');?>
+                        <input type="text" class="required form-control" name="supplier_name" id="supplier_name" value="<?php echo Form::value('supplier_name');?>" />
+                        <input type="hidden" name="supplier_id" id="supplier_id" value="0" />
                     </div>
                 </div>
                 <div class="form-group row ">
                     <label class="col-lg-1 col-md-2 mb-md-3">Contact</label>
                     <div class="col-lg-3 col-md-4">
-                        <input type="text" class="form-control customer" name="customer_contact" id="customer_contact" value="<?php echo Form::value('customer_contact');?>" />
+                        <input type="text" class="form-control supplier" name="supplier_contact" id="supplier_contact" value="<?php echo Form::value('supplier_contact');?>" />
                     </div>
                     <label class="col-lg-1 col-md-2">Email</label>
                     <div class="col-lg-3 col-md-4">
-                        <input type="text" class="form-control customer" name="customer_email" id="customer_email" value="<?php echo Form::value('customer_email');?>" />
+                        <input type="text" class="form-control supplier" name="supplier_email" id="supplier_email" value="<?php echo Form::value('supplier_email');?>" />
                     </div>
                     <label class="col-lg-1 col-md-2">Phone</label>
                     <div class="col-lg-3 col-md-4">
-                        <input type="text" class="form-control customer" name="customer_phone" id="customer_phone" value="<?php echo Form::value('customer_phone');?>" />
+                        <input type="text" class="form-control supplier" name="supplier_phone" id="supplier_phone" value="<?php echo Form::value('supplier_phone');?>" />
                     </div>
                 </div>
                 <div class="row">
@@ -184,48 +183,48 @@ $date_ed = (empty(Form::value('date_ed_value')))? "" : date('d/m/Y', Form::value
                     <div class="form-group row">
                         <label class="col-md-3 col-form-label">Address Line 1</label>
                         <div class="col-md-4">
-                            <input type="text" class="form-control customer" name="customer_address" id="customer_address" value="<?php echo Form::value('customer_address');?>" />
-                            <?php echo Form::displayError('customer_address');?>
+                            <input type="text" class="form-control supplier" name="supplier_address" id="supplier_address" value="<?php echo Form::value('supplier_address');?>" />
+                            <?php echo Form::displayError('supplier_address');?>
                         </div>
                         <div class="col-md-3 checkbox checkbox-default">
-                            <input class="form-check-input styled" type="checkbox" id="ignore_address_error" name="ignore_address_error" />
-                            <label for="ignore_address_error">No need for a number</label>
+                            <input class="form-check-input styled" type="checkbox" id="ignore_supplier_address_error" name="ignore_supplier_address_error" />
+                            <label for="ignore_supplier_address_error">No need for a number</label>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-3 col-form-label">Address Line 2</label>
                         <div class="col-md-4">
-                            <input type="text" class="form-control customer" name="customer_address2" id="customer_address2" value="<?php echo Form::value('customer_address2');?>" />
+                            <input type="text" class="form-control supplier" name="supplier_address2" id="supplier_address2" value="<?php echo Form::value('supplier_address2');?>" />
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-3 col-form-label">Suburb/Town</label>
                         <div class="col-md-4">
-                            <input type="text" class="form-control customer" name="customer_suburb" id="customer_suburb" value="<?php echo Form::value('customer_suburb');?>" />
-                            <?php echo Form::displayError('customer_suburb');?>
+                            <input type="text" class="form-control supplier" name="supplier_suburb" id="supplier_suburb" value="<?php echo Form::value('supplier_suburb');?>" />
+                            <?php echo Form::displayError('supplier_suburb');?>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-3 col-form-label">State</label>
                         <div class="col-md-4">
-                            <input type="text" class="form-control customer" name="customer_state" id="customer_state" value="<?php echo Form::value('customer_state');?>" />
+                            <input type="text" class="form-control supplier" name="supplier_state" id="supplier_state" value="<?php echo Form::value('supplier_state');?>" />
                             <span class="inst">for AU addresses use VIC, NSW, QLD, ACT, TAS, WA, SA, NT only</span>
-                            <?php echo Form::displayError('customer_state');?>
+                            <?php echo Form::displayError('supplier_state');?>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-3 col-form-label">Postcode</label>
                         <div class="col-md-4">
-                            <input type="text" class="form-control customer" name="customer_postcode" id="customer_postcode" value="<?php echo Form::value('customer_postcode');?>" />
-                            <?php echo Form::displayError('customer_postcode');?>
+                            <input type="text" class="form-control supplier" name="supplier_postcode" id="supplier_postcode" value="<?php echo Form::value('supplier_postcode');?>" />
+                            <?php echo Form::displayError('supplier_postcode');?>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-3 col-form-label">Country</label>
                         <div class="col-md-4">
-                            <input type="text" class="form-control customer" name="customer_country" id="customer_country" value="<?php echo Form::value('customer_country');?>" />
+                            <input type="text" class="form-control supplier" name="supplier_country" id="supplier_country" value="<?php echo Form::value('supplier_country');?>" />
                             <span class="inst">use the 2 letter ISO code</span>
-                            <?php echo Form::displayError('customer_country');?>
+                            <?php echo Form::displayError('supplier_country');?>
                         </div>
                     </div>
                 </div>
