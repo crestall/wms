c1beef3ac4b890cbbd97207f81a1cdb9ba33cabe
marxolly <mark@solly.com.au>
2020-09-11 09:34:47 +1000

    Update viewJobs.php

--- app/views/jobs/viewJobs.php
+++ app/views/jobs/viewJobs.php
@@ -1,11 +1,37 @@
 <div id="page-wrapper">
-    <div id="page_container" class="container-xl">
+    <div id="page_container" class="container-xxl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <?php if(count($jobs)):?>
-            <div class="row">
-                <div class="col-12">
-                    <?php echo "<pre>",print_r($jobs),"</pre>";?>
+            <div id="waiting" class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Drawing Table..</h2>
+                    <p>May take a few moments</p>
+                    <img class='loading' src='/images/preloader.gif' alt='loading...' />
                 </div>
+            </div>
+            <div class="row" id="table_holder" style="display:none">
+                <div class="col-12">
+                    <table class="table-striped table-hover" id="production_jobs_table">
+                        <thead>
+                            <tr>
+                                <th>Job Number</th>
+                                <th>Related Job</th>
+                                <th>Client</th>
+                                <th>Description</th>
+                                <th>Notes</th>
+                                <th>Status</th>
+                                <th>Sales Rep</th>
+                                <th>Supplier</th>
+                                <th>Date Entered</th>
+                                <th>Due Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
         <?php else:?>
                 <div class="row">
