918af789a4e029f93a50431c2ae72e6cc9f748d8
marxolly <mark@solly.com.au>
2020-09-11 08:50:30 +1000

    Update viewJobs.php

--- app/views/jobs/viewJobs.php
+++ app/views/jobs/viewJobs.php
@@ -1,5 +1,20 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+        <?php if(count($jobs)):?>
+            <div class="row">
+                <div class="col-12">
+                    <?php echo "<pre>",print_r($jobs),"</pre>";?>
+                </div>
+            </div>
+        <?php else:?>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="errorbox">
+                            <h2><i class="fas fa-exclamation-triangle"></i> No Jobs Listed</h2>
+                        </div>
+                    </div>
+                </div>
+        <?php endif;?>
     </div>
 </div>
