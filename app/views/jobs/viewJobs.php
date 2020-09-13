6d795a01935e21a06ae80a80706ef38e3aa8259c
marxolly <mark@solly.com.au>
2020-09-14 09:12:28 +1000

    Update viewJobs.php

--- app/views/jobs/viewJobs.php
+++ app/views/jobs/viewJobs.php
@@ -64,12 +64,29 @@ function getCustomerPopOver($job)
                                     <td data-label="Related Job" class="number"><?php echo $job['previous_job_id'];?></td>
                                     <td data-label="Client">
                                         <?php if($user_role == "production_admin"):?>
-                                            <a href="/customers/edit-customer/customer=<?php echo $job['customer_id'];?>"><?php echo $job['customer_name'];?></a>
+                                            <h5><a href="/customers/edit-customer/customer=<?php echo $job['customer_id'];?>"><?php echo $job['customer_name'];?></a></h5>
                                         <?php else:?>
                                             <?php echo $job['customer_name'];?>
                                         <?php endif;?>
                                         <div class="contact_details mb-3 ">
-
+                                            <div class='row'>
+                                                <label class='col-4'>Contact</label>
+                                                <div class='col-8'>
+                                                    <?php echo $job['customer_contact'];?>
+                                                </div>
+                                            </div>
+                                            <div class='row'>
+                                                <label class='col-4'>Email</label>
+                                                <div class='col-8'>
+                                                    <?php echo$job['customer_email';?>
+                                                </div>
+                                            </div>
+                                            <div class='row'>
+                                                <label class='col-4'>Phone</label>
+                                                <div class='col-8'>
+                                                    <?php echo$job['customer_phone';?>
+                                                </div>
+                                            </div>
                                         </div>
                                     </td>
                                     <td data-label="Description"><?php echo $job['description'];?></td>
