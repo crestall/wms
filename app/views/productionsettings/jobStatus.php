4ccb238ce11df9db174ca346e9a1cb79c7c739d9
marxolly <mark@solly.com.au>
2020-09-14 14:05:46 +1000

    Update jobStatus.php

--- app/views/productionsettings/jobStatus.php
+++ app/views/productionsettings/jobStatus.php
@@ -67,7 +67,7 @@
                         <div class="col-md-2">
                             <label class="col-form-label">Colour</label>
                             <div class="colour-picker input-group">
-                                <input type="text" class="form-control form-control-sm" name="colour" id="colour" value="<?php echo $s['colour'];?>" >
+                                <input type="text" class="form-control" name="colour" id="colour" value="<?php echo $s['colour'];?>" style='font-size: 0.7rem;' >
                                 <div class="input-group-append">
                                     <span class="input-group-text colorpicker-input-addon"><i></i></span>
                                 </div>
