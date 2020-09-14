5e4e8a35e6616caa9cb433939f3e63363ec118ab
marxolly <mark@solly.com.au>
2020-09-14 14:30:51 +1000

    Update footer.php

--- app/views/layout/productionsettings/footer.php
+++ app/views/layout/productionsettings/footer.php
@@ -36,7 +36,26 @@
                             $('input.default_checkbox').not(this).prop("checked", false);
                         });
                         $('.colour-picker').colorpicker({
-                            format: 'rgba'
+                            format: 'rgba',
+                            extensions: [
+                                {
+                                    name: 'swatches', // extension name to load
+                                    options: { // extension options
+                                        colors: {
+                                            'black': '#000000',
+                                            'gray': '#888888',
+                                            'white': '#ffffff',
+                                            'red': 'red',
+                                            'default': '#777777',
+                                            'primary': '#337ab7',
+                                            'success': '#5cb85c',
+                                            'info': '#5bc0de',
+                                            'warning': '#f0ad4e',
+                                            'danger': '#d9534f'
+                                        }
+                                    }
+                                }
+                            ]
                         });
                     }
                 },
