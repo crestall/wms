e16d2bc4ec5c41b0bc586a74cea65acac7794120
marxolly <mark@solly.com.au>
2020-09-15 14:34:54 +1000

    Update updateJob.php

--- app/views/jobs/updateJob.php
+++ app/views/jobs/updateJob.php
@@ -4,8 +4,49 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-        <?php echo "JOB<pre>",print_r($job),"</pre>";?>
-        <?php echo "CUSTOMER<pre>",print_r($customer),"</pre>";?>
-        <?php echo "SUPPLIER<pre>",print_r($supplier),"</pre>";?>
+        <?php //echo "JOB<pre>",print_r($job),"</pre>";?>
+        <?php //echo "CUSTOMER<pre>",print_r($customer),"</pre>";?>
+        <?php //echo "SUPPLIER<pre>",print_r($supplier),"</pre>";?>
+        <div class="row">
+            <div class="col-sm-12 col-md-6 mb-3">
+                <div class="card h-100 border-secondary order-card">
+                    <div class="card-header bg-secondary text-white">
+                        Job Details
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                    <div class="card-footer text-right">
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6 mb-3">
+                <div class="card h-100 border-secondary order-card">
+                    <div class="card-header bg-secondary text-white">
+                        Customer Details
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                    <div class="card-footer text-right">
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6 mb-3">
+                <div class="card h-100 border-secondary order-card">
+                    <div class="card-header bg-secondary text-white">
+                        Supplier Details
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                    <div class="card-footer text-right">
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
