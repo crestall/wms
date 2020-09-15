f302960f90a5d17e8484de15831293ca7a6a805b
marxolly <mark@solly.com.au>
2020-09-15 14:57:20 +1000

    Update updateJob.php

--- app/views/jobs/updateJob.php
+++ app/views/jobs/updateJob.php
@@ -1,6 +1,7 @@
 <?php
 $job_id = (!empty(Form::value('job_id')))? Form::value('job_id'):$job['job_id'];
 $previous_job_id = (!empty(Form::value('previous_job_id')))? Form::value('previous_job_id'):$job['previous_job_id'];
+$status_id = (!empty(Form::value('status_id')))? Form::value('status_id'):$job['status_id'];
 ?>
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
@@ -16,15 +17,29 @@ $previous_job_id = (!empty(Form::value('previous_job_id')))? Form::value('previo
                     </div>
                     <div class="card-body">
                         <div class="form-group row">
-                            <label class="col-md-4">Job Id</label>
+                            <label class="col-md-4"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Job Id</label>
                             <div class="col-md-8">
                                 <input type="text" class="form-control required number" name="job_id" id="job_id" value="<?php echo $job_id;?>" />
+                                <?php echo Form::displayError('job_id');?>
                             </div>
                         </div>
                         <div class="form-group row">
                             <label class="col-md-4">Related Job Id</label>
                             <div class="col-md-8">
                                 <input type="text" class="form-control number" name="previous_job_id" id="previous_job_id" value="<?php echo $previous_job_id;?>" />
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-4">Related Job Id</label>
+                            <div class="col-md-8">
+                                <input type="text" class="form-control number" name="previous_job_id" id="previous_job_id" value="<?php echo $previous_job_id;?>" />
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-4"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Status</label>
+                            <div class="col-md-8">
+                                <select id="status_id" class="form-control selectpicker" name="status_id" data-style="btn-outline-secondary"><option value="0">-- Select One --</option><?php echo $this->controller->jobstatus->getSelectJobStatus(Form::value('status_id'));?></select>
+                                <?php echo Form::displayError('status_id');?>
                             </div>
                         </div>
                     </div>
