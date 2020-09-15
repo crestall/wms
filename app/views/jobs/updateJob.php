82cac6b3cfda3d4dcb1a61c9065e1a32602426c3
marxolly <mark@solly.com.au>
2020-09-15 14:15:35 +1000

    Update updateJob.php

--- app/views/jobs/updateJob.php
+++ app/views/jobs/updateJob.php
@@ -4,6 +4,8 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-
+        <?php echo "JOB<pre>",print_r($job),"</pre>";?>
+        <?php echo "CUSTOMER<pre>",print_r($customer),"</pre>";?>
+        <?php echo "SUPPLIER<pre>",print_r($supplier),"</pre>";?>
     </div>
 </div>
