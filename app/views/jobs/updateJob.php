439bc901164e3b1cb4253a3726c92bf4834d0c3e
marxolly <mark@solly.com.au>
2020-09-16 14:03:13 +1000

    Update updateJob.php

--- app/views/jobs/updateJob.php
+++ app/views/jobs/updateJob.php
@@ -119,84 +119,87 @@ $customer_phone = (!empty(Form::value('customer_phone')))? Form::value('customer
                         Customer Details
                     </div>
                     <div class="card-body">
-                        <div class="form-group row">
-                            <label class="col-md-4"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Customer Name</label>
-                            <div class="col-md-8">
-                                <input type="text" class="required form-control" name="customer_name" id="customer_name" value="<?php echo $customer_name;?>" />
-                                <input type="hidden" name="customer_id" id="customer_id" value="0" />
-                                <?php echo Form::displayError('customer_name');?>
-                            </div>
-                        </div>
-                        <div class="form-group row ">
-                            <label class="col-md-4">Contact</label>
-                            <div class="col-md-8">
-                                <input type="text" class="form-control customer" name="customer_contact" id="customer_contact" value="<?php echo $customer_contact;?>" />
-                            </div>
-                        </div>
-                        <div class="form-group row ">
-                            <label class="col-md-4">Email</label>
-                            <div class="col-md-8">
-                                <input type="text" class="form-control customer email" name="customer_email" id="customer_email" value="<?php echo $customer_email;?>" />
-                                <?php echo Form::displayError('customer_email');?>
-                            </div>
-                        </div>
-                        <div class="form-group row ">
-                            <label class="col-md-4">Phone</label>
-                            <div class="col-md-8">
-                                <input type="text" class="form-control customer" name="customer_phone" id="customer_phone" value="<?php echo $customer_phone;?>" />
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-md-4 col-form-label">Address Line 1</label>
-                            <div class="col-md-8">
-                                <input type="text" class="form-control customer" name="customer_address" id="customer_address" value="<?php echo Form::value('customer_address');?>" /><br>
-                                <div class="checkbox checkbox-default" style="margin-left:20px;margin-top:-25px">
-                                    <input class="form-check-input styled" type="checkbox" id="ignore_customer_address_error" name="ignore_customer_address_error" />
-                                    <label for="ignore_customer_address_error"><span class="inst">No need for a number</span></label>
+                        <form id="customer_details_update" method="post" action="/form/jobCustomerUpdate">
+                            <div class="form-group row">
+                                <label class="col-md-4"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Customer Name</label>
+                                <div class="col-md-8">
+                                    <input type="text" class="required form-control" name="customer_name" id="customer_name" value="<?php echo $customer_name;?>" />
+                                    <input type="hidden" name="customer_id" id="customer_id" value="<?php echo $customer['id'];?>" />
+                                    <?php echo Form::displayError('customer_name');?>
                                 </div>
-                                <?php echo Form::displayError('customer_address');?>
                             </div>
+                            <div class="form-group row ">
+                                <label class="col-md-4">Contact</label>
+                                <div class="col-md-8">
+                                    <input type="text" class="form-control customer" name="customer_contact" id="customer_contact" value="<?php echo $customer_contact;?>" />
+                                </div>
+                            </div>
+                            <div class="form-group row ">
+                                <label class="col-md-4">Email</label>
+                                <div class="col-md-8">
+                                    <input type="text" class="form-control customer email" name="customer_email" id="customer_email" value="<?php echo $customer_email;?>" />
+                                    <?php echo Form::displayError('customer_email');?>
+                                </div>
+                            </div>
+                            <div class="form-group row ">
+                                <label class="col-md-4">Phone</label>
+                                <div class="col-md-8">
+                                    <input type="text" class="form-control customer" name="customer_phone" id="customer_phone" value="<?php echo $customer_phone;?>" />
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-md-4 col-form-label">Address Line 1</label>
+                                <div class="col-md-8">
+                                    <input type="text" class="form-control customer" name="customer_address" id="customer_address" value="<?php echo Form::value('customer_address');?>" /><br>
+                                    <div class="checkbox checkbox-default" style="margin-left:20px;margin-top:-25px">
+                                        <input class="form-check-input styled" type="checkbox" id="ignore_customer_address_error" name="ignore_customer_address_error" />
+                                        <label for="ignore_customer_address_error"><span class="inst">No need for a number</span></label>
+                                    </div>
+                                    <?php echo Form::displayError('customer_address');?>
+                                </div>
 
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-md-4 col-form-label">Address Line 2</label>
-                            <div class="col-md-8">
-                                <input type="text" class="form-control customer" name="customer_address2" id="customer_address2" value="<?php echo Form::value('customer_address2');?>" />
                             </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-md-4 col-form-label">Suburb/Town</label>
-                            <div class="col-md-8">
-                                <input type="text" class="form-control customer" name="customer_suburb" id="customer_suburb" value="<?php echo Form::value('customer_suburb');?>" />
-                                <?php echo Form::displayError('customer_suburb');?>
+                            <div class="form-group row">
+                                <label class="col-md-4 col-form-label">Address Line 2</label>
+                                <div class="col-md-8">
+                                    <input type="text" class="form-control customer" name="customer_address2" id="customer_address2" value="<?php echo Form::value('customer_address2');?>" />
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-md-4 col-form-label">State</label>
-                            <div class="col-md-8">
-                                <input type="text" class="form-control customer" name="customer_state" id="customer_state" value="<?php echo Form::value('customer_state');?>" />
-                                <span class="inst">for AU addresses use VIC, NSW, QLD, ACT, TAS, WA, SA, NT only</span>
-                                <?php echo Form::displayError('customer_state');?>
+                            <div class="form-group row">
+                                <label class="col-md-4 col-form-label">Suburb/Town</label>
+                                <div class="col-md-8">
+                                    <input type="text" class="form-control customer" name="customer_suburb" id="customer_suburb" value="<?php echo Form::value('customer_suburb');?>" />
+                                    <?php echo Form::displayError('customer_suburb');?>
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-md-4 col-form-label">Postcode</label>
-                            <div class="col-md-8">
-                                <input type="text" class="form-control customer" name="customer_postcode" id="customer_postcode" value="<?php echo Form::value('customer_postcode');?>" />
-                                <?php echo Form::displayError('customer_postcode');?>
+                            <div class="form-group row">
+                                <label class="col-md-4 col-form-label">State</label>
+                                <div class="col-md-8">
+                                    <input type="text" class="form-control customer" name="customer_state" id="customer_state" value="<?php echo Form::value('customer_state');?>" />
+                                    <span class="inst">for AU addresses use VIC, NSW, QLD, ACT, TAS, WA, SA, NT only</span>
+                                    <?php echo Form::displayError('customer_state');?>
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-md-4 col-form-label">Country</label>
-                            <div class="col-md-8">
-                                <input type="text" class="form-control customer" name="customer_country" id="customer_country" value="<?php echo Form::value('customer_country');?>" />
-                                <span class="inst">use the 2 letter ISO code</span>
-                                <?php echo Form::displayError('customer_country');?>
+                            <div class="form-group row">
+                                <label class="col-md-4 col-form-label">Postcode</label>
+                                <div class="col-md-8">
+                                    <input type="text" class="form-control customer" name="customer_postcode" id="customer_postcode" value="<?php echo Form::value('customer_postcode');?>" />
+                                    <?php echo Form::displayError('customer_postcode');?>
+                                </div>
                             </div>
-                        </div>
+                            <div class="form-group row">
+                                <label class="col-md-4 col-form-label">Country</label>
+                                <div class="col-md-8">
+                                    <input type="text" class="form-control customer" name="customer_country" id="customer_country" value="<?php echo Form::value('customer_country');?>" />
+                                    <span class="inst">use the 2 letter ISO code</span>
+                                    <?php echo Form::displayError('customer_country');?>
+                                </div>
+                            </div>
+                            <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+                        </form>
                     </div>
                     <div class="card-footer text-right">
-
+                        <button id="customer_details_update_submitter" class="btn btn-outline-secondary">Save Changes</button>
                     </div>
                 </div>
             </div>
