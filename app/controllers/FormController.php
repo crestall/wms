806757472b54fb6b3d3aea27c8bf7383a60b6f0b
marxolly <mark@solly.com.au>
2020-09-16 11:20:18 +1000

    Update FormController.php

--- app/controllers/FormController.php
+++ app/controllers/FormController.php
@@ -181,7 +181,8 @@ class FormController extends Controller {
         }
         else
         {
-            echo "<pre>",print_r($post_data),"</pre>"; die();
+            //echo "<pre>",print_r($post_data),"</pre>"; die();
+            $this->productionjob->updateJobDetails($post_data);
             Session::set('jobdetailsfeedback',"<h3><i class='far fa-check-circle'></i>The Job Details Have Been Updated</h3><p>The changes should be showing below</p>");
         }
         return $this->redirector->to(PUBLIC_ROOT."jobs/update-job/job={$id}#jobdetails");
