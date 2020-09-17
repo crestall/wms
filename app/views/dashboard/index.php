37700a5b1a8ba77588597d7a2002ab2e3717400a
marxolly <mark@solly.com.au>
2020-09-17 15:00:50 +1000

    Update index.php

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -114,7 +114,7 @@ $c = 1;
                         <h4>Add A New Customer</h4>
                     </div>
                     <div class="card-body text-center">
-                    	<a class="btn btn-lg btn-outline-fsg" href="/customers/add-customer"><span class="fa-layers fa-fw fa-2x align-middle"><i class="fad fa-user-tie"></i><i class="far fa-plus" data-fa-transform="shrink-4 up-3 right-4"></i></span>&nbsp;<span style="font-size:larger">Add!</span></a>
+                    	<a class="btn btn-lg btn-outline-fsg" href="/customers/add-customer"><span class="fa-layers fa-fw fa-3x align-middle"><i class="fad fa-user-tie"></i><i class="far fa-plus" data-fa-transform="shrink-4 up-3 right-4"></i></span>&nbsp;<span style="font-size:larger">Add!</span></a>
                     </div>
                 </div>
                 <div class="card homepagecard">
