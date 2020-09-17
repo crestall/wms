5cebd71e071a394c294055a75076fdabc22c41e8
marxolly <mark@solly.com.au>
2020-09-17 12:59:56 +1000

    Update updateJob.php

--- app/views/jobs/updateJob.php
+++ app/views/jobs/updateJob.php
@@ -90,7 +90,7 @@ $supplier_country = (!empty(Form::value('supplier_country')))? Form::value('supp
                                 <input type="hidden" name="date_entered_value" id="date_entered_value" value="<?php echo $date_entered;?>" />
                             </div>
                             <div class="row form-group">
-                                <label class="col-md-4 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Due Date</label>
+                                <label class="col-md-4 col-form-label">Due Date</label>
                                 <div class="col-md-8">
                                     <div class="input-group">
                                         <input type="text" class="form-control" name="date_due" id="date_due" value="<?php if(!empty($date_due)) echo date('d/m/Y', $date_due);?>" />
