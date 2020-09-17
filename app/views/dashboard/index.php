ffef6923d6f6dbdcb08a36e5497962f5eee6e9cd
marxolly <mark@solly.com.au>
2020-09-17 15:09:57 +1000

    Update index.php

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -101,30 +101,32 @@ $c = 1;
                 <h2>Quick Links</h2>
             </div>
             <div class="card-deck homepagedeck">
-                <div class="card homepagecard">
-                    <div class="card-header">
-                        <h4>Add A New Job</h4>
+                <?php if($user_role == "production_admin"):?>
+                    <div class="card homepagecard">
+                        <div class="card-header">
+                            <h4>Add A New Job</h4>
+                        </div>
+                        <div class="card-body text-center">
+                        	<a class="btn btn-lg btn-outline-fsg" href="/jobs/add-job"><span class="fa-layers fa-fw fa-3x align-middle"><i class="fad fa-tasks"></i><i class="far fa-plus" data-fa-transform="shrink-4 up-3 right-4"></i></span>&nbsp;<span style="font-size:larger">Add!</span></a>
+                        </div>
                     </div>
-                    <div class="card-body text-center">
-                    	<a class="btn btn-lg btn-outline-fsg" href="/jobs/add-job"><span class="fa-layers fa-fw fa-3x align-middle"><i class="fad fa-tasks"></i><i class="far fa-plus" data-fa-transform="shrink-4 up-3 right-4"></i></span>&nbsp;<span style="font-size:larger">Add!</span></a>
+                    <div class="card homepagecard">
+                        <div class="card-header">
+                            <h4>Add A New Customer</h4>
+                        </div>
+                        <div class="card-body text-center">
+                        	<a class="btn btn-lg btn-outline-fsg" href="/customers/add-customer"><span class="fa-layers fa-fw fa-3x align-middle"><i class="fad fa-user-tie"></i><i class="far fa-plus" data-fa-transform="shrink-4 up-3 right-4"></i></span>&nbsp;<span style="font-size:larger">Add!</span></a>
+                        </div>
                     </div>
-                </div>
-                <div class="card homepagecard">
-                    <div class="card-header">
-                        <h4>Add A New Customer</h4>
+                    <div class="card homepagecard">
+                        <div class="card-header">
+                            <h4>Add A New Supplier</h4>
+                        </div>
+                        <div class="card-body text-center">
+                        	<a class="btn btn-lg btn-outline-fsg" href="/suppliers/add-supplier"><span class="fa-layers fa-fw fa-3x align-middle"><i class="fad fa-people-arrows"></i><i class="far fa-plus" data-fa-transform="shrink-4 up-3 right-4"></i></span>&nbsp;<span style="font-size:larger">Add!</span></a>
+                        </div>
                     </div>
-                    <div class="card-body text-center">
-                    	<a class="btn btn-lg btn-outline-fsg" href="/customers/add-customer"><span class="fa-layers fa-fw fa-3x align-middle"><i class="fad fa-user-tie"></i><i class="far fa-plus" data-fa-transform="shrink-4 up-3 right-4"></i></span>&nbsp;<span style="font-size:larger">Add!</span></a>
-                    </div>
-                </div>
-                <div class="card homepagecard">
-                    <div class="card-header">
-                        <h4>Add A New Supplier</h4>
-                    </div>
-                    <div class="card-body text-center">
-                    	<a class="btn btn-lg btn-outline-fsg" href="/suppliers/add-supplier"><span class="fa-layers fa-fw fa-3x align-middle"><i class="fad fa-people-arrows"></i><i class="far fa-plus" data-fa-transform="shrink-4 up-3 right-4"></i></span>&nbsp;<span style="font-size:larger">Add!</span></a>
-                    </div>
-                </div>
+                <?php endif;?>
                 <div class="card homepagecard">
                     <div class="card-header">
                         <h4>View Jobs</h4>
