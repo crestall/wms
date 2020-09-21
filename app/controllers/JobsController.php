d345eaec6afb8de9b6886d6eae80e4ea95941d40
marxolly <mark@solly.com.au>
2020-09-21 12:05:12 +1000

    Update JobsController.php

--- app/controllers/JobsController.php
+++ app/controllers/JobsController.php
@@ -42,7 +42,7 @@ class JobsController extends Controller
             'customer_ids'       =>  array(),
             'supplier_ids'       =>  array(),
             'salesrep_ids'       =>  array(),
-            'status_id'         =>  0,
+            'status_ids'         =>  array(),
             'date_from_value'   =>  0,
             'date_to_value'     =>  0
         ]);
@@ -66,7 +66,7 @@ class JobsController extends Controller
         $customer_ids = isset($this->request->query['customer_ids'])? $this->request->query['customer_ids']: array();
         $supplier_ids = isset($this->request->query['supplier_ids'])? $this->request->query['supplier_ids']: array();
         $salesrep_ids = isset($this->request->query['salesrep_ids'])? $this->request->query['salesrep_ids']: array();
-        $status_id = $this->request->query['status_id'];
+        $status_ids = isset($this->request->query['status_ids'])? $this->request->query['status_ids']: array();;
         $date_from_value = $this->request->query['date_from_value'];
         $date_to_value = $this->request->query['date_to_value'];
         $args = array(
@@ -74,7 +74,7 @@ class JobsController extends Controller
             'customer_ids'       =>  $customer_ids,
             'supplier_ids'       =>  $supplier_ids,
             'salesrep_ids'       =>  $salesrep_ids,
-            'status_id'         =>  $status_id,
+            'status_ids'         =>  $status_ids,
             'date_from_value'   =>  $date_from_value,
             'date_to_value'     =>  $date_to_value
         );
