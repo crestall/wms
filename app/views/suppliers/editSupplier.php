5d1e0a23b9e61c04035699e7d8cb3170f525b2ac
marxolly <mark@solly.com.au>
2020-09-22 12:36:25 +1000

    Update editSupplier.php

--- app/views/suppliers/editSupplier.php
+++ app/views/suppliers/editSupplier.php
@@ -41,9 +41,9 @@ $website    = empty(Form::value('website'))?    $supplier['website']      : Form
                 </div>
             </div>
             <div class="form-group row">
-                <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Email</label>
+                <label class="col-md-3">Email</label>
                 <div class="col-md-4">
-                    <input type="text" class="form-control required email" name="email" id="email" value="<?php echo $email;?>" />
+                    <input type="text" class="form-control email" name="email" id="email" value="<?php echo $email;?>" />
                     <?php echo Form::displayError('email');?>
                 </div>
             </div>
