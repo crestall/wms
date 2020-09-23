163ba481d50e0ea775c7366983cce016c3482218
marxolly <mark@solly.com.au>
2020-09-23 12:51:46 +1000

    Update footer_scripts.php

--- app/views/layout/page-includes/footer_scripts.php
+++ app/views/layout/page-includes/footer_scripts.php
@@ -43,7 +43,6 @@
         <script src="https://kit.fontawesome.com/cc79da085a.js" crossorigin="anonymous"></script>
         <!-- Bootstrap Colour Picker -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-colorpicker/3.2.0/js/bootstrap-colorpicker.min.js" integrity="sha512-INXxqXxcP6zawSei7i47Xmu+6ZIBRbsYN1LHAy5H1gzl1XIfTbI/OLjUcvBnDD8F3ZSVB6mf8asEPTMxz4VNjw==" crossorigin="anonymous"></script>
-        <!--script src="/scripts/bootstrap-colorpicker.min.js"></script>
         <!-- Assign CSRF Token to JS variable -->
         <?php Config::setJsConfig('csrfToken', Session::generateCsrfToken()); ?>
         <!-- Assign pages for menu highlighting -->
