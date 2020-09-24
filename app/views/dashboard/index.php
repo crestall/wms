e715219539c502834b445ba922a7d9117a035f89
marxolly <mark@solly.com.au>
2020-09-24 15:28:00 +1000

    Update index.php

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -132,7 +132,7 @@ $c = 1;
                         <h4>View Jobs</h4>
                     </div>
                     <div class="card-body text-center">
-                    	<a class="btn btn-lg btn-outline-fsg" href="/jobs/view-jobs"><span class="fa-layers fa-fw fa-3x align-middle"><i class="fad fa-tasks"></i><i class="fal fa-binoculars" data-fa-transform="shrink-4 up-1 right-4"></i></span>&nbsp;<span style="font-size:larger">Look!</span></a>
+                    	<a class="btn btn-lg btn-outline-fsg" href="/jobs/view-jobs"><span class="fa-layers fa-fw fa-3x align-middle"><i class="fad fa-tasks"></i><i class="fal fa-binoculars" data-fa-transform="shrink-6 up-1 right-4 rotate-30"></i></span>&nbsp;<span style="font-size:larger">Look!</span></a>
                     </div>
                 </div>
                 <?php if($user_role == "production"):?>
