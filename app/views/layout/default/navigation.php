03808af37dc496bfc078d5fbc0cd6b16277a8fe8
marxolly <mark@solly.com.au>
2020-09-28 12:09:30 +1000

    Update navigation.php

--- app/views/layout/default/navigation.php
+++ app/views/layout/default/navigation.php
@@ -49,6 +49,7 @@ endif;
                 <img class="img-user" src="<?php echo $image;?>" /><br/>
                 <strong><?php echo Session::getUsersName(); ?></strong>
             </a>
+            <div id="countdown">Page wil refresh in <span></span></div>
             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                 <a href="/user/profile" class="dropdown-item"><i class="fa fa-user fa-fw"></i> Profile</a>
                 <a href="/login/logOut" class="dropdown-item"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
