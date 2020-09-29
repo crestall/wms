3f11f539201bd4448b47c64c51204845233b63df
marxolly <mark@solly.com.au>
2020-09-29 12:36:31 +1000

    Create drivers.php

---
+++ app/views/sitesettings/drivers.php
@@ -0,0 +1,82 @@
+<?php
+
+?>
+<div id="page-wrapper">
+    <div id="page_container" class="container-xl">
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
+        <?php echo Form::displayError('general');?>
+        <form id="add-courier"  method="post" enctype="multipart/form-data" action="/form/procDriverAdd">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h3>Add New Driver</h3>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Name</label>
+                <div class="col-md-4">
+                    <input type="text" class="form-control required" name="name" id="name" value="<?php echo Form::value('name');?>" />
+                    <?php echo Form::displayError('name');?>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-md-3 col-form-label">Phone</label>
+                <div class="col-md-4">
+                    <input type="text" class="form-control" name="phone" id="phone" value="<?php echo Form::value('phone');?>" />
+                    <?php echo Form::displayError('phone');?>
+                </div>
+            </div>
+            <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+            <div class="form-group row">
+                <div class="col-md-4 offset md-3">
+                    <button type="submit" class="btn btn-outline-secondary">Add Driver</button>
+                </div>
+            </div>
+        </form>
+        <div class="row">
+            <div class="col-lg-12">
+                <h2>Current Drivers</h2>
+            </div>
+        </div>
+        <?php if(count($drivers)):?>
+            <?php foreach($driver as $d):?>
+                <form class="edit-driver border-bottom border-secondary border-bottom-dashed mb-3" action="/form/procDriverEdit" method="post">
+                    <div class="form-group row">
+                        <div class="col-md-3">
+                            <label class="col-form-label">Name</label>
+                            <input type="text" class="form-control required" name="name_<?php echo $d['id'];?>" id="name_<?php echo $d['id'];?>" value="<?php echo $d['name'];?>" />
+                            <?php echo Form::displayError("name_{$d['id']}");?>
+                        </div>
+                        <div class="col-md-3">
+                            <label class="col-form-label">Phone</label>
+                            <input type="text" class="form-control" name="phone_<?php echo $d['id'];?>" id="phone_<?php echo $d['id'];?>" value="<?php echo $d['phone'];?>" />
+                            <?php echo Form::displayError("phone{$d['id']}");?>
+                        </div>
+                        <div class="col-md-1">
+                            <label class="col-form-label" for="active_<?php echo $d['id'];?>">Active</label>
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" id="active_<?php echo $d['id'];?>" name="active_<?php echo $d['id'];?>" <?php if($d['active'] > 0) echo "checked";?> />
+                                <label class="custom-control-label" for="active_<?php echo $d['id'];?>"></label>
+                            </div>
+                        </div>
+                        <div class="col-md-1">
+                            <label class="col-form-label">&nbsp;</label>
+                            <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+                            <input type="hidden" name="line_id" value="<?php echo $d['id'];?>" />
+                            <button type="submit" class="btn btn-sm btn-outline-secondary">Update</button>
+                        </div>
+                    </div>
+                </form>
+            <?php endforeach;?>
+        <?php else:?>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="errorbox">
+                        <h2><i class="fas fa-exclamation-triangle"></i> No Drivers Listed</h2>
+                        <p>You will need to add some first</p>
+                    </div>
+                </div>
+            </div>
+        <?php endif;?>
+    </div>
+</div>
