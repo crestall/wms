df6155bf93e21977d49a854aca8469ec5b0cb932
marxolly <mark@solly.com.au>
2020-09-30 08:33:20 +1000

    Update footer.php

--- app/views/layout/sitesettings/footer.php
+++ app/views/layout/sitesettings/footer.php
@@ -114,34 +114,6 @@
                         });
                     }
                 },
-                'store-chains':{
-                    init: function(){
-                        $('form#add-storechain, form.edit-storechain').submit(function(){
-                            if($(this).valid())
-                            {
-                                $.blockUI({ message: '<div style="height:140px; padding-top:20px;"><h2>Processing form...</h2></div>' });
-                            }
-                            else
-                            {
-                                return false;
-                            }
-                        });
-                    }
-                },
-                'solar-order-types':{
-                    init: function(){
-                        $('form#add-solartype, form.edit-solartype').submit(function(){
-                            if($(this).valid())
-                            {
-                                $.blockUI({ message: '<div style="height:140px; padding-top:20px;"><h2>Processing form...</h2></div>' });
-                            }
-                            else
-                            {
-                                return false;
-                            }
-                        });
-                    }
-                },
                 'couriers':{
                     init: function(){
                         $('form#add-courier, form.edit-courier').submit(function(){
