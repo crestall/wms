97ebd031c6ccd9960e33052e2ea0c51bcb9e034e
marxolly <mark@solly.com.au>
2020-09-29 12:41:12 +1000

    Update footer.php

--- app/views/layout/sitesettings/footer.php
+++ app/views/layout/sitesettings/footer.php
@@ -156,6 +156,20 @@
                         });
                     }
                 },
+                'drivers':{
+                    init: function(){
+                        $('form#add-driver, form.edit-driver').submit(function(){
+                            if($(this).valid())
+                            {
+                                $.blockUI({ message: '<div style="height:140px; padding-top:20px;"><h2>Processing form...</h2></div>' });
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        });
+                    }
+                },
                 'user-roles':{
                     init: function(){
                         /*
