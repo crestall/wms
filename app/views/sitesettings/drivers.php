d54099ed496bd6c9b672223f7abb3cde219a4016
marxolly <mark@solly.com.au>
2020-09-30 14:02:15 +1000

    Update drivers.php

--- app/views/sitesettings/drivers.php
+++ app/views/sitesettings/drivers.php
@@ -64,7 +64,7 @@
                             <label class="col-form-label">&nbsp;</label>
                             <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
                             <input type="hidden" name="line_id" value="<?php echo $d['id'];?>" />
-                            <input type="hidden" class="current_driver_name" name="current_name" value="<?php echo $d['name'];?>" />
+                            <input type="hidden" class="current_driver_name" name="current_name" id="current_name_<?php echo $d['id'];?>" value="<?php echo $d['name'];?>" />
                             <button type="submit" class="btn btn-sm btn-outline-secondary">Update</button>
                         </div>
                     </div>
