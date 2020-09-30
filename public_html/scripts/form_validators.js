ee99220c59443854d92d56e480a62006f0e2c8bf
marxolly <mark@solly.com.au>
2020-09-30 14:29:15 +1000

    Update form_validators.js

--- public_html/scripts/form_validators.js
+++ public_html/scripts/form_validators.js
@@ -600,11 +600,11 @@ $(document).ready(function() {
     $('input.driver_name').each(function(i,e){
         var line_id = $(e).prop('id').split('_').pop();
         $(this).rules('add',{
+                required: true,
                 remote: {
                     url: '/ajaxfunctions/checkDriverNames',
                     data: { 'current_name': function() { return $('input#current_name_'+line_id).val();}
                 },
-                required: true,
                 messages:{
                     remote: '<p>This name is already in the system.<br>Driver names must be unique</p>',
                     required: 'A name is required'
