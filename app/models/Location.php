d7a59222cbdf78e0e77c1e953680cebf2150125a
marxolly <mark@solly.com.au>
2020-10-06 14:26:37 +1100

    Update Location.php

--- app/models/Location.php
+++ app/models/Location.php
@@ -54,6 +54,7 @@ class Location extends Model{
 
     public function moveAllItemsInLocation($from_id, $to_id)
     {
+        $db = Database::openConnection();
         $q = "UPDATE `items_locations` SET location_id = $to_id WHERE location_id = $from_id";
         $db->queryData($q);
         return true;
