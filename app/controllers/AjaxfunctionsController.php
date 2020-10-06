56cc43c3fd0cc8a6a051a81535e794bb049e1ac7
marxolly <mark@solly.com.au>
2020-10-07 08:59:11 +1100

    Update AjaxfunctionsController.php

--- app/controllers/AjaxfunctionsController.php
+++ app/controllers/AjaxfunctionsController.php
@@ -467,7 +467,7 @@ class ajaxfunctionsController extends Controller
 
     public function selectCourier()
     {
-        //echo "<pre>",print_r($this->request->data['order_ids']),"</pre>"; die();
+        echo "<pre>",print_r($this->request->data),"</pre>"; die();
         $data = array(
             'error'         => false,
             'error_string'  => '',
