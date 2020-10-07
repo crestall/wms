a4534a933b50a643f1668b52dcbd62e0aea9a914
marxolly <mark@solly.com.au>
2020-10-08 09:00:52 +1100

    Update production_jobs_table.php

--- app/views/layout/page-includes/production_jobs_table.php
+++ app/views/layout/page-includes/production_jobs_table.php
@@ -111,7 +111,7 @@
                     <?php else:
                         if($job['runsheet_id'] > 0):?>
                             <p>This Job is on the runsheet for <strong><?php echo date('l jS \of F',$job['runsheet_day']);?></strong></p>
-                            <p><button class="btn btn-outline-danger remove-from-runsheet" data-jobid="<?php echo $job['id'];?>" data-runsheetid="<?php echo $job['runsheet_id'];?>">Remove It</button></p>
+                            <p class="text-center"><button class="btn btn-outline-danger remove-from-runsheet" data-jobid="<?php echo $job['id'];?>" data-runsheetid="<?php echo $job['runsheet_id'];?>">Remove It</button></p>
                         <?php else:
                             $date = strtotime("today");?>
                             <div class="input-group">
