76a12a7d7972813ddf0c6ec7d8dae73a0b89f127
marxolly <mark@solly.com.au>
2020-10-08 12:26:34 +1100

    Update viewRunsheets.php

--- app/views/runsheets/viewRunsheets.php
+++ app/views/runsheets/viewRunsheets.php
@@ -2,8 +2,8 @@
 
 ?>
 <div id="page-wrapper">
-    <div id="page_container" class="container-xxl">
+    <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-        
+
     </div>
 </div>
