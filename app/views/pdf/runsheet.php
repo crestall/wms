e1d76e9df054677d932b3d5e38356b3bf64ad4dc
marxolly <mark@solly.com.au>
2020-10-09 15:01:42 +1100

    Update runsheet.php

--- app/views/pdf/runsheet.php
+++ app/views/pdf/runsheet.php
@@ -1,16 +1,16 @@
 <div id="runsheet_body">
     <div class="landscape">
-        <table width="100%">
+        <table>
             <thead>
                 <tr>
                     <th style="width: 16mm">Job Id/Order Number</th>
                     <th style="width: 38mm">Customer - Client</th>
-                    <th>Description</th>
-                    <th>Address</th>
-                    <th>Units</th>
-                    <th>FSG Contact</th>
-                    <th>Received By</th>
-                    <th>Time of Drop</th>
+                    <th style="width: 74mm">Description</th>
+                    <th style="width: 41mm">Address</th>
+                    <th style="width: 12mm">Units</th>
+                    <th style="width: 27mm">FSG Contact</th>
+                    <th style="width: 27mm">Received By</th>
+                    <th style="width: 25mm">Time of Drop</th>
                 </tr>
             </thead>
             <tbody>
