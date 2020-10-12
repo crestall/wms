10898c841f59c33ab83c40b9f3614a0accc47723
marxolly <mark@solly.com.au>
2020-10-12 11:57:11 +1100

    Update runsheet.php

--- app/views/pdf/runsheet.php
+++ app/views/pdf/runsheet.php
@@ -4,8 +4,8 @@
             <table>
                 <tr>
                     <td><img width="130" src="https://wms.fsg.com.au/images/FSG_logo_white@130px.png" alt="FSG Logo"> </td>
-                    <td style="width: 20cm"></td>
-                    <td style="text-transform: uppercase; color: #fff;">RunSheet</td>
+                    <td style="width: 18cm"></td>
+                    <td style="text-transform: uppercase; color: #fff;">Driver RunSheet</td>
                 </tr>
             </table>
         </div>
