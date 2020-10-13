5dc29f7c7d0565ab370c8ed3fea1753b2948abf0
marxolly <mark@solly.com.au>
2020-10-14 09:26:47 +1100

    Update form_validators.js

--- public_html/scripts/form_validators.js
+++ public_html/scripts/form_validators.js
@@ -113,6 +113,17 @@ $(document).ready(function() {
 
 	//Validators
     ///////////////////////////////////////////////////////////////////////////////
+    $("#print_runsheet").validate({
+        rules:{
+            jobs:{
+                require_from_group: [1, ".task"]
+            },
+            orders:{
+                require_from_group: [1, ".task"]
+            }
+    	}
+	});
+    ///////////////////////////////////////////////////////////////////////////////
     $('form#add_production_job, form#job_details_update').validate({
          rules:{
     		status_id:{
