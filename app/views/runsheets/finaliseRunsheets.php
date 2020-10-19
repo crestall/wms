b9c57fea512ad099496c1ca3d0a2b0398b65ce51
marxolly <mark@solly.com.au>
2020-10-19 11:26:01 +1100

    Create finaliseRunsheets.php

---
+++ app/views/runsheets/finaliseRunsheets.php
@@ -0,0 +1,8 @@
+<?php
+$db = Database::openConnection();
+?>
+<div id="page-wrapper">
+    <div id="page_container" class="container-xl">
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+    </div>
+</div>
