3fbf2a61eff7775432025a0cb4f5a8de7728c5d3
marxolly <mark@solly.com.au>
2020-10-20 09:08:02 +1100

    Update RunsheetsController.php

--- app/controllers/RunsheetsController.php
+++ app/controllers/RunsheetsController.php
@@ -208,7 +208,7 @@ class RunsheetsController extends Controller
     {
         $rss = $this->runsheet->getRunsheetsForDisplay(false, true);  //NOT COMPLTED PRINTED
         $runsheets = array();
-        //echo "<pre>",print_r($rss),"</pre>";die();
+        echo "<pre>",print_r($rss),"</pre>";die();
         $di = 0;
         foreach($rss as $rs)
         {
@@ -221,7 +221,7 @@ class RunsheetsController extends Controller
                 $runsheets[$rs['runsheet_day']]['drivers'] = array();
             if(($tdi = array_search($rs['driver_id'], array_column($runsheets[$rs['runsheet_day']]['drivers'], 'id'))) === false)
             {
-                //echo "<p>No id for {$rs['driver_id']} found. Will add it with index $di</p>";
+                echo "<p>No id for {$rs['driver_id']} found. Will add it with index $di</p>";
                 $runsheets[$rs['runsheet_day']]['drivers'][$di] = array(
                     'id'    => $rs['driver_id'],
                     'name'  => $rs['driver_name'],
@@ -243,7 +243,7 @@ class RunsheetsController extends Controller
             }
             else
             {
-                //echo "<p>Id {$rs['driver_id']} found. It is $tdi</p>";
+                echo "<p>Id {$rs['driver_id']} found. It is $tdi</p>";
                 if(!empty($rs['job_id']))
                     $runsheets[$rs['runsheet_day']]['drivers'][$tdi]['tasks'][] = array(
                         'task_id'       => $rs['id'],
@@ -258,7 +258,7 @@ class RunsheetsController extends Controller
                     );
             }
         }
-        //echo "<pre>",print_r($runsheets),"/<pre>";die();
+        echo "<pre>",print_r($runsheets),"</pre>";die();
         //render the page
         Config::setJsConfig('curPage', "finalise-runsheets");
         Config::set('curPage', "finalise-runsheets");
