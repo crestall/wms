dada1e862c5cf051a35d6b09ae74ca10894b290f
marxolly <mark@solly.com.au>
2020-10-19 11:54:05 +1100

    Update finaliseRunsheets.php

--- app/views/runsheets/finaliseRunsheets.php
+++ app/views/runsheets/finaliseRunsheets.php
@@ -4,6 +4,16 @@ $db = Database::openConnection();
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-        <?php echo "<pre>",print_r($runsheets),"/<pre>";?>
+        <?php if(count($runsheets)):?>
+            <?php echo "<pre>",print_r($runsheets),"/<pre>";?>
+        <?php else:?>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="errorbox">
+                    <h2><i class="fas fa-exclamation-triangle"></i> No Runsheets Listed For Finalising</h2>
+                </div>
+            </div>
+        </div>
+        <?php endif;?>
     </div>
 </div>
