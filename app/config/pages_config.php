a3c0134e9910c03965456e7306066206617a4164
marxolly <mark@solly.com.au>
2020-10-19 11:03:07 +1100

    Update pages_config.php

--- app/config/pages_config.php
+++ app/config/pages_config.php
@@ -60,7 +60,7 @@ $padmin = array(
         ),
         'finalise-runsheets'    => array(
             'display'   => true,
-            'icon'      => '<div class="fa-2x"><span class="fa-layers fa-fw"><i class="fad fa-list-ol"></i><i class="fal fa-check" style="color:black"></i><i class="fal fa-check" data-fa-transform="shrink-3" style="color:#66ff33"></i></span></div>'
+            'icon'      => '<div class="fa-2x"><span class="fa-layers fa-fw"><i class="fad fa-list-ol"></i><i class="fal fa-check" style="color:black"></i><i class="fal fa-check" data-fa-transform="shrink-6" style="color:#66ff33"></i></span></div>'
         )
     ),
     'production-settings'   => array(
