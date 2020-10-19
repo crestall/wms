16e0481a60f2e726b9633b06277850aac4d9ad75
marxolly <mark@solly.com.au>
2020-10-19 11:44:00 +1100

    Update finaliseRunsheets.php

--- app/views/runsheets/finaliseRunsheets.php
+++ app/views/runsheets/finaliseRunsheets.php
@@ -4,5 +4,6 @@ $db = Database::openConnection();
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+        <?php echo "<pre>",print_r($runsheets),"/<pre>";?>
     </div>
 </div>
