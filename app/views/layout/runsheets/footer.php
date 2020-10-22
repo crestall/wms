511b4fa10985b03affb2a3317a47fcafbfac922a
marxolly <mark@solly.com.au>
2020-10-22 12:19:12 +1100

    Update footer.php

--- app/views/layout/runsheets/footer.php
+++ app/views/layout/runsheets/footer.php
@@ -34,6 +34,10 @@
                             $.blockUI({ message: '<div style="height:120px; padding-top:40px;"><h1>Collecting Runsheets...</h1></div>' });
                             actions['common']['generateURL']('runsheets/runsheet-report');
                         });
+                        $('select#driver_selector').change(function(e){
+                            $.blockUI({ message: '<div style="height:120px; padding-top:40px;"><h1>Collecting Runsheets...</h1></div>' });
+                            actions['common']['generateURL']('runsheets/runsheet-report');
+                        });
                     }
                 },
                 'view-runsheets':{
