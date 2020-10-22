75527e64f094e9aa8ce7dce49150a82aed6f0430
marxolly <mark@solly.com.au>
2020-10-22 14:42:47 +1100

    Update completeRunsheets.php

--- app/views/runsheets/completeRunsheets.php
+++ app/views/runsheets/completeRunsheets.php
@@ -26,6 +26,7 @@ $date_filter = "Completed";
             </div>
             <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
                 <div class="form-group">
+                    <label>&nbsp;</label>
                     <button id="csv_download" class="btn btn-outline-success"><i class="far fa-file-alt"></i>&nbsp;Download As CSV</button>
                 </div>
             </div>
