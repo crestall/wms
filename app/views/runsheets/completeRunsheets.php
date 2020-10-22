3a1e4634a0e8ba0324f95f9aa812875419b391a5
marxolly <mark@solly.com.au>
2020-10-22 11:17:00 +1100

    Update completeRunsheets.php

--- app/views/runsheets/completeRunsheets.php
+++ app/views/runsheets/completeRunsheets.php
@@ -5,18 +5,19 @@
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <div class="row">
-        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
-            <div class="form-group">
-                <label>Filter By Driver</label>
-                <select id="driver_selector" class="form-control selectpicker" data-style="btn-outline-secondary"><option value="0">All Drivers</option><?php echo $this->controller->driver->getSelectDrivers($driver_id);?></select>
+            <?php include(Config::get('VIEWS_PATH')."layout/page-includes/between_dates.php");?>
+            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
+                <div class="form-group">
+                    <label>Filter By Driver</label>
+                    <select id="driver_selector" class="form-control selectpicker" data-style="btn-outline-secondary"><option value="0">All Drivers</option><?php echo $this->controller->driver->getSelectDrivers($driver_id);?></select>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
+                <div class="form-group">
+                    <label>Search</label>
+                    <input type="text" class="form-control" id="table_searcher" />
+                </div>
             </div>
         </div>
-        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
-            <div class="form-group">
-                <label>Search</label>
-                <input type="text" class="form-control" id="table_searcher" />
-            </div>
-        </div>
-    </div>
     </div>
 </div>
