bde717b6f4f29aadf6c85389677bb7e1b109c023
marxolly <mark@solly.com.au>
2020-10-22 14:55:50 +1100

    Update Runsheet.php

--- app/models/Runsheet.php
+++ app/models/Runsheet.php
@@ -24,29 +24,7 @@ class Runsheet extends Model{
     public function getCompletedRunsheets($args)
     {
         $db = Database::openConnection();
-        $defaults = array(
-            'from'          => strtotime('monday this week'),
-            'to'            => time(),
-            'client_id'     => 0,
-            'customer_id'   => 0,
-            'driver_id'     => 0,
-        );
-        $args = array_merge($defaults, $args);
-        extract($args);
-        //echo "<pre>",print_r($args),"</pre>";//die();
-        $q = $this->getRunsheetQuery();
-        $q .= "
-            WHERE (rst.completed = 1 AND rs.updated_date >= $from AND rs.updated_date <= $to)
-        ";
-
-        if($client_id > 0)
-            $q .= " AND( o.client_id = $client_id )";
-        if($customer_id > 0)
-            $q .= " AND( pj.customer_id = $customer_id )";
-        if($driver_id > 0)
-            $q .= " AND( rst.driver_id = $driver_id)";
-        $q .= " ORDER BY rs.runsheet_day DESC";
-        //echo $q; die();
+        $q = $this->getCompletedRunsheetsQuery($args);
         return $db->queryData($q);
     }
 
@@ -281,6 +259,34 @@ class Runsheet extends Model{
         }
 
         return true;
+    }
+
+    private function getCompletedRunsheetsQuery($args)
+    {
+        $defaults = array(
+            'from'          => strtotime('monday this week'),
+            'to'            => time(),
+            'client_id'     => 0,
+            'customer_id'   => 0,
+            'driver_id'     => 0,
+        );
+        $args = array_merge($defaults, $args);
+        extract($args);
+        //echo "<pre>",print_r($args),"</pre>";//die();
+        $q = $this->getRunsheetQuery();
+        $q .= "
+            WHERE (rst.completed = 1 AND rs.updated_date >= $from AND rs.updated_date <= $to)
+        ";
+
+        if($client_id > 0)
+            $q .= " AND( o.client_id = $client_id )";
+        if($customer_id > 0)
+            $q .= " AND( pj.customer_id = $customer_id )";
+        if($driver_id > 0)
+            $q .= " AND( rst.driver_id = $driver_id)";
+        $q .= " ORDER BY rs.runsheet_day DESC";
+        //echo $q; die();
+        return $q;
     }
 
     private function getRunsheetQuery()
