3e8c7dcf82af8d32761847f690080dca28992e20
marxolly <mark@solly.com.au>
2020-10-27 09:14:39 +1100

    Update getQuotes.php

--- app/views/orders/getQuotes.php
+++ app/views/orders/getQuotes.php
@@ -1,5 +1,5 @@
 <?php
-
+$suburb = Form::value('suburb');
 ?>
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
@@ -10,6 +10,13 @@
         <form id="get_quotes" method="post" action="/form/procGetQuotes">
             <div class="col-12 mb-2">
                 <p class="inst">Only the weight is required for Australia Post</p>
+            </div>
+            <div class="form-group row">
+                <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Suburb/Town</label>
+                <div class="col-md-4">
+                    <input type="text" class="form-control required" name="suburb" id="suburb" value="<?php echo $suburb;?>" />
+                    <?php echo Form::displayError('suburb');?>
+                </div>
             </div>
             <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
             <div class="form-group row">
