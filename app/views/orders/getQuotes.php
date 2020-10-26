c878a6baacadd5c86b8e2fea8a9c8504440d5bf5
marxolly <mark@solly.com.au>
2020-10-27 09:25:34 +1100

    Update getQuotes.php

--- app/views/orders/getQuotes.php
+++ app/views/orders/getQuotes.php
@@ -20,12 +20,18 @@ $postcode = Form::value('postcode');
                     <?php echo Form::displayError('suburb');?>
                 </div>
             </div>
-            <div class="form-group row">
+            <!--div class="form-group row">
                 <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> State</label>
                 <div class="col-md-4">
                     <input type="text" class="form-control" name="state" id="state" value="<?php echo $state;?>" />
                     <span class="inst">use VIC, NSW, QLD, ACT, TAS, WA, SA, NT only</span>
                     <?php echo Form::displayError('state');?>
+                </div>
+            </div-->
+            <div class="form-group row">
+                <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> State</label>
+                <div class="col-md-4">
+                    <select id="state" name="state" class="form-control selectpicker" data-style="btn-outline-secondary" required><option value="0">--Select One--</option><?php echo Utility::getStateSelect($state);?></select>
                 </div>
             </div>
             <div class="form-group row">
