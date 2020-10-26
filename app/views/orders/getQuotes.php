9d26142fe9847ed7fabef11b893190887e396291
marxolly <mark@solly.com.au>
2020-10-27 09:05:23 +1100

    Update getQuotes.php

--- app/views/orders/getQuotes.php
+++ app/views/orders/getQuotes.php
@@ -4,5 +4,18 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
+        <?php echo Form::displayError('general');?>
+        <form id="get_quotes" method="post" action="/form/procGetQuotes">
+            <div class="col-12 mb-2">
+                <p class="inst">Only the weight is required for Australia Post</p>
+            </div>
+            <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+            <div class="form-group row">
+                <div class="col-md-4 offset-md-6">
+                    <button type="submit" class="btn btn-outline-secondary">Book Collection</button>
+                </div>
+            </div>
+        </form>
     </div>
 </div>
