edcbb6befa7a4b631bf2ac765be1558e0118d8f4
marxolly <mark@solly.com.au>
2020-10-27 09:52:01 +1100

    Update getQuotes.php

--- app/views/orders/getQuotes.php
+++ app/views/orders/getQuotes.php
@@ -11,31 +11,22 @@ $postcode = Form::value('postcode');
         <?php echo Form::displayError('general');?>
         <form id="get_quotes" method="post" action="/form/procGetQuotes">
             <div class="col-12 mb-2">
-                <p class="inst">Only the weight is required for Australia Post</p>
             </div>
             <div class="form-group row">
-                <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Suburb/Town</label>
+                <label class="col-md-2"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Suburb/Town</label>
                 <div class="col-md-4">
                     <input type="text" class="form-control required" name="suburb" id="suburb" value="<?php echo $suburb;?>" />
                     <?php echo Form::displayError('suburb');?>
                 </div>
             </div>
-            <!--div class="form-group row">
-                <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> State</label>
-                <div class="col-md-4">
-                    <input type="text" class="form-control" name="state" id="state" value="<?php echo $state;?>" />
-                    <span class="inst">use VIC, NSW, QLD, ACT, TAS, WA, SA, NT only</span>
-                    <?php echo Form::displayError('state');?>
-                </div>
-            </div-->
             <div class="form-group row">
-                <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> State</label>
+                <label class="col-md-2"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> State</label>
                 <div class="col-md-4">
                     <select id="state" name="state" class="form-control selectpicker" data-style="btn-outline-secondary" required><option value="0">--Select One--</option><?php echo Utility::getStateSelect($state);?></select>
                 </div>
             </div>
             <div class="form-group row">
-                <label class="col-md-3 "><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Postcode</label>
+                <label class="col-md-2"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Postcode</label>
                 <div class="col-md-4">
                     <input type="text" class="form-control required" name="postcode" id="postcode" value="<?php echo $postcode;?>" />
                     <?php echo Form::displayError('postcode');?>
