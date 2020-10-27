78e257ffe41664d93c682fcbc7d4aedfa1c63e16
marxolly <mark@solly.com.au>
2020-10-27 15:26:07 +1100

    Update shipping_quotes.php

--- app/views/orders/shipping_quotes.php
+++ app/views/orders/shipping_quotes.php
@@ -1,1 +1,7 @@
+<div class="col-12 mb-2">
+    <p class="inst">These prices are GST and Fuel Levee Inclusive</p>
+    <p class="inst">No margin has been added. Please do not make these prices available to the customer/client</p>
+</div>
+
+
 <?php echo "<pre>",print_r($eparcel_response),"</pre>";?>
