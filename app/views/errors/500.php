bbcda2f264b852b8c8a5a6bf33bbc79fa74439ac
marxolly <mark@solly.com.au>
2020-11-04 11:41:19 +1100

    Update 500.php

--- app/views/errors/500.php
+++ app/views/errors/500.php
@@ -10,16 +10,27 @@ $quote = $quotes[$credit];
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
         <div class="row">
-            <div class="col-12">
-                <div class="bubble bubble-bottom-left">
-                    <div class="error-name float-left">
+            <div class="bubble bubble-bottom-left col-10 offset-1">
+                <div class="row">
+                    <div class="error-name col-4">
                         <h1>500</h1>
-                        <h2>ERROR</h2>
+                        <h2>Internal Error</h2>
                     </div>
-                    <div class="error-quote float-right">
+                    <div class="error-quote col-6 offset-1">
                         <?php echo $quote;?>
                     </div>
                 </div>
+            </div>
+        </div>
+        <div class="row error-foot mt-4">
+            <div class="offset-1 col-3 quoter">
+                <?php echo $credit;?>
+            </div>
+            <div class="col-8">
+                <p>Oops, we are sorry but our system encountered an internal error</p>
+                <p>This means the error is in our coding and you have not done anything wrong</p>
+                <p class="text-muted">If you wish to report this error, please include the URL (shown in the address bar) and time of the error</p>
+                <p><a href="/dashboard" class="btn btn-info">Back to home</a></p>
             </div>
         </div>
     </div>
