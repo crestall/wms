3393058ca3b7b74506f2fc06f16bee8cdf30ed57
marxolly <mark@solly.com.au>
2020-11-06 12:05:35 +1100

    Update 403.php

--- app/views/errors/403.php
+++ app/views/errors/403.php
@@ -1,8 +1,8 @@
 <?php
 $quotes = array(
-    'Detective David Mills - Se7en'     => "<i class='fal fa-quote-left'></i> What's on the page?! <i class='fal fa-quote-right'></i>",
-    'Bill Lumbergh - Office Space'      => "<i class='fal fa-quote-left'></i> Yeah... I'm gonna need you to go ahead and find another page. <i class='fal fa-quote-right'></i>",
-    'Nathan R. Jessup - A Few Good Men' => "<i class='fal fa-quote-left'></i> The page? You can't handle the page! <i class='fal fa-quote-right'></i>"
+    'Detective David Mills - Se7en'                     => "What's on the page?!",
+    'Bill Lumbergh - Office Space'                      => "Yeah... I'm gonna need you to go ahead and find another page.",
+    'Daniel Kaffee & Nathan R. Jessup - A Few Good Men' => "'I want the page!'<br>'You can't handle the page!'"
 );
 $credit = array_rand($quotes);
 $quote = $quotes[$credit];
