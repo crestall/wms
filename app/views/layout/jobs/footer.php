81c48406f116e93363a07f48b377848645db5e94
marxolly <mark@solly.com.au>
2020-11-09 08:54:15 +1100

    Update footer.php

--- app/views/layout/jobs/footer.php
+++ app/views/layout/jobs/footer.php
@@ -13,6 +13,9 @@
                         });
                         table.on( 'draw', function () {
                             console.log( 'Redraw occurred at: '+new Date().getTime() );
+                            $(".dataTables_length select").addClass("form-control selectpicker");
+                            $(".dataTables_filter input").addClass("form-control");
+                            $(".dataTables_filter").addClass("form-group")
                         });
                     },
                     selectAll: function(){
