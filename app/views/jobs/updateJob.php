d707b9e9d6a12694e67ef8917b78f40752023f73
marxolly <mark@solly.com.au>
2020-11-10 08:42:58 +1100

    Update updateJob.php

--- app/views/jobs/updateJob.php
+++ app/views/jobs/updateJob.php
@@ -523,7 +523,7 @@ else
                                     <div class="input-group">
                                         <input type="text" class="form-control" name="date_ed2" id="date_ed2" value="<?php if(!empty($date_ed2)) echo date('d/m/Y', $date_ed2);?>" />
                                         <div class="input-group-append">
-                                            <span id="date_ed_calendar" class="input-group-text"><i class="fad fa-calendar-alt"></i></span>
+                                            <span id="date_ed2_calendar" class="input-group-text"><i class="fad fa-calendar-alt"></i></span>
                                         </div>
                                     </div>
                                 </div>
@@ -635,7 +635,7 @@ else
                                     <div class="input-group">
                                         <input type="text" class="form-control" name="date_ed3" id="date_ed3" value="<?php if(!empty($date_ed3)) echo date('d/m/Y', $date_ed3);?>" />
                                         <div class="input-group-append">
-                                            <span id="date_ed_calendar" class="input-group-text"><i class="fad fa-calendar-alt"></i></span>
+                                            <span id="date_ed3_calendar" class="input-group-text"><i class="fad fa-calendar-alt"></i></span>
                                         </div>
                                     </div>
                                 </div>
