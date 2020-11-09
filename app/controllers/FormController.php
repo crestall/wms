d21bea35f8c665109faf8a85678fa2a04f196b01
marxolly <mark@solly.com.au>
2020-11-10 09:27:42 +1100

    Update FormController.php

--- app/controllers/FormController.php
+++ app/controllers/FormController.php
@@ -490,6 +490,7 @@ class FormController extends Controller {
                 $post_data[$field] = $value;
             }
         }
+        $post_data['fn'] = $fn;
         echo "<pre>POST DATA",print_r($post_data),"</pre>"; die();
         $date_ed_value = (!empty($date_ed_value))? $date_ed_value: 0;
         if($this->dataSubbed($finisher_email))
