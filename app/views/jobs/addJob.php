2790ed328ec90116a991129760297a64a82caac8
marxolly <mark@solly.com.au>
2020-11-10 11:58:36 +1100

    Update addJob.php

--- app/views/jobs/addJob.php
+++ app/views/jobs/addJob.php
@@ -238,7 +238,7 @@ else
                         <input class="form-check-input styled send_to_address" type="checkbox" id="send_to_finisher3" name="send_to_finisher3" />
                         <label for="send_to_finisher3">Send to Finisher Three</label>
                     </div>
-                    <div class="col-md-6 checkbox checkbox-default">
+                    <div class="offset-1 col-5 checkbox checkbox-default">
                         <input class="form-check-input styled send_to_address" type="checkbox" id="held_in_store" name="held_in_store" />
                         <label for="held_in_store">Held In Store</label>
                     </div>
