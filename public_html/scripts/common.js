1f72cf85f490e5692591ebd9a319d4f5cdd1aae1
marxolly <mark@solly.com.au>
2020-11-10 15:34:57 +1100

    Update common.js

--- public_html/scripts/common.js
+++ public_html/scripts/common.js
@@ -286,9 +286,7 @@ var jobDeliveryDestinations = {
         $('input#send_to_customer').change(function(e){
             if($('input#send_to_customer').prop('checked'))
             {
-                var cname = $('#customer_name').val();
-                console.log('customer_name: '+cname);
-                $('#ship_to').val(cname);
+                $('#ship_to').val($('#customer_name').val());
             }
         });
     }
