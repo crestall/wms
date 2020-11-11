91fc7da95031557c3776cd48fee8086eda0b99ef
marxolly <mark@solly.com.au>
2020-11-12 10:39:35 +1100

    form validation

--- public_html/scripts/form_validators.js
+++ public_html/scripts/form_validators.js
@@ -223,6 +223,25 @@ $(document).ready(function() {
 			}
 		}
     });
+    ////////////////////////////////////////////////////////////
+    $('form.edit-job-status').each(function(i,e){
+        $(this).validate();
+    });
+    $('input.status_name').each(function(i,e){
+        var line_id = $(e).prop('id').split('_').pop();
+        $(this).rules('add',{
+                required: true,
+                remote: {
+                    url: '/ajaxfunctions/checkJobStatusNames',
+                    data: { current_name: function() { return $('input#currentname_'+line_id).val();}
+                },
+                messages:{
+                    remote: '<p>This name is already in the system.<br>Status names must be unique</p>',
+                    required: 'A name is required'
+                }
+            }
+        })
+    })
     ///////////////////////////////////////////////////////////////////////////////
     $("form#add-finisher-category").validate({
     	rules:{
