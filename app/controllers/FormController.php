51a3a419a436e016593e9b93c2c690678068c60e
marxolly <mark@solly.com.au>
2020-11-12 09:50:25 +1100

    Update FormController.php

--- app/controllers/FormController.php
+++ app/controllers/FormController.php
@@ -143,7 +143,7 @@ class FormController extends Controller {
 
     public function procFinisherCategoryEdit()
     {
-        echo "<pre>",print_r($this->request->data),"</pre>"; die();
+        //echo "<pre>",print_r($this->request->data),"</pre>"; die();
         $post_data = array();
         $response = array();
         foreach($this->request->data as $field => $value)
