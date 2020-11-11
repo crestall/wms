e8712bd99d09763fe82021f75a2b2946036762f5
marxolly <mark@solly.com.au>
2020-11-11 14:02:53 +1100

    Update addFinisher.php

--- app/views/finishers/addFinisher.php
+++ app/views/finishers/addFinisher.php
@@ -10,7 +10,7 @@ $country = Form::value('country');
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
-        <?php echo "<pre>",print_r(Form::value('categories')),"</pre>";?>
+        <?php var_dump(Form::value('categories'));?>
         <form id="add_production_finisher" method="post" action="/form/procAddProductionFinisher">
             <div class="form-group row">
                 <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Name</label>
