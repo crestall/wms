1d6a4f7ed52aca1cca9a09cea58ee077fa318d32
marxolly <mark@solly.com.au>
2020-11-12 15:29:02 +1100

    Update pages_config.php

--- app/config/pages_config.php
+++ app/config/pages_config.php
@@ -65,6 +65,10 @@ $padmin = array(
         'completed-runsheets'    => array(
             'display'   => true,
             'icon'      => '<div class="fa-2x"><span class="fa-layers fa-fw"><i class="fad fa-list-ol"></i><i class="fal fa-file-check" data-fa-transform="right-4 shrink-4 up-2 rotate-15"></i></span></div>'
+        ),
+        'prepare-runsheets'     => array(
+            'display'   => true,
+            'icon'      => '<div class="fa-2x"><span class="fa-layers fa-fw"><i class="fad fa-list-ol"></i><i class="fal fa-pencil-alt" data-fa-transform="right-4 shrink-4 up-2"></i></span></div>'
         )
     ),
     'production-reports'    => array(
