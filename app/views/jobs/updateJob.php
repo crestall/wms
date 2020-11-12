4b746919059a5b5fd8565ad68bdf795c28e4494e
marxolly <mark@solly.com.au>
2020-11-13 09:18:13 +1100

    Update updateJob.php

--- app/views/jobs/updateJob.php
+++ app/views/jobs/updateJob.php
@@ -244,7 +244,7 @@ else
                                 <div class="form-group row">
                                     <label class="col-md-4 col-form-label">Attention</label>
                                     <div class="col-md-8">
-                                        <input type="text" class="form-control required" name="attention" id="attention" value="<?php echo $attention;?>" />
+                                        <input type="text" class="form-control" name="attention" id="attention" value="<?php echo $attention;?>" />
                                     </div>
                                 </div>
                                 <div class="form-group row">
