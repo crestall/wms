55d4553b068874b9486e19e5fe8d68305a2f30a0
marxolly <mark@solly.com.au>
2020-11-13 12:39:13 +1100

    Update updateJob.php

--- app/views/jobs/updateJob.php
+++ app/views/jobs/updateJob.php
@@ -379,10 +379,10 @@ while($f < $finisher_count)
                             Finisher <?php echo $fwn;?> Details
                         </div>
                         <div class="card-body">
-                            <?php if(isset($_SESSION['jobfinisher{$fn}detailsfeedback'])) :?>
+                            <?php if(isset($_SESSION['jobfinisher'.$fn.'detailsfeedback'])) :?>
                                 <div class='feedbackbox'><?php echo Session::getAndDestroy('jobfinisher{$fn}detailsfeedback');?></div>
                             <?php endif; ?>
-                            <?php if(isset($_SESSION['jobfinisher{$fn}detailserrorfeedback'])) :?>
+                            <?php if(isset($_SESSION['jobfinisher'.$fn.'detailserrorfeedback'])) :?>
                                 <div class='errorbox'><?php echo Session::getAndDestroy('jobfinisher${fn}detailserrorfeedback');?></div>
                             <?php endif; ?>
                             <form id="finisher<?php echo $fn;?>_details_update" method="post" action="/form/procJobfinisherUpdate">
