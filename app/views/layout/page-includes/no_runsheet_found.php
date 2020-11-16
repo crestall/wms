f82ab1c7d1f345a0f634693eb58dc25041d2bce5
marxolly <mark@solly.com.au>
2020-11-16 11:02:38 +1100

    Update no_runsheet_found.php

--- app/views/layout/page-includes/no_runsheet_found.php
+++ app/views/layout/page-includes/no_runsheet_found.php
@@ -8,7 +8,7 @@
                 <div class="col-8">
                     <h2>No Runsheet Or Tasks Found</h2>
                     <p>Either that runsheet id is invalid, or all tasks associated with it have been allocated to a driver</p>
-                    <p><a href="/runsheets/prepare-runsheets">Please click here to choose another runshet to update and allocate a driver</a></p>
+                    <p><a href="/runsheets/prepare-runsheets">Please click here to choose another runsheet to update and allocate a driver</a></p>
                     <p><a href="/runsheets/view-runsheets">Please click here if to you need to change a task to another driver or update the address</a></p>
                 </div>
             </div>
