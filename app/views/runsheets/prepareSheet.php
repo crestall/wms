5658a70807b1368bf4561325e6331158d9ea3c73
marxolly <mark@solly.com.au>
2020-11-16 15:55:00 +1100

    Update prepareSheet.php

--- app/views/runsheets/prepareSheet.php
+++ app/views/runsheets/prepareSheet.php
@@ -107,7 +107,7 @@
                                             <div class="col-12">
                                                 <label class="col-form-label" for="task_<?php echo $task['task_id'];?>"></label>
                                                 <div class="custom-control custom-checkbox">
-                                                    <input type="checkbox" class="custom-control-input task" id="task_<?php echo $task['task_id'];?>" name="tasks[orders][<?php echo $task['order_id'];?>][include]" checked />
+                                                    <input type="checkbox" class="custom-control-input task" data-taskid="<?php echo $task['task_id'];?>" id="task_<?php echo $task['task_id'];?>" name="tasks[orders][<?php echo $task['order_id'];?>][include]" checked />
                                                     <label class="custom-control-label" for="task_<?php echo $task['task_id'];?>"><span class="font-weight-bold"><?php echo $task['order_number'];?></span> - <?php echo $task['order_customer']."(".$task['order_client'].")";?></label>
                                                 </div>
                                             </div>
