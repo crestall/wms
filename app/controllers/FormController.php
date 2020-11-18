c9a801057bcce006f9ebd51a4e8f916f6dab6e13
marxolly <mark@solly.com.au>
2020-11-19 09:22:01 +1100

    Update FormController.php

--- app/controllers/FormController.php
+++ app/controllers/FormController.php
@@ -209,11 +209,12 @@ class FormController extends Controller {
                     if(Form::$num_errors == 0)
                     {
                         $array = array(
-                            'task_id'   => $task_id,
-                            'driver_id' => $driver_id,
-                            'address'   => $jd['address'],
-                            'suburb'    => $jd['suburb'],
-                            'postcode'  => $jd['postcode']
+                            'task_id'       => $task_id,
+                            'driver_id'     => $driver_id,
+                            'address'       => $jd['address'],
+                            'suburb'        => $jd['suburb'],
+                            'postcode'      => $jd['postcode'],
+                            'runsheet_id'   => $runsheet_id
                         );
                         if($this->dataSubbed($jd['address2']))
                             $array['address_2'] = $jd['address2'];
@@ -264,11 +265,12 @@ class FormController extends Controller {
                     if(Form::$num_errors == 0)
                     {
                         $array = array(
-                            'task_id'   => $task_id,
-                            'driver_id' => $driver_id,
-                            'address'   => $od['address'],
-                            'suburb'    => $od['suburb'],
-                            'postcode'  => $od['postcode']
+                            'task_id'       => $task_id,
+                            'driver_id'     => $driver_id,
+                            'address'       => $od['address'],
+                            'suburb'        => $od['suburb'],
+                            'postcode'      => $od['postcode'],
+                            'runsheet_id'   => $runsheet_id
                         );
                         if($this->dataSubbed($od['address2']))
                             $array['address_2'] = $od['address2'];
@@ -290,9 +292,9 @@ class FormController extends Controller {
         }
         else
         {
-            echo "JOBS<pre>",print_r($tasks['jobs']),"</pre>";
-            echo "ORDERS<pre>",print_r($tasks['orders']),"</pre>";
-            echo "POST DATA<pre>",print_r($post_data),"</pre>"; //die();
+            //echo "JOBS<pre>",print_r($tasks['jobs']),"</pre>";
+            //echo "ORDERS<pre>",print_r($tasks['orders']),"</pre>";
+            //echo "POST DATA<pre>",print_r($post_data),"</pre>"; //die();
             echo "WILL SAVE THE FOLLOWING<pre>",print_r($tts),"</pre>"; die();
         }
         return $this->redirector->to(PUBLIC_ROOT."runsheets/prepare-runsheet/runsheet=$runsheet_id");
