9da06061628cf0d4d94e3c05da61f29329f77452
marxolly <mark@solly.com.au>
2020-11-18 11:22:09 +1100

    Update prepareSheet.php

--- app/views/runsheets/prepareSheet.php
+++ app/views/runsheets/prepareSheet.php
@@ -1,7 +1,7 @@
 <?php
 function getFormValue($arraypath = "tasks,jobs,381,units")
 {
-    echo "<p>".$arraypath."</p>";
+    //echo "<p>".$arraypath."</p>";
     $path = explode(',', $arraypath);
     $result = Form::$values;
     //$max_ind = 0;
@@ -68,7 +68,7 @@ function getFormValue($arraypath = "tasks,jobs,381,units")
                                             <div class="form-group row">
                                                 <label class="col-3">Units</label>
                                                 <div class="col-6">
-                                                    <input type="text" class="form-control" name="tasks[jobs][<?php echo $task['job_id'];?>][units]" value="<?php echo getFormValue("task,jobs,".$task['job_id'].",units");?>">
+                                                    <input type="text" class="form-control" name="tasks[jobs][<?php echo $task['job_id'];?>][units]" value="<?php echo getFormValue("tasks,jobs,".$task['job_id'].",units");?>">
                                                 </div>
                                             </div>
                                             <div class="form-group row">
