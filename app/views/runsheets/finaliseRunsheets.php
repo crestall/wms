a378993898043b30c4c86dae4242a408d482db93
marxolly <mark@solly.com.au>
2020-11-20 15:19:25 +1100

    Update finaliseRunsheets.php

--- app/views/runsheets/finaliseRunsheets.php
+++ app/views/runsheets/finaliseRunsheets.php
@@ -30,8 +30,8 @@
                             $rows = count($rs['drivers']);?>
                             <tr>
                                 <td rowspan="<?php echo $rows;?>" style="vertical-align: middle"><h4><?php echo date('D jS M', $timestamp );?></h4></td>
-                                <td><?php echo ucwords($rs['drivers'][0]['name']);?></td>
-                                <td><a class="btn btn-lg btn-outline-success" href="runsheets/finalise-runsheet/runsheet=<?php echo $rs['runsheet_id'];?>/driver=<?php echo $rs['drivers'][0]['id'];?>">Finalise</a></td>
+                                <td style="vertical-align: middle"><?php echo ucwords($rs['drivers'][0]['name']);?></td>
+                                <td style="text-align:center;"><a class="btn btn-lg btn-outline-success" href="runsheets/finalise-runsheet/runsheet=<?php echo $rs['runsheet_id'];?>/driver=<?php echo $rs['drivers'][0]['id'];?>">Finalise</a></td>
                             </tr>
                             <?php for($i = 1; $i < $rows; ++$i):?>
                                 <tr>
