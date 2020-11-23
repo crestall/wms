54129ec60edfe446f80edd0932a6405239ba28d0
marxolly <mark@solly.com.au>
2020-11-23 11:48:50 +1100

    Update completeRunsheet.php

--- app/views/runsheets/completeRunsheet.php
+++ app/views/runsheets/completeRunsheet.php
@@ -15,37 +15,47 @@
             //$driver_id = (empty(Form::value('driver_id')))? $runsheet['driver_id'] : Form::value('driver_id');
             //$units = (empty(Form::value('units')))? ($runsheet['units'] > 0)?$runsheet['units']: "" : Form::value('units');
             //echo "<p>Form Values For 381: ".getFormValue()."</p>";
-            echo "<pre>",print_r($tasks),"</pre>";//die();
+            //echo "<pre>",print_r($tasks),"</pre>";//die();
             ?>
             <div class="row">
                 <div class="col-12">
                     <h2>Complete Tasks for Runsheet <?php echo date('D jS M', $tasks[0]['runsheet_day']);?> using <?php echo $tasks[0]['driver_name'];?></h2>
                 </div>
             </div>
-            <div class="row">
-                <label class="col-md-4 col-form-label">Tasks To Be Completed</label>
-                <div class="col-md-8 mb-3">
-                    <?php foreach($tasks as $task):
-                        $task_id = $task['id'];
-                        if($task['job_id'] > 0)
-                        {
-                            $label_string = "<span class='font-weight-bold'>".$task['job_number']."</span> - ".$task['customer_name'];
-                        }
-                        else
-                        {
-                            $label_string = "<span class='font-weight-bold'>".$task['order_number']."</span> - ".$task['order_customer']."(".$task['order_client_name'].")";
-                            $label_string .= (empty($task['client_order_id']))? "" : " - ".$task['client_order_id'];
-                        }?>
-                        <div class="col-12">
-                            <label class="col-form-label" for="task_<?php echo $task_id;?>"></label>
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input task" data-taskid="<?php echo $task_id;?>" id="task_<?php echo $task_id;?>" name="tasks[<?php echo $task_id;?>]" <?php if(!empty(Form::value('tasks['.$task_id.']'))) echo "checked";?> />
-                                <label class="custom-control-label" for="task_<?php echo $task['id'];?>"><span class="font-weight-bold"><?php echo $label_string;?></label>
+            <form id="complete_runsheet_tasks" method="post" action="/form/procCompletRunsheetTasks">
+                <div class="row">
+                    <label class="col-md-4 col-form-label">Tasks To Be Completed</label>
+                    <div class="col-md-8 mb-3">
+                        <?php foreach($tasks as $task):
+                            $task_id = $task['id'];
+                            if($task['job_id'] > 0)
+                            {
+                                $label_string = "<span class='font-weight-bold'>".$task['job_number']."</span> - ".$task['customer_name'];
+                            }
+                            else
+                            {
+                                $label_string = "<span class='font-weight-bold'>".$task['order_number']."</span> - ".$task['order_customer']."(".$task['order_client_name'].")";
+                                $label_string .= (empty($task['client_order_id']))? "" : " - ".$task['client_order_id'];
+                            }?>
+                            <div class="col-12">
+                                <label class="col-form-label" for="task_<?php echo $task_id;?>"></label>
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" class="custom-control-input task" data-taskid="<?php echo $task_id;?>" id="task_<?php echo $task_id;?>" name="tasks[<?php echo $task_id;?>]" checked />
+                                    <label class="custom-control-label" for="task_<?php echo $task['id'];?>"><span class="font-weight-bold"><?php echo $label_string;?></label>
+                                </div>
                             </div>
-                        </div>
-                    <?php endforeach;?>
+                        <?php endforeach;?>
+                    </div>
                 </div>
-            </div>
+                <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" >
+                <input type="hidden" name="runsheet_id" id="runsheet_id" value="<?php echo $runsheet_id;?>" >
+                <input type="hidden" name="driver_id" id="driver_id" value="<?php echo $driver_id;?>" >
+                <div class="form-group row">
+                    <div class="col-md-5 offset-md-2">
+                        <button type="submit" class="btn btn-outline-secondary" id="submitter">Complete Selected Tasks</button>
+                    </div>
+                </div>
+            </form>
         <?php endif;?>
     </div>
 </div>
