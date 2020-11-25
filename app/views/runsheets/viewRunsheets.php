1adc8a0fb9a36f6c101c72db4e4af4f13de16001
marxolly <mark@solly.com.au>
2020-11-25 15:41:50 +1100

    Update viewRunsheets.php

--- app/views/runsheets/viewRunsheets.php
+++ app/views/runsheets/viewRunsheets.php
@@ -6,17 +6,26 @@
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <?php if(count($runsheets)):?>
             <?php echo "<pre>",print_r($runsheets),"</pre>"; //die();?>
-            <div class="row">
-                <div class="col-12">
-                    <?php if(isset($_SESSION['feedback'])) :?>
-                       <div class='feedbackbox'><?php echo Session::getAndDestroy('feedback');?></div>
-                    <?php endif; ?>
-                    <?php if(isset($_SESSION['errorfeedback'])) :?>
-                       <div class='errorbox'><?php echo Session::getAndDestroy('errorfeedback');?></div>
-                    <?php endif; ?>
+            <div id="waiting" class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Drawing Table..</h2>
+                    <p>May take a few moments</p>
+                    <img class='loading' src='/images/preloader.gif' alt='loading...' />
                 </div>
-                <div class="col-12">
-                    <?php include(Config::get('VIEWS_PATH')."layout/page-includes/print_runsheets_table.php");?>
+            </div>
+            <div class="row mt-4" id="table_holder" style="display:none">
+                <div class="row">
+                    <div class="col-12">
+                        <?php if(isset($_SESSION['feedback'])) :?>
+                           <div class='feedbackbox'><?php echo Session::getAndDestroy('feedback');?></div>
+                        <?php endif; ?>
+                        <?php if(isset($_SESSION['errorfeedback'])) :?>
+                           <div class='errorbox'><?php echo Session::getAndDestroy('errorfeedback');?></div>
+                        <?php endif; ?>
+                    </div>
+                    <div class="col-12">
+                        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/print_runsheets_table.php");?>
+                    </div>
                 </div>
             </div>
         <?php else:?>
