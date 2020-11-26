1a69b96b72f837d42fa511345b2b75b177fa95ed
marxolly <mark@solly.com.au>
2020-11-26 11:26:53 +1100

    datatable tidy

--- public_html/scripts/common.js
+++ public_html/scripts/common.js
@@ -357,7 +357,40 @@ var dataTable = {
                 "sLengthMenu": "Rows to Show _MENU_"
             },
             "lengthMenu": [[25, 50, 75, -1], [25, 50, 75, "All"]],
-            "pageLength": 25
+            "pageLength": 25,
+            "fnDrawCallback": function() {
+                $table = $(this);
+                // only apply this to specific tables
+                if ($table.closest(".datatable-multi-row").length) {
+                    // for each row in the table body...
+                    $table.find("tbody>tr").each(function() {
+                        var $tr = $(this);
+                        // get the "extra row" content from the <script> tag.
+                        // note, this could be any DOM object in the row.
+                        var extra_row = $tr.find(".extra-row-content").html();
+                        // in case draw() fires multiple times,
+                        // we only want to add new rows once.
+                        if (!$tr.next().hasClass('dt-added')) {
+                            $tr.after(extra_row);
+                            $tr.find("td").each(function() {
+                                // for each cell in the top row,
+                                // set the "rowspan" according to the data value.
+                                var $td = $(this);
+                                var rowspan = parseInt($td.data("datatable-multi-row-rowspan"), 10);
+                                if (rowspan) {
+                                    $td.attr('rowspan', rowspan);
+                                }
+                            });
+                        }
+                    });
+                } // end if the table has the proper class
+                actions['common']['runsheetPrint']();
+                $('button.print-sheet').each(function(i,e){
+                    $(this).click(function(e){
+                        location.reload();
+                    });
+                });
+            } // end fnDrawCallback()
         };
 
         $.extend( opts, options );
