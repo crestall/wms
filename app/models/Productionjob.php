053e2b85fc41e31736c53e077cd380b0f78e90d4
marxolly <mark@solly.com.au>
2020-11-27 10:59:08 +1100

    add job priority

--- app/models/Productionjob.php
+++ app/models/Productionjob.php
@@ -157,6 +157,7 @@ class Productionjob extends Model{
         if(!empty($data['salesrep_id'])) $vals['salesrep_id'] = $data['salesrep_id'];
         if(!empty($data['designer'])) $vals['designer'] = $data['designer'];
         if(!empty($data['notes'])) $vals['notes'] = $data['notes'];
+        if(!empty($data['priority'])) $vals['priority'] = $data['priority'];
         $id = $db->insertQuery($this->table, $vals);
         return $id;
     }
