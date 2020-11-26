d3a4072a54378ac78ca93899c0aea108410570a6
marxolly <mark@solly.com.au>
2020-11-26 11:46:12 +1100

    Update common.js

--- public_html/scripts/common.js
+++ public_html/scripts/common.js
@@ -360,21 +360,14 @@ var dataTable = {
             "pageLength": 25,
             "fnDrawCallback": function() {
                 $table = $(this);
-                // only apply this to specific tables
                 if ($table.closest(".datatable-multi-row").length) {
                     // for each row in the table body...
                     $table.find("tbody>tr").each(function() {
                         var $tr = $(this);
-                        // get the "extra row" content from the <script> tag.
-                        // note, this could be any DOM object in the row.
                         var extra_row = $tr.find(".extra-row-content").html();
-                        // in case draw() fires multiple times,
-                        // we only want to add new rows once.
                         if (!$tr.next().hasClass('dt-added')) {
                             $tr.after(extra_row);
                             $tr.find("td").each(function() {
-                                // for each cell in the top row,
-                                // set the "rowspan" according to the data value.
                                 var $td = $(this);
                                 var rowspan = parseInt($td.data("datatable-multi-row-rowspan"), 10);
                                 if (rowspan) {
@@ -383,7 +376,15 @@ var dataTable = {
                             });
                         }
                     });
-                } // end if the table has the proper class
+                }
+                if ($table.closest(".datatable-viewrunsheets").length) {
+                    actions['common']['runsheetPrint']();
+                    $('button.print-sheet').each(function(i,e){
+                        $(this).click(function(e){
+                            location.reload();
+                        });
+                    });
+                }
             } // end fnDrawCallback()
         };
 
