d048090134f675c5b65e2d7b8a284a95a3511580
marxolly <mark@solly.com.au>
2020-11-27 14:52:11 +1100

    Update add_finisher_contact.php

--- app/views/layout/page-includes/add_finisher_contact.php
+++ app/views/layout/page-includes/add_finisher_contact.php
@@ -3,10 +3,21 @@ $i = (isset($i))? $i : 0;
 ?>
 <div class="p-3 light-grey mb-3 acontact">
     <div class="form-group row">
-        <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Name</label>
-        <div class="col-md-4">
-            <input type="text" class="form-control required" name="name" id="name" value="<?php echo Form::value('name');?>" />
-            <?php echo Form::displayError('name');?>
+        <label class="col-md-2 mb-3">Name</label>
+        <div class="col-md-4 mb-3">
+            <input type="text" class="form-control" name="contact[<?php echo $i;?>][name]" value="" />
+        </div>
+        <label class="col-md-2 mb-3">Role</label>
+        <div class="col-md-4 mb-3">
+            <input type="text" class="form-control" name="contact[<?php echo $i;?>][role]" value="" />
+        </div>
+        <label class="col-md-2 mb-3">Email</label>
+        <div class="col-md-4 mb-3">
+            <input type="text" class="form-control email" name="contact[<?php echo $i;?>][email]" value="" />
+        </div>
+        <label class="col-md-2 mb-3">Phone</label>
+        <div class="col-md-4 mb-3">
+            <input type="text" class="form-control" name="contact[<?php echo $i;?>][phone]" value="" />
         </div>
     </div>
 </div>
