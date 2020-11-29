bfa50be2fba588e5f542b24f8ceae95031c8cd48
marxolly <mark@solly.com.au>
2020-11-30 10:37:45 +1100

    Update addMiscTask.php

--- app/views/runsheets/addMiscTask.php
+++ app/views/runsheets/addMiscTask.php
@@ -7,7 +7,11 @@
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
         <?php echo Form::displayError('general');?>
         <form id="complete_runsheet_tasks" method="post" action="/form/procAddMiscTask">
-            <div class="row">
+            <div class="form-gropu row">
+                <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Deliver To</label>
+                <div class="col-md-4">
+                    <input type="text" class="required form-control" name="ship_to" id="ship_to" value="<?php echo Form::value('ship_to');?>">
+                </div>
             </div>
             <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" >
             <input type="hidden" name="runsheet_id" id="runsheet_id" value="<?php echo $runsheet_id;?>" >
