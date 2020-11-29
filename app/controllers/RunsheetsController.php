284ff3476d2af6e4a66f99f722b9de155cb5c7d0
marxolly <mark@solly.com.au>
2020-11-30 09:25:59 +1100

    Update RunsheetsController.php

--- app/controllers/RunsheetsController.php
+++ app/controllers/RunsheetsController.php
@@ -370,17 +370,15 @@ class RunsheetsController extends Controller
     {
         if(!(isset($this->request->params['args']['runsheet'])))
         {
-             return (new ErrorsController())->error(400)->send();
+            return (new ErrorsController())->error(400)->send();
         }
-        else
-        {
-            $runsheet_id = $this->request->params['args']['runsheet'];
-        }
+        $runsheet_id = $this->request->params['args']['runsheet'];
+        $rs = $this->runsheet->getRunsheetById($runsheet_id)
         //render the page
         Config::setJsConfig('curPage', "add-misc-task");
         Config::set('curPage', "add-misc-task");
         $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/runsheets/", Config::get('VIEWS_PATH') . 'runsheets/addMiscTask.php', [
-            'page_title'    =>  "Add Miscellaneous Task To Runsheet",
+            'page_title'    =>  "Add Miscellaneous Task To Runsheet For ".date('D jS M', $rs['runsheet_day'] ),
             'pht'           =>  ": Add Misc Task",
             'runsheet_id'   =>  $runsheet_id
         ]);
