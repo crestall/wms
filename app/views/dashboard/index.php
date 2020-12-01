57f4c41d30cd7a2a7292f08d3a54e9e67a070f64
marxolly <mark@solly.com.au>
2020-12-01 14:29:23 +1100

    Update index.php

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -116,14 +116,6 @@ $c = 1;
                 ------------------------------------------------------------------------------------------------------------------------------------------>
                     <div class="card homepagecard">
                         <div class="card-header">
-                            <h4>Add A New Job</h4>
-                        </div>
-                        <div class="card-body text-center">
-                        	<a class="btn btn-lg btn-outline-fsg" href="/jobs/add-job"><span class="fa-layers fa-fw fa-3x align-middle"><i class="fad fa-tasks"></i><i class="far fa-plus" data-fa-transform="shrink-4 up-3 right-4"></i></span>&nbsp;<span style="font-size:larger">Add!</span></a>
-                        </div>
-                    </div>
-                    <div class="card homepagecard">
-                        <div class="card-header">
                             <h4>Add A New Customer</h4>
                         </div>
                         <div class="card-body text-center">
@@ -147,11 +139,19 @@ $c = 1;
                     	<a class="btn btn-lg btn-outline-fsg" href="/jobs/view-jobs"><span class="fa-layers fa-fw fa-3x align-middle"><i class="fad fa-tasks"></i><i class="fal fa-binoculars" data-fa-transform="shrink-6 up-2 right-6 rotate-30"></i></span>&nbsp;<span style="font-size:larger">Look!</span></a>
                     </div>
                 </div>
+                <div class="card homepagecard">
+                    <div class="card-header">
+                        <h4>Add A New Job</h4>
+                    </div>
+                    <div class="card-body text-center">
+                    	<a class="btn btn-lg btn-outline-fsg" href="/jobs/add-job"><span class="fa-layers fa-fw fa-3x align-middle"><i class="fad fa-tasks"></i><i class="far fa-plus" data-fa-transform="shrink-4 up-3 right-4"></i></span>&nbsp;<span style="font-size:larger">Add!</span></a>
+                    </div>
+                </div>
                 <?php if($user_role == "production"):?>
                 <!----------------------------------------------------------------------------------------------------------------------------------------
                 ---------------------------------------     Production     --------------------------------------------------------------------------
                 ------------------------------------------------------------------------------------------------------------------------------------------>
-                    
+
                 <?php endif;?>
                 <?php if($user_role == "production sales admin"):?>
                 <!----------------------------------------------------------------------------------------------------------------------------------------
