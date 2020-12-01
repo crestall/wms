561c1197ae966f03e47e35bd4813c7aa924a98af
Mark Solly <msolly@contactpoint.com.au>
2020-12-02 10:19:15 +1100

    Update production_home.php

--- app/views/layout/page-includes/home_pages/production_home.php
+++ app/views/layout/page-includes/home_pages/production_home.php
@@ -2,20 +2,8 @@
     <h2>Quick Links</h2>
 </div>
 <div class="card-deck homepagedeck">
-    <div class="card homepagecard">
-        <div class="card-header">
-            <h4>View Jobs</h4>
-        </div>
-        <div class="card-body text-center">
-        	<a class="btn btn-lg btn-outline-fsg" href="/jobs/view-jobs"><span class="fa-layers fa-fw fa-3x align-middle"><i class="fad fa-tasks"></i><i class="fal fa-binoculars" data-fa-transform="shrink-6 up-2 right-6 rotate-30"></i></span>&nbsp;<span style="font-size:larger">Look!</span></a>
-        </div>
-    </div>
-    <div class="card homepagecard">
-        <div class="card-header">
-            <h4>Add A New Job</h4>
-        </div>
-        <div class="card-body text-center">
-        	<a class="btn btn-lg btn-outline-fsg" href="/jobs/add-job"><span class="fa-layers fa-fw fa-3x align-middle"><i class="fad fa-tasks"></i><i class="far fa-plus" data-fa-transform="shrink-4 up-3 right-4"></i></span>&nbsp;<span style="font-size:larger">Add!</span></a>
-        </div>
-    </div>
+    <?php
+    include(Config::get('VIEWS_PATH')."layout/page-includes/home_pages/quick_links/view-jobs.php");
+    include(Config::get('VIEWS_PATH')."layout/page-includes/home_pages/quick_links/add-job.php");
+    ?>
 </div>
