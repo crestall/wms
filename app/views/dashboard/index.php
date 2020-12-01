1abbc6e1f49bd22c7ccbbac2a8cecba421a75073
marxolly <mark@solly.com.au>
2020-12-01 14:18:33 +1100

    Update index.php

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -153,14 +153,6 @@ $c = 1;
                 ------------------------------------------------------------------------------------------------------------------------------------------>
                     <div class="card homepagecard">
                         <div class="card-header">
-                            <h4>View Customers</h4>
-                        </div>
-                        <div class="card-body text-center">
-                        	<a class="btn btn-lg btn-outline-fsg" href="/customers/view-customers"><span class="fa-layers fa-fw fa-3x align-middle"><i class="fad fa-user-tie"></i><i class="fal fa-binoculars" data-fa-transform="shrink-6 up-2 right-6 rotate-30"></i></span>&nbsp;<span style="font-size:larger">Look!</span></a>
-                        </div>
-                    </div>
-                    <div class="card homepagecard">
-                        <div class="card-header">
                             <h4>View Finishers</h4>
                         </div>
                         <div class="card-body text-center">
