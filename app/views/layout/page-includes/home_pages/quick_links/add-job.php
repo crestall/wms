86497c8f79f8eaad4075130ac6731088560b6c74
Mark Solly <msolly@contactpoint.com.au>
2020-12-02 10:00:15 +1100

    index tidying

---
+++ app/views/layout/page-includes/home_pages/quick_links/add-job.php
@@ -0,0 +1,8 @@
+    <div class="card homepagecard">
+        <div class="card-header">
+            <h4>Add A New Job</h4>
+        </div>
+        <div class="card-body text-center">
+        	<a class="btn btn-lg btn-outline-fsg" href="/jobs/add-job"><span class="fa-layers fa-fw fa-3x align-middle"><i class="fad fa-tasks"></i><i class="far fa-plus" data-fa-transform="shrink-4 up-3 right-4"></i></span>&nbsp;<span style="font-size:larger">Add!</span></a>
+        </div>
+    </div>
