b2ba08432ed4fc71a8d7f9c7f38e8702ccb6c1eb
marxolly <mark@solly.com.au>
2020-12-01 12:19:24 +1100

    Update view_runsheets_table.php

--- app/views/layout/page-includes/view_runsheets_table.php
+++ app/views/layout/page-includes/view_runsheets_table.php
@@ -9,6 +9,7 @@ function getDriverTasks($driver, $runsheet_id)
     $html .= "<td data-label='Tasks'>";
     foreach($driver['tasks'] as $task)
     {
+        $html .= "<pre>".print_r($task, true)."</pre>";
         $can_be_completed = true;
         $task_number = (!empty($task['job_number']))? "JOB: ".$task['customer']." - ".$task['job_number'] : ($task['order_number'] > 0)? "ORDER: ".$task['customer']." - ".$task['order_number'] : "MISCELLANEOUS TASK";
         $task_number .= (isset($task['client_order_id']) && !empty($task['client_order_id']))? " (".$task['client_order_id'].")" : "";
