1c8cf3e919693b274e99e9b8feae2d6138caecb3
marxolly <mark@solly.com.au>
2020-12-03 11:46:07 +1100

    Update JobsController.php

--- app/controllers/JobsController.php
+++ app/controllers/JobsController.php
@@ -158,6 +158,11 @@ class JobsController extends Controller
             'index',
             'viewJobs'
         ));
+        //production sales users
+        Permission::allow(['production_sales_admin', 'production_sales'], $resource, array(
+            'index',
+            'viewJobs'
+        ));
 
         return Permission::check($role, $resource, $action);
     }
