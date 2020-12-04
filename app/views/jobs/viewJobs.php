35c93d2b49396782c188764cf4329ce0f6304db7
marxolly <mark@solly.com.au>
2020-12-04 14:45:53 +1100

    Update viewJobs.php

--- app/views/jobs/viewJobs.php
+++ app/views/jobs/viewJobs.php
@@ -27,13 +27,13 @@ $need_checkbox = ($can_do_runsheets || $can_change_status);
                 <div class="col-md-4 mb-3">
                     <select id="status_id" name="status_ids[]" class="form-control selectpicker" data-style="btn-outline-secondary" data-live-search="true" data-actions-box="true" multiple title="Filter by any of the following..."><?php echo $this->controller->jobstatus->getMultiSelectJobStatus($status_ids, 1, true);?></select>
                 </div>
-                <div class="custom-control custom-checkbox col-md-4">
+                <div class="custom-control custom-checkbox custom-control-right col-md-4">
                     <input class="custom-control-input" type="checkbox" id="completed" name="completed"  />
-                    <label class="custom-control-label col-md-4" for="completed">Include Completed Jobs</label>
+                    <label class="custom-control-label col-md-8" for="completed">Include Completed Jobs</label>
                 </div>
-                <div class="custom-control custom-checkbox col-md-4">
+                <div class="custom-control custom-checkbox custom-control-right col-md-4">
                     <input class="custom-control-input" type="checkbox" id="cancelled" name="cancelled"  />
-                    <label class="custom-control-label col-md-4" for="completed">Include Cancelled Jobs</label>
+                    <label class="custom-control-label col-md-8" for="completed">Include Cancelled Jobs</label>
                 </div>
                 <div class="col-md-2 offset-md-8 mb-3">
                     <button class="btn btn-outline-danger" id="unfilter_jobs">Remove Filters</button>
