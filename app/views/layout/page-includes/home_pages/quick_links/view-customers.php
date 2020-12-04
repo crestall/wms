fc6aa0d1229b27e58c380bc12df93a6a5a3a2d85
marxolly <mark@solly.com.au>
2020-12-04 11:31:49 +1100

    Update view-customers.php

--- app/views/layout/page-includes/home_pages/quick_links/view-customers.php
+++ app/views/layout/page-includes/home_pages/quick_links/view-customers.php
@@ -3,6 +3,6 @@
             <h4>View Customers</h4>
         </div>
         <div class="card-body text-center">
-        	<a class="btn btn-lg btn-outline-fsg" href="/finishers/view-finishers"><span class="fa-layers fa-fw fa-3x align-middle"><i class="fad fa-user-tie"></i><i class="fal fa-binoculars" data-fa-transform="shrink-6 up-2 right-6 rotate-30"></i></span>&nbsp;<span style="font-size:larger">Look!</span></a>
+        	<a class="btn btn-lg btn-outline-fsg" href="/customers/view-customers"><span class="fa-layers fa-fw fa-3x align-middle"><i class="fad fa-user-tie"></i><i class="fal fa-binoculars" data-fa-transform="shrink-6 up-2 right-6 rotate-30"></i></span>&nbsp;<span style="font-size:larger">Look!</span></a>
         </div>
     </div>
