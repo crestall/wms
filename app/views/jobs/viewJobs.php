50f0b2d6dd04ac15eea0b490686d31d61cfbbc4b
marxolly <mark@solly.com.au>
2020-12-08 14:11:05 +1100

    Update viewJobs.php

--- app/views/jobs/viewJobs.php
+++ app/views/jobs/viewJobs.php
@@ -1,7 +1,7 @@
 <?php
-$can_do_runsheets  = ($user_role == "production" || $user_role == "production_admin" || $user_role == "admin" || $user_role == "super_admin");
+$can_do_runsheets  = Permission::canDoRunsheets($user_role);
 //$can_change_status = ($user_role == "production" || $user_role == "production_admin" || $user_role == "admin" || $user_role == "super_admin");
-$can_change_status = true;
+$can_change_status = Permission::canChangeStatus($user_role);
 $need_checkbox = ($can_do_runsheets || $can_change_status);
 ?>
 <div id="page-wrapper">
