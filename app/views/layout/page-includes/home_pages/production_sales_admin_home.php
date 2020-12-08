8e9212d12496b04c20bcc4d092bf819fc0b6e5b6
marxolly <mark@solly.com.au>
2020-12-09 09:26:19 +1100

    Update production_sales_admin_home.php

--- app/views/layout/page-includes/home_pages/production_sales_admin_home.php
+++ app/views/layout/page-includes/home_pages/production_sales_admin_home.php
@@ -5,6 +5,8 @@
     <?php
     include(Config::get('VIEWS_PATH')."layout/page-includes/home_pages/quick_links/view-jobs.php");
     include(Config::get('VIEWS_PATH')."layout/page-includes/home_pages/quick_links/view-customers.php");
+    include(Config::get('VIEWS_PATH')."layout/page-includes/home_pages/quick_links/add-customer.php");
     include(Config::get('VIEWS_PATH')."layout/page-includes/home_pages/quick_links/view-finishers.php");
+    include(Config::get('VIEWS_PATH')."layout/page-includes/home_pages/quick_links/add-finisher.php");
     ?>
 </div>
