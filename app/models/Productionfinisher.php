1ab8c91ccbd70661c72730956376251450d6f9d1
marxolly <mark@solly.com.au>
2020-12-09 15:35:49 +1100

    Update Productionfinisher.php

--- app/models/Productionfinisher.php
+++ app/models/Productionfinisher.php
@@ -219,6 +219,7 @@ class Productionfinisher extends Model{
         return "
             SELECT
                 pf.*,
+                GROUP_CONCAT(DISTINCT pc.id SEPARATOR ', ') AS contact_id,
                 GROUP_CONCAT(DISTINCT pc.name SEPARATOR ', ') AS contact_name,
                 GROUP_CONCAT(DISTINCT pc.email SEPARATOR ', ') AS contact_email,
                 GROUP_CONCAT(DISTINCT pc.phone SEPARATOR ', ') AS contact_phone,
