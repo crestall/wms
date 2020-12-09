0df7d04f4e783475e8acdc40730378dc20d09e34
marxolly <mark@solly.com.au>
2020-12-09 12:53:55 +1100

    Update add_finisher_contact.php

--- app/views/layout/page-includes/add_finisher_contact.php
+++ app/views/layout/page-includes/add_finisher_contact.php
@@ -9,7 +9,7 @@ $details['phone'] = isset($d['phone'])? $d['phone']:"";
     <div class="form-group row">
         <label class="col-md-2 mb-3">Name</label>
         <div class="col-md-4 mb-3">
-            <input type="text" class="form-control" name="contacts[<?php echo $i;?>][name]" value="<?php echo $details['name'];?>" />
+            <input type="text" class="form-control required" name="contacts[<?php echo $i;?>][name]" value="<?php echo $details['name'];?>" />
             <?php echo Form::displayError('contactname_'.$i);?>
         </div>
         <label class="col-md-2 mb-3">Role</label>
@@ -19,6 +19,7 @@ $details['phone'] = isset($d['phone'])? $d['phone']:"";
         <label class="col-md-2 mb-3">Email</label>
         <div class="col-md-4 mb-3">
             <input type="text" class="form-control email" name="contacts[<?php echo $i;?>][email]" value="<?php echo $details['email'];?>" />
+            <?php echo Form::displayError('contactemail_'.$i);?>
         </div>
         <label class="col-md-2 mb-3">Phone</label>
         <div class="col-md-4 mb-3">
