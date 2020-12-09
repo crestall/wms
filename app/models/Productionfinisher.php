baa90e3c50227fb65edbfb4059ae12bfa64a7f71
marxolly <mark@solly.com.au>
2020-12-09 14:28:02 +1100

    Update Productionfinisher.php

--- app/models/Productionfinisher.php
+++ app/models/Productionfinisher.php
@@ -110,7 +110,7 @@ class Productionfinisher extends Model{
         if(!empty($data['postcode'])) $vals['postcode'] = $data['postcode'];
         if(!empty($data['country'])) $vals['country'] = $data['country'];
         if(!empty($data['website'])) $vals['website'] = $data['website'];
-        //$id = $db->insertQuery($this->table, $vals);
+        $id = $db->insertQuery($this->table, $vals);
         if(isset($data['categories']) && is_array($data['categories']))
         {
             $fcat = new Finishercategories();
@@ -118,7 +118,6 @@ class Productionfinisher extends Model{
         }
         if(isset($data['contacts']) && is_array($data['contacts']))
         {
-            $id = 126;
             foreach($data['contacts'] as $contact)
             {
                 $contact['finisher_id'] = $id;
