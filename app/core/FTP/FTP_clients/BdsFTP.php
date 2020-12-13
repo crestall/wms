ebce1aebfabc5496739795694904a8f7a26316bf
marxolly <mark@solly.com.au>
2020-12-14 10:51:31 +1100

    Create BdsFTP.php

---
+++ app/core/FTP/FTP_clients/BdsFTP.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * BDS implimentation of the FTP class.
+ *
+ *
+ * @author     Mark Solly <mark.solly@fsg.com.au>
+ */
+
+class BdsFTP extends FTP
+{
+    private $client_id = 86;
+
+    private $return_array = array(
+        'orders_created'        => 0,
+        'invoices_processed'    => 0,
+        'import_error'          => false,
+        'error'                 => false,
+        'error_count'           => 0,
+        'error_string'          => '',
+        'import_error_string'   => '',
+        'import_message'        => ''
+    );
+
+    public function init()
+    {
+        //Client Specific Credentials
+        $this->URL = 'ftp://ftp.bahai.org.au/bdsorders';
+        $this->USERNAME = 'bdsorders';
+        $this->PASSWORD = 'mN**s735a';
+    }
+} //end class
+?>
