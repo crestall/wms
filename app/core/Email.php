af453123b2c97b452da6386b75eddb8e16f596c7
marxolly <mark@solly.com.au>
2020-12-16 16:05:05 +1100

    Update Email.php

--- app/core/Email.php
+++ app/core/Email.php
@@ -275,9 +275,25 @@
             $inventory_errors = "";
             if($inventory_error)
             {
-                $inventory_errors .= "<h3 class='error'>The Following Feedback Has Been Supplied Regarding Import Errors</h3><div class='errorbox'>$inventory_error_string</div>";
+                $inventory_errors .= "
+                    <h3 class='error'>The Following Feedback Has Been Supplied Regarding Inventory Errors</h3>
+                    <table cellspacing='0' cellpadding='0' border='0' style='background: none repeat scroll 0 0 #f8dbdb;border: 1px solid #ff3333;color: #ff3333;'>
+                        <tr>
+                            <td style='padding: 20px;'>$inventory_error_string</td>
+                        </tr>
+                    </table>
+                ";
             }
-            $imports = "<h3 class='success'>The Following Feedback Has Been Supplied Regarding Successful Imports</h3><div class='feedbackbox'>$import_string</div>";
+            $imports = "
+                <h3 class='success'>The Following Feedback Has Been Supplied Regarding Successful Imports</h3>
+                <table cellspacing='0' cellpadding='0' border='0' style='background: none repeat scroll 0 0 #ccebd6;border: 3px solid #009933;color: #009933;'>
+                    <tr>
+                        <td style='padding: 20px;'>$import_string</td>
+                    </tr>
+                </table>
+
+                <div class='feedbackbox'>$import_string</div>
+            ";
             $body = file_get_contents(Config::get('EMAIL_TEMPLATES_PATH')."bdsimportfeedback.html");
             $replace_array = array("{TOTAL_IMPORT}","{IMPORT_ERROR_COUNT}","{INVENTORY_ERROR_COUNT}","{IMPORT_COUNT}","{IMPORT_ERRORS}","{INVENTORY_ERRORS}","{IMPORTS}");
 		    $replace_with_array = array($total_import, $import_error_count, $inventory_error_count, $import_count,$import_errors,$inventory_errors,$imports);
