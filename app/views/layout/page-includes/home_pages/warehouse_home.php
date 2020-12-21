96a3c489433136975866423005dfe23de74a9a8b
marxolly <mark@solly.com.au>
2020-12-21 12:46:04 +1100

    Update warehouse_home.php

--- app/views/layout/page-includes/home_pages/warehouse_home.php
+++ app/views/layout/page-includes/home_pages/warehouse_home.php
@@ -35,3 +35,24 @@
 <div class="col-md-12 text-right">
     <button class="btn btn-sm btn-outline-fsg" id="chart_button_1"></button>
 </div>
+<?php if(count($backorders)):?>
+    <div class="col-md-12 text-center">
+        <h2>Current Orders With Backorders</h2>
+    </div>
+    <div class="card-deck homepagedeck">
+        <?php foreach($backorders as $o):
+            $s = ($o['order_count'] > 1)? "s" : ""; ?>
+            <div class="card homepagecard">
+                <div class="card-header">
+                    <h4><?php echo $o['client_name'];?></h4>
+                </div>
+                <div class="card-body">
+                	<i class="fad fa-truck fa-3x fa-flip-horizontal" style="vertical-align: middle;"></i>&nbsp;<span style="font-size:larger"><?php echo $o['order_count'];?> New Order<?php echo $s;?></span>
+                </div>
+                <div class="card-footer text-right">
+                    <a class="btn btn-lg btn-outline-fsg" href="/orders/view-orders/client=<?php echo $o['client_id'];?>">Manage Backorders</a>
+                </div>
+            </div>
+        <?php ++$c; endforeach;?>
+    </div>
+<?php endif;?>
