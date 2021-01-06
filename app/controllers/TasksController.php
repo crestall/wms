4be4be86c10b84cc070d469291d44df4f8093c31
marxolly <mark@solly.com.au>
2021-01-06 11:19:03 +1100

    Update TasksController.php

--- app/controllers/TasksController.php
+++ app/controllers/TasksController.php
@@ -121,7 +121,7 @@ class TasksController extends Controller
                         ++$i;
                     }
                     $rows[] = $row;
-                    $this->order->updateFTPUploaded($o['id']);
+                    $this->order->updateFTPUploaded($o['order_id']);
                 }
                 $i = 1;
                 while($i <= $extra_cols)
