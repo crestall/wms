ee6527ac975dbe7c39faeeadfb0adcc74eaacee4
marxolly <mark@solly.com.au>
2021-01-08 11:04:04 +1100

    Update AjaxfunctionsController.php

--- app/controllers/AjaxfunctionsController.php
+++ app/controllers/AjaxfunctionsController.php
@@ -1112,8 +1112,8 @@ class ajaxfunctionsController extends Controller
 
     public function fillBackorders()
     {
-        echo "<pre>",print_r($this->request),"</pre>"; die();
-        $this->order->cancelOrders($this->request->data['orderids']);
+        //echo "<pre>",print_r($this->request),"</pre>"; die();
+        $this->order->fillBackorders($this->request->data['orderids']);
         if(isset($this->request->data['showfeedback']) && $this->request->data['showfeedback'])
         {
             Session::set('feedback', '<h3>That order has been cancelled</h3><p>It should <em>NOT</em> be showing below</p>');
