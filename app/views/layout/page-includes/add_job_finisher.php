bc03d28cf9076b891026aea0bb46d317b01e2aec
marxolly <mark@solly.com.au>
2021-01-11 14:54:22 +1100

    Update add_job_finisher.php

--- app/views/layout/page-includes/add_job_finisher.php
+++ app/views/layout/page-includes/add_job_finisher.php
@@ -1,29 +1,10 @@
 <?php
 $i = (isset($i))? $i : 0;
-$details['name'] = isset($d['name'])? $d['name']:"";
-$details['role'] = isset($d['role'])? $d['role']:"";
-$details['email'] = isset($d['email'])? $d['email']:"";
-$details['phone'] = isset($d['phone'])? $d['phone']:"";
+$this_finisher = $i + 1;
+$f = new NumberFormatter("en", NumberFormatter::SPELLOUT);
 ?>
 <div class="p-3 light-grey mb-3 afinisher">
     <div class="form-group row">
-        <label class="col-md-2 mb-3">Name</label>
-        <div class="col-md-4 mb-3">
-            <input type="text" class="form-control required" name="contacts[<?php echo $i;?>][name]" value="<?php echo $details['name'];?>" />
-            <?php echo Form::displayError('contactname_'.$i);?>
-        </div>
-        <label class="col-md-2 mb-3">Role</label>
-        <div class="col-md-4 mb-3">
-            <input type="text" class="form-control" name="contacts[<?php echo $i;?>][role]" value="<?php echo $details['role'];?>" />
-        </div>
-        <label class="col-md-2 mb-3">Email</label>
-        <div class="col-md-4 mb-3">
-            <input type="text" class="form-control email" name="contacts[<?php echo $i;?>][email]" value="<?php echo $details['email'];?>" />
-            <?php echo Form::displayError('contactemail_'.$i);?>
-        </div>
-        <label class="col-md-2 mb-3">Phone</label>
-        <div class="col-md-4 mb-3">
-            <input type="text" class="form-control" name="contacts[<?php echo $i;?>][phone]" value="<?php echo $details['phone'];?>" />
-        </div>
+    <h4>Finisher <?php echo ucwords($f->format($i));?>'s Details</h4>
     </div>
 </div>
