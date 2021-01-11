d8ba8fe979f41532643e1c577b56ec073fa932e5
marxolly <mark@solly.com.au>
2021-01-11 15:24:37 +1100

    Update add_job_finisher.php

--- app/views/layout/page-includes/add_job_finisher.php
+++ app/views/layout/page-includes/add_job_finisher.php
@@ -7,7 +7,7 @@ $f = new NumberFormatter("en", NumberFormatter::SPELLOUT);
     <div class="form-group row">
         <h4 class="col-md-4">Finisher <?php echo ucwords($f->format($this_finisher));?>'s Details</h4>
         <div class="col-md-4">
-            <a data-finisher="<?php echo $i;?>" class="remove-finisher" style="cursor:pointer" title="Remove Finisher"><h4><i class="fad fa-times-square text-danger"></i> Remove This Finisher</a></h4>
+            <a data-finisher="<?php echo $i;?>" class="remove-finisher" style="cursor:pointer" title="Remove Finisher"><h5><i class="fad fa-times-square text-danger"></i> Remove This Finisher</a></h5>
         </div>
     </div>
     <div class="form-group row">
