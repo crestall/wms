d984e744cea5994b3058f4064e1744c197b85c05
marxolly <mark@solly.com.au>
2021-01-12 10:33:20 +1100

    Update footer.php

--- app/views/layout/jobs/footer.php
+++ app/views/layout/jobs/footer.php
@@ -28,7 +28,8 @@
                             $("div#finisher_"+this_finisher).remove();
                             $("div#finishers_holder div.afinisher").each(function(i,e){
                                 $(this).attr("id", "finisher_"+i);
-                                var new_num = toWords(++i);
+                                var plusi = i + 1;
+                                var new_num = toWords(plusi);
                                 var uc_new_num = new_num.charAt(0).toUpperCase() + new_num.slice(1)
                                 $(this).find("h4.finisher_title").text("Finisher "+uc_new_num+"'s Details");
                                 $(this).find("a.remove-finisher").data("finisher", i);
