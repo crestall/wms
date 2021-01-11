13737d0e981c8d95c64a440b6e5d431e9af2966f
marxolly <mark@solly.com.au>
2021-01-11 15:13:45 +1100

    Update add_job_finisher.php

--- app/views/layout/page-includes/add_job_finisher.php
+++ app/views/layout/page-includes/add_job_finisher.php
@@ -5,6 +5,13 @@ $f = new NumberFormatter("en", NumberFormatter::SPELLOUT);
 ?>
 <div class="p-3 light-grey mb-3 afinisher">
     <div class="form-group row">
-    <h4>Finisher <?php echo ucwords($f->format($this_finisher));?>'s Details</h4>
+        <h4>Finisher <?php echo ucwords($f->format($this_finisher));?>'s Details</h4>
+    </div>
+    <div class="form-group row">
+        <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Finisher Name</label>
+        <div class="col-md-4">
+            <input type="text" class="form-control required" name="finishers[<?php echo $i;?>][name]">
+            <?php echo Form::displayError('finishername_'.$i);?>
+        </div>
     </div>
 </div>
