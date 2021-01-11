b4d009a6e2f00184ff5d5da2bb5810b9d978ecbf
marxolly <mark@solly.com.au>
2021-01-11 15:20:28 +1100

    Update add_job_finisher.php

--- app/views/layout/page-includes/add_job_finisher.php
+++ app/views/layout/page-includes/add_job_finisher.php
@@ -3,9 +3,12 @@ $i = (isset($i))? $i : 0;
 $this_finisher = $i + 1;
 $f = new NumberFormatter("en", NumberFormatter::SPELLOUT);
 ?>
-<div class="p-3 light-grey mb-3 afinisher">
+<div id="finisher_<?php echo $i;?>" class="p-3 light-grey mb-3 afinisher">
     <div class="form-group row">
-        <h4>Finisher <?php echo ucwords($f->format($this_finisher));?>'s Details</h4>
+        <h4 class="col-md-4">Finisher <?php echo ucwords($f->format($this_finisher));?>'s Details</h4>
+        <div class="col-md-4">
+            <a data-finisher="<?php echo $i;?>" class="remove-finisher" style="cursor:pointer" title="Leave Only First"><h4><i class="fad fa-times-square text-danger"></i> Remove This Finisher</a></h4>
+        </div>
     </div>
     <div class="form-group row">
         <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Finisher Name</label>
