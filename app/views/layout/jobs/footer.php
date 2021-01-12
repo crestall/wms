c15ba3ede25bb62a6315eea58f597a41f5809df1
marxolly <mark@solly.com.au>
2021-01-12 11:07:32 +1100

    Update footer.php

--- app/views/layout/jobs/footer.php
+++ app/views/layout/jobs/footer.php
@@ -20,6 +20,9 @@
                             });
                         });
                     },
+                    finisherAutocomplete: function(){
+
+                    },
                     removeFinisher: function(){
                         $("a.remove-finisher").off('click').click(function(e){
                             e.preventDefault();
@@ -267,135 +270,6 @@
                         }
                         autoCompleter.addressAutoComplete($('#address'));
                         autoCompleter.suburbAutoComplete($('#suburb'));
-                        autoCompleter.addressAutoComplete($('#finisher_address'), 'finisher_');
-                        autoCompleter.suburbAutoComplete($('#finisher_suburb'), 'finisher_');
-                        $("input#finisher_name").each(function(i,e){
-                            if($(this).data('ui-autocomplete') != undefined)
-                            {
-                                $(this).autocomplete( "destroy" );
-                            }
-                            autoCompleter.productionJobFinisherAutoComplete($(this), selectFinisherCallback, changeFinisherCallback);
-                        });
-                        function selectFinisherCallback(event, ui)
-                        {
-                            $('input#finisher_contact').val(ui.item.contact);
-                            $('input#finisher_email').val(ui.item.email);
-                            $('input#finisher_phone').val(ui.item.phone);
-                            $('input#finisher_id').val(ui.item.finisher_id);
-                            $('input#finisher_address').val(ui.item.address);
-                            $('input#finisher_address2').val(ui.item.address_2);
-                            $('input#finisher_suburb').val(ui.item.suburb);
-                            $('input#finisher_state').val(ui.item.state);
-                            $('input#finisher_country').val(ui.item.country);
-                            $('input#finisher_postcode').val(ui.item.postcode);
-                            if($('#send_to_finisher').prop('checked'))
-                            {
-                                $('input#address').val(ui.item.address).valid();
-                                $('input#address2').val(ui.item.address_2);
-                                $('input#suburb').val(ui.item.suburb).valid();
-                                $('input#state').val(ui.item.state).valid();
-                                $('input#country').val(ui.item.country).valid();
-                                $('input#postcode').val(ui.item.postcode).valid();
-                            }
-                            return false;
-                        }
-                        function changeFinisherCallback(event, ui)
-                        {
-                            if (!ui.item)
-                	        {
-                                $('input#finisher_id').val(0);
-                                $('input.finisher').each(function(element, index){
-                                    $(this).val("");
-                                })
-                                return false;
-                            }
-                        }
-                        autoCompleter.addressAutoComplete($('#finisher2_address'), 'finisher2_');
-                        autoCompleter.suburbAutoComplete($('#finisher2_suburb'), 'finisher2_');
-                        $("input#finisher2_name").each(function(i,e){
-                            if($(this).data('ui-autocomplete') != undefined)
-                            {
-                                $(this).autocomplete( "destroy" );
-                            }
-                            autoCompleter.productionJobFinisherAutoComplete($(this), selectFinisher2Callback, changeFinisher2Callback);
-                        });
-                        function selectFinisher2Callback(event, ui)
-                        {
-                            $('input#finisher2_contact').val(ui.item.contact);
-                            $('input#finisher2_email').val(ui.item.email);
-                            $('input#finisher2_phone').val(ui.item.phone);
-                            $('input#finisher2_id').val(ui.item.finisher_id);
-                            $('input#finisher2_address').val(ui.item.address);
-                            $('input#finisher2_address2').val(ui.item.address_2);
-                            $('input#finisher2_suburb').val(ui.item.suburb);
-                            $('input#finisher2_state').val(ui.item.state);
-                            $('input#finisher2_country').val(ui.item.country);
-                            $('input#finisher2_postcode').val(ui.item.postcode);
-                            if($('#send_to_finisher2').prop('checked'))
-                            {
-                                $('input#address').val(ui.item.address).valid();
-                                $('input#address2').val(ui.item.address_2);
-                                $('input#suburb').val(ui.item.suburb).valid();
-                                $('input#state').val(ui.item.state).valid();
-                                $('input#country').val(ui.item.country).valid();
-                                $('input#postcode').val(ui.item.postcode).valid();
-                            }
-                            return false;
-                        }
-                        function changeFinisher2Callback(event, ui)
-                        {
-                            if (!ui.item)
-                	        {
-                                $('input#finisher2_id').val(0);
-                                $('input.finisher2').each(function(element, index){
-                                    $(this).val("");
-                                })
-                                return false;
-                            }
-                        }
-                        autoCompleter.addressAutoComplete($('#finisher3_address'), 'finisher3_');
-                        autoCompleter.suburbAutoComplete($('#finisher3_suburb'), 'finisher3_');
-                        $("input#finisher3_name").each(function(i,e){
-                            if($(this).data('ui-autocomplete') != undefined)
-                            {
-                                $(this).autocomplete( "destroy" );
-                            }
-                            autoCompleter.productionJobFinisherAutoComplete($(this), selectFinisher3Callback, changeFinisher3Callback);
-                        });
-                        function selectFinisher3Callback(event, ui)
-                        {
-                            $('input#finisher3_contact').val(ui.item.contact);
-                            $('input#finisher3_email').val(ui.item.email);
-                            $('input#finisher3_phone').val(ui.item.phone);
-                            $('input#finisher3_id').val(ui.item.finisher_id);
-                            $('input#finisher3_address').val(ui.item.address);
-                            $('input#finisher3_address2').val(ui.item.address_2);
-                            $('input#finisher3_suburb').val(ui.item.suburb);
-                            $('input#finisher3_state').val(ui.item.state);
-                            $('input#finisher3_country').val(ui.item.country);
-                            $('input#finisher3_postcode').val(ui.item.postcode);
-                            if($('#send_to_finisher3').prop('checked'))
-                            {
-                                $('input#address').val(ui.item.address).valid();
-                                $('input#address2').val(ui.item.address_2);
-                                $('input#suburb').val(ui.item.suburb).valid();
-                                $('input#state').val(ui.item.state).valid();
-                                $('input#country').val(ui.item.country).valid();
-                                $('input#postcode').val(ui.item.postcode).valid();
-                            }
-                            return false;
-                        }
-                        function changeFinisher3Callback(event, ui)
-                        {
-                            if (!ui.item)
-                	        {
-                                $('input#finisher3_id').val(0);
-                                $('input.finisher3').each(function(element, index){
-                                    $(this).val("");
-                                })
-                                return false;
-                            }
-                        }
                     }
                 },
                 'add-job':{
