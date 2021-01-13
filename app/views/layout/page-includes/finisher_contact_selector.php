b7e7a5bc20f8f677f78a20148e3151eeda0bb752
marxolly <mark@solly.com.au>
2021-01-13 14:00:33 +1100

    Update finisher_contact_selector.php

--- app/views/layout/page-includes/finisher_contact_selector.php
+++ app/views/layout/page-includes/finisher_contact_selector.php
@@ -1,4 +1,7 @@
 <?php
 
 ?>
-<select id="finisher_contact_id" class="form-control selectpicker" data-style="btn-outline-secondary"><option value="0">Choose One</option><?php echo $this->controller->productionfinisher->getSelectFinisherContacts($finisher_id);?></select>
+<label class="col-md-4">Finisher Contact</label>
+<div class="col-md-4">
+    <select id="finisher_contact_id" class="form-control selectpicker" data-style="btn-outline-secondary"><option value="0">Choose One</option><?php echo $this->controller->productionfinisher->getSelectFinisherContacts($finisher_id);?></select>
+</div>
