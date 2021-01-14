e677ae140858f8cdccfa534c6019161e9554247a
marxolly <mark@solly.com.au>
2021-01-14 14:26:22 +1100

    Update addCustomer.php

--- app/views/customers/addCustomer.php
+++ app/views/customers/addCustomer.php
@@ -18,6 +18,25 @@ $country = Form::value('country');
                     <?php echo Form::displayError('name');?>
                 </div>
             </div>
+            <div class="form-group row">
+                <label class="col-md-3">Phone</label>
+                <div class="col-md-4">
+                    <input type="text" class="form-control" name="phone" id="phone" value="<?php echo Form::value('phone');?>" />
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-md-3">Email</label>
+                <div class="col-md-4">
+                    <input type="text" class="form-control email" name="email" id="email" value="<?php echo Form::value('email');?>" />
+                    <?php echo Form::displayError('email');?>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-md-3">Website</label>
+                <div class="col-md-4">
+                    <input type="text" class="form-control" name="website" id="website" value="<?php echo Form::value('website');?>" />
+                </div>
+            </div>
             <div class="p-3 pb-0 mb-2 rounded-top mid-grey">
                 <div class="row mb-0">
                     <div class="col-md-4">
