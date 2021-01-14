10563124b41eaa83850358c3be509cb7dc796892
marxolly <mark@solly.com.au>
2021-01-14 14:46:56 +1100

    Update editCustomer.php

--- app/views/customers/editCustomer.php
+++ app/views/customers/editCustomer.php
@@ -1,6 +1,6 @@
 <?php
 $name       = empty(Form::value('name'))?       $customer['name']         : Form::value('name');
-$contact    = empty(Form::value('contact'))?    $customer['contact']      : Form::value('contact');
+
 $email      = empty(Form::value('email'))?      $customer['email']        : Form::value('email');
 $phone      = empty(Form::value('phone'))?      $customer['phone']        : Form::value('phone');
 $address    = empty(Form::value('address'))?    $customer['address']      : Form::value('address');
@@ -9,6 +9,26 @@ $suburb     = empty(Form::value('suburb'))?     $customer['suburb']       : Form
 $state      = empty(Form::value('state'))?      $customer['state']        : Form::value('state');
 $postcode   = empty(Form::value('postcode'))?   $customer['postcode']     : Form::value('postcode');
 $country    = empty(Form::value('country'))?    $customer['country']      : Form::value('country');
+$website    = empty(Form::value('website'))?    $customer['website']      : Form::value('website');
+//create the contacts array
+$contacts   = empty(Form::value('contacts'))?    $customer['contacts']    : Form::value('contacts');
+if(!is_array($contacts))
+{
+    $contact_array = array();
+    if(!empty($contacts))
+    {
+        $ca = explode("|", $contacts);
+        foreach($ca as $c)
+        {
+            list($a['contact_id'], $a['name'],$a['email'],$a['phone'],$a['role']) = explode(',', $c);
+            $contact_array[] = $a;
+        }
+    }
+}
+else
+{
+    $contact_array = $contacts;
+}
 ?>
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
@@ -23,12 +43,6 @@ $country    = empty(Form::value('country'))?    $customer['country']      : Form
                 </div>
             </div>
             <div class="form-group row">
-                <label class="col-md-3">Contact</label>
-                <div class="col-md-4">
-                    <input type="text" class="form-control" name="contact" id="contact" value="<?php echo $contact;?>" />
-                </div>
-            </div>
-            <div class="form-group row">
                 <label class="col-md-3">Phone</label>
                 <div class="col-md-4">
                     <input type="text" class="form-control" name="phone" id="phone" value="<?php echo $phone;?>" />
@@ -39,6 +53,42 @@ $country    = empty(Form::value('country'))?    $customer['country']      : Form
                 <div class="col-md-4">
                     <input type="text" class="form-control email" name="email" id="email" value="<?php echo $email;?>" />
                     <?php echo Form::displayError('email');?>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-md-3">Website</label>
+                <div class="col-md-4">
+                    <input type="text" class="form-control" name="website" id="website" value="<?php echo $website;?>" />
+                </div>
+            </div>
+            <div class="p-3 pb-0 mb-2 rounded-top mid-grey">
+                <div class="row mb-0">
+                    <div class="col-md-4">
+                        <h4>Contacts</h4>
+                    </div>
+                    <div class="col-md-4">
+                        <a class="add-contact" style="cursor:pointer" title="Add Another Contact"><h4><i class="fad fa-plus-square text-success"></i> Add another</a></h4>
+                    </div>
+                    <div class="col-md-4">
+                        <a id="remove-all-contacts" style="cursor:pointer" title="Leave Only First"><h4><i class="fad fa-times-square text-danger"></i> Leave only one contact</a></h4>
+                    </div>
+                </div>
+                <div id="contacts_holder">
+                    <div class="form-group row">
+                        <div class="col-12">
+                            <span class="inst">At least one contact name is required</span>
+                        </div>
+                    </div>
+                    <?php //echo "<pre>", var_dump($contact_array) ,"</pre>";//die(); ?>
+                    <?php
+                    if(!empty($contact_array)):
+                        foreach($contact_array as $i => $d)
+                        {
+                            include(Config::get('VIEWS_PATH')."layout/page-includes/add_production_contact.php");
+                        }
+                    else:
+                        include(Config::get('VIEWS_PATH')."layout/page-includes/add_production_contact.php");
+                    endif;?>
                 </div>
             </div>
             <?php include(Config::get('VIEWS_PATH')."forms/address_nr.php");?>
