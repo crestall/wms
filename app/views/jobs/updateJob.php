64ca140525aea478722624f3d975cdcf53d14916
marxolly <mark@solly.com.au>
2021-01-20 08:54:12 +1100

    Update updateJob.php

--- app/views/jobs/updateJob.php
+++ app/views/jobs/updateJob.php
@@ -76,6 +76,7 @@ while($f < $finisher_count)
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <?php echo "JOB<pre>",print_r($job),"</pre>";?>
         <?php echo "CUSTOMER<pre>",print_r($customer),"</pre>";?>
+        <?php echo "CUSTOMER CONTACTS<pre>",print_r($customer_contacts),"</pre>";?>
         <?php //echo "finisher<pre>",print_r($finisher),"</pre>";?>
         <?php //echo "SESSION<pre>",print_r($_SESSION),"</pre>";?>
         <div class="row">
