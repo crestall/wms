c29a61c6e4219ca2d5b6bd6143de608a951f7911
marxolly <mark@solly.com.au>
2021-01-19 15:17:50 +1100

    Update JobsController.php

--- app/controllers/JobsController.php
+++ app/controllers/JobsController.php
@@ -151,6 +151,9 @@ class JobsController extends Controller
             return (new ErrorsController())->error(404)->send();
         }
         $customer_info = $this->productioncustomer->getCustomerById($job_info['customer_id']);
+        $finisher_info = ($job_info['finisher_id'] > 0)? $this->productionfinisher->getFinisherById($job_info['finisher_id']) : array();
+        $finisher2_info = ($job_info['finisher2_id'] > 0)? $this->productionfinisher->getFinisherById($job_info['finisher2_id']) : array();
+        $finisher3_info = ($job_info['finisher3_id'] > 0)? $this->productionfinisher->getFinisherById($job_info['finisher3_id']) : array();
         //render the page
         Config::setJsConfig('curPage', "update-job");
         Config::set('curPage', "update-job");
@@ -159,6 +162,9 @@ class JobsController extends Controller
             'pht'           =>  ": Update Production Job",
             'job'           =>  $job_info,
             'customer'      =>  $customer_info,
+            'finisher'      =>  $finisher_info,
+            'finisher2'     =>  $finisher2_info,
+            'finisher3'     =>  $finisher3_info
         ]);
     }
 
