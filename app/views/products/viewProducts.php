fcd88c8118ccce352f75323d8d109d0725ea4a0d
marxolly <mark@solly.com.au>
2021-01-27 12:13:14 +1100

    Update viewProducts.php

--- app/views/products/viewProducts.php
+++ app/views/products/viewProducts.php
@@ -69,7 +69,7 @@
                                         <td data-label="Weight" class="number"><?php echo $p['weight'];?> kg</td>
                                         <td data-label="Pallet Item" class='text-center'><?php if($p['palletized'] > 0) echo "Yes"; else echo "No";?></td>
                                         <td data-label="Requires Bubblewrap" class='text-center'><?php if($p['requires_bubblewrap'] > 0) echo "Yes"; else echo "No";?></td>
-                                        <td data-label="Product Image"><?php echo $p['image'];?></td>
+                                        <td data-label="Product Image"><?php echo $image;?></td>
                                     </tr>
                                 <?php endforeach;?>
                             </tbody>
