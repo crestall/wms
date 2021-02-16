57bf9e6d2c8d9d4ced7d39ef93ea2105ba0843b6
marxolly <mark@solly.com.au>
2021-02-17 10:54:09 +1100

    Update createDeliveryDocket.php

--- app/views/jobs/createDeliveryDocket.php
+++ app/views/jobs/createDeliveryDocket.php
@@ -8,12 +8,10 @@
         <?php //echo "<pre>",print_r($job),"</pre>";?>
         <form id="create_delivery_docket" method="post" action="/pdf/createDeliveryDocket">
             <div class="form-group row">
-                <div class="form-group row">
-                    <label class="col-md-3">Send As</label>
-                    <div class="col-md-4">
-                        <select id="sender_id" name="sender_id" class="form-control selectpicker" data-style="btn-outline-secondary"><?php echo $this->controller->deliverydocketsender->getSelectSender(Form::value('sender_id'));?></select>
-                        <?php echo Form::displayError('sender_id');?>
-                    </div>
+                <label class="col-md-3">Send As</label>
+                <div class="col-md-4">
+                    <select id="sender_id" name="sender_id" class="form-control selectpicker" data-style="btn-outline-secondary"><?php echo $this->controller->deliverydocketsender->getSelectSender(Form::value('sender_id'));?></select>
+                    <?php echo Form::displayError('sender_id');?>
                 </div>
             </div>
         </form>
