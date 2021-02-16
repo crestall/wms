d0de03f344d28839d6774db38c73a169d4593785
marxolly <mark@solly.com.au>
2021-02-16 12:14:40 +1100

    Update viewFinishers.php

--- app/views/finishers/viewFinishers.php
+++ app/views/finishers/viewFinishers.php
@@ -5,23 +5,31 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-        <?php if(count($finishers)):
-            //echo "<pre>",print_r($finishers),"</pre>";?>
-            <?php if($role == "production admin" || $role == "production sales admin"):?>
-                <div class="row">
-                    <div class="col-lg-3 col-md-4 col-sm-6 mb-3"><button class="btn btn-sm btn-block btn-outline-danger" id="deactivate"><i class="fal fa-times-circle"></i> Delete Selected Finishers</button></div>
+        <div class="row">
+            <div class="col-12">
+                <?php if(isset($_SESSION['feedback'])) :?>
+                   <div class='feedbackbox'><?php echo Session::getAndDestroy('feedback');?></div>
+                <?php endif; ?>
+                <?php if(isset($_SESSION['errorfeedback'])) :?>
+                   <div class='errorbox'><?php echo Session::getAndDestroy('errorfeedback');?></div>
+                <?php endif; ?>
+            </div>
+        </div>
+        <?php if(count($finishers)):?>
+            <div id="waiting" class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Drawing Table..</h2>
+                    <p>May take a few moments</p>
+                    <img class='loading' src='/images/preloader.gif' alt='loading...' />
                 </div>
-            <?php endif;?>
-            <div class="row">
+            </div>
+            <div class="row" id="table_holder" style="display:none">
+                <?php if($role == "production admin" || $role == "production sales admin"):?>
+                    <div class="col-12">
+                        <div class="col-lg-3 col-md-4 col-sm-6 mb-3"><button class="btn btn-sm btn-block btn-outline-danger" id="deactivate"><i class="fal fa-times-circle"></i> Delete Selected Finishers</button></div>
+                    </div>
+                <?php endif;?>
                 <div class="col-12">
-                    <?php if(isset($_SESSION['feedback'])) :?>
-                       <div class='feedbackbox'><?php echo Session::getAndDestroy('feedback');?></div>
-                    <?php endif; ?>
-                    <?php if(isset($_SESSION['errorfeedback'])) :?>
-                       <div class='errorbox'><?php echo Session::getAndDestroy('errorfeedback');?></div>
-                    <?php endif; ?>
-                </div>
-                <div class="col" id="table_holder" style="display:none">
                     <table id="finisher_list_table" class="table-striped table-hover" style="width:90%">
                         <thead>
                         	<tr>
