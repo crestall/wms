73d0b7544067ff6b1da51dae059ccb3259587ccf
marxolly <mark@solly.com.au>
2021-02-16 14:39:31 +1100

    Update PdfController.php

--- app/controllers/PdfController.php
+++ app/controllers/PdfController.php
@@ -127,7 +127,7 @@ class pdfController extends Controller
 
     public function printJobsTable()
     {
-        echo "<pre>",print_r($this->request),"</pre>";die();
+        //echo "<pre>",print_r($this->request),"</pre>";die();
 
         $pdf = new Mympdf(['mode' => 'utf-8', 'format' => 'A4']);
         $pdf->SetDisplayMode('fullpage');
