65ed606172923cdcb8f6345b7db055c4b4a4a088
marxolly <mark@solly.com.au>
2021-02-16 11:14:26 +1100

    Update viewCustomer.php

--- app/views/customers/viewCustomer.php
+++ app/views/customers/viewCustomer.php
@@ -4,7 +4,7 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
         <?php if(empty($customer['id'])):?>
-            <?php include(Config::get('VIEWS_PATH')."layout/page-includes/no_order_found.php");?>
+            <?php include(Config::get('VIEWS_PATH')."layout/page-includes/no_customer_found.php");?>
         <?php else:?>
             <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
             <?php echo "<pre>",print_r($customer),"</pre>";?>
