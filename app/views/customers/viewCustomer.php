01455d57cb05001acb7166a230119cec13895dba
marxolly <mark@solly.com.au>
2021-02-16 11:09:02 +1100

    Update viewCustomer.php

--- app/views/customers/viewCustomer.php
+++ app/views/customers/viewCustomer.php
@@ -1,9 +1,13 @@
 <?php
-    
+
 ?>
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-        <?php echo "<pre>",print_r($customer),"</pre>";?>
+        <?php if(empty($customer)):?>
+            <?php include(Config::get('VIEWS_PATH')."layout/page-includes/no_order_found.php");?>
+        <?php else:?>
+            <?php echo "<pre>",print_r($customer),"</pre>";?>
+        <?php endif;?>
     </div>
 </div>
