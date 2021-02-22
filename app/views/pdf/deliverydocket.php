638374213ff17181e389c8d7a715d40189354ad4
marxolly <mark@solly.com.au>
2021-02-22 14:46:41 +1100

    Update deliverydocket.php

--- app/views/pdf/deliverydocket.php
+++ app/views/pdf/deliverydocket.php
@@ -1,9 +1,14 @@
 <?php
-echo "<pre>",print_r($sender_details),"</pre>";
+//echo "<pre>",print_r($sender_details),"</pre>";
 ?>
 <div id="dd_body">
     <div id="top_half">
-
+        <table id="page_head">
+            <tr>
+                <td><img src="/images/deliver_docket_logos/<?php echo $sender_details['logo'];?>"></td>
+                <td class="left-align"><?php echo $sender_details['address'];?></td>
+            </tr>
+        </table>
     </div>
     <div id="divider">
         <p>-------------------------------------------------------------------------------------------------------------------------------<br>
