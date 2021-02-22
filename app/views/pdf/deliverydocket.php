864e0549ec1187746a80be4aa44c3f2b3d2e8778
marxolly <mark@solly.com.au>
2021-02-22 15:01:06 +1100

    Update deliverydocket.php

--- app/views/pdf/deliverydocket.php
+++ app/views/pdf/deliverydocket.php
@@ -5,7 +5,7 @@
     <div id="top_half">
         <table id="page_head">
             <tr>
-                <td  style="width: 150mm"><img src="https://wms.fsg.com.au/images/delivery_docket_logos/<?php echo $sender_details['logo'];?>"></td>
+                <td  style="width: 125mm"><img src="https://wms.fsg.com.au/images/delivery_docket_logos/<?php echo $sender_details['logo'];?>"></td>
                 <td class="right-align sender-address"><?php echo $sender_details['address'];?></td>
             </tr>
         </table>
