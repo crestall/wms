034487a170ff876178957842dc17cdd3b7a19208
marxolly <mark@solly.com.au>
2021-02-22 14:24:25 +1100

    dd styling

--- app/views/pdf/deliverydocket.php
+++ app/views/pdf/deliverydocket.php
@@ -1,6 +1,15 @@
 <?php
 echo "<pre>",print_r($sender_details),"</pre>";
 ?>
-<div id="runsheet_body">
+<div id="dd_body">
+    <div id="top_half">
 
+    </div>
+    <div id="divider">
+        <p>-------------------------------------------------------------------------------------------------------------------------------</p>
+        <p class="inst">[Detach Here]</p>
+    </div>
+    <div id="bottom_half">
+
+    </div>
 </div>
