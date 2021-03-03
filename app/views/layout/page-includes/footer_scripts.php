7394a21bb8bc8f136d6a28a1d6454ebb2d335490
marxolly <mark@solly.com.au>
2021-03-03 14:29:51 +1100

    Update footer_scripts.php

--- app/views/layout/page-includes/footer_scripts.php
+++ app/views/layout/page-includes/footer_scripts.php
@@ -17,6 +17,8 @@
         <script src="//cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
         <script src="//cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
         <script src="//cdn.datatables.net/fixedheader/3.1.5/js/dataTables.fixedHeader.min.js"></script>
+        <script src="//cdn.datatables.net/plug-ins/1.10.22/features/searchHighlight/dataTables.searchHighlight.min.js"></script>
+        <script src="//bartaz.github.io/sandbox.js/jquery.highlight.js"></script>
         <!-- Block UI JavaScript -->
         <script src="/scripts/jquery.blockUI.js"></script>
         <!-- Print Area Javascript -->
