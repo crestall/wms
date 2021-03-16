d39d741487bb07acceb77df8db176725c993e2c8
marxolly <mark@solly.com.au>
2021-03-17 10:42:50 +1100

    Update productionDatabaseTablesUpdate.php

--- app/views/adminOnly/productionDatabaseTablesUpdate.php
+++ app/views/adminOnly/productionDatabaseTablesUpdate.php
@@ -2,5 +2,9 @@
 
 ?>
 <div id="page-wrapper">
-    <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php"); ?>
+    <div id="page_container" class="container-l">
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php"); ?>
+        <?php echo "FINISHERS<pre>",print_r($production_finishers)."</pre>";?>
+        <?php echo "CUSTOMERS<pre>",print_r($production_customers)."</pre>";?>
+    </div>
 </div>
