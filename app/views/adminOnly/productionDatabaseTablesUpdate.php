2629f38521425c9e9bd80ab5e9e8db474b4636f7
marxolly <mark@solly.com.au>
2021-03-17 10:59:05 +1100

    Update productionDatabaseTablesUpdate.php

--- app/views/adminOnly/productionDatabaseTablesUpdate.php
+++ app/views/adminOnly/productionDatabaseTablesUpdate.php
@@ -4,7 +4,30 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-l">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php"); ?>
-        <?php echo "FINISHERS<pre>",print_r($production_finishers)."</pre>";?>
-        <?php echo "CUSTOMERS<pre>",print_r($production_customers)."</pre>";?>
+        <?php //echo "FINISHERS<pre>",print_r($production_finishers)."</pre>";?>
+        <?php //echo "CUSTOMERS<pre>",print_r($production_customers)."</pre>";?>
+        <div class="row">
+            <div class="col">
+                <?php foreach($production_finishers as $pf)
+                {
+                    if(!empty($pf['contacts']))
+                    {
+                        if(!empty($pf['contact']))
+                        {
+                            echo "<p>Will Add ".$pf['contact']." for ".$pf['name']." (".$pf['id'].")</p>";
+                        }
+                        else
+                        {
+                            echo "<p>No contact listed for ".$pf['name']." (".$pf['id'].")</p>";
+                        }
+                    }
+                    else
+                    {
+                        echo "<p>Contacts already listed for ".$pf['name']." (".$pf['id'].")</p>";
+                    }
+                }
+                ?>
+            </div>
+        </div>
     </div>
 </div>
