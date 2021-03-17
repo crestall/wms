070384d7ed7abbbc4437f74a147a3f642eebb6be
marxolly <mark@solly.com.au>
2021-03-17 15:10:23 +1100

    Update add_job_finisher.php

--- app/views/layout/page-includes/add_job_finisher.php
+++ app/views/layout/page-includes/add_job_finisher.php
@@ -5,14 +5,14 @@ $f = new NumberFormatter("en", NumberFormatter::SPELLOUT);
 ?>
 <div id="finisher_<?php echo $i;?>" class="p-3 mid-grey mb-3 afinisher">
     <div class="form-group row">
-        <h4 class="col-md-4 finisher_title">Finisher <?php echo ucwords($f->format($this_finisher));?>'s Details</h4>
-        <div class="col-md-4">
+        <h4 class="col-md-5 finisher_title">Finisher <?php echo ucwords($f->format($this_finisher));?>'s Details</h4>
+        <div class="col-md-7">
             <h5><a data-finisher="<?php echo $i;?>" class="remove-finisher" style="cursor:pointer" title="Remove Finisher"><i class="fad fa-times-square text-danger"></i> Remove This Finisher</a></h5>
         </div>
     </div>
     <div class="form-group row">
-        <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Finisher Name</label>
-        <div class="col-md-4">
+        <label class="col-md-4"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Finisher Name</label>
+        <div class="col-md-8">
             <input type="text" class="form-control finisher_name required" data-finisher="<?php echo $i;?>" name="finishers[<?php echo $i;?>][name]">
             <span class="inst">
                 Start typing a name and choose a finisher from the list<br>
@@ -30,13 +30,13 @@ $f = new NumberFormatter("en", NumberFormatter::SPELLOUT);
         <div id="contact_selector_<?php echo $i;?>" class="form-group row contact_selector"></div>
         <div class="form-group row">
             <label class="col-md-4">Purchase Order Number</label>
-            <div class="col-md-4">
+            <div class="col-md-8">
                 <input type="text" class="form-control finisher_po" name="finishers[<?php echo $i;?>][purchase_order]">
             </div>
         </div>
         <div class="row form-group">
             <label class="col-md-4 col-form-label">Expected Delivery Date</label>
-            <div class="col-md-3">
+            <div class="col-md-8">
                 <div class="input-group">
                     <input type="text" class="form-control finisher_ed_date" name="finishers[<?php echo $i;?>][ed_date]">
                     <div class="input-group-append">
