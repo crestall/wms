3def195b77a918269f4368ef25bd7f1938b36c87
marxolly <mark@solly.com.au>
2021-03-17 15:44:06 +1100

    Update addJob.php

--- app/views/jobs/addJob.php
+++ app/views/jobs/addJob.php
@@ -304,7 +304,46 @@ else
                                         <textarea class="form-control" name="delivery_instructions" id="delivery_instructions" placeholder="Instructions For Driver"><?php echo Form::value('delivery_instructions');?></textarea>
                                     </div>
                                 </div>
-                                <?php include(Config::get('VIEWS_PATH')."forms/address_auonly.php");?>
+                                <div class="form-group row">
+                                    <label class="col-md-4"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Address Line 1</label>
+                                    <div class="col-md-7">
+                                        <input type="text" class="form-control required" name="address" id="address" value="<?php echo $address;?>" />
+                                        <?php echo Form::displayError('address');?>
+                                    </div>
+                                    <div class="custom-control custom-checkbox col-md-1">
+                                        <input type="checkbox" class="custom-control-input" id="ignore_address_error" name="ignore_address_error" <?php if(!empty(Form::value('ignore_address_error'))) echo 'checked';?> />
+                                        <label class="custom-control-label" for="ignore_address_error">No need for a number</label>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-4">Address Line 2</label>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control" name="address2" id="address2" value="<?php echo $address2;?>" />
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-4"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Suburb/Town</label>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control required" name="suburb" id="suburb" value="<?php echo $suburb;?>" />
+                                        <?php echo Form::displayError('suburb');?>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-4"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> State</label>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control required" name="state" id="state" value="<?php echo $state;?>" />
+                                        <span class="inst">Use VIC, NSW, QLD, ACT, TAS, WA, SA, NT only</span>
+                                        <?php echo Form::displayError('state');?>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-4 "><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Postcode</label>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control required" name="postcode" id="postcode" value="<?php echo $postcode;?>" />
+                                        <?php echo Form::displayError('postcode');?>
+                                    </div>
+                                </div>
+                                <input type="hidden" name="country" value = "AU">
                             </div>
                         </div>
                     </div>
