05a7275d6c4410fb2ecf00ea7393f84f20557459
marxolly <mark@solly.com.au>
2021-03-22 16:31:39 +1100

    Update Ebay.php

--- app/core/Ebay.php
+++ app/core/Ebay.php
@@ -116,6 +116,7 @@ https://auth.sandbox.ebay.com/oauth2/authorize?client_id=MarkSoll-PBAFSG-SBX-a41
         curl_setopt($ch, CURLOPT_POSTFIELDS, "grant_type=authorization_code&code=".$this->authCode."&redirect_uri=".$this->ruName);
         $response = curl_exec($ch);
         $json = json_decode($response, true);
+        echo "<pre>",print_r($json),"</pre>"; die();
         $info = curl_getinfo($ch);
         curl_close($ch);
         if($json != null)
