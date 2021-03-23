9afba62bd98056dcf6d391886c322a145b4f59ae
marxolly <mark@solly.com.au>
2021-03-23 14:45:24 +1100

    Update EbayAPI.php

--- app/core/EbayAPI.php
+++ app/core/EbayAPI.php
@@ -106,7 +106,7 @@ https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/buy.or
         $codeAuth = base64_encode($this->clientID.':'.$this->certID);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
             'Content-Type: application/x-www-form-urlencoded',
-            'Authorization: Basic '.$codeAuth
+            'Authorization: Bearer '.$codeAuth
         ));
         $result = curl_exec($ch);
         $err = curl_error($ch);
