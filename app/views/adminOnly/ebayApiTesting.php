3d95aba6505de176e2e5a41bbdb491ac0cf39ca4
marxolly <mark@solly.com.au>
2021-03-23 14:22:23 +1100

    Update ebayApiTesting.php

--- app/views/adminOnly/ebayApiTesting.php
+++ app/views/adminOnly/ebayApiTesting.php
@@ -2,5 +2,17 @@
 
 ?>
 <div id="page-wrapper">
-    <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php"); ?>
+    <div id="page_container" class="container-xl">
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php"); ?>
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Current Orders</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <?php echo "<pre>",print_r($orders),"</pre>";?>
+            </div>
+        </div>
+    </div>
 </div>
