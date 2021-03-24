6b6747310b46fa386b5f6c0d6919bd9d1b0bbece
marxolly <mark@solly.com.au>
2021-03-24 13:57:11 +1100

    Update dataTablesTesting.php

--- app/views/adminOnly/dataTablesTesting.php
+++ app/views/adminOnly/dataTablesTesting.php
@@ -2,5 +2,12 @@
 
 ?>
 <div id="page-wrapper">
-    <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php"); ?>
+    <div id="page_container" class="container-xl">
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php"); ?>
+        <div class="row">
+            <div class="col">
+                <?php echo "<pre>",print_r($products),"</pre>";?>
+            </div>
+        </div>
+    </div>
 </div>
