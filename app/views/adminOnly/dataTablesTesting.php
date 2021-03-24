af4cbf6d33ff579cb87a5c9f7877ae51f5979ce1
marxolly <mark@solly.com.au>
2021-03-24 15:35:57 +1100

    datatables testing

--- app/views/adminOnly/dataTablesTesting.php
+++ app/views/adminOnly/dataTablesTesting.php
@@ -6,7 +6,34 @@
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php"); ?>
         <div class="row">
             <div class="col">
-                <?php echo "<pre>",print_r($products),"</pre>";?>
+                <?php //echo "<pre>",print_r($products),"</pre>";?>
+            </div>
+        </div>
+        <div id="waiting" class="row">
+            <div class="col-lg-12 text-center">
+                <h2>Drawing Table..</h2>
+                <p>May take a few moments</p>
+                <img class='loading' src='/images/preloader.gif' alt='loading...' />
+            </div>
+        </div>
+        <div class="row" id="table_holder" style="display:none">
+            <div class="col-md-12">
+                <table class="table-striped table-hover" id="view_items_table">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>SKU</th>
+                            <th>Barcode</th>
+                            <th>Client Product ID</th>
+                            <th>On Hand</th>
+                            <th>Allocated</th>
+                            <th>Under Quality Control</th>
+                            <th>Available</th>
+                            <th>Locations</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                </table>
             </div>
         </div>
     </div>
