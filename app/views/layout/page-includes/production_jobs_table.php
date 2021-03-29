8fa036d77f59c17cdc5391985278364a5fa112ac
marxolly <mark@solly.com.au>
2021-03-29 16:01:35 +1100

    Update production_jobs_table.php

--- app/views/layout/page-includes/production_jobs_table.php
+++ app/views/layout/page-includes/production_jobs_table.php
@@ -2,7 +2,7 @@
   $today = strtotime('today');
   //echo "<p>User Role: $user_role</p>";
   //echo "<pre>",print_r($jobs),"</pre>";
-  $can_do_runsheet = false;
+  $can_do_runsheets = false;
 ?>
 <table class="table-striped table-hover" id="production_jobs_table">
     <thead>
