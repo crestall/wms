70c63633265e8cc0ec982d63044bdc761da93715
marxolly <mark@solly.com.au>
2021-03-30 11:07:23 +1100

    Update add_production_job_note.php

--- app/views/dashboard/add_production_job_note.php
+++ app/views/dashboard/add_production_job_note.php
@@ -4,9 +4,8 @@
         <?php //echo "<pre>",print_r($job),"</pre>";?>
         <form id="jobs-add-production-note" method="post" action="/form/procAddProductionNote">
             <div class="form-group row">
-                <label class="col-md-4">Notes/Comments</label>
-                <div class="col-md-8">
-                    <textarea name="notes" id="notes" class="form-control" rows="3"><?php echo $note; ?></textarea>
+                <div class="col-md-12">
+                    <textarea name="notes" id="notes" class="form-control" rows="8"><?php echo $note; ?></textarea>
                 </div>
             </div>
         </form>
