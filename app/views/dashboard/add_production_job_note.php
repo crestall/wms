e45d6c8872ab7c9938a5106934e8ddf27860fdcd
marxolly <mark@solly.com.au>
2021-03-31 12:31:53 +1100

    Update add_production_job_note.php

--- app/views/dashboard/add_production_job_note.php
+++ app/views/dashboard/add_production_job_note.php
@@ -9,5 +9,5 @@
     </div>
     <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
     <input type="hidden" name="job_id" value="<?php echo $job_id; ?>" />
-    <input type="hidden" name="job_id" value="<?php echo $job_no; ?>" />
+    <input type="hidden" name="job_no" value="<?php echo $job_no; ?>" />
 </form>
