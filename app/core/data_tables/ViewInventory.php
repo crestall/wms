86b4cdf69e833b65f4ad4c990f9900114d62d862
marxolly <mark@solly.com.au>
2021-04-01 15:02:13 +1100

    Update ViewInventory.php

--- app/core/data_tables/ViewInventory.php
+++ app/core/data_tables/ViewInventory.php
@@ -53,13 +53,13 @@
         $data = $db->queryData($query, self::$db_array);
 
         // Data set length after filtering
-        $resFilterLength = $db->queryRow("SELECT count".self::from().$where, self::$db_array);
+        $resFilterLength = $db->queryRow("SELECT count(*)".self::from().$where, self::$db_array);
         //echo "<pre>",print_r($resFilterLength),"</pre>";die();
-        $recordsFiltered = $resFilterLength['count'];
+        $recordsFiltered = $resFilterLength['count(*)'];
 
         // Total data set length
-        $resTotalLength = $db->queryRow("SELECT count".self::from());
-        $recordsTotal = $resTotalLength['count'];
+        $resTotalLength = $db->queryRow("SELECT count(*)".self::from());
+        $recordsTotal = $resTotalLength['count(*)'];
 
         return array(
             "draw"            => isset ( $request['draw'] ) ?
