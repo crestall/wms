6d311355f2b9cf52b0a2eb371e5f40d5212ac318
marxolly <mark@solly.com.au>
2021-04-07 09:11:06 +1000

    Update footer.php

--- app/views/layout/adminonly/footer.php
+++ app/views/layout/adminonly/footer.php
@@ -15,11 +15,7 @@
                         dataTable.init($('table#view_items_table'), {
                             "processing": true,
                             "serverSide": true,
-                            "ajax": "/ajaxfunctions/data-tables/class=ViewInventory",
-                            "columnDefs": [
-                                { "orderable": false, "targets": [8,9] }
-                            ],
-                            "order": []
+                            "ajax": "/ajaxfunctions/data-tables/class=ViewInventory"
                         } );
                     }
                 },
