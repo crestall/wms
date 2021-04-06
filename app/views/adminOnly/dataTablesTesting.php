c9a84930133be37654ed5b0101066b8fe30f8848
marxolly <mark@solly.com.au>
2021-04-07 09:01:17 +1000

    Update dataTablesTesting.php

--- app/views/adminOnly/dataTablesTesting.php
+++ app/views/adminOnly/dataTablesTesting.php
@@ -8,10 +8,7 @@
             <div class="col">
                 <?php //echo "<pre>",print_r($products),"</pre>";?>
                 <?php echo "Client ID: ".ViewInventory::getClientId();?>
-                <?php //echo "<pre>",print_r(ViewInventory::collectData(array(
-                    //'start' => 0,
-                    //'length'    => 25
-                ))),"</pre>" ;?>
+                
             </div>
         </div>
         <div id="waiting" class="row">
