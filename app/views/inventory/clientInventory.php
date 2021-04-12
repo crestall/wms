4575dcd9a1f85544093c7ba9c472e32e47b65601
marxolly <mark@solly.com.au>
2021-04-13 08:48:04 +1000

    Update clientInventory.php

--- app/views/inventory/clientInventory.php
+++ app/views/inventory/clientInventory.php
@@ -7,39 +7,31 @@
                 <h2>Products for <?php echo $client_name;?></h2>
             </div>
         </div>
-        <?php if(count($products)):?>
-            <div id="waiting" class="row">
-                <div class="col-lg-12 text-center">
-                    <h2>Drawing Table..</h2>
-                    <p>May take a few moments</p>
-                    <img class='loading' src='/images/preloader.gif' alt='loading...' />
-                </div>
+        <div id="waiting" class="row">
+            <div class="col-lg-12 text-center">
+                <h2>Drawing Table..</h2>
+                <p>May take a few moments</p>
+                <img class='loading' src='/images/preloader.gif' alt='loading...' />
             </div>
-            <div class="row" id="table_holder" style="display:none">
-                <div class="col-md-12">
-                    <table class="table-striped table-hover" id="client_inventory_table" width="100%">
-                        <thead>
-                            <tr>
-                                <th data-priority="1">Name</th>
-                                <th>SKU</th>
-                                <th>Details</th>
-                                <th data-priority="2">On Hand</th>
-                                <th data-priority="2">Allocated</th>
-                                <th>Under Quality Control</th>
-                                <th data-priority="1">Available</th>
-                                <th data-priority="3">Total Bay Usage</th>
-                                <th data-priority="1">Warning Level</th>
-                            </tr>
-                        </thead>
-                    </table>
-                </div>
+        </div>
+        <div class="row" id="table_holder" style="display:none">
+            <div class="col-md-12">
+                <table class="table-striped table-hover" id="client_inventory_table" width="100%">
+                    <thead>
+                        <tr>
+                            <th data-priority="1">Name</th>
+                            <th>SKU</th>
+                            <th>Details</th>
+                            <th data-priority="2">On Hand</th>
+                            <th data-priority="2">Allocated</th>
+                            <th>Under Quality Control</th>
+                            <th data-priority="1">Available</th>
+                            <th data-priority="3">Total Bay Usage</th>
+                            <th data-priority="1">Warning Level</th>
+                        </tr>
+                    </thead>
+                </table>
             </div>
-        <?php else:?>
-            <div class="col-lg-12">
-                <div class="errorbox">
-                    <p>No products listed for <?php echo $client_name;?></p>
-                </div>
-            </div>
-        <?php endif;?>
+        </div>
     </div>
 </div>
