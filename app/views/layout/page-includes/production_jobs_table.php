313f84d89ac29e7f290100b0077038c2ae9fc647
marxolly <mark@solly.com.au>
2021-04-13 16:18:28 +1000

    Update production_jobs_table.php

--- app/views/layout/page-includes/production_jobs_table.php
+++ app/views/layout/page-includes/production_jobs_table.php
@@ -3,12 +3,13 @@
   //echo "<p>User Role: $user_role</p>";
   //echo "<pre>",print_r($jobs),"</pre>";
   $can_do_runsheets = false;
+  echo "<p>USERROLE: $user_role</p>";
 ?>
 <table class="table-striped table-hover" id="production_jobs_table" width="100%">
     <thead>
         <tr>
             <th data-priority="10001" nowwrap>Priority</th>
-            <th data-priority="1">Job Number</th> 
+            <th data-priority="1">Job Number</th>
         </tr>
     </thead>
 </table>
