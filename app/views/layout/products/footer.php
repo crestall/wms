ebbb923e1a041ea1e1eaf4d17c87ec63e024db5d
marxolly <mark@solly.com.au>
2021-04-13 11:10:51 +1000

    Update footer.php

--- app/views/layout/products/footer.php
+++ app/views/layout/products/footer.php
@@ -56,10 +56,6 @@
                         });
 
                         dataTable.init($('table#view_items_table'), {
-                            "columnDefs": [
-                                { "searchable": false, "targets": [2,3,4,5,6,7,8] },
-                                { "orderable": false, "targets": [2,7,8] }
-                            ],
                             "processing": true,
                             "mark": true,
                             "language": {
