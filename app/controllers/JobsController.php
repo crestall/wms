bc945c390ef966a5096cf592eb4aa60ee896583e
marxolly <mark@solly.com.au>
2021-04-14 10:46:14 +1000

    Update JobsController.php

--- app/controllers/JobsController.php
+++ app/controllers/JobsController.php
@@ -219,7 +219,7 @@ class JobsController extends Controller
             'createDeliveryDocket',
             'jobSearch',
             'jobSearchResults',
-            'updateJob'
+            'updateJob',
             'viewJobs'
         ));
 
