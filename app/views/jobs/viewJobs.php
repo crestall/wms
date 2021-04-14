d40c5e4d74a6d1bb8d026566a058869dda2903f9
marxolly <mark@solly.com.au>
2021-04-14 12:11:03 +1000

    Update viewJobs.php

--- app/views/jobs/viewJobs.php
+++ app/views/jobs/viewJobs.php
@@ -1,12 +1,15 @@
 <?php
+$today = strtotime('today');
 $can_do_runsheets  = false;
 $can_change_status = Permission::canChangeStatus($user_role);
 $need_checkbox = ($can_do_runsheets || $can_change_status);
+$c_ids_string = implode(",", $customer_ids);
 ?>
 <div id="page-wrapper">
     <input type="hidden" id="completed" value="<?php echo $completed;?>" >
     <input type="hidden" id="user_role" value="<?php echo $user_role;?>" >
-        <input type="hidden" id="cancelled" value="<?php echo $cancelled;?>" >
+    <input type="hidden" id="cancelled" value="<?php echo $cancelled;?>" >
+    <input type="hidden" id="customer_ids" value="<?php echo $c_ids_string;?>" >
     <div id="page_container" class="container-xxl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <div class="row">
@@ -56,44 +59,42 @@ $need_checkbox = ($can_do_runsheets || $can_change_status);
             </div>
         </div>
         <?php //echo "<pre>",print_r($status_ids),"</pre>";?>
-        <?php if(count($jobs)):?>
-            <div id="waiting" class="row">
-                <div class="col-lg-12 text-center">
-                    <h2>Drawing Table..</h2>
-                    <p>May take a few moments</p>
-                    <img class='loading' src='/images/preloader.gif' alt='loading...' />
-                </div>
+        <div id="waiting" class="row">
+            <div class="col-lg-12 text-center">
+                <h2>Drawing Table..</h2>
+                <p>May take a few moments</p>
+                <img class='loading' src='/images/preloader.gif' alt='loading...' />
             </div>
-            <div class="row mt-4" id="table_holder" style="display:none">
-                <?php //echo "<pre>",print_r($jobs),"</pre>";?>
-                <?php if($can_do_runsheets):?>
-                    <div class="col-lg-3 col-md-4 col-sm-6 mb-3"><button class="btn btn-sm btn-block btn-outline-primary" id="runsheet"><i class="fas fa-truck"></i> Add Selected to Chosen Day's Runsheet</button></div>
-                <?php endif;?>
-                <?php if($can_change_status):?>
-                    <div class="col-lg-3 col-md-4 col-sm-6 mb-3"><button class="btn btn-sm btn-block btn-outline-fsg" id="status"><i class="fal fa-file-check"></i> Update Status for Selected</button></div>
-                <?php endif;?>
-                <div class="col-lg-3 col-md-4 col-sm-6 mb-3"><button class="btn btn-sm btn-block btn-outline-secondary" id="priority_change"><i class="fal fa-file-plus"></i> Update Priority for Selected</button></div>
-                <div class="col-lg-3 col-md-4 col-sm-6 mb-3"><button class="btn btn-sm btn-block btn-outline-success" id="create_pdf"><i class="fal fa-file-pdf"></i> Create PDF for Selected</button></div> 
-                <div class="col-12">
-                    <?php if(isset($_SESSION['feedback'])) :?>
-                       <div class='feedbackbox'><?php echo Session::getAndDestroy('feedback');?></div>
-                    <?php endif; ?>
-                    <?php if(isset($_SESSION['errorfeedback'])) :?>
-                       <div class='errorbox'><?php echo Session::getAndDestroy('errorfeedback');?></div>
-                    <?php endif; ?>
-                </div>
-                <div class="col-12">
-                    <?php include(Config::get('VIEWS_PATH')."layout/page-includes/production_jobs_table.php");?>
-                </div>
+        </div>
+        <div class="row mt-4" id="table_holder" style="display:none">
+            <?php //echo "<pre>",print_r($jobs),"</pre>";?>
+            <?php if($can_change_status):?>
+                <div class="col-lg-3 col-md-4 col-sm-6 mb-3"><button class="btn btn-sm btn-block btn-outline-fsg" id="status"><i class="fal fa-file-check"></i> Update Status for Selected</button></div>
+            <?php endif;?>
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-3"><button class="btn btn-sm btn-block btn-outline-secondary" id="priority_change"><i class="fal fa-file-plus"></i> Update Priority for Selected</button></div>
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-3"><button class="btn btn-sm btn-block btn-outline-success" id="create_pdf"><i class="fal fa-file-pdf"></i> Create PDF for Selected</button></div>
+            <div class="col-12">
+                <?php if(isset($_SESSION['feedback'])) :?>
+                   <div class='feedbackbox'><?php echo Session::getAndDestroy('feedback');?></div>
+                <?php endif; ?>
+                <?php if(isset($_SESSION['errorfeedback'])) :?>
+                   <div class='errorbox'><?php echo Session::getAndDestroy('errorfeedback');?></div>
+                <?php endif; ?>
             </div>
-        <?php else:?>
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="errorbox">
-                            <h2><i class="fas fa-exclamation-triangle"></i> No Jobs Listed</h2>
-                        </div>
-                    </div>
-                </div>
-        <?php endif;?>
+            <div class="col-12">
+                <table class="table-striped table-hover" id="production_jobs_table" width="100%">
+                    <thead>
+                        <tr>
+                            <th data-priority="10001" nowwrap>Priority</th>
+                            <th data-priority="1">Job Number</th>
+                            <th data-priority="1">Client</th>
+                            <th data-priority="2" style="max-width: 250px;">Description</th>
+                            <th>Finisher(s)</th>
+                            <th>FSG Contact</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        </div>
     </div>
 </div>
