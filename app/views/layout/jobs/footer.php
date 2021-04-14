ff8857a48e08e6c02dbfefe02bec9801e99eacd8
marxolly <mark@solly.com.au>
2021-04-14 15:02:49 +1000

    Update footer.php

--- app/views/layout/jobs/footer.php
+++ app/views/layout/jobs/footer.php
@@ -326,7 +326,7 @@
                                 { "orderDataType": "dom-select", "targets": [0]},
                                 { "orderable": false, "targets": [3,4] },
                                 { "createdCell": function(td, cellData, rowData, row, col){
-                                        console.log("rowData: "+rowData);
+                                        console.log("rowData.status_colour: "+rowData.status_colour);
                                     },
                                   "targets": [6]
                                 }
