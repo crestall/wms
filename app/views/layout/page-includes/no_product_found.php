466e28251594d4820f399607ed94e4c629bafc27
marxolly <mark@solly.com.au>
2021-04-27 14:38:36 +1000

    Create no_product_found.php

---
+++ app/views/layout/page-includes/no_product_found.php
@@ -0,0 +1,16 @@
+<div class="row">
+    <div class="col">
+        <div class="errorbox">
+            <div class="row">
+                <div class="col-4 text-right">
+                    <i class="fad fa-exclamation-triangle fa-6x"></i>
+                </div>
+                <div class="col-8">
+                    <h2>No Product Found</h2>
+                    <p>No product was found with that ID, or the product associated with that ID belongs to another client</p>
+                    <p>Please <a href="/inventory/client-inventory">click here</a> to go back to the list of products you can update.</p> 
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
