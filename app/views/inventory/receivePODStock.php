67e2de70240e492415e28f3f9bfc9b3e016b4a1c
marxolly <mark@solly.com.au>
2021-05-07 12:53:27 +1000

    Update receivePODStock.php

--- app/views/inventory/receivePODStock.php
+++ app/views/inventory/receivePODStock.php
@@ -2,9 +2,9 @@
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <div class="row mb-3">
-            <label class="col-md-3">Select a Client</label>
+            <label class="col-md-3">POD Invoice</label>
             <div class="col-md-4">
-                <select id="client_selector" class="form-control selectpicker" data-style="btn-outline-secondary" data-live-search="true"><option value="0">Select</option><?php echo $this->controller->client->getSelectClients();?></select>
+                <select id="client_selector" class="form-control selectpicker" data-style="btn-outline-secondary" data-live-search="true"><option value="0">Select</option><?php echo $this->controller->order->getPODIdSelect($pod_id);?></select>
             </div>
         </div>
     </div>
