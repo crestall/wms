80d1ab6b7383a09047d0cb5271f5ea1fcf32b2e7
marxolly <mark@solly.com.au>
2021-05-10 09:33:42 +1000

    Update footer.php

--- app/views/layout/inventory/footer.php
+++ app/views/layout/inventory/footer.php
@@ -334,7 +334,13 @@
                             var barcode = $("#item_barcode").val();
                             var data = {barcode: barcode, pod_invoice: $('#pod_invoice_selector').val()};
                             var url = "/ajaxfunctions/get-pod-item-by-barcode";
+                            if( barcode != "")
+                            {
+
+                            }
                         });
+
+
                     }
                 },
                 "scan-to-inventory": {
