9281e3e03fc542968b1a481f77a2f20bd72a36a0
marxolly <mark@solly.com.au>
2021-05-10 15:27:54 +1000

    Update footer.php

--- app/views/layout/inventory/footer.php
+++ app/views/layout/inventory/footer.php
@@ -349,8 +349,11 @@
                                         var order_item_id = $(this).data("orderitemid");
                                         var num_received = $("input#received_"+order_item_id).val();
                                         var num_required = $("input#required_"+order_item_id).val();
-                                        console.log("received: "+num_received);
-                                        console.log("required: "+num_required);
+                                        if(num_received)
+                                        {
+                                            console.log("received: "+num_received);
+                                            console.log("required: "+num_required);
+                                        }
                                     });
                                 });
                             }
