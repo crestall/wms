af477edf5c0863dfff49c07266506e1c31514a14
marxolly <mark@solly.com.au>
2021-05-10 12:06:30 +1000

    Update receivePODStock.php

--- app/views/inventory/receivePODStock.php
+++ app/views/inventory/receivePODStock.php
@@ -4,7 +4,6 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
         <div class="row mb-3">
             <label class="col-md-3">POD Invoice</label>
             <div class="col-md-4">
