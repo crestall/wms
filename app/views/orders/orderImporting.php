655b1c07aed3f4d84e12d652ac8dfa51f804b490
marxolly <mark@solly.com.au>
2021-05-19 09:41:26 +1000

    Update orderImporting.php

--- app/views/orders/orderImporting.php
+++ app/views/orders/orderImporting.php
@@ -30,6 +30,11 @@
             </div>
         </div>
         <div class="row">
+            <div class="col-12 mb-3">
+                <h2>Woo Commerce Order Importing</h2>
+            </div>
+        </div>
+        <div class="row">
             <div class="col-sm-12 col-md-6 mb-3">
                 <div class="card">
                     <a name="nuchev"></a>
@@ -94,68 +99,96 @@
                     </div>
                 </div>
             </div>
-            <?php if($super_admin):?>
-                <div class="col-sm-12 col-md-6 mb-3">
-                    <div class="card">
-                        <a name="pba-woo"></a>
-                        <h4 class="card-header">Performance Brands Woo-Commerce Orders</h4>
-                        <div class="card-body">
-                            <div class="form-group row full_import">
-                                <label class="col-5"><h5 class="card-title">Run Full Import</h5></label>
-                                <div class="col-7">
-                                    <button class="btn btn-outline-secondary" id="pbawoocommerce_full_import" data-function="importPBAOrders">Run It</button>
-                                </div>
-                            </div>
-                            <h5 class="card-title">Import single Order</h5>
-                            <form id="oneplate_single_import" action="/orders/importPbaWoocommerceOrder" method="post">
-                                <div class="form-group row">
-                                    <label class="col-5">WooCommerce Order ID</label>
-                                    <div class="col-7">
-                                        <input type="text" class="form-control required" name="pbawoocommerce_id" id="pbawoocommerce_id" value="<?php echo Form::value('pbawoocommerce_id');?>" />
-                                        <?php echo Form::displayError('pbawoocommerce_id');?>
-                                    </div>
-                                </div>
-                                <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
-                                <input type="hidden" name="client_id" value="<?php echo $pba_clientid; ?>" />
-                                <div class="form-group row">
-                                    <label class="col-5">&nbsp;</label>
-                                    <div class="col-4">
-                                        <button type="submit" class="btn btn-outline-secondary">Import It</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-12 col-md-6 mb-3">
-                    <div class="card">
-                        <a name="pba-shopify"></a>
-                        <h4 class="card-header">Performance Brands Shopify Orders</h4>
-                        <div class="card-body">
-                            <div class="form-group row full_import">
-                                <label class="col-5"><h5 class="card-title">Run Full Import</h5></label>
-                                <div class="col-7">
-                                    <button class="btn btn-outline-secondary" id="pbashopfy_full_import" data-function="importPBAShopifyOrders">Run It</button>
-                                </div>
+        <?php if($super_admin):?>
+            <div class="col-sm-12 col-md-6 mb-3">
+                <div class="card">
+                    <a name="pba-woo"></a>
+                    <h4 class="card-header">Performance Brands Woo-Commerce Orders</h4>
+                    <div class="card-body">
+                        <div class="form-group row full_import">
+                            <label class="col-5"><h5 class="card-title">Run Full Import</h5></label>
+                            <div class="col-7">
+                                <button class="btn btn-outline-secondary" id="pbawoocommerce_full_import" data-function="importPBAOrders">Run It</button>
                             </div>
                         </div>
+                        <h5 class="card-title">Import single Order</h5>
+                        <form id="oneplate_single_import" action="/orders/importPbaWoocommerceOrder" method="post">
+                            <div class="form-group row">
+                                <label class="col-5">WooCommerce Order ID</label>
+                                <div class="col-7">
+                                    <input type="text" class="form-control required" name="pbawoocommerce_id" id="pbawoocommerce_id" value="<?php echo Form::value('pbawoocommerce_id');?>" />
+                                    <?php echo Form::displayError('pbawoocommerce_id');?>
+                                </div>
+                            </div>
+                            <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+                            <input type="hidden" name="client_id" value="<?php echo $pba_clientid; ?>" />
+                            <div class="form-group row">
+                                <label class="col-5">&nbsp;</label>
+                                <div class="col-4">
+                                    <button type="submit" class="btn btn-outline-secondary">Import It</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
-                <div class="col-sm-12 col-md-6 mb-3">
-                    <div class="card">
-                        <a name="freedom"></a>
-                        <h4 class="card-header">Freedom Orders</h4>
-                        <div class="card-body">
-                            <div class="form-group row full_import">
-                                <label class="col-5"><h5 class="card-title">Run Full Import From MYOB</h5></label>
-                                <div class="col-7">
-                                    <button class="btn btn-outline-secondary" id="freedom_full_import" data-function="importFreedomOrders">Run It</button>
-                                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 mb-3">
+                <h2>Shopify Order Importing</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 col-md-6 mb-3">
+                <div class="card">
+                    <a name="pba-shopify"></a>
+                    <h4 class="card-header">Performance Brands Shopify Orders</h4>
+                    <div class="card-body">
+                        <div class="form-group row full_import">
+                            <label class="col-5"><h5 class="card-title">Run Full Import</h5></label>
+                            <div class="col-7">
+                                <button class="btn btn-outline-secondary" id="pbashopfy_full_import" data-function="importPBAShopifyOrders">Run It</button>
                             </div>
                         </div>
                     </div>
                 </div>
-            <?php endif;?>
+            </div>
+            <div class="col-sm-12 col-md-6 mb-3">
+                <div class="card">
+                    <a name="buzz-bee"></a>
+                    <h4 class="card-header">Buzz Bee Shopify Orders</h4>
+                    <div class="card-body">
+                        <div class="form-group row full_import">
+                            <label class="col-5"><h5 class="card-title">Run Full Import</h5></label>
+                            <div class="col-7">
+                                <button class="btn btn-outline-secondary" id="bbshopfy_full_import" data-function="importBBShopifyOrders">Run It</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 mb-3">
+                <h2>MYOB Order Importing</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 col-md-6 mb-3">
+                <div class="card">
+                    <a name="freedom"></a>
+                    <h4 class="card-header">Freedom Orders</h4>
+                    <div class="card-body">
+                        <div class="form-group row full_import">
+                            <label class="col-5"><h5 class="card-title">Run Full Import From MYOB</h5></label>
+                            <div class="col-7">
+                                <button class="btn btn-outline-secondary" id="freedom_full_import" data-function="importFreedomOrders">Run It</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endif;?>
         </div>
     </div>
 </div>
