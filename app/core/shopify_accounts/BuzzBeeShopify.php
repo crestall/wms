b5b25ee1a559ee0ae4ef70b7e7c68c5dc42fb931
marxolly <mark@solly.com.au>
2021-05-20 15:56:11 +1000

    Update BuzzBeeShopify.php

--- app/core/shopify_accounts/BuzzBeeShopify.php
+++ app/core/shopify_accounts/BuzzBeeShopify.php
@@ -104,16 +104,17 @@ class BuzzBeeShopify extends Shopify
             foreach($co['line_items'] as $li)
             {
                 echo "<p>Doing Item: ".$li['name']." (".$li['product_id'].")</p>";
-                echo "<pre>",print_r($li),"</pre>";
+                //echo "<pre>",print_r($li),"</pre>";
                 $variant_id = $li['variant_id'];
                 echo "<p> Doing Variant ".$li['variant_title']." ($variant_id)</p>";
                 try{
                     $variant = $this->shopify->ProductVariant($variant_id)->get();
+                    echo "THE VARIANT<pre>",print_r($variant),"</pre>";
                 } catch(Exception $e){
-                    echo "Error<pre>",print_r($e),"</pre>";die();
+                    echo "Error<pre>",print_r($e),"</pre>";//die();
                 }
 
-                echo "<pre>",print_r($variant),"</pre>";
+
                 echo "<p>--------------------------------------------</p>";
                 /*
                 try {
