bfcb977e4e113c86d264ae8e576bbc38703158d2
marxolly <mark@solly.com.au>
2021-05-21 15:03:15 +1000

    Update BuzzBeeShopify.php

--- app/core/shopify_accounts/BuzzBeeShopify.php
+++ app/core/shopify_accounts/BuzzBeeShopify.php
@@ -96,84 +96,35 @@ class BuzzBeeShopify extends Shopify
         $filtered_count = count($filtered_orders);
         echo "<h1>There are $filtered_count Orders Left</h1>";
 
-        /*
-        foreach($collected_orders as $coi => $co)
+        foreach($filtered_orders as $foi => $fo)
         {
-            $order_id = $co['id'];
-            $order_number = $co['order_number'];
-            echo "<p>Doing order: $order_number ($order_id)</p>";
-            //echo "THE ORDER<pre>",print_r($co),"</pre>";
-            echo "<p>=========================================</p>";
-            $item_count = count($collected_orders[$coi]['line_items']);
-            echo "<p>ITEM COUNT IS $item_count</p>";
-            $order_fulfillments = $this->shopify->Order($order_id)->FulfillmentOrder->get();
-            //echo "The Fulfillments<pre>",print_r($order_fulfillments),"</pre>";
-            foreach($order_fulfillments as $of)
+            if(!isset($fo['shipping_address']))
             {
-                if(!preg_match("/FSG/i", $of['assigned_location']['name']))
-                {
-                    foreach($of['line_items'] as $ofli)
-                    {
-                        $line_item_id = $ofli['line_item_id'];
-                        //echo "<p>$line_item_id does not belong</p>";
-                        //$olii = array_search($line_item_id, $co['line_items']);
-                        $key = array_search($line_item_id, array_column($co['line_items'], 'id'));
-                        unset($collected_orders[$coi]['line_items'][$key]);
-                        //echo "<p>Gonna delete line_itemm with index $key</p>";
-                    }
-                }
-            }
-            $item_count = count($collected_orders[$coi]['line_items']);
-            echo "<p>ITEM COUNT IS NOW $item_count</p>";
-            if( $item_count == 0 )
-            {
-                unset($collected_orders[$coi]);
-            }
-            echo "<p>=========================================</p>";
-            /*
-            $collected_orders[$coi]['total_weight'] = $co['total_weight']/1000;
-            if( isset($co['shipping_lines']) && !empty($co['shipping_lines']) )
-            {
-                if(preg_match("/FSG/i", $co['shipping_lines'][0]['code']))
-                {
-                    if(!isset($co['shipping_address']))
-                    {
-                        $collected_orders[$coi]['shipping_address'] = array(
-                            'first_name'    => $co['customer']['first_name'],
-                            'address1'      => $this->from_address_array['lines'][0],
-                            'phone'         => $co['customer']['phone'],
-                            'city'          => $this->from_address_array['suburb'],
-                            'zip'           => $this->from_address_array['postcode'],
-                            'province'      => $this->from_address_array['state'],
-                            'country'       => $this->from_address_array['country'],
-                            'last_name'     => $co['customer']['last_name'],
-                            'address2'      => '',
-                            'company'       => $co['customer']['default_address']['company'],
-                            'latitude'      => '',
-                            'longitude'     => '',
-                            'name'          => $co['customer']['default_address']['name'],
-                            'country_code'  => $this->from_address_array['country'],
-                            'province_code' => $this->from_address_array['state']
-                        );
-                        $collected_orders[$coi]['pickup'] = 1;
-                    }
-                    else
-                    {
-                        $collected_orders[$coi]['pickup'] = 0;
-                    }
-                }
-                else
-                {
-                    unset($collected_orders[$coi]);
-                }
+                $filtered_orders[$foi]['shipping_address'] = array(
+                    'first_name'    => $fo['customer']['first_name'],
+                    'address1'      => $this->from_address_array['lines'][0],
+                    'phone'         => $fo['customer']['phone'],
+                    'city'          => $this->from_address_array['suburb'],
+                    'zip'           => $this->from_address_array['postcode'],
+                    'province'      => $this->from_address_array['state'],
+                    'country'       => $this->from_address_array['country'],
+                    'last_name'     => $fo['customer']['last_name'],
+                    'address2'      => '',
+                    'company'       => $fo['customer']['default_address']['company'],
+                    'latitude'      => '',
+                    'longitude'     => '',
+                    'name'          => $fo['customer']['default_address']['name'],
+                    'country_code'  => $this->from_address_array['country'],
+                    'province_code' => $this->from_address_array['state']
+                );
+                $filtered_orders[$foi]['pickup'] = 1;
             }
             else
             {
-                unset($collected_orders[$coi]);
+                $filtered_orders[$foi]['pickup'] = 0;
             }
-
         }
-        */
+        echo "FILTERED<pre>",print_r($filtered_orders),"</pre>";
         die();
         //return $collected_orders;
         if($orders = $this->procOrders($collected_orders))
