bc1d8f8a155d49671d25bd0c58a5241afbda658c
marxolly <mark@solly.com.au>
2021-05-26 15:01:45 +1000

    Update footer.php

--- app/views/layout/orders/footer.php
+++ app/views/layout/orders/footer.php
@@ -1018,16 +1018,17 @@
                         });
                         /* */
                        jQuery.extend(jQuery.fn.dataTableExt.oSort, {
-                        "extract-date-pre": function(value) {
-                            var date = $(value, 'span')[0].innerHTML;
-                            date = date.split('/');
-                            return Date.parse(date[1] + '/' + date[0] + '/' + date[2])
-                        },
-                        "extract-date-asc": function(a, b) {
-                            return ((a < b) ? -1 : ((a > b) ? 1 : 0));
-                        },
-                        "extract-date-desc": function(a, b) {
-                            return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+                            "extract-date-pre": function(value) {
+                                var date = $(value, 'span')[0].innerHTML;
+                                date = date.split('/');
+                                return Date.parse(date[1] + '/' + date[0] + '/' + date[2])
+                            },
+                            "extract-date-asc": function(a, b) {
+                                return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+                            },
+                            "extract-date-desc": function(a, b) {
+                                return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+                            }
                         });
                         var dt_options = {
                             "columnDefs": [
