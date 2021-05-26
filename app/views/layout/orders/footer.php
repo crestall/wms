5526fe4085c1f633fbfe05be7b995844934b5bd8
marxolly <mark@solly.com.au>
2021-05-26 15:33:14 +1000

    Update footer.php

--- app/views/layout/orders/footer.php
+++ app/views/layout/orders/footer.php
@@ -1017,18 +1017,6 @@
                             });
                         });
                         /* */
-                       jQuery.extend(jQuery.fn.dataTableExt.oSort, {
-                            "extract-date-pre": function(value) {
-                                date = value.split('-');
-                                return Date.parse(date[1] + '/' + date[0] + '/' + date[2])
-                            },
-                            "extract-date-asc": function(a, b) {
-                                return ((a < b) ? -1 : ((a > b) ? 1 : 0));
-                            },
-                            "extract-date-desc": function(a, b) {
-                                return ((a < b) ? 1 : ((a > b) ? -1 : 0));
-                            }
-                        });
                         var dt_options = {
                             "columnDefs": [
                                 { "orderable": false, "targets": [3,5,9,10,11] },
