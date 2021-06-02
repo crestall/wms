adc8ade418c0a248b95c77b149c521ce00a54e13
marxolly <mark@solly.com.au>
2021-06-03 09:42:54 +1000

    Update PbaPerfectPracticeGolfShopify.php

--- app/core/shopify_accounts/PbaPerfectPracticeGolfShopify.php
+++ app/core/shopify_accounts/PbaPerfectPracticeGolfShopify.php
@@ -98,7 +98,7 @@ class PbaPerfectPracticeGolfShopify extends Shopify
         //else
         //{
             echo "<p>Gonna send the email</p>";
-            Email::sendPBAShopifyImportSummary($this->return_array);
+            Email::sendPBAShopifyImportSummary($this->return_array,"PPG Site");
             echo "<p>Sent the email</p>";die();
         //}
         //echo "<pre>",print_r($this->return_array),"</pre>";
