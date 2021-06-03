c88652294d6cf9b2d8db15bc9571c2d7e98401eb
marxolly <mark@solly.com.au>
2021-06-03 14:50:57 +1000

    Update BuzzBeeShopify.php

--- app/core/shopify_accounts/BuzzBeeShopify.php
+++ app/core/shopify_accounts/BuzzBeeShopify.php
@@ -39,7 +39,7 @@ class BuzzBeeShopify extends Shopify
 
         try{
             $this->shopify = new PHPShopify\ShopifySDK($this->config);
-            //echo "BUZZ BEE<pre>",var_dump($this->shopify),"</pre>";die();
+            echo "BUZZ BEE<pre>",print_r($this->shopify::$config),"</pre>";die();
         } catch (Exception $e) {
             echo "<pre>",print_r($e),"</pre>";die();
             $this->output .=  $e->getMessage() .PHP_EOL;
