d1417d5cdd3797d62737c0ae71ecede2ce713aa8
marxolly <mark@solly.com.au>
2021-06-21 12:34:58 +1000

    Update BuzzBeeShopify.php

--- app/core/shopify_accounts/BuzzBeeShopify.php
+++ app/core/shopify_accounts/BuzzBeeShopify.php
@@ -77,13 +77,13 @@ class BuzzBeeShopify extends Shopify
                     return $this->return_array;
             }
         }
-        echo "COLLECTED<pre>",print_r($collected_orders),"</pre>";
+        //echo "COLLECTED<pre>",print_r($collected_orders),"</pre>";
         //Also need to check for customer collect and no FSG handling
         $order_count = count($collected_orders);
         echo "<h1>Collected $order_count Orders</h1>";
         $filtered_orders = $this->filterForFSG($collected_orders);
         $filtered_count = count($filtered_orders);
-        //echo "<h1>There are $filtered_count Orders Left</h1>";
+        echo "<h1>There are $filtered_count Orders Left</h1>";
 
         foreach($filtered_orders as $foi => $fo)
         {
@@ -109,7 +109,7 @@ class BuzzBeeShopify extends Shopify
                 $filtered_orders[$foi]['pickup'] = 1;
             }
         }
-        echo "FILTERED<pre>",print_r($filtered_orders),"</pre>";
+        //echo "FILTERED<pre>",print_r($filtered_orders),"</pre>";
         die();
         //return $collected_orders;
         if($orders = $this->procOrders($filtered_orders))
