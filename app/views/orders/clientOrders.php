887f5f0f9ac3e25db099c9f2b1b3c86b0305c4ef
marxolly <mark@solly.com.au>
2021-06-21 14:55:59 +1000

    Update clientOrders.php

--- app/views/orders/clientOrders.php
+++ app/views/orders/clientOrders.php
@@ -1,4 +1,5 @@
 <div id="page-wrapper">
+    <input type="hidden" id="client_id" value="<?php echo $clientd_id;?>">
     <div id="page_container" class="container-xxl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <div class="row form-group">
