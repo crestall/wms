044bef2614496ac517ad691af933d8fa15f8a02c
marxolly <mark@solly.com.au>
2021-06-21 15:01:21 +1000

    Update clientDispatch.php

--- app/views/reports/clientDispatch.php
+++ app/views/reports/clientDispatch.php
@@ -1,7 +1,6 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
         <input type="hidden" name="client_id" id="client_id" value="<?php echo $client_id;?>" />
-        
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/between_dates.php");?>
         <?php if(count($client_orders)):?>
