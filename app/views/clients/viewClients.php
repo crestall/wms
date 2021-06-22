925395069cc3fa2133f6370ffeea0cf489dc0d8c
marxolly <mark@solly.com.au>
2021-06-22 10:49:51 +1000

    Update viewClients.php

--- app/views/clients/viewClients.php
+++ app/views/clients/viewClients.php
@@ -26,6 +26,7 @@
                             <th>Client Name</th>
                             <th>Contact Name</th>
                             <th>Contact Email</th>
+                            <th>Locked Orders</th>
                             <th></th>
                         </tr>
                     </thead>
@@ -36,6 +37,7 @@
                             <td data-label="Client Name"><img src="/images/client_logos/tn_<?php echo $c['logo'];?>" alt="client logo" class="img-thumbnail" /> <?php echo $c['client_name'];?></td>
                             <td data-label="Contact Name"><?php echo $c['contact_name'];?></td>
                             <td data-label="Contact Email"><?php echo $c['billing_email'];?></td>
+                            <td data-label="Locked Orders"><?php echo ($c['can_adjust'] > 0)?  "No" : "Yes";?></td>
                             <td>
                                 <p><a class="btn btn-outline-secondary" href="/clients/edit-client/client=<?php echo $c['id'];?>" >Edit Details</a></p>
                             </td>
