a6564baa9523780329bee6cdee6115a11a158d8f
marxolly <mark@solly.com.au>
2021-06-22 11:05:36 +1000

    Update viewClients.php

--- app/views/clients/viewClients.php
+++ app/views/clients/viewClients.php
@@ -11,40 +11,42 @@
             </div>
         </div>
         <?php if(count($clients)):?>
-            <div id="waiting" class="col">
+            <div id="waiting" class="row">
                 <div class="col-lg-12 text-center">
                     <h2>Drawing Table..</h2>
                     <p>May take a few moments</p>
                     <img class='loading' src='/images/preloader.gif' alt='loading...' />
                 </div>
             </div>
-            <div class="col" id="table_holder" style="display:none">
-                <table id="client_list_table" class="table-striped table-hover">
-                    <thead>
-                    	<tr>
-                            <th></th>
-                            <th>Client Name</th>
-                            <th>Contact Name</th>
-                            <th>Contact Email</th>
-                            <th>Locked Orders</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <?php foreach($clients as $c):?>
-                    	<tr>
-                            <td><?php echo $i;?></td>
-                            <td data-label="Client Name"><img src="/images/client_logos/tn_<?php echo $c['logo'];?>" alt="client logo" class="img-thumbnail" /> <?php echo $c['client_name'];?></td>
-                            <td data-label="Contact Name"><?php echo $c['contact_name'];?></td>
-                            <td data-label="Contact Email"><?php echo $c['billing_email'];?></td>
-                            <td data-label="Locked Orders"><?php echo ($c['can_adjust'] > 0)?  "No" : "Yes";?></td>
-                            <td>
-                                <p><a class="btn btn-outline-secondary" href="/clients/edit-client/client=<?php echo $c['id'];?>" >Edit Details</a></p>
-                            </td>
-                        </tr>
-                    <?php ++$i; endforeach;?>
-                    </tbody>
-                </table>
+            <div class="row" id="table_holder" style="display:none">
+                <div class="col-12">
+                    <table id="client_list_table" class="table-striped table-hover">
+                        <thead>
+                        	<tr>
+                                <th></th>
+                                <th>Client Name</th>
+                                <th>Contact Name</th>
+                                <th>Contact Email</th>
+                                <th>Locked Orders</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach($clients as $c):?>
+                        	<tr>
+                                <td><?php echo $i;?></td>
+                                <td data-label="Client Name"><img src="/images/client_logos/tn_<?php echo $c['logo'];?>" alt="client logo" class="img-thumbnail" /> <?php echo $c['client_name'];?></td>
+                                <td data-label="Contact Name"><?php echo $c['contact_name'];?></td>
+                                <td data-label="Contact Email"><?php echo $c['billing_email'];?></td>
+                                <td data-label="Locked Orders"><?php echo ($c['can_adjust'] > 0)?  "No" : "Yes";?></td>
+                                <td>
+                                    <p><a class="btn btn-outline-secondary" href="/clients/edit-client/client=<?php echo $c['id'];?>" >Edit Details</a></p>
+                                </td>
+                            </tr>
+                        <?php ++$i; endforeach;?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         <?php else:?>
             <div class="row">
