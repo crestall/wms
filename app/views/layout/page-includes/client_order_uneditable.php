501e06c580f0bcaef38047d39249cd5af940757a
marxolly <mark@solly.com.au>
2021-06-22 11:57:47 +1000

    blocking order item edits

---
+++ app/views/layout/page-includes/client_order_uneditable.php
@@ -0,0 +1,16 @@
+<div class="row">
+    <div class="col">
+        <div class="errorbox">
+            <div class="row">
+                <div class="col-4 text-right">
+                    <i class="fad fa-exclamation-triangle fa-6x"></i>
+                </div>
+                <div class="col-8">
+                    <h2>Order Not Editable</h2>
+                    <p>Orders for this client cannot be adjusted</p>
+                    <p>They will need to submit a new order with the updated details</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
