a04b2b9d50fa22229f91d4fb7cf4075ed1248801
marxolly <mark@solly.com.au>
2021-07-01 14:42:09 +1000

    Update inventoryCompare.php

--- app/views/adminOnly/inventoryCompare.php
+++ app/views/adminOnly/inventoryCompare.php
@@ -2,10 +2,12 @@
 
 ?>
 <div id="page-wrapper">
-    <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php"); ?>
-    <div class="row">
-        <div class="col-lg-12">
-            
+    <div id="page_container" class="container">
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php"); ?>
+        <div class="row">
+            <div class="col-lg-12">
+
+            </div>
         </div>
     </div>
 </div>
