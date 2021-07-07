3dee0f486c51168ba1f03fd881d3b8d0e7fecff7
marxolly <mark@solly.com.au>
2021-07-07 11:02:07 +1000

    Update pages_config.php

--- app/config/pages_config.php
+++ app/config/pages_config.php
@@ -899,7 +899,7 @@ $client = array(
             'display'   => true,
             'icon'      => '<i class="fad fa-shipping-timed fa-3x"></i>'
         ),
-        'register-new-product'    => array(
+        'record-new-product'    => array(
             'display'   => true,
             'icon'      => '<i class="fad fa-clipboard-check fa-3x"></i>'
         )
