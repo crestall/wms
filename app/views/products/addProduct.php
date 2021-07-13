be2572fc07eb54902cecd27bfa784f483e59ccdc
marxolly <mark@solly.com.au>
2021-07-13 12:41:14 +1000

    Update addProduct.php

--- app/views/products/addProduct.php
+++ app/views/products/addProduct.php
@@ -1,5 +1,17 @@
 <?php
 $client_id = Form::value('client_id');
+if(!empty(Form::value('external_image')))
+{
+    $eximage_display = "display:inline";
+    $image_display = "display:none";
+    $check = true;
+}
+else
+{
+    $eximage_display = "display:none";
+    $image_display = "display:inline";
+    $check = false;
+}
 ?>
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
@@ -28,6 +40,25 @@ $client_id = Form::value('client_id');
                             <?php echo Form::displayError('image');?>
                         </div>
                     </div>
+
+
+                    <div class="form-group row">
+                        <label class="col-md-3 col-form-label">Image</label>
+                        <div class="col-md-4">
+                            <div class="col checkbox checkbox-default">
+                                <input class="form-check-input styled" type="checkbox" id="external_image" name="external_image" <?php if($check) echo "checked";?> />
+                                <label for="external_image"><small><em>Image URL</em></small></label>
+                            </div>
+                            <input type="file" name="image" id="image" class="product_image" style="<?php echo $image_display;?>" />
+                            <input type="text" class="product_image form-control" name="eximage" id="eximage" style="<?php echo $eximage_display;?>" value="<?php echo $product['image'];?>">
+                            <?php echo Form::displayError('image');?>
+                        </div>
+                    </div>
+
+
+
+
+
                     <div class="form-group row">
                         <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Client</label>
                         <div class="col-md-4">
