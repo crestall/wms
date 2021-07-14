3374a5ca2fd904b7f44c0fb007927b86b94a805a
marxolly <mark@solly.com.au>
2021-07-14 16:25:55 +1000

    Update EbayAPI.php

--- app/core/eBay/EbayAPI.php
+++ app/core/eBay/EbayAPI.php
@@ -187,8 +187,8 @@
                 return $json['access_token'];
             }
         }
-        //echo "JSON<pre>",print_r($json),"</pre>";
-        //die("did a refresh");
+        echo "JSON<pre>",print_r($json),"</pre>";
+        die("did a refresh");
         return false;
     }
 }//end class
