a733751cc97cd3f238f84fbcbd5ebec49a6f7f24
marxolly <mark@solly.com.au>
2021-07-20 09:41:08 +1000

    ebay api tidying

--- app/core/Ebay.php
+++ app/core/Ebay.php
@@ -9,83 +9,70 @@
  * @author     Mark Solly <mark.solly@fsg.com.au>
  */
 
- class Ebay
+ class EbayAPI
  {
     public $userToken;
     public $controller;
 
-    protected $devID;
-    protected $appID;
-    protected $certID;
-    protected $clientID;
-    protected $serverUrl;
-    protected $authURL;
-    protected $paypalEmailAddress;
-    protected $ruName;
-    protected $APIHost;
-    protected $authToken;
-    protected $refreshToken;
-    protected $scope;
-    protected $authCode;
-
-    private $isLive = false;
-    private $table = "ebay_access_tokens";
+    protected $serverUrl = 'https://api.ebay.com';
+    protected $authURL = 'https://auth.ebay.com';
+    protected $output;
+    protected $return_array = array(
+        'import_count'          => 0,
+        'imported_orders'       => array(),
+        'error_orders'          => array(),
+        'import_error'          => false,
+        'error'                 => false,
+        'error_count'           => 0,
+        'error_string'          => '',
+        'import_error_string'   => ''
+    );
+    protected $ua;
+    protected $order_items;
 
     public function __construct(Controller $controller)
     {
         $this->controller = $controller;
-        $db = Database::openConnection();
-        $access_tokens = $db->queryByID($this->table, 1) ;
-        if($this->isLive)
-        {
-            $this->devID    = '<your-dev-id>'; // these prod keys are different from sandbox keys
-            $this->appID    = '<your-app-id>';
-            $this->certID   = '<your-cert-id>';
-            $this->clientID = '<your-client-id>';
-            $this->ruName   = 'Mark_Solly-MarkSoll-PBAFSG-xuwmap';
-            $this->serverUrl  = 'https://api.ebay.com';
-            $this->authURL = 'https://auth.ebay.com';
-            $this->scope    = '
-https://auth.ebay.com/oauth2/authorize?client_id=MarkSoll-PBAFSG-PRD-5418204ca-f642538e&response_type=code&redirect_uri=Mark_Solly-MarkSoll-PBAFSG-xuwmap&scope=https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/sell.marketing.readonly https://api.ebay.com/oauth/api_scope/sell.marketing https://api.ebay.com/oauth/api_scope/sell.inventory.readonly https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account.readonly https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly https://api.ebay.com/oauth/api_scope/sell.fulfillment https://api.ebay.com/oauth/api_scope/sell.analytics.readonly https://api.ebay.com/oauth/api_scope/sell.finances https://api.ebay.com/oauth/api_scope/sell.payment.dispute https://api.ebay.com/oauth/api_scope/commerce.identity.readonly';
-        }
-        else
-        {
-            $this->devID    = 'beaed030-6fea-4467-aafb-2b415518d84c';
-            $this->appID    = 'MarkSoll-PBAFSG-SBX-a4198d68c-6ede1508';
-            $this->certID   = 'SBX-4198d68c17e5-d20b-413a-9a6a-b93e';
-            $this->clientID = 'MarkSoll-PBAFSG-SBX-a4198d68c-6ede1508';
-            $this->ruName   = 'Mark_Solly-MarkSoll-PBAFSG-foldelyct';
-            $this->serverUrl  = 'https://api.sandbox.ebay.com';
-            $this->authURL = "https://auth.sandbox.ebay.com/";
-            $this->scope    = '
-https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/buy.order.readonly https://api.ebay.com/oauth/api_scope/buy.guest.order https://api.ebay.com/oauth/api_scope/sell.marketing.readonly https://api.ebay.com/oauth/api_scope/sell.marketing https://api.ebay.com/oauth/api_scope/sell.inventory.readonly https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account.readonly https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly https://api.ebay.com/oauth/api_scope/sell.fulfillment https://api.ebay.com/oauth/api_scope/sell.analytics.readonly https://api.ebay.com/oauth/api_scope/sell.marketplace.insights.readonly https://api.ebay.com/oauth/api_scope/commerce.catalog.readonly https://api.ebay.com/oauth/api_scope/buy.shopping.cart https://api.ebay.com/oauth/api_scope/buy.offer.auction https://api.ebay.com/oauth/api_scope/commerce.identity.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.email.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.phone.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.address.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.name.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.status.readonly https://api.ebay.com/oauth/api_scope/sell.finances https://api.ebay.com/oauth/api_scope/sell.item.draft https://api.ebay.com/oauth/api_scope/sell.payment.dispute https://api.ebay.com/oauth/api_scope/sell.item https://api.ebay.com/oauth/api_scope/sell.reputation https://api.ebay.com/oauth/api_scope/sell.reputation.readonly';
-        }
-        if(empty($access_tokens['code']))
-        {
-            $this->firstAuthAppToken();
-        }
-        else
-        {
-            $this->authCode = $access_tokens['code'];
-            $this->refreshToken = $access_tokens['refresh_token'];
-            if( time() >= $access_tokens['refresh_expires'] )
-            {
-                $this->authorizationToken();
-            }
-            elseif( time() >= $access_tokens['access_expires'] )
-            {
-                $this->refreshToken();
-            }
-        }
-        die( "current: ".time()." expires: ".$access_tokens['access_expires']);
-        $this->paypalEmailAddress= 'PAYPAL_EMAIL_ADDRESS';
-
     }
 
-    private function firstAuthAppToken() {
+//Background Helper Functions
+    protected function sendGetRequest($s_action, $authToken)
+    {
+        $url = $this->serverUrl."/".$s_action;
+        //die($url);
+        //die("authToken: ".$authToken);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+        $codeAuth = base64_encode($authToken);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Authorization: Bearer '.$authToken
+        ));
+        $result = curl_exec($ch);
+        $err = curl_error($ch);
+        $info = curl_getinfo($ch);
+        curl_close($ch);
+        if ($err)
+        {
+            die('Could not write to eBay API '.$err);
+        }
+        else
+        {
+            return $result;
+        }
+    }
+
+//Authorisation Functions
+    /* This one doesn't work*/
+    public function firstAuthAppToken() {
         $db = Database::openConnection();
 
-        $url = $this->authURL."/oauth2/authorize?client_id=".$this->clientID."&response_type=code&redirect_uri=".$this->ruName."&scope=".$this->scope;
+        //$url = $this->authURL."/oauth2/authorize?client_id=".$this->clientID."&response_type=code&redirect_uri=".$this->ruName."&scope=".$this->scope;
+
+        $url = $this->authURL."/oauth2/authorize?client_id=MarkSoll-PBAFSG-PRD-5418204ca-f642538e&response_type=code&redirect_uri=Mark_Solly-MarkSoll-PBAFSG-xuwmap&scope=https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/sell.marketing.readonly https://api.ebay.com/oauth/api_scope/sell.marketing https://api.ebay.com/oauth/api_scope/sell.inventory.readonly https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account.readonly https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly https://api.ebay.com/oauth/api_scope/sell.fulfillment https://api.ebay.com/oauth/api_scope/sell.analytics.readonly https://api.ebay.com/oauth/api_scope/sell.finances https://api.ebay.com/oauth/api_scope/sell.payment.dispute https://api.ebay.com/oauth/api_scope/commerce.identity.readonly";
+
 
         //$response = file_get_contents($url);
         die($url);
@@ -100,11 +87,12 @@ https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/buy.or
         ), 1);
     }
 
-    public function authorizationToken()
+    protected function authorizationToken(array $args)
     {
+        extract($args);
         $db = Database::openConnection();
         $link = $this->serverUrl."/identity/v1/oauth2/token";
-        $codeAuth = base64_encode($this->clientID.':'.$this->certID);
+        $codeAuth = base64_encode($this->clientID.':'.$certID);
         $ch = curl_init($link);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
             'Content-Type: application/x-www-form-urlencoded',
@@ -114,10 +102,10 @@ https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/buy.or
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "grant_type=authorization_code&code=".$this->authCode."&redirect_uri=".$this->ruName);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "grant_type=authorization_code&code=".$authCode."&redirect_uri=".$ruName);
         $response = curl_exec($ch);
         $json = json_decode($response, true);
-        //echo "<pre>",print_r($json),"</pre>"; die();
+        echo "<pre>",print_r($json),"</pre>"; die();
         $info = curl_getinfo($ch);
         curl_close($ch);
         if($json != null)
@@ -136,27 +124,49 @@ https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/buy.or
                     'access_expires'    => time() + $json['expires_in'],
                     'refresh_token'     => $json['refresh_token'],
                     'refresh_expires'   => time() + $json['refresh_token_expires_in']
-                ), 1);
+                ), $this->line_id);
             }
         }
     }
 
-    public function refreshToken()
+    protected function refreshToken(array $args)
     {
+       //echo "ARGS<pre>",print_r($args),"</pre>";
+        extract($args);
+        //ebay are a PACK!!!!!!!!! url encoding is the key to making it work!!!!! Not mentioned in the docs anywhere!!!!!
+        $scope = urlencode($scope);
+        
         $link = $this->serverUrl."/identity/v1/oauth2/token";
-        $codeAuth = base64_encode($this->clientID.':'.$this->certID);
-        $ch = curl_init($link);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/x-www-form-urlencoded',
-            'Authorization: Basic '.$codeAuth
+        //echo "<p>Link: $link</p>"; //die();
+        $codeAuth = base64_encode($clientID.':'.$certID);
+        $ch = curl_init();
+        curl_setopt_array($ch, array(
+            CURLOPT_URL => $link,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => 'grant_type=refresh_token&refresh_token='.$refreshToken.'&scope='.$scope,
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Basic '.$codeAuth,
+                'Content-Type: application/x-www-form-urlencoded'
+            ),
         ));
-        //echo $this->refreshToken;
-        //curl_setopt($ch, CURLHEADER_SEPARATE, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "grant_type=refresh_token&refresh_token=".$this->refreshToken."&scope=https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/buy.order.readonly https://api.ebay.com/oauth/api_scope/buy.guest.order https://api.ebay.com/oauth/api_scope/sell.marketing.readonly https://api.ebay.com/oauth/api_scope/sell.marketing https://api.ebay.com/oauth/api_scope/sell.inventory.readonly https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account.readonly https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly https://api.ebay.com/oauth/api_scope/sell.fulfillment https://api.ebay.com/oauth/api_scope/sell.analytics.readonly https://api.ebay.com/oauth/api_scope/sell.marketplace.insights.readonly https://api.ebay.com/oauth/api_scope/commerce.catalog.readonly https://api.ebay.com/oauth/api_scope/buy.shopping.cart https://api.ebay.com/oauth/api_scope/buy.offer.auction https://api.ebay.com/oauth/api_scope/commerce.identity.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.email.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.phone.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.address.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.name.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.status.readonly https://api.ebay.com/oauth/api_scope/sell.finances https://api.ebay.com/oauth/api_scope/sell.item.draft https://api.ebay.com/oauth/api_scope/sell.payment.dispute https://api.ebay.com/oauth/api_scope/sell.item https://api.ebay.com/oauth/api_scope/sell.reputation https://api.ebay.com/oauth/api_scope/sell.reputation.readonly");
         $response = curl_exec($ch);
+
+        if ($response === FALSE) {
+            printf("cUrl error (#%d): %s<br>\n", curl_errno($ch),
+                   htmlspecialchars(curl_error($ch)));
+            rewind($verbose);
+            $verboseLog = stream_get_contents($verbose);
+            echo "Verbose information:\n<pre>", htmlspecialchars($verboseLog), "</pre>\n";
+            die();
+        }
+
+        //echo "response<pre>",print_r($response),"</pre>"; die();
         $json = json_decode($response, true);
         $info = curl_getinfo($ch);
         curl_close($ch);
@@ -169,15 +179,16 @@ https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/buy.or
             }
             else
             {
-                $this->authToken = $json["access_token"];
                 $db = Database::openConnection();
                 $db->updateDatabaseFields($this->table, array(
                     'access_token'      => $json['access_token'],
                     'access_expires'    => time() + $json['expires_in']
-                ), 1);
+                ), $this->line_id);
+                return $json['access_token'];
             }
         }
-        echo "<pre>",print_r($json),"</pre>";
-        die("did a refresh");
+        //echo "JSON<pre>",print_r($json),"</pre>";
+        //die("did a refresh");
+        return false;
     }
 }//end class
--- app/core/eBay/eBay_clients/PBAeBay.php
+++ app/core/eBay/eBay_clients/PBAeBay.php
@@ -35,7 +35,6 @@
         $this->certID   = 'PRD-418204ca8801-818f-4441-94d4-d28c';
         $this->clientID = 'MarkSoll-PBAFSG-PRD-5418204ca-f642538e';
         $this->ruName   = 'Mark_Solly-MarkSoll-PBAFSG-xuwmap';
-
         $this->scope    = 'https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/sell.marketing.readonly https://api.ebay.com/oauth/api_scope/sell.marketing https://api.ebay.com/oauth/api_scope/sell.inventory.readonly https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account.readonly https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly https://api.ebay.com/oauth/api_scope/sell.fulfillment https://api.ebay.com/oauth/api_scope/sell.analytics.readonly https://api.ebay.com/oauth/api_scope/sell.finances https://api.ebay.com/oauth/api_scope/sell.payment.dispute https://api.ebay.com/oauth/api_scope/commerce.identity.readonly';
     }
 
