57edc36846ea98b170b8109701c776b7a437fb47
marxolly <mark@solly.com.au>
2021-07-28 12:50:00 +1000

    Update warehouseOrders.php

--- app/views/productionreports/warehouseOrders.php
+++ app/views/productionreports/warehouseOrders.php
@@ -4,24 +4,26 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/between_dates.php");?>
-        <div class="row">
-            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
-                <div class="form-group">
-                    <label>Filter By Client</label>
-                    <select id="client_selector" class="form-control selectpicker" data-style="btn-outline-secondary"><option value="0">All Production Clients</option><?php echo $this->controller->client->getSelectProductionClients($client_id);?></select>
+        <div class="border border-secondary p-3 m-3 rounded bg-light">
+            <?php include(Config::get('VIEWS_PATH')."layout/page-includes/between_dates.php");?>
+            <div class="row">
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
+                    <div class="form-group">
+                        <label>Filter By Client</label>
+                        <select id="client_selector" class="form-control selectpicker" data-style="btn-outline-secondary"><option value="0">All Production Clients</option><?php echo $this->controller->client->getSelectProductionClients($client_id);?></select>
+                    </div>
                 </div>
-            </div>
-            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
-                <div class="form-group">
-                    <label>Filter By Status</label>
-                    <select id="status_selector" class="form-control selectpicker" data-style="btn-outline-secondary"><option value="0">All Status</option><?php echo $this->controller->order->getSelectStatuses($status_id);?></select>
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
+                    <div class="form-group">
+                        <label>Filter By Status</label>
+                        <select id="status_selector" class="form-control selectpicker" data-style="btn-outline-secondary"><option value="0">All Status</option><?php echo $this->controller->order->getSelectStatuses($status_id);?></select>
+                    </div>
                 </div>
-            </div>
-            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
-                <div class="form-group">
-                    <label>Search</label>
-                    <input type="text" class="form-control" id="table_searcher" />
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
+                    <div class="form-group">
+                        <label>Search</label>
+                        <input type="text" class="form-control" id="table_searcher" />
+                    </div>
                 </div>
             </div>
         </div>
