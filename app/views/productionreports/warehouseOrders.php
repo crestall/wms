b6153e1ed00f42d013de449081263adab9821a9a
marxolly <mark@solly.com.au>
2021-07-28 11:04:44 +1000

    Update warehouseOrders.php

--- app/views/productionreports/warehouseOrders.php
+++ app/views/productionreports/warehouseOrders.php
@@ -4,6 +4,7 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/between_dates.php");?>
         <div class="row">
             <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
                 <div class="form-group">
