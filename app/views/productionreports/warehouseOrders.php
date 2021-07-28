520fb0ad07a28b1d3cf92ae010c92a6a04440151
marxolly <mark@solly.com.au>
2021-07-28 14:05:53 +1000

    Update warehouseOrders.php

--- app/views/productionreports/warehouseOrders.php
+++ app/views/productionreports/warehouseOrders.php
@@ -5,6 +5,7 @@
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <div class="border border-secondary p-3 m-3 rounded bg-light">
+            <h3>Filter These Orders</h3>
             <?php include(Config::get('VIEWS_PATH')."layout/page-includes/between_dates.php");?>
             <div class="row">
                 <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
@@ -25,6 +26,14 @@
                         <input type="text" class="form-control" id="table_searcher" />
                     </div>
                 </div>
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
+                    <div class="form-group">
+                        <a href="/production-reports/warehouse-orders" class="btn btn-outline-danger" >Remove Filters</a>
+                    </div>
+                </div>
+
+
+
             </div>
         </div>
     </div>
