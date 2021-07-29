e2816f59f16ae7c664740e4970a59ad3f7d8f814
marxolly <mark@solly.com.au>
2021-07-29 12:44:37 +1000

    Create warehouse-orders.php

---
+++ app/views/layout/page-includes/home_pages/quick_links/warehouse-orders.php
@@ -0,0 +1,8 @@
+    <div class="card homepagecard">
+        <div class="card-header">
+            <h4>Warehouse Orders</h4>
+        </div>
+        <div class="card-body text-center">
+        	<a class="btn btn-lg btn-outline-fsg" href="/production-reports/warehouse-orders"><i class="fad fa-warehouse-alt fa-3x"></i></a>
+        </div>
+    </div>
