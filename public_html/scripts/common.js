dbecfbdf0045de148663723e4ca94cfa46be5186
marxolly <mark@solly.com.au>
2021-07-29 15:04:18 +1000

    Update common.js

--- public_html/scripts/common.js
+++ public_html/scripts/common.js
@@ -279,6 +279,7 @@ var jobDeliveryDestinations = {
                     {
                         $( this ).prop( "disabled", true );
                         $( this ).val( "" );
+                        $(this).valid();
                     }
                 });
             }
