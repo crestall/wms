93cd8c83c1e6f0507b05abbb02f2e49131eeb676
marxolly <mark@solly.com.au>
2021-08-02 11:32:22 +1000

    Update production_sales_home.php

--- app/views/layout/page-includes/home_pages/production_sales_home.php
+++ app/views/layout/page-includes/home_pages/production_sales_home.php
@@ -4,11 +4,10 @@
 <div class="card-deck homepagedeck">
     <?php
     include(Config::get('VIEWS_PATH')."layout/page-includes/home_pages/quick_links/view-jobs.php");
-    include(Config::get('VIEWS_PATH')."layout/page-includes/home_pages/quick_links/warehouse-orders.php");
     include(Config::get('VIEWS_PATH')."layout/page-includes/home_pages/quick_links/add-customer.php");
     include(Config::get('VIEWS_PATH')."layout/page-includes/home_pages/quick_links/view-customers.php");
     include(Config::get('VIEWS_PATH')."layout/page-includes/home_pages/quick_links/view-finishers.php");
     ?>
 </div>
-<?php include(Config::get('VIEWS_PATH')."layout/page-includes/home_pages/quick_links/production-orders.php"); ?>  
+<?php include(Config::get('VIEWS_PATH')."layout/page-includes/home_pages/quick_links/production-orders.php"); ?>
 <?php include(Config::get('VIEWS_PATH')."layout/page-includes/home_pages/activity_charts.php"); ?> 
