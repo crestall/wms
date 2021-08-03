4e5a3775b5fd6deb6780189a2c8f68b750707466
marxolly <mark@solly.com.au>
2021-08-03 15:20:15 +1000

    Update noJobId.php

--- app/views/errors/noJobId.php
+++ app/views/errors/noJobId.php
@@ -11,7 +11,7 @@
                         <div class="col-8">
                             <h2>No Job ID Supplied</h2>
                             <p>Please only follow links from within the web application</p>
-                            <p>If you did click on a link and still got this message, please let us know the number that appears in the URL after job= in the address bar</p>
+                            <p>If this error persists and you would like to report it, please let us know which page has the link/button you originally clicked on</p>
                         </div>
                     </div>
                 </div>
