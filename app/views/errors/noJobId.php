37b124e52f07586a3bfb12e558f4f6dab9b012b5
marxolly <mark@solly.com.au>
2021-08-03 15:17:23 +1000

    Update noJobId.php

--- app/views/errors/noJobId.php
+++ app/views/errors/noJobId.php
@@ -1,5 +1,6 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <div class="row">
             <div class="col">
                 <div class="errorbox">
