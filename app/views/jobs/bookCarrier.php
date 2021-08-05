1cc00c91e82a53989891c808983473b766b03333
marxolly <mark@solly.com.au>
2021-08-05 11:05:10 +1000

    Update bookCarrier.php

--- app/views/jobs/bookCarrier.php
+++ app/views/jobs/bookCarrier.php
@@ -1,11 +1,112 @@
 <?php
-
+$ship_to    = (empty(Form::value('ship_to')))?  $job['ship_to']      : Form::value('ship_to');
+$address    = empty(Form::value('address'))?    $job['address']      : Form::value('address');
+$address2   = empty(Form::value('address2'))?   $job['address_2']    : Form::value('address2');
+$suburb     = empty(Form::value('suburb'))?     $job['suburb']       : Form::value('suburb');
+$state      = empty(Form::value('state'))?      $job['state']        : Form::value('state');
+$postcode   = empty(Form::value('postcode'))?   $job['postcode']     : Form::value('postcode');
+$country    = empty(Form::value('country'))?    $job['country']      : Form::value('country');
+$delivery_instructions = empty(Form::value('delivery_instructions'))? $job['delivery_instructions'] : Form::value('delivery_instructions');
+$attention = empty(Form::value('attention'))? $job['attention'] : Form::value('attention');
 ?>
 <div id="page-wrapper">
     <div id="page_container" class="container-xxl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <div class="row">
-            <?php echo "<pre>",print_r($job),"</pre>";?>
+            <?php //echo "<pre>",print_r($job),"</pre>";?>
+<!------------------------------------------------------------------------------------------------------------------------------------------->
+<!-------------------------------------------------     Delivery Details     ---------------------------------------------------------------->
+<!------------------------------------------------------------------------------------------------------------------------------------------->
+            <div class="col-sm-12 col-md-6 mb-3">
+                <div class="card h-100 border-secondary order-card" id="deliverydetails">
+                    <div class="card-header bg-secondary text-white">
+                        Delivery Details
+                    </div>
+                    <div class="card-body">
+                        <?php if(isset($_SESSION['jobdeliverydetailsfeedback'])) :?>
+                            <div class='feedbackbox'><?php echo Session::getAndDestroy('jobdeliverydetailsfeedback');?></div>
+                        <?php endif; ?>
+                        <?php if(isset($_SESSION['jobdeliverydetailserrorfeedback'])) :?>
+                            <div class='errorbox'><?php echo Session::getAndDestroy('jobdeliverydetailserrorfeedback');?></div>
+                        <?php endif; ?>
+                        <form id="job_delivery_details_update" method="post" action="/form/procProductionJobDeliveryUpdate">
+                            <div id="delivery_address_holder">
+                                <div class="form-group row">
+                                    <label class="col-md-4 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Deliver To</label>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control required" name="ship_to" id="ship_to" value="<?php echo $ship_to;?>" />
+                                        <?php echo Form::displayError('ship_to');?>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-4 col-form-label">Attention</label>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control" name="attention" id="attention" value="<?php echo $attention;?>" />
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-4 col-form-label">Delivery Instructions</label>
+                                    <div class="col-md-8">
+                                        <textarea class="form-control" name="delivery_instructions" id="delivery_instructions" placeholder="Instructions For Driver"><?php echo $delivery_instructions;?></textarea>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-4"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Address Line 1</label>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control required" name="address" id="address" value="<?php echo $address;?>" /><br>
+                                        <div class="checkbox checkbox-default" style="margin-left:20px;margin-top:-25px">
+                                            <input class="form-check-input styled" type="checkbox" id="ignore_address_error" name="ignore_address_error" <?php if(!empty(Form::value('ignore_address_error'))) echo 'checked';?> />
+                                            <label for="ignore_address_error"><span class="inst">No need for a number</span></label>
+                                        </div>
+                                        <?php echo Form::displayError('address');?>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-4">Address Line 2</label>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control" name="address2" id="address2" value="<?php echo $address2;?>" />
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-4"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Suburb/Town</label>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control required" name="suburb" id="suburb" value="<?php echo $suburb;?>" />
+                                        <?php echo Form::displayError('suburb');?>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-4">State</label>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control" name="state" id="state" value="<?php echo $state;?>" />
+                                        <span class="inst">for AU addresses use VIC, NSW, QLD, ACT, TAS, WA, SA, NT only</span>
+                                        <?php echo Form::displayError('state');?>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-4 "><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Postcode</label>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control required" name="postcode" id="postcode" value="<?php echo $postcode;?>" />
+                                        <?php echo Form::displayError('postcode');?>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-4"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Country</label>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control required" name="country" id="country" value="<?php echo $country;?>" />
+                                        <span class="inst">use the 2 letter ISO code</span>
+                                        <?php echo Form::displayError('country');?>
+                                    </div>
+                                </div>
+                                <input type="hidden" id="csrf_token" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+                                <input type="hidden" name="job_id" id="job_id" value="<?php echo $job['id'];?>" >
+                            </div>
+                        </form>
+                    </div>
+                    <div class="card-footer text-right">
+                        <button id="delivery_details_update_submitter" class="btn btn-outline-secondary">Save Changes</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
