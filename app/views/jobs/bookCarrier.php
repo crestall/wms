cb44b76a95b23e48ccb7b8d635e00e5c855575eb
marxolly <mark@solly.com.au>
2021-08-05 14:05:51 +1000

    Update bookCarrier.php

--- app/views/jobs/bookCarrier.php
+++ app/views/jobs/bookCarrier.php
@@ -13,7 +13,7 @@ $tracking_email = Form::value('tracking_email');
 <div id="page-wrapper">
     <div id="page_container" class="container-xxl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-        <div class="row">
+        <div class="row card-columns">
             <?php //echo "<pre>",print_r($job),"</pre>";?>
 <!------------------------------------------------------------------------------------------------------------------------------------------->
 <!-------------------------------------------------     Delivery Details     ---------------------------------------------------------------->
