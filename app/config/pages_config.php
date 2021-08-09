e08242c95b0c6da113e497ef851bac2ecbeb2f28
marxolly <mark@solly.com.au>
2021-08-09 12:34:52 +1000

    Update pages_config.php

--- app/config/pages_config.php
+++ app/config/pages_config.php
@@ -793,7 +793,7 @@ $admin = array(
             'display'   => true,
             'icon'      => '<div class="fa-2x"><span class="fa-layers fa-fw"><i class="fad fa-list-ol"></i><i class="far fa-check" data-fa-transform="right-3 shrink-2 up-1" style="color:#66ff33"></i></span></div>'
         ),
-        'data-tables-testing'   => array(
+        'shopify-api-testing'   => array(
             'display'   => true,
             'icon'      => "<i class='fal fa-question-square fa-3x'></i>"
         ),
