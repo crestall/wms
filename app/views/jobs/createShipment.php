4166401843b6dab341fa6b1501b267b2749318b4
marxolly <mark@solly.com.au>
2021-08-13 12:42:29 +1000

    Update createShipment.php

--- app/views/jobs/createShipment.php
+++ app/views/jobs/createShipment.php
@@ -51,12 +51,10 @@ $tracking_email = empty(Form::value('tracking_email'))? $shipment_details['deliv
                                 <label class="col-5">Signature Required</label>
                                 <div class="col-7"><?php if($shipment_details['signature_required'] == 0) echo "NO"; else echo "YES";?></div>
                             </div>
-                            <?php if(!empty($shipment_details['tracking_email'])):?>
-                                <div class="row">
-                                    <label class="col-5">Tracking Email</label>
-                                    <div class="col-7"><?php echo $shipment_details['tracking_email'];?></div>
-                                </div>
-                            <?php endif;?>
+                            <div class="row">
+                                <label class="col-5">Tracking Email</label>
+                                <div class="col-7"><?php if(!empty($shipment_details['tracking_email'])) echo $shipment_details['tracking_email']; else echo "NOT LISTED"?></div>
+                            </div>
                             <?php if(!empty($shipment_details['contact_phone'])):?>
                                 <div class="row">
                                     <label class="col-5">Contact Phone</label>
