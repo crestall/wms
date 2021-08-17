f52ef140581634278ee9325a2af3c20f554503ca
marxolly <mark@solly.com.au>
2021-08-17 11:27:21 +1000

    Update BuzzBeeShopify.php

--- app/core/shopify_accounts/BuzzBeeShopify.php
+++ app/core/shopify_accounts/BuzzBeeShopify.php
@@ -118,7 +118,7 @@ class BuzzBeeShopify extends Shopify
         $filtered_orders = $this->filterForFSG($collected_orders);
         $filtered_count = count($filtered_orders);
         //echo "<h1>There are $filtered_count Orders Left</h1>";die();
-        echo "FILTERED PRIOR<pre>",print_r($filtered_orders),"</pre>";
+        //echo "FILTERED PRIOR<pre>",print_r($filtered_orders),"</pre>";
         foreach($filtered_orders as $foi => $fo)
         {
             if(!isset($fo['shipping_address']))
@@ -179,7 +179,7 @@ class BuzzBeeShopify extends Shopify
             } catch (Exception $e) {
                 echo "In the Filter<pre>",print_r($e),"</pre>";die();
             }
-            //echo "<pre>Order Fulfillments",print_r($order_filfilments),"</pre>";
+            echo "<pre>Order Fulfillments",print_r($order_fulfillments),"</pre>";
             foreach($order_fulfillments as $of)
             {
                 if( !preg_match("/FSG/i", $of['assigned_location']['name']) || $of['status'] == 'closed' )
@@ -201,7 +201,7 @@ class BuzzBeeShopify extends Shopify
                 //echo "<p>Gonna remove $order_number</p>";
                 unset($collected_orders[$coi]);
             }
-            //echo "<p>-------------------------------------------------------------------------------------------------------</p>";
+            echo "<p>-------------------------------------------------------------------------------------------------------</p>";
         }
         return $collected_orders;
     }
