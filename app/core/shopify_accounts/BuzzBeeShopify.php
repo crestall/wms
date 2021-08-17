480c9d8d8de898d8363810ba70891e7316c855e1
marxolly <mark@solly.com.au>
2021-08-17 12:41:35 +1000

    Update BuzzBeeShopify.php

--- app/core/shopify_accounts/BuzzBeeShopify.php
+++ app/core/shopify_accounts/BuzzBeeShopify.php
@@ -88,6 +88,7 @@ class BuzzBeeShopify extends Shopify
             'fulfillment_status'    => 'unfulfilled',
             'fields'                => 'id,created_at,order_number,email,total_weight,shipping_address,line_items,shipping_lines,customer',
             //'ids'					=> $ids,
+            'limit'                 =>  250,
             'since_id'              => '3670246097047'
         );
         $shopify = $this->resetConfig($this->config);
@@ -114,10 +115,10 @@ class BuzzBeeShopify extends Shopify
         //echo "COLLECTED<pre>",print_r($collected_orders),"</pre>";
         //Also need to check for customer collect and no FSG handling
         $order_count = count($collected_orders);
-        //echo "<h1>Collected $order_count Orders</h1>";
+        echo "<h1>Collected $order_count Orders</h1>";
         $filtered_orders = $this->filterForFSG($collected_orders);
         $filtered_count = count($filtered_orders);
-        //echo "<h1>There are $filtered_count Orders Left</h1>";die();
+        echo "<h1>There are $filtered_count Orders Left</h1>";die();
         //echo "FILTERED PRIOR<pre>",print_r($filtered_orders),"</pre>";
         foreach($filtered_orders as $foi => $fo)
         {
