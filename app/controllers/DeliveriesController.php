fccad09182f1646181e87228d119017c06fb944a
marxolly <mark@solly.com.au>
2021-09-15 15:42:02 +1000

    Create DeliveriesController.php

---
+++ app/controllers/DeliveriesController.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Deliveries controller
+ *
+
+ * @author     Mark Solly <mark.solly@fsg.com.au>
+ */
+
+class DeliveriesController extends Controller
+{
+    public function beforeAction()
+    {
+        parent::beforeAction();
+        $this->Security->config("form", [ 'fields' => ['csrf_token']]);
+    }
+
+    public function index()
+    {
+        //set the page name for menu display
+        Config::setJsConfig('curPage', 'deliveries-index');
+        parent::displayIndex(get_class());
+    }
+
+
+
+    public function isAuthorized(){
+        $action = $this->request->param('action');
+        //$role = Session::getUserRole();
+        $role = (Session::isAdminUser())? 'admin' : Session::getUserRole();
+        $resource = "deliveries";
+
+        //only for delivery clients
+        if(Session::isDeliveryClientUser())
+        {
+            //Permission::allow('client', $resource, "*");
+            //return Permission::check($role, $resource, $action);
+            return true;
+        }
+        return false;
+    }
+}
+?>
