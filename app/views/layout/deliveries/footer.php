b0cece69f3d528bc2c51ed9d61fd2b1f669dd852
marxolly <mark@solly.com.au>
2021-09-16 15:08:11 +1000

    Update footer.php

--- app/views/layout/deliveries/footer.php
+++ app/views/layout/deliveries/footer.php
@@ -22,10 +22,13 @@
                         select: function(event, ui) {
                             var item_count = ($("div.item_holder").length) - 1;
                             var html = "<div id='item_holder_"+item_count+" class='item_holder p-3 pb-0 mb-2 rounded-top mid-grey>";
-                            html += "<h3 class='text-center'>"+ui.item.value+"</h3>";
+                            html += "<h4 class='text-center'>"+ui.item.value+"</h4>";
                             html += "</div>";
 
                             $('div#items_holder').append(html);
+                            var add_id = $('input#selected_items').val()+","+ui.item.item_id;
+                            var new_remove = add_id.replace(/^,|,$/g,'');
+                            $('input#selected_items').val(new_remove);
                         },
                         change: function (event, ui) {
                             if (!ui.item)
