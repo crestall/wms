516f824b80ce7c9bb66f58ecfead7ada5191be70
marxolly <mark@solly.com.au>
2021-09-16 15:54:21 +1000

    Update footer.php

--- app/views/layout/deliveries/footer.php
+++ app/views/layout/deliveries/footer.php
@@ -21,10 +21,19 @@
                         },
                         select: function(event, ui) {
                             var item_count = ($("div.item_holder").length) - 1;
-                            var ta = parseInt(ui.item.total_available )
+                            var ta = parseInt(ui.item.total_available );
+                            var locations = ui.item.locations.split("~");
                             var html = "<div id='item_holder_"+item_count+"' class='item_holder p-3 pb-0 mb-2 rounded-top mid-grey'>";
                             html += "<h5 class='text-center'>"+ui.item.value+"</h5>";
                             html += "<p class='text-center'>Currently "+ta.toLocaleString('en')+" available in total</p>";
+                            html += "<div class='row'>";
+                            locations.forEach(function location)
+                            {
+                                loc_array = location.split("|");
+                                html += "<div class='col-5'>Pallet With "+loc_array[0]+"</div>";
+                                html += "<div class='col-1'><input type='checkbox'></div>";
+                            }
+                            html += "</div>"
                             html += "</div>";
 
                             $('div#items_holder').append(html);
