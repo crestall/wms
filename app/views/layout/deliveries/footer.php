0201caf27b7d125cadd24021f3f023af754e481d
marxolly <mark@solly.com.au>
2021-09-16 12:25:07 +1000

    Update footer.php

--- app/views/layout/deliveries/footer.php
+++ app/views/layout/deliveries/footer.php
@@ -8,6 +8,30 @@
                 'common':{
 
                 },
+                'item-searcher':function(){
+                    var client_id = $('#client_id').val();
+                    var selected_items = $('#selected_items').val();
+                    $("input#item_searcher").autocomplete({
+                        source: function(req, response){
+                            var url = "/ajaxfunctions/getDeliveryItems/?item="+req.term+"&clientid="+client_id+"&exclude="+selected_items;
+                            //console.log(url);
+                        	$.getJSON(url, function(data){
+                        		response(data);
+                        	});
+                        },
+                        select: function(event, ui) {
+
+                        },
+                        change: function (event, ui) {
+                            if (!ui.item)
+                	        {
+                                $(event.target).val("");
+                                return false;
+                            }
+                        },
+                        minLength: 2
+                    });
+                },
                 'book-delivery':{
                     init: function(){
 
