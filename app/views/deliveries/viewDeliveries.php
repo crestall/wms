626c4c492aee0f5ca6f519bc7346c426a0a61b1e
marxolly <mark@solly.com.au>
2021-09-21 09:57:01 +1000

    Update viewDeliveries.php

--- app/views/deliveries/viewDeliveries.php
+++ app/views/deliveries/viewDeliveries.php
@@ -10,5 +10,28 @@
                 <span class="inst">These are deliveries yet to be completed.<br>Complete deliveries can be found in the "Reports" section</span>
             </div>
         </div>
+        <?php if(count($deliveries)):?>
+            <div id="waiting" class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Drawing Table..</h2>
+                    <p>May take a few moments</p>
+                    <img class='loading' src='/images/preloader.gif' alt='loading...' />
+                </div>
+            </div>
+            <div class="row" id="table_holder" style="display:none">
+
+            </div>
+        <?php else:?>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="errorbox">
+                        <h2><i class="fas fa-exclamation-triangle"></i> No Open Deliveries Found</h2>
+                        <p>You can use the Report Links above to view completed deliveries</p>
+                        <p>You can search for deliveries <a href="/deliveries/search">here</a></p>
+                        <p>You can book a new delivery <a href="/deliveries/book-delivery">here</a></p>
+                    </div>
+                </div>
+            </div>
+        <?php endif;?>
     </div>
 </div>
