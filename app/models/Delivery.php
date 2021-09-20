6d67ebea723685ed1070bc891497722623330600
marxolly <mark@solly.com.au>
2021-09-21 09:37:25 +1000

    Update Delivery.php

--- app/models/Delivery.php
+++ app/models/Delivery.php
@@ -74,7 +74,7 @@ class Delivery extends Model{
                 d.*,
                 s.name AS status, s.stage, s.class AS status_class,
                 (SELECT MAX(stage) FROM {$this->status_table}) AS total_stages,
-                u.name AS delivery_window, u.rank AS importance,
+                u.name AS delivery_window, u.rank AS importance, u.class AS delivery_window_class,
                 GROUP_CONCAT(
                     i.item_id,'|',
                     items.name,'|',
