f743acf63e5f61acc56c55e9b86b2fdf0152cb6b
marxolly <mark@solly.com.au>
2021-09-21 14:11:11 +1000

    Update DeliveriesController.php

--- app/controllers/DeliveriesController.php
+++ app/controllers/DeliveriesController.php
@@ -59,18 +59,26 @@ class DeliveriesController extends Controller
 
     public function isAuthorized(){
         $action = $this->request->param('action');
-        $role = Session::getUserRole();
-        //$role = (Session::isAdminUser())? 'admin' : Session::getUserRole();
+        //$role = Session::getUserRole();
+        $role = (Session::isAdminUser())? 'admin' : Session::getUserRole();
         $resource = "deliveries";
-
+        Permission::allow('admin', $resource, "[
+            'index',
+            'manage-deliveries'
+        ]");
         //only for delivery clients
         if(Session::isDeliveryClientUser())
         {
-            Permission::allow('client', $resource, "*");
-            return Permission::check($role, $resource, $action);
+            Permission::allow('client', $resource, "[
+                'index',
+                'book-delivery',
+                'view-deliveries'
+            ]");
+
             //return true;
         }
-        return false;
+        return Permission::check($role, $resource, $action);
+        //return false;
     }
 }
 ?>
