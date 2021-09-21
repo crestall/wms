6504418e943199c5ae8df7f842ba47316e26ac2b
marxolly <mark@solly.com.au>
2021-09-21 11:25:38 +1000

    Update viewDeliveries.php

--- app/views/deliveries/viewDeliveries.php
+++ app/views/deliveries/viewDeliveries.php
@@ -50,6 +50,7 @@
                                     <td class="bg-<?php echo $d['delivery_window_class'];?> delivery-window"><?php echo ucwords($d['delivery_window']);?></td>
                                     <td>The Items</td>
                                     <td>
+                                        <p><?php echo $d['status'];?></p>
                                         <div class="progress">
                                             <div class="progress-bar progress-bar-striped progress-bar-animated bg-<?php echo $d['status_class'];?>" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
                                         </div>
