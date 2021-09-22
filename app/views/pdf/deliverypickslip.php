e8cb4ce80c7c3420b569871dca3c585535aa614c
marxolly <mark@solly.com.au>
2021-09-22 16:18:21 +1000

    Update deliverypickslip.php

--- app/views/pdf/deliverypickslip.php
+++ app/views/pdf/deliverypickslip.php
@@ -55,7 +55,7 @@ foreach($delivery_ids as $id):
                     <td><?php echo $location;?></td>
                     <td><?php echo $item_qty;?> - Full Pallet</td>
                     <td class='centre'><table class="checkbox"><tr><td></td></tr></table></td>
-                    <td class='centre'><span class='check_box'></span></td>
+                    <td class='centre'><table class="checkbox"><tr><td></td></tr></table></td>
                 </tr>
             <?php endforeach;?>
         </table>
