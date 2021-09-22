8b5cfd835024e017267e563e51f1a826beef5ffe
marxolly <mark@solly.com.au>
2021-09-22 15:19:12 +1000

    Update deliverypickslip.php

--- app/views/pdf/deliverypickslip.php
+++ app/views/pdf/deliverypickslip.php
@@ -1,9 +1,40 @@
 <?php
 
 foreach($delivery_ids as $id):
-    $dd = $this->controller->delivery->getDeliveryDetails($id);
-    echo "<pre>",print_r($dd),"</pre>";//die();
+    $d = $this->controller->delivery->getDeliveryDetails($id);
+    //echo "<pre>",print_r($dd),"</pre>";//die();
+    $address_string = "";
+    if(!empty($d['address'])) $address_string .= $d['address'];
+    if(!empty($d['address_2'])) $address_string .= "<br/>".$d['address_2'];
+    if(!empty($d['suburb'])) $address_string .= "<br/>".$d['suburb'];
+    if(!empty($d['state'])) $address_string .= "<br/>".$d['state'];
+    if(!empty($d['country'])) $address_string .= "<br/>".$d['country'];
+    if(!empty($d['postcode'])) $address_string .= "<br/>".$d['postcode'];
+    //$this->controller->delivery->markDeliveryPicked($id)
     ?>
-    
+    <div class="pickslip">
+        <h2>FSG Delivery Picking Slip</h2>
+        <table width="100%">
+            <tr>
+                <td class="right">Printed</td>
+                <td><?php echo date("h:i a d/m/Y");?></td>
+            </tr>
+            <tr>
+                <td class="right">Date/Time Requested</td>
+                <td><?php echo date('D d/m/Y - g:i A', $d['date_entered']);?></td>
+            </tr>
+            <tr>
+                <td class="right">Delivery Window/td>
+                <td><?php echo ucwords($d['delivery_window']);?></td>
+            </tr>
+            <tr>
+                <td class="right">Deliver To</td>
+                <td>
+                    <p style="font-weight:bold"><?php echo $d['attention'];?></p>
+                    <p><?php echo $address_string;?></p>
+                </td>
+            </tr>
+        </table>
+    </div>
     <pagebreak />
 <?php endforeach; ?>
