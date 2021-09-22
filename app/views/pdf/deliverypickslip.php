f2c907d8238339e142c245abbb23a8304414c374
marxolly <mark@solly.com.au>
2021-09-22 16:05:08 +1000

    Update deliverypickslip.php

--- app/views/pdf/deliverypickslip.php
+++ app/views/pdf/deliverypickslip.php
@@ -12,6 +12,7 @@ foreach($delivery_ids as $id):
     if(!empty($d['country'])) $address_string .= "<br/>".$d['country'];
     if(!empty($d['postcode'])) $address_string .= "<br/>".$d['postcode'];
     //$this->controller->delivery->markDeliveryPicked($id)
+    $items = explode("~",$d['items']);
     ?>
     <div class="pickslip">
         <h2>FSG Delivery Picking Slip</h2>
@@ -45,6 +46,18 @@ foreach($delivery_ids as $id):
                 <th>Picked</th>
                 <th>Checked</th>
             </tr>
+            <?php foreach($items as $i):
+                list($item_id, $item_name, $item_sku, $item_qty, $location_id) = explode("|",$i);
+                $location = $this->controller->location->getLocationName($location_id);?>
+                <tr>
+                    <td><?php echo $item_name;?></td>
+                    <td><?php echo $item_sku;?></td>
+                    <td><?php echo $location;?></td>
+                    <td><?php echo $item_qty;?> - Full Pallet</td>
+                    <td class='centre'><span class='check_box'></span></td>
+                    <td class='centre'><span class='check_box'></span></td>
+                </tr>
+            <?php endforeach;?>
         </table>
     </div>
     <?php if($count < count($delivery_ids)):?>
