13e9404203f21624449df3dab5461d8dc4bad58d
marxolly <mark@solly.com.au>
2021-09-23 10:00:08 +1000

    Update footer.php

--- app/views/layout/deliveries/footer.php
+++ app/views/layout/deliveries/footer.php
@@ -166,6 +166,45 @@
                                 });
                             }
                         });
+                        $('a.docket-print').click(function(e){
+                            e.preventDefault();
+                            console.log('click');
+                            if($('input.select:checked').length)
+                            {
+                                var ids = [];
+                                $('input.select').each(function(i,e){
+                                    if($(this).prop('checked'))
+                                    {
+                                        ids.push($(this).data('deliveryid'));
+                                    }
+                                });
+                                //console.log(ids);
+                                //return;
+                                var form = document.createElement('form');
+                                form.setAttribute("method", "post");
+                                form.setAttribute("action", "/pdf/printDeliveryPickslips");
+                                //form.setAttribute("action", "/misc-functions/make-packslips-pdf.php");
+                                form.setAttribute("target", "formresult");
+                                $.each( ids, function( index, value ) {
+                                    var hiddenField = document.createElement("input");
+                                    hiddenField.setAttribute("type", "hidden");
+                                    hiddenField.setAttribute("name", "delivery_ids[]");
+                                    hiddenField.setAttribute("value", value);
+                                    form.appendChild(hiddenField);
+                                });
+                                document.body.appendChild(form);
+                                window.open('','formresult');
+                                form.submit();
+                            }
+                            else
+                            {
+                                swal({
+                                    title: "No Deliveries Selected",
+                                    text: "Please select deliveries by checking their checkbox",
+                                    icon: "error",
+                                });
+                            }
+                        });
                     }
                 }
             }
