b0f47254cc11563ad5d5185c019bd8ece262dc5b
marxolly <mark@solly.com.au>
2021-09-28 10:52:54 +1000

    Update warehouse_home_new.php

--- app/views/layout/page-includes/home_pages/warehouse_home_new.php
+++ app/views/layout/page-includes/home_pages/warehouse_home_new.php
@@ -17,7 +17,7 @@
                             <div class="card-body">
                             	<i class="fad fa-truck fa-2x fa-flip-horizontal" style="vertical-align: middle;"></i>&nbsp;<span style="font-size:larger"><?php echo $o['order_count'];?> New Order<?php echo $s;?></span>
                             </div>
-                            <div class="card-footer text-right">
+                            <div class="card-footer text-center">
                                 <a class="btn btn-outline-fsg" href="/orders/view-orders/client=<?php echo $o['client_id'];?>">Manage Orders</a>
                             </div>
                         </div>
