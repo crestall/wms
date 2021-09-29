1ed574af25e043b4f7f57e1efade75b7e4f3a7cc
marxolly <mark@solly.com.au>
2021-09-29 11:17:52 +1000

    Update client_home.php

--- app/views/layout/page-includes/home_pages/client_home.php
+++ app/views/layout/page-includes/home_pages/client_home.php
@@ -4,8 +4,8 @@
 <div class="col-md-12 text-center">
     <h2>Quick Links</h2>
 </div>
-<div class="card-deck homepagedeck">
-    <div class="card homepagecard">
+<div class="card-deck indexpagedeck">
+    <div class="card indexpagecard">
         <div class="card-header">
             <h4>Create An Order</h4>
         </div>
@@ -13,7 +13,7 @@
         	<a class="btn btn-lg btn-outline-fsg" href="/orders/add-order"><i class="fad fa-shipping-fast fa-3x fa-flip-horizontal" style="vertical-align: middle;"></i>&nbsp;<span style="font-size:larger">Go!</span></a>
         </div>
     </div>
-    <div class="card homepagecard">
+    <div class="card indexpagecard">
         <div class="card-header">
             <h4>View Orders</h4>
         </div>
@@ -21,7 +21,7 @@
         	<a class="btn btn-lg btn-outline-fsg" href="/orders/client-orders"><i class="fad fa-th-list fa-3x" style="vertical-align: middle;"></i>&nbsp;<span style="font-size:larger">Look!</span></a>
         </div>
     </div>
-    <div class="card homepagecard">
+    <div class="card indexpagecard">
         <div class="card-header">
             <h4>View Inventory</h4>
         </div>
@@ -29,7 +29,7 @@
         	<a class="btn btn-lg btn-outline-fsg" href="/inventory/client-inventory"><i class="fad fa-inventory fa-3x" style="vertical-align: middle;"></i>&nbsp;<span style="font-size:larger">Check!</span></a>
         </div>
     </div>
-    <div class="card homepagecard">
+    <div class="card indexpagecard">
         <div class="card-header">
             <h4>Dispatch Reports</h4>
         </div>
