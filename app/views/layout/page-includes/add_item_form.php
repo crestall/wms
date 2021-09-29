5fd341cf30f09e7bb0dcea965b4a30fefa8ebb14
marxolly <mark@solly.com.au>
2021-09-30 09:40:18 +1000

    Update add_item_form.php

--- app/views/layout/page-includes/add_item_form.php
+++ app/views/layout/page-includes/add_item_form.php
@@ -8,18 +8,18 @@
             <div class="col-md-6">
                 <input type="text" class="form-control" name="name" id="name" value="" />
             </div>
-            <div class="form-group row">
-                <label class="col-md-3 col-form-label">Your Product ID/SKU</label>
-                <div class="col-md-6">
-                    <input type="text" class="form-control" name="client_product_id" id="client_product_id" value="" />
-                </div>
+        </div>
+        <div class="form-group row">
+            <label class="col-md-3 col-form-label">Your Product ID/SKU</label>
+            <div class="col-md-6">
+                <input type="text" class="form-control" name="client_product_id" id="client_product_id" value="" />
             </div>
-            <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
-            <input type="hidden" name="client_id" value="<?php echo $client_id; ?>" />
-            <div class="form-group row">
-                <div class="col-md-4 offset-md-3">
-                    <button type="submit" class="btn btn-outline-fsg">Record Info</button>
-                </div>
+        </div>
+        <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+        <input type="hidden" name="client_id" value="<?php echo $client_id; ?>" />
+        <div class="form-group row">
+            <div class="col-md-4 offset-md-3">
+                <button type="submit" class="btn btn-outline-fsg">Record Info</button>
             </div>
         </div>
     </form>
