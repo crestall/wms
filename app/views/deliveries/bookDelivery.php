20a5e5b44a55be09b757f0157e9e8b0b5a1247c9
marxolly <mark@solly.com.au>
2021-09-29 12:01:26 +1000

    Update bookDelivery.php

--- app/views/deliveries/bookDelivery.php
+++ app/views/deliveries/bookDelivery.php
@@ -40,7 +40,7 @@ $delivery_postcode = (empty(Form::value('delivery_postcode')))? $client['postcod
                 <div class="col-md-12 col-lg-6 mb-3" id="itemslist">
                     <div class="card h-100 border-secondary order-card">
                         <div class="card-header bg-secondary text-white">
-                            Items
+                            Items To Deliver
                         </div>
                         <div class="card-body">
                             <div class="form-group row">
