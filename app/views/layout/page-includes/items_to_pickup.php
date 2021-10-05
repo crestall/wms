d8bbebc64bf8447ed7c8a13159602190cfacbb4e
marxolly <mark@solly.com.au>
2021-10-05 12:47:29 +1100

    Update items_to_pickup.php

--- app/views/layout/page-includes/items_to_pickup.php
+++ app/views/layout/page-includes/items_to_pickup.php
@@ -2,12 +2,14 @@
 if(!isset($i)) $i = 1;
 ?>
 <div id="pickup_item_<?php echo $i;?>" class="pickup_item row">
-    <div class="col-9 offset-3"><?php echo $label;?></div>
-    <label class="col-4 col-form-label">Pallets To Collect</label>
-    <div class="col-6">
-        <input type="text" class="form-control required number" name="pickup_items[<?php echo $item_id;?>]" >
-    </div>
-    <div class="col-1 text-right">
-        <button class="btn btn-sm btn-danger" data-rowid="<?php echo $i;?>">Remove</button>
+    <div class="col-11 offset-1"><?php echo $label;?></div>
+    <div class="form-group row">
+        <label class="col-4 col-form-label">Pallets To Collect</label>
+        <div class="col-6">
+            <input type="text" class="form-control required number" name="pickup_items[<?php echo $item_id;?>]" >
+        </div>
+        <div class="col-1 text-right">
+            <button class="btn btn-sm btn-danger" data-rowid="<?php echo $i;?>">Remove</button>
+        </div>
     </div>
 </div>
