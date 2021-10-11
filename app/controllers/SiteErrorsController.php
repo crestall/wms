d572444674fb80b3a4928ddef8f2c57d960b41e3
marxolly <mark@solly.com.au>
2021-10-12 09:56:31 +1100

    Update SiteErrorsController.php

--- app/controllers/SiteErrorsController.php
+++ app/controllers/SiteErrorsController.php
@@ -38,46 +38,6 @@ class SiteErrorsController extends Controller{
         //render the error page
         Config::setJsConfig('curPage', "errors");
         Config::set('curPage', "errors");
-        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/errors/", Config::get('ERRORS_PATH') . 'noShipmentFound.php', []);
-    }
-
-    public function NotFound(){
-        Config::setJsConfig('curPage', "error-404");
-        Config::set('curPage', "error-404");
-        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/errors/", Config::get('ERRORS_PATH') . "404.php",[
-            'pht' => ": Page Not Found"
-        ]);
-    }
-
-    public function Unauthenticated(){
-        Config::setJsConfig('curPage', "error-401");
-        Config::set('curPage', "error-401");
-        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/errors/", Config::get('ERRORS_PATH') . "401.php",[
-            'pht' => ": Unauthorized"
-        ]);
-    }
-
-    public function Unauthorized(){
-        Config::setJsConfig('curPage', "error-403");
-        Config::set('curPage', "error-403");
-        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/errors/", Config::get('ERRORS_PATH') . "403.php",[
-            'pht' => ": Forbidden"
-        ]);
-    }
-
-    public function BadRequest(){
-        Config::setJsConfig('curPage', "error-400");
-        Config::set('curPage', "error-400");
-        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/errors/", Config::get('ERRORS_PATH') . "400.php",[
-            'pht' => ": Bad Request"
-        ]);
-    }
-
-    public function System(){
-        Config::setJsConfig('curPage', "error-500");
-        Config::set('curPage', "error-500");
-        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/errors/", Config::get('ERRORS_PATH') . "500.php",[
-            'pht' => ": Internal Server Error"
-        ]);
+        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/errors/", Config::get('ERRORS_PATH') . 'noPickupId.php', []);
     }
 }
