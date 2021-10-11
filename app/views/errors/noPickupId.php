b3ba08c86b89883cfa02079e25c1dd3228983c43
marxolly <mark@solly.com.au>
2021-10-12 09:57:49 +1100

    Create noPickupId.php

---
+++ app/views/errors/noPickupId.php
@@ -0,0 +1,21 @@
+<div id="page-wrapper">
+    <div id="page_container" class="container-xl">
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+        <div class="row">
+            <div class="col">
+                <div class="errorbox">
+                    <div class="row">
+                        <div class="col-4 text-right">
+                            <i class="fad fa-exclamation-triangle fa-6x"></i>
+                        </div>
+                        <div class="col-8">
+                            <h2>No Pickup ID Supplied</h2>
+                            <p>Please only follow links from within the web application you cunt</p>
+                            <p>If this error persists and you would like to report it, please let us know which page has the button/link you originally clicked on</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<div>
