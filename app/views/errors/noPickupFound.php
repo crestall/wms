2a99467a33b9a607ddae090e6eda417ad72fa215
marxolly <mark@solly.com.au>
2021-10-12 12:12:07 +1100

    Update noPickupFound.php

--- app/views/errors/noPickupFound.php
+++ app/views/errors/noPickupFound.php
@@ -5,7 +5,7 @@
             <div class="col">
                 <div class="errorbox">
                     <div class="row">
-                        <div class="col-4 text-right">
+                        <div class="col-3 text-right">
                             <i class="fad fa-exclamation-triangle fa-6x"></i>
                         </div>
                         <div class="col-8">
