96d67db5c3c1b4660b5fd40270daf4364b97007e
marxolly <mark@solly.com.au>
2021-10-13 09:45:48 +1100

    Update managePickup.php

--- app/views/deliveries/managePickup.php
+++ app/views/deliveries/managePickup.php
@@ -16,13 +16,13 @@ $items = explode("~",$pickup['items']);
                         $pc = 1;
                         while($pc <= $pallet_count):?>
                             <div class="form-group row">
-                                <div class="col-md-7">
+                                <div class="col-md-6">
                                     Pallet <?php echo $pc;?> of <?php echo $item_name."(".$item_sku.")";?>
                                 </div>
                                 <div class="col-md-1">
-                                    <input name="items[<?php echo $item_id;?>]['qty']" class="form-control required number" plaeholder="qty">
+                                    <input name="items[<?php echo $item_id;?>]['qty']" class="form-control required number" placeholder="qty">
                                 </div>
-                                <div class="col=md-4">
+                                <div class="col=md-5">
                                     <select name="items[<?php echo $item_id;?>]['location_id']" class="form-control selectpicker" data-style="btn-outline-secondary" required><option value="0">Select Location</option></select>
                                 </div>
                             </div>
@@ -32,7 +32,7 @@ $items = explode("~",$pickup['items']);
                     <input type="hidden" name="client_id" id="client_id" value="<?php echo $client_id;?>" />
                     <input type="hidden" name="pickup_id" id="pickup_id" value="<?php echo $pickup_id;?>" />
                     <div class="form-group row">
-                        <div class="offset-md-4 col-md-4">
+                        <div class="offset-md-6 col-md-4">
                             <button type="submit" class="btn btn-sm btn-outline-secondary">Put Items Away</button>
                         </div>
                     </div>
