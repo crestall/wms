ce83abd66fd42811054fe2fbe751375870bf0973
marxolly <mark@solly.com.au>
2021-10-12 14:35:36 +1100

    eroors tidy

--- app/views/layout/default/footer.php
+++ app/views/layout/default/footer.php
@@ -1,9 +1,6 @@
 
 	<?php include(Config::get('VIEWS_PATH')."layout/page-includes/footer_common.php");?>
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/footer_scripts.php");?>
-    <script>
-        var actions = {};
-    </script>
 
 		<?php Database::closeConnection(); ?>
 	</body>
