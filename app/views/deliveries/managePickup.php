6025ad5a523408b59aa3bc974a95556f59eec9dc
marxolly <mark@solly.com.au>
2021-10-13 10:59:01 +1100

    Update managePickup.php

--- app/views/deliveries/managePickup.php
+++ app/views/deliveries/managePickup.php
@@ -17,13 +17,13 @@ $items = explode("~",$pickup['items']);
                         while($pc <= $pallet_count):?>
                             <div class="border-bottom border-secondary border-bottom-dashed pt-2">
                                 <div class="row">
-                                    <div class="col-md-6 mb-3">
+                                    <div class="col-sm-6 mb-3">
                                         Pallet <?php echo $pc;?> of <?php echo $item_name." (".$item_sku.")";?>
                                     </div>
-                                    <div class="col-md-2 mb-3">
+                                    <div class="col-sm-2 mb-3">
                                         <input name="items[<?php echo $item_id;?>]['qty']" class="form-control required number" placeholder="qty">
                                     </div>
-                                    <div class="col-md-3 mb-3">
+                                    <div class="col-sm-3 mb-3">
                                         <select name="items[<?php echo $item_id;?>]['location_id']" class="form-control selectpicker" data-style="btn-outline-secondary" required><option value="0">Select Location</option></select>
                                     </div>
                                 </div>
