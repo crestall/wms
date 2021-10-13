f4700078d0584d003676848b64a88d3221c2a3b2
marxolly <mark@solly.com.au>
2021-10-13 15:00:44 +1100

    Update managePickup.php

--- app/views/deliveries/managePickup.php
+++ app/views/deliveries/managePickup.php
@@ -60,7 +60,7 @@ $cover_class = (!empty($pickup['vehicle_type']))? "" : "covered";
                                         <input name="locations[<?php echo $ii;?>][qty]" class="form-control required number" placeholder="qty">
                                     </div>
                                     <div class="col-sm-3 mb-3">
-                                        <select name="locations[<?php echo $ii;?>][location_id]" class="form-control selectpicker" data-live-search="true" data-style="btn-outline-secondary" required><option value="0">Select Location</option><?php echo $this->controller->location->getSelectEmptyLocations();?></select>
+                                        <select name="locations[<?php echo $ii;?>][location_id]" class="form-control selectpicker pallet_location" data-live-search="true" data-style="btn-outline-secondary" required><option value="0">Select Location</option><?php echo $this->controller->location->getSelectEmptyLocations();?></select>
                                     </div>
                                 </div>
                             </div>
