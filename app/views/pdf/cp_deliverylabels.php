ed23c1e08f6f187c1f4c488e289a9f6ee6bbdc12
marxolly <mark@solly.com.au>
2021-10-14 12:36:35 +1100

    Update cp_deliverylabels.php

--- app/views/pdf/cp_deliverylabels.php
+++ app/views/pdf/cp_deliverylabels.php
@@ -1,11 +1,34 @@
-
+<?php
+//echo "dl_details<pre>",print_r($dl_details),"</pre>";
+//echo "sender_details<pre>",print_r($sender_details),"</pre>";
+$address_string = $dl_details['ship_to'];
+if(!empty($dl_details['attention'])) $address_string .= "<br>".$dl_details['attention'];
+$address_string .= "<br>".$dl_details['address'];
+if(!empty($dl_details['address2'])) $address_string .= "<br>".$dl_details['address2'];
+$address_string .= "<br>".$dl_details['suburb']." ".$dl_details['state']." ".$dl_details['postcode'];
+$bc = (!empty($dl_details['box_count']))? $dl_details['box_count'] : 1;
+$pc = (!empty($dl_details['pallet_count']))? $dl_details['pallet_count'] : 0;
+$box_label = "Box";
+if(isset($dl_details['order_number']))
+{
+    $bc = (!empty($dl_details['box_count']))? $dl_details['box_count'] : 0;
+    $pc = (!empty($dl_details['pallet_count']))? $dl_details['pallet_count'] : 0;
+    $box_label = "Box/Pallet";
+}
+$bc += $pc;
+$tb = 1;
+?>
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;700&display=swap" rel="stylesheet">
 <body>
-<?php
-echo "dl_details<pre>",print_r($dl_details),"</pre>";
-echo "sender_details<pre>",print_r($sender_details),"</pre>";
-?>
+    <?php while ($tb <= $bc):?>
+        <div class="dl_body">
+            LABEL NUMBER <?php echo $tb;?>
+        </div>
+        <?php if($tb < $bc):?>
+            <pagebreak />
+        <?php endif; ++$tb;
+    endwhile;?>
 </body>
