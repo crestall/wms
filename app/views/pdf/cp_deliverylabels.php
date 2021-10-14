9c7e976cc6b08dda628d7240c0ee84c0b8bfb1e4
marxolly <mark@solly.com.au>
2021-10-14 12:23:55 +1100

    Update cp_deliverylabels.php

--- app/views/pdf/cp_deliverylabels.php
+++ app/views/pdf/cp_deliverylabels.php
@@ -1,4 +1,11 @@
+
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;700&display=swap" rel="stylesheet">
+<body>
 <?php
 echo "dl_details<pre>",print_r($dl_details),"</pre>";
 echo "sender_details<pre>",print_r($sender_details),"</pre>";
 ?>
+</body>
