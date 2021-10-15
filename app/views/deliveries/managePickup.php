4e13cd011c441c3ac127c290a62b606074788c88
marxolly <mark@solly.com.au>
2021-10-15 14:43:56 +1100

    Update managePickup.php

--- app/views/deliveries/managePickup.php
+++ app/views/deliveries/managePickup.php
@@ -65,7 +65,7 @@ $cover_class = (!empty($pickup['vehicle_type']))? "" : "covered";
                         <input type="hidden" name="locations[<?php echo $ii;?>][item_id]" value="<?php echo $item_id;?>">
                             <div class="border-bottom border-secondary border-bottom-dashed pt-2">
                                 <div class="row">
-                                    <div class="col-md-5 mb-3">
+                                    <div class="col-md-4 mb-3">
                                         <label class="col-12">&nbsp;</label>
                                         Pallet <?php echo $pc;?> of <?php echo $item_name." (".$item_sku.")";?>
                                     </div>
@@ -77,9 +77,9 @@ $cover_class = (!empty($pickup['vehicle_type']))? "" : "covered";
                                         <label class="col-12">Pallet Size</label>
                                         <select id="size_<?php echo $ii;?>" name="locations[<?php echo $ii;?>][size]" class="form-control selectpicker pallet_size" data-live-search="true" data-style="btn-outline-secondary" required><option value="0">Select Size</option><?php echo Utility::getPalletSizeSelect(Form::value("locations,$ii,size"));?></select>
                                     </div>
-                                    <div class="col-md-2 mb-3">
+                                    <div class="col-md-3 mb-3">
                                         <label class="col-12">Location</label>
-                                        <select id="location_id_<?php echo $ii;?>" name="locations[<?php echo $ii;?>][location_id]" class="form-control selectpicker pallet_location" data-live-search="true" data-style="btn-outline-secondary" required><option value="0">Select Location</option><?php echo $this->controller->location->getSelectEmptyLocations(Form::value("locations,$ii,location_id"));?></select>
+                                        <select id="location_id_<?php echo $ii;?>" name="locations[<?php echo $ii;?>][location_id]" class="form-control selectpicker pallet_location" data-live-search="true" data-style="btn-outline-secondary" required><option value="0">--Select One--</option><?php echo $this->controller->location->getSelectEmptyLocations(Form::value("locations,$ii,location_id"));?></select>
                                     </div>
                                 </div>
                             </div>
@@ -89,7 +89,7 @@ $cover_class = (!empty($pickup['vehicle_type']))? "" : "covered";
                     <input type="hidden" name="client_id" id="client_id" value="<?php echo $client_id;?>" />
                     <input type="hidden" name="pickup_id" id="pickup_id" value="<?php echo $pickup_id;?>" />
                     <div class="form-group row">
-                        <div class="offset-sm-6 col-sm-4 pt-2">
+                        <div class="offset-md-4 col-sm-md pt-2">
                             <button type="submit" class="btn btn-sm btn-outline-secondary">Put Items Away</button>
                         </div>
                     </div>
