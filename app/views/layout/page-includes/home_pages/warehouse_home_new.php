f5f079a3fcabe257a6c49c32467ee874be56f961
marxolly <mark@solly.com.au>
2021-10-22 10:42:27 +1100

    Update warehouse_home_new.php

--- app/views/layout/page-includes/home_pages/warehouse_home_new.php
+++ app/views/layout/page-includes/home_pages/warehouse_home_new.php
@@ -12,7 +12,7 @@
                         $logo_path = DOC_ROOT.'/images/client_logos/tn_'.$o['logo'];
                         $s = ($o['order_count'] > 1)? "s" : ""; ?>
                         <div class="card homepagecard">
-                            <div class="card-header d-flex align-items-center h-100">
+                            <div class="card-header align-items-center">
                                 <div class="row">
                                     <?php if(file_exists($logo_path)):?>
                                         <div class="col-5 d-sm-none d-md-block col-md-5">
@@ -26,7 +26,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="card-body flex-column h-100">
+                            <div class="card-body">
                             	<i class="fad fa-truck fa-2x fa-flip-horizontal" style="vertical-align: middle;"></i>&nbsp;<span style="font-size:larger"><?php echo $o['order_count'];?> New Order<?php echo $s;?></span>
                             </div>
                             <div class="card-footer text-center">
