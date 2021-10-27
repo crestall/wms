ac85dfd74df074bae5b51738bb4924e8df0a5832
marxolly <mark@solly.com.au>
2021-10-27 12:33:37 +1100

    Update editClient.php

--- app/views/clients/editClient.php
+++ app/views/clients/editClient.php
@@ -153,6 +153,72 @@ $country    = empty(Form::value('country'))?    $client['country']      : Form::
                         </div>
                         <?php include(Config::get('VIEWS_PATH')."forms/address.php");?>
                     </div>
+                    <div class="p-3 pb-0 mb-2 rounded mid-grey">
+                        <div class="form-group row">
+                            <h4 class="col-md-8">Local Delivery Charges</h4>
+                        </div>
+                        <?php echo "<pre>",print_r($dcs),"</pre>";?>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group row">
+                                    <h5 class="text-center">Truck Charges</h5>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-5">Standard Charge</label>
+                                    <div class="col-md-5">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="far fa-dollar-sign"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control" data-rule-number="true" name="truck_standard_charge" id="truck_standard_charge" value="<?php echo Form::value('truck_standard_charge');?>" />
+                                        </div>
+                                        <?php echo Form::displayError('truck_standard_charge');?>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-5">Urgent Charge</label>
+                                    <div class="col-md-5">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="far fa-dollar-sign"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control" data-rule-number="true" name="truck_urgent_charge" id="truck_urgent_charge" value="<?php echo Form::value('truck_urgent_charge');?>" />
+                                        </div>
+                                        <?php echo Form::displayError('truck_urgent_charge');?>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group row">
+                                    <h5 class="text-center">Ute Charges</h5>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-5">Standard Charge</label>
+                                    <div class="col-md-5">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="far fa-dollar-sign"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control" data-rule-number="true" name="ute_standard_charge" id="ute_standard_charge" value="<?php echo Form::value('ute_standard_charge');?>" />
+                                        </div>
+                                        <?php echo Form::displayError('ute_standard_charge');?>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-5">Urgent Charge</label>
+                                    <div class="col-md-5">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="far fa-dollar-sign"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control" data-rule-number="true" name="ute_urgent_charge" id="ute_urgent_charge" value="<?php echo Form::value('ute_urgent_charge');?>" />
+                                        </div>
+                                        <?php echo Form::displayError('ute_urgent_charge');?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div class="form-group row">
                         <label class="col-md-3">Local Delivery Charge For Truck</label>
                         <div class="col-md-4">
