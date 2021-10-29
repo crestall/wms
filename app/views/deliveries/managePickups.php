2ac5f0885ddd618558b01254bfb5d242bb5b7dc6
marxolly <mark@solly.com.au>
2021-10-29 15:43:08 +1100

    Update managePickups.php

--- app/views/deliveries/managePickups.php
+++ app/views/deliveries/managePickups.php
@@ -3,7 +3,7 @@
 ?>
 <input type="hidden" name="client_id" id="client_id" value="<?php echo $client_id;?>" />
 <div id="page-wrapper">
-    <div id="page_container" class="container-xl">
+    <div id="page_container" class="container-xxl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <div class="row view-orders-buttons" >
             <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
