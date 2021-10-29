3a886c54a5990740bc1007d9bfe16121717bfcd9
marxolly <mark@solly.com.au>
2021-10-29 15:11:44 +1100

    Update addBulkOrders.php

--- app/views/orders/addBulkOrders.php
+++ app/views/orders/addBulkOrders.php
@@ -2,7 +2,7 @@
 
 ?>
 <div id="page-wrapper">
-    <div id="page_container" class="container">
+    <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
         <?php echo Form::displayError('general');?>
