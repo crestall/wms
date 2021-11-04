4b0734b0c065ebca619d788ef443e59f43fd3f74
marxolly <mark@solly.com.au>
2021-11-04 14:39:55 +1100

    Update pickupsReport.php

--- app/views/reports/pickupsReport.php
+++ app/views/reports/pickupsReport.php
@@ -19,7 +19,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="errorbox">
-                            <h2>No Orders Listed</h2>
+                            <h2>No Pickups Listed</h2>
                             <p>There are no pickups listed as being completed between <?php echo date("d/m/Y", $from);?> and <?php echo date("d/m/Y", $to);?> for <?php echo $client_name;?></p>
                             <p>If you believe this is an error, please let Solly know</p>
                             <p>Alternatively, use the date selectors above to change the date range or choose another client</p>
