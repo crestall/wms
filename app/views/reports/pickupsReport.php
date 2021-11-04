46b929fe30d453b7faaaf17d416b2eef2f428335
marxolly <mark@solly.com.au>
2021-11-04 14:05:33 +1100

    Update pickupsReport.php

--- app/views/reports/pickupsReport.php
+++ app/views/reports/pickupsReport.php
@@ -5,5 +5,11 @@
     <div id="page_container" class="container-xxl">
         <input type="hidden" name="client_id" id="client_id" value="<?php echo $client_id;?>" />
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+        <div class="row form-group">
+            <label class="col-md-3">Select a Client</label>
+            <div class="col-md-4">
+                <p><select id="client_selector" class="form-control selectpicker"  data-style="btn-outline-secondary"><option value="0">Select</option><?php echo $this->controller->client->getSelectDeliveryClients($client_id);?></select></p>
+            </div>
+        </div>
     </div>
 </div>
