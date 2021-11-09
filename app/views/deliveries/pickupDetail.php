8fb8b88e3ba5242daeca5e136c9c31be5f3c6abc
marxolly <mark@solly.com.au>
2021-11-09 13:55:07 +1100

    Update pickupDetail.php

--- app/views/deliveries/pickupDetail.php
+++ app/views/deliveries/pickupDetail.php
@@ -3,6 +3,7 @@
 ?>
 <div id="page-wrapper">
     <div id="page_container" class="container-xxl">
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <div id="print_this">
             <div class="row">
                 <div class="col">
