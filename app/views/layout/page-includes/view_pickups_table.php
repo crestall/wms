cf322fe103a21ae77c5e5b19bfb4e09fb3a488ba
marxolly <mark@solly.com.au>
2021-11-09 11:25:29 +1100

    Update view_pickups_table.php

--- app/views/layout/page-includes/view_pickups_table.php
+++ app/views/layout/page-includes/view_pickups_table.php
@@ -69,7 +69,7 @@
                 <td><?php echo $d['vehicle_type'];?></td>
                 <td class="middle">
                     <a id="print_docket_<?php echo $d['id'];?>" class="btn btn-block btn-outline-secondary print_docket" role="button" target="_blank" href="/pdf/printPickupDocket/pickup=<?php echo $d['id'];?>/vehicle=<?php echo $d['vehicle_type'];?>">Print Pickup Docket</a>
-                    <?php if($d['date_completed'] > 0):?>
+                    <?php if($d['date_completed'] == 0):?>
                         <div class="border-bottom border-secondary border-bottom-dashed my-2"></div>
                         <a class="btn btn-block btn-outline-fsg" role="button" href="/deliveries/manage-pickup/pickup=<?php echo $d['id'];?>">Manage</a>
                     <?php endif;?>
