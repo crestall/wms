cbee31bcadd3c75ebd8c487f621d2e30f21ce2de
marxolly <mark@solly.com.au>
2021-11-09 14:55:53 +1100

    Update DeliveriesController.php

--- app/controllers/DeliveriesController.php
+++ app/controllers/DeliveriesController.php
@@ -233,7 +233,7 @@ class DeliveriesController extends Controller
             (new SiteErrorsController())->siteError("noPickupFound")->send();
             return;
         }
-        //echo "<pre>",print_r($pickup),"</pre>";
+        echo "<pre>",print_r($pickup),"</pre>";die();
         $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/deliveries/", Config::get('VIEWS_PATH') . 'deliveries/pickupDetail.php', [
             'page_title'    =>  "Pickup Detail",
             'pht'           =>  ": Pickup Detail",
