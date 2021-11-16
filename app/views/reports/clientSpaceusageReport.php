95ad194a0b344b9133f0aaa55f45674f565fb7ee
marxolly <mark@solly.com.au>
2021-11-16 15:59:48 +1100

    Update clientSpaceusageReport.php

--- app/views/reports/clientSpaceusageReport.php
+++ app/views/reports/clientSpaceusageReport.php
@@ -9,7 +9,7 @@ echo "<p>TO: ".date("Y-m-d H:i:s", $to)."</p>";
         <div class="row form-group">
             <label class="col-md-3">Select a Client</label>
             <div class="col-md-4">
-                <p><select id="client_selector" class="form-control selectpicker"  data-style="btn-outline-secondary"><option value="0">Select</option><?php echo $this->controller->client->getSelectPPClients($client_id);?></select></p>
+                <p><select id="client_selector" class="form-control selectpicker"  data-style="btn-outline-secondary"><option value="0">All Clients</option><?php echo $this->controller->client->getSelectPPClients($client_id);?></select></p>
             </div>
         </div>
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/between_dates.php");?>
