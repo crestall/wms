36e913c25e28bc575d237726f92c741b8e22f5d4
marxolly <mark@solly.com.au>
2021-11-16 16:09:02 +1100

    Update ReportsController.php

--- app/controllers/ReportsController.php
+++ app/controllers/ReportsController.php
@@ -39,6 +39,7 @@ class ReportsController extends Controller
         $from = (isset($this->request->params['args']['from']))? $this->request->params['args']['from'] : strtotime('- 7 days', strtotime('monday this week 00:00:00'));
         $to = (isset($this->request->params['args']['to']))? $this->request->params['args']['to'] : strtotime('monday this week 00:00:00');
         $spaces = $this->clientsbays->getSpaceUsage($from, $to, $client_id);
+        echo "SPACES<pre>",print_r($spaces),"</pre>"; die();
         Config::setJsConfig('curPage', "client-space-usage-report");
         Config::set('curPage', "client-space-usage-report");
         $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/reports/", Config::get('VIEWS_PATH') . 'reports/clientSpaceUsageReport.php',[
