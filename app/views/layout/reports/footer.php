24d471bc7e997946e6d93a94fa6e471cd66f3026
marxolly <mark@solly.com.au>
2021-11-18 09:07:05 +1100

    space usage table

--- app/views/layout/reports/footer.php
+++ app/views/layout/reports/footer.php
@@ -772,6 +772,19 @@
                                 url += "/client="+$(this).val()
                             window.location.href = url;
                         });
+                        var dtOptions = {
+                            "columnDefs": [
+                                { "orderable": false, "targets": [4] }
+                            ],
+                            "paging": false,
+                            "order": [],
+                            "dom" : '<<"row"<"col-lg-4"><"col-lg-6">><"row">t>',
+                            "mark": true
+                        }
+                        var table = dataTable.init($('table#client_space_usage_table'), dtOptions );
+                        $('#client_space_usage_table').on( 'keyup search', function () {
+                            table.search( this.value ).draw();
+                        } );
                     }
                 }
             }
--- app/views/reports/clientSpaceUsageReport.php
+++ app/views/reports/clientSpaceUsageReport.php
@@ -13,6 +13,50 @@
             </div>
         </div>
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/between_dates.php");?>
-        <?php echo "<pre>",print_r($bays),"</pre>"; ?>
+        <?php //echo "<pre>",print_r($bays),"</pre>"; ?>
+        <?php if(count($bays)):?>
+            <div id="waiting" class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Drawing Table..</h2>
+                    <p>May take a few moments</p>
+                    <img class='loading' src='/images/preloader.gif' alt='loading...' />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p class="text-right">
+                        <button id="csv_download" class="btn btn-outline-success"><i class="far fa-file-alt"></i>&nbsp;Download As CSV</button>
+                    </p>
+                </div>
+            </div>
+            <div class="row" id="table_holder" style="display:none">
+                <div class="col-lg-12">
+                    <table id="client_space_usage_table" class="table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th data-priority="2">Client</th>
+                                <th>Bay Name</th>
+                                <th>Date Added</th>
+                                <th>Date Removed</th>
+                                <th data-priority="1">Days Held</th>
+                                <th data-priorty="1">Charge Rate</th>
+                                <th data-priority="1">Charge</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        <?php else:?>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="errorbox">
+                        <h2>No Space Usage Found</h2>
+                        <p>There are no spaces listed as being used between <?php echo date("d/m/Y", $from);?> and <?php echo date("d/m/Y", $to);?></p>
+                        <p>If you believe this is an error, please let Solly know</p>
+                        <p>Alternatively, use the date selectors above to change the date range</p>
+                    </div>
+                </div>
+            </div>
+        <?php endif;?>
     </div>
 </div>
