f5512b3549ea057f2802a962332b0884a4bc3563
marxolly <mark@solly.com.au>
2021-11-19 08:48:02 +1100

    Update ReportsController.php

--- app/controllers/ReportsController.php
+++ app/controllers/ReportsController.php
@@ -72,7 +72,23 @@ class ReportsController extends Controller
 
     private function clientDeliveryClientSpaceUsageReport()
     {
-        echo "this is for delivery clients";
+        //echo "this is for delivery clients";
+        $client_id = Session::getUserClientId();
+        $client_name = $this->client->getClientName($client_id);
+        $from = (isset($this->request->params['args']['from']))? $this->request->params['args']['from'] : strtotime('- 7 days', strtotime('monday this week 00:00:00'));
+        $to = (isset($this->request->params['args']['to']))? $this->request->params['args']['to'] : strtotime('monday this week 00:00:00');
+        $bays = $this->deliveryclientsbay->getSpaceUsage($from, $to, $client_id);
+        Config::setJsConfig('curPage', "space-usage-report");
+        Config::set('curPage', "space-usage-report");
+        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/reports/", Config::get('VIEWS_PATH') . 'reports/deliveryClientSpaceUsageReport.php',[
+            'page_title'    =>  'Space Usage Report For '.$client_name,
+            'client_id'     =>  $client_id,
+            'from'          =>  $from,
+            'to'            =>  $to,
+            'date_filter'   =>  "",
+            'client_name'   =>  $client_name,
+            'bays'          =>  $bays
+        ]);
     }
 
     private function clientClientSpaceUsageReport()
