0f89cbc747029ffe1a24326e7caacde0109c0a69
marxolly <mark@solly.com.au>
2021-11-19 09:57:08 +1100

    Update clientDeliveryClientSpaceUsageReport.php

--- app/views/reports/clientDeliveryClientSpaceUsageReport.php
+++ app/views/reports/clientDeliveryClientSpaceUsageReport.php
@@ -6,7 +6,7 @@ echo "<p>THE_DATE: ".date("Y-m-d H:i:s", $date)."</p>";
         <input type="hidden" name="client_id" id="client_id" value="<?php echo $client_id;?>" />
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/from_date.php");?>
-        <?php //echo "<pre>",print_r($bays),"</pre>"; //die();?>
+        <?php echo "<pre>",print_r($bays),"</pre>"; die();?>
         <?php if(count($bays)):?>
             <div id="waiting" class="row">
                 <div class="col-lg-12 text-center">
