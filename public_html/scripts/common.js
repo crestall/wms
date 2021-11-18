ca392a3e272f4a0696ef249b5f987be1879e2c43
marxolly <mark@solly.com.au>
2021-11-18 12:46:27 +1100

    Update common.js

--- public_html/scripts/common.js
+++ public_html/scripts/common.js
@@ -381,6 +381,7 @@ var dataTable = {
                 $("div#table_holder").show();
                 $(".dataTables_length").addClass("col-md-5");
                 $(".dataTables_length select").addClass("form-control col-md-7 selectpicker");
+                $(".dataTables_filter input").removeClass("form-control-sm");
                 $(".dataTables_filter input").addClass("form-control");
                 $(".dataTables_filter").addClass("form-group");
                 $(".dataTables_length label select").detach().appendTo($("div.dataTables_length").parent());
