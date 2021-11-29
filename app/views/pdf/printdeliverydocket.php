1f3128d9234c2aec5c43907faba2af091edbb993
marxolly <mark@solly.com.au>
2021-11-29 14:04:05 +1100

    Update printdeliverydocket.php

--- app/views/pdf/printdeliverydocket.php
+++ app/views/pdf/printdeliverydocket.php
@@ -38,8 +38,8 @@ foreach($delivery_ids as $id):
     endforeach;
     //echo "<pre>",print_r($items_array),"</pre>";
     $item_string = "";
-    foreach($items_array as $sku => $qty):
-        $item_string .= "<br><strong>$qty</strong> of <strong>$sku</strong";
+    foreach($items_array as $sku => $item):
+        $item_string .= "<br><strong>".$item['qty']."</strong> of <strong>".$item['name']." (".$sku.")</strong";
     endforeach;
     //echo "<p>$item_string</p>";
     //Receivers Address Table
@@ -98,7 +98,7 @@ foreach($delivery_ids as $id):
         <div class="top_half">
             <table>
                 <tr>
-                    <td  style="width: 125mm"><img style="height:18mm;width:auto;" src="/images/delivery_docket_logos/fsg_logo.png"></td>
+                    <td  style="width: 125mm"><img style="height:18mm;width:auto;" src="https://wms.fsg.com.au/images/delivery_docket_logos/fsg_logo.png"></td>
                     <td class="right-align sender-address"><strong>FSG Priniting & 3PL Services</strong><br>865 Mountain Hwy, Bayswater VIC 3153<br><strong>T.</strong>03 9873 5144 - <strong>E.</strong>info@fsg.com.au<br><strong>www.fsg.com.au</strong></td>
                 </tr>
             </table>
