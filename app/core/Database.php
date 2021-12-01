734f8f1d63aa5a5c849c07558b69153ecccc0830
marxolly <mark@solly.com.au>
2021-12-01 13:51:31 +1100

    Update Database.php

--- app/core/Database.php
+++ app/core/Database.php
@@ -108,7 +108,7 @@ class Database {
     * @$params: array - parameters to bind to query
     * returns null
     ***********************************************/
-    public function query($query, $params = array(), $buffer = true)
+    public function query($query, $params = array(), $buffer = false)
     {
         try
         {
