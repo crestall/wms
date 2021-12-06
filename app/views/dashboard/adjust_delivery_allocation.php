856beff7d80a9eac1eadfc8ecfceabfa4d1c5e00
marxolly <mark@solly.com.au>
2021-12-06 11:56:54 +1100

    Update adjust_delivery_allocation.php

--- app/views/dashboard/adjust_delivery_allocation.php
+++ app/views/dashboard/adjust_delivery_allocation.php
@@ -5,8 +5,7 @@ $items = explode("~",$dd['items']);
     <div class="row" id="feedback_holder" style="display:none"></div>
     <div class="row">
         <form id="adjust-allocation" method="post" action="/form/procAdjustAllocations">
-            <?php foreach($items as $i):
-                ++$pallet_count;
+            <?php foreach($items as $i): 
                 list($item_id, $item_name, $item_sku, $item_qty, $location_id) = explode("|",$i);?>
                 <p>Need a location for <?php echo $item_name."( ".$item_id." )";?> with a quantity of <?php echo $item_qty;?></p>
             <?php endforeach;?>
