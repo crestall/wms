e4d6fc8608909213819437c012544c60494bf8e0
marxolly <mark@solly.com.au>
2021-12-06 13:48:39 +1100

    Update adjust_delivery_allocation.php

--- app/views/dashboard/adjust_delivery_allocation.php
+++ app/views/dashboard/adjust_delivery_allocation.php
@@ -4,10 +4,15 @@ $items = explode("~",$dd['items']);
 <div class="page-wrapper">
     <div class="row" id="feedback_holder" style="display:none"></div>
     <div class="row">
-        <form id="adjust-allocation" method="post" action="/form/procAdjustAllocations">
-            <?php foreach($items as $i): 
-                list($item_id, $item_name, $item_sku, $item_qty, $location_id) = explode("|",$i);?>
-                <p>Need a location for <?php echo $item_name."( ".$item_id." )";?> with a quantity of <?php echo $item_qty;?></p>
+        <form id="adjust-allocation" method="post" action="/form/procAdjustDeliveryAllocations">
+            <?php foreach($items as $i):
+                list($item_id, $item_name, $item_sku, $item_qty, $location_id, $line_id) = explode("|",$i);?>
+                <div class="form-group row">
+                    <label class="col-12 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Locations With <?php echo $item_qty;?> of <?php echo $item_name." (".$item_sku.")";?></label>
+                    <div class="col-12">
+                        <select  name="allocation[<?php echo $line_id;?>][location_id]" class="form-control selectpicker" data-live-search="true" required><option value="">--Select One--</option><?php echo $this->controller->location->getSelectLocationsForDeliveryItem($item_id, $item_qty, $location_id);?></select>
+                    </div>
+                </div>
             <?php endforeach;?>
         </form>
     </div>
