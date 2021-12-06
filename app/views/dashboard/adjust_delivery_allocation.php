ce59b2f88c1b84e99e28d5defa8fffca5b546c39
marxolly <mark@solly.com.au>
2021-12-06 11:24:56 +1100

    Create adjust_delivery_allocation.php

---
+++ app/views/dashboard/adjust_delivery_allocation.php
@@ -0,0 +1,8 @@
+<div class="page-wrapper">
+    <div class="row" id="feedback_holder" style="display:none"></div>
+    <div class="row">
+        <form id="adjust-allocation" method="post" action="/form/procAdjustAllocations">
+         <?php echo "<pre>",print_r($dd),"</pre>";?>
+        </form>
+    </div>
+</div>
