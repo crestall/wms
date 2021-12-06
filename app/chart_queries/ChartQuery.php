557db18a83d14b96ede410f37d5d420c16993415
marxolly <mark@solly.com.au>
2021-12-06 16:27:35 +1100

    chart queries class creation

---
+++ app/chart_queries/ChartQuery.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * ChartQuery class.
+ *
+ * Generates database queries and returns array to aid google charts to graphically represent date
+
+ * @author     Mark Solly <mark.solly@fsg.com.au>
+ */
+class ChartQuery{
+    private function __construct()
+    {
+        //somehow work out how to call the create procedure functions
+    }
+
+    private static function createFillYearWeekProcedure()
+    {
+        return "
+            DROP PROCEDURE IF EXISTS fillyearweak;
+            DELIMITER //
+            CREATE PROCEDURE fillyearweak(dateStart DATE, dateEnd DATE)
+            BEGIN
+                WHILE dateStart <= dateEnd DO
+                    INSERT INTO yw (id) VALUES (YEARWEEK(dateStart));
+                    SET dateStart = date_add(dateStart, INTERVAL 7 DAY);
+                END WHILE;
+            END;
+            //
+            DELIMITER ;
+        ";
+    }
+
+    private static function createFillDatesProcedure()
+    {
+        return "
+            DROP PROCEDURE IF EXISTS filldates;
+            DELIMITER //
+            CREATE PROCEDURE filldates(dateStart DATE, dateEnd DATE)
+            BEGIN
+                WHILE dateStart <= dateEnd DO
+                    INSERT INTO yw (id) VALUES (YEARWEEK(dateStart));
+                    SET dateStart = date_add(dateStart, INTERVAL 7 DAY);
+                END WHILE;
+            END;
+            //
+            DELIMITER ;
+        ";
+    }
+}
