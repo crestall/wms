2b87bc615c332448b1dffc5b88badafe1c58ce9f
marxolly <mark@solly.com.au>
2021-12-06 13:56:45 +1100

    Update adjust_delivery_allocation.php

--- app/views/dashboard/adjust_delivery_allocation.php
+++ app/views/dashboard/adjust_delivery_allocation.php
@@ -2,15 +2,16 @@
 $items = explode("~",$dd['items']);
 ?>
 <div class="page-wrapper">
-        <form id="adjust-allocation" method="post" action="/form/procAdjustDeliveryAllocations">
-            <?php foreach($items as $i):
-                list($item_id, $item_name, $item_sku, $item_qty, $location_id, $line_id) = explode("|",$i);?>
-                <div class="form-group row">
-                    <label class="col-12 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Locations With <?php echo $item_qty;?> of <?php echo $item_name." (".$item_sku.")";?></label>
-                    <div class="col-12">
-                        <select  name="allocation[<?php echo $line_id;?>][location_id]" class="form-control selectpicker" data-live-search="true" required><option value="">--Select One--</option><?php echo $this->controller->location->getSelectLocationsForDeliveryItem($item_id, $item_qty, $location_id);?></select>
-                    </div>
+    <div class="row" id="feedback_holder" style="display:none"></div>
+    <form id="adjust-allocation" method="post" action="/form/procAdjustDeliveryAllocations">
+        <?php foreach($items as $i):
+            list($item_id, $item_name, $item_sku, $item_qty, $location_id, $line_id) = explode("|",$i);?>
+            <div class="form-group row">
+                <label class="col-10 offset-1 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Locations With <?php echo $item_qty;?> of <?php echo $item_name." (".$item_sku.")";?></label>
+                <div class="col-12">
+                    <select  name="allocation[<?php echo $line_id;?>][location_id]" class="form-control selectpicker" data-live-search="true" required><option value="">--Select One--</option><?php echo $this->controller->location->getSelectLocationsForDeliveryItem($item_id, $item_qty, $location_id);?></select>
                 </div>
-            <?php endforeach;?>
-        </form> 
+            </div>
+        <?php endforeach;?>
+    </form>
 </div>
