8abd99b93b199bbabf7eda652031995ab49cba1e
marxolly <mark@solly.com.au>
2021-12-07 11:55:11 +1100

    Update ChartQuery.php

--- app/chart_queries/ChartQuery.php
+++ app/chart_queries/ChartQuery.php
@@ -5,50 +5,34 @@
  *
  * Generates database queries and returns array to aid google charts to graphically represent date
 
+ * The following stored procedure queries need to be executed first
+    DROP PROCEDURE IF EXISTS fillyearweak;
+    DELIMITER //
+    CREATE PROCEDURE fillyearweak(dateStart DATE, dateEnd DATE)
+    BEGIN
+        WHILE dateStart <= dateEnd DO
+            INSERT INTO yw (id) VALUES (YEARWEEK(dateStart));
+            SET dateStart = date_add(dateStart, INTERVAL 7 DAY);
+        END WHILE;
+    END;
+    //
+    DELIMITER ;
+
+    DROP PROCEDURE IF EXISTS filldates;
+    DELIMITER |
+    CREATE PROCEDURE filldates(dateStart DATE, dateEnd DATE)
+    BEGIN
+        WHILE dateStart <= dateEnd DO
+            INSERT INTO date_list (id) VALUES (dateStart);
+            SET dateStart = date_add(dateStart, INTERVAL 1 DAY);
+        END WHILE;
+    END;
+    |
+    DELIMITER ;
+
  * @author     Mark Solly <mark.solly@fsg.com.au>
  */
 class ChartQuery{
     private function __construct(){}
 
-    public static function init()
-    {
-        self::createFillYearWeekProcedure();
-    }
-
-
-
-    private static function createFillYearWeekProcedure()
-    {
-        $db = Database::openConnection();
-        if(!$db->queryRow("SHOW PROCEDURE STATUS WHERE NAME=:name", ['name' => 'fillyearweak']))
-        {
-            $db::closeConnection();
-            $db = Database::openConnection();
-            $db->query("CREATE PROCEDURE fillyearweak(dateStart DATE, dateEnd DATE)
-            BEGIN
-                WHILE dateStart <= dateEnd DO
-                    INSERT INTO yw (id) VALUES (YEARWEEK(dateStart));
-                    SET dateStart = date_add(dateStart, INTERVAL 7 DAY);
-                END WHILE;
-            END;");
-        }
-
-    }
-
-    private static function createFillDatesProcedure()
-    {
-        return "
-            DROP PROCEDURE IF EXISTS filldates;
-            DELIMITER //
-            CREATE PROCEDURE filldates(dateStart DATE, dateEnd DATE)
-            BEGIN
-                WHILE dateStart <= dateEnd DO
-                    INSERT INTO yw (id) VALUES (YEARWEEK(dateStart));
-                    SET dateStart = date_add(dateStart, INTERVAL 7 DAY);
-                END WHILE;
-            END;
-            //
-            DELIMITER ;
-        ";
-    }
 }
