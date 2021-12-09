880459d196cac24fa4c2afe77846acaa6a7522a4
marxolly <mark@solly.com.au>
2021-12-09 12:15:40 +1100

    Update NuchevMarketplacer.php

--- app/core/marketplacer/marketplacer_clients/NuchevMarketplacer.php
+++ app/core/marketplacer/marketplacer_clients/NuchevMarketplacer.php
@@ -47,15 +47,115 @@ class NuchevMarketplacer extends Marketplacer{
         {
             $this->addNuchevOrders($orders);
         }
-        die('procOrders failed');
+        //die('procOrders failed');
+        Logger::logOrderImports('order_imports/nuchev', $this->output); //die();
+        if ($this->ua != "CRON" )
+        {
+            return $this->return_array;
+        }
+        else
+        {
+            Email::sendNuchevMarketplacerImportSummary($this->return_array);
+        }
+        //echo "<pre>",print_r($this->return_array),"</pre>";
     }
 
     private function addNuchevOrders($orders)
     {
-        echo "<pre>",print_r($orders),"</pre>";//die(); 
+        //echo "<pre>",print_r($orders),"</pre>";//die();
         $nuchevoitems = $this->controller->allocations->createOrderItemsArray($orders['orders_items']);
         unset($orders['orders_items']);
-        echo "<pre>",print_r($nuchevoitems),"</pre>";die();
+        //echo "<pre>",print_r($nuchevoitems),"</pre>";die();
+        foreach($orders as $o)
+        {
+            //check for errors first
+            $item_error = false;
+            $error_string = "";
+            $import_error = false;
+            $import_error_string = "";
+            $items_errors = false;
+            $items_errors_string = "";
+            if($o['items_errors'])
+            {
+                $items_errors = true;
+                $items_errors_string .= $o['items_errors_string'];
+            }
+            foreach($nuchevoitems[$o['client_order_id']] as $item)
+            {
+                //echo "Doing {$o['client_order_id']}<pre>",print_r($item),"</pre>";
+                if($item['item_error'])
+                {
+                    $item_error = true;
+                    $error_string .= $item['item_error_string'];
+                }
+                if($item['import_error'])
+                {
+                    $import_error = true;
+                    $import_error_string .= $item['import_error_string'];
+                }
+            }
+            if($items_errors || $item_error || $import_error)
+            {
+                $args = array(
+                    'import_error'          => $import_error,
+                    'import_error_string'   => $import_error_string,
+                    'item_error'            => $item_error,
+                    'item_error_string'     => $error_string,
+                    'items_errors'          => $items_errors,
+                    'items_errors_string'   => $items_errors_string,
+                    'email_function'        => "sendNuchevMarketplacerImportError",
+                    'od'                    => $o
+                );
+                //echo "THE ARGS for {$o['client_order_id']}<pre>",print_r($args),"</pre>";
+                ++$this->return_array['error_count'];
+                //$this->return_array['error_string'] .= $message;
+                $this->return_array['error_orders'][] = $o['client_order_id'];
+                if ($this->ua == "CRON" )
+                {
+                    $this->sendItemErrorEmail($args);
+                }
+                else
+                {
+                    $args['send_no_message'] = 1;
+                    $message = $this->sendItemErrorEmail($args);
+                    $this->return_array['error_string'] .= $message;
+                }
+                continue;
+            }
+            //insert the order
+            $client_id = $this->client_id;
+            $vals = array(
+                'client_order_id'       => $o['client_order_id'],
+                'client_id'             => $client_id,
+                'deliver_to'            => $o['ship_to'],
+                'company_name'          => $o['company_name'],
+                'date_ordered'          => $o['date_ordered'],
+                'tracking_email'        => $o['tracking_email'],
+                'weight'                => $o['weight'],
+                'delivery_instructions' => $o['instructions'],
+                'errors'                => $o['errors'],
+                'error_string'          => $o['error_string'],
+                'address'               => $o['address'],
+                'address2'              => $o['address_2'],
+                'state'                 => $o['state'],
+                'suburb'                => $o['suburb'],
+                'postcode'              => $o['postcode'],
+                'country'               => $o['country'],
+                'contact_phone'         => $o['contact_phone'],
+                'is_marketplacer'       => 1,
+                'marketplacer_id'       => $o['marketplacer_id']
+            );
+            if($o['signature_req'] == 1) $vals['signature_req'] = 1;
+            if($o['eparcel_express'] == 1) $vals['express_post'] = 1;
+            $itp = array($pbaoitems[$o['client_order_id']]);
+            //$itp = array($o['items'][$o['client_order_id']]);
+            $order_number = $this->controller->order->addOrder($vals, $itp);
+            $this->output .= "Inserted Order: $order_number".PHP_EOL;
+            $this->output .= print_r($vals,true).PHP_EOL;
+            $this->output .= print_r($o['items'][$o['client_order_id']], true).PHP_EOL;
+            ++$this->return_array['import_count'];
+            $this->return_array['imported_orders'][] = $o['client_order_id'];
+        }
     }
 }//end class
 ?>
