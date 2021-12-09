410346c51fa47858e51c1750b8bc001aa2938ab5
marxolly <mark@solly.com.au>
2021-12-09 12:18:56 +1100

    Update marketplacerTesting.php

--- app/views/adminOnly/marketplacerTesting.php
+++ app/views/adminOnly/marketplacerTesting.php
@@ -6,12 +6,12 @@
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php"); ?>
         <div class="row">
             <div class="col-md-12">
-                <h2>Current Orders</h2>
+                <h2>Importing Result</h2>
             </div>
         </div>
         <div class="row">
             <div class="col-md-12">
-                <?php echo "<pre>",print_r($orders),"</pre>";?>
+                <?php echo "<pre>",print_r($result),"</pre>";?>
             </div>
         </div>
     </div>
