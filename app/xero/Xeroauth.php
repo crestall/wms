9687c2b2650815eb730d67e4e79911bc2eb15fee
marxolly <mark@solly.com.au>
2021-12-10 15:55:53 +1100

    Update Xeroauth.php

--- app/xero/Xeroauth.php
+++ app/xero/Xeroauth.php
@@ -20,35 +20,30 @@ class Xeroauth{
     /* Class constructor */
     public function __construct(){
         $db = Database::openConnection();
-        try{
-             $this->provider = new Xero([
-                'clientId'      => Config::get('PBAXEROCLIENTID'),
-                'clientSecret'  => Config::get('PBAXEROCLIENTSECRET'),
-                'redirectUri'   => Config::get('PBAXEROREDIRECTURL'),
-            ]);
-        } catch (ForbiddenException $e){
-           var_dump($e);
-        };
         $this->token_details = $db->queryByID($this->table, 1);
         //echo "<pre>",print_r($this->token_details),"</pre>";die();
         //die('refresh token: '.$this->token_details['expires']);
         if($this->tokenExpired())
         {
             //Gotta get a new one
-            //die('expired token');
+            //create the provider object
+            $this->provider = new Xero([
+                'clientId'      => Config::get('PBAXEROCLIENTID'),
+                'clientSecret'  => Config::get('PBAXEROCLIENTSECRET'),
+                'redirectUri'   => Config::get('PBAXEROREDIRECTURL'),
+            ]);
             $newAccessToken = $this->provider->getAccessToken('refresh_token', [
                 'refresh_token' => $this->token_details['refresh_token']
             ]);
-            // Save my new token, expiration and refresh token
+            // Save the new token, expiration and refresh token
             $db->updateDatabaseFields($this->table, array(
-                'token' => $newAccessToken->getToken(),
+                'token'         => $newAccessToken->getToken(),
                 'refresh_token' => $newAccessToken->getRefreshToken(),
                 'expires'       => $newAccessToken->getExpires()
             ), 1);
             //update details with new token
             $this->token_details = $db->queryByID($this->table, 1);
         }
-
         $this->xero_app = new \XeroPHP\Application(
             $this->token_details['token'],
             $this->token_details['tenant_id']
@@ -71,8 +66,8 @@ class Xeroauth{
         $endDateString = date('Y, m, d', strtotime('-28 days'));
 
         //$invoices = $this->xero_app->load('Accounting\\Invoice')
-        $invoices = $this->xero_app->load(Invoice::class)
-        //$invoices = $this->xero_app->load(\XeroPHP\Models\Accounting\Invoice::class)
+        //$invoices = $this->xero_app->load(Invoice::class)
+        $invoices = $this->xero_app->load(\XeroPHP\Models\Accounting\Invoice::class)
             ->orderBy("Date", "DESC")
             ->where(sprintf('Date >= DateTime(%s) && Date < DateTime(%s)', $endDateString, $startDateString))
             ->where('Status', \XeroPHP\Models\Accounting\Invoice::INVOICE_STATUS_AUTHORISED)
