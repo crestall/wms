af5ae3c2fee222fe2535b3f8e04390538574729d
marxolly <mark@solly.com.au>
2021-12-10 15:01:28 +1100

    Update xeroTesting.php

--- app/views/adminOnly/xeroTesting.php
+++ app/views/adminOnly/xeroTesting.php
@@ -4,5 +4,15 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-xxl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php"); ?>
+        <div class="row">
+            <div class='col text-center'>
+                <h2>Invoices</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="cpl">
+                <?php echo "<pre>",print_r($invoices),"</pre>";?>
+            </div>
+        </div>
     </div>
 </div>
