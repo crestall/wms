6c54f229f0d7df960ff80241ef83930140d9bb0a
marxolly <mark@solly.com.au>
2021-12-16 11:24:29 +1100

    Update Xeroauth.php

--- app/xero/Xeroauth.php
+++ app/xero/Xeroauth.php
@@ -96,7 +96,7 @@ class Xeroauth{
         */
         $xeroTenantId = $this->token_details['tenant_id'] ;
         $ifModifiedSince = new DateTime("2021-12-02");
-        $where = 'Reference=="123456"';
+        $where = 'Reference<>"123456"';
         //$where = 'Type="' . \XeroAPI\XeroPHP\Models\Accounting\Invoice::TYPE_ACCREC . '"';
         $iDs = ["6da5160c-347d-4eba-b487-942dd16c7d44"];
         $statuses = array("PAID");
