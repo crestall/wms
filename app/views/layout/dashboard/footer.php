f536f4d21ace391c573f2d9bfeab33a2dde7f929
marxolly <mark@solly.com.au>
2021-12-17 09:34:45 +1100

    Update footer.php

--- app/views/layout/dashboard/footer.php
+++ app/views/layout/dashboard/footer.php
@@ -20,7 +20,7 @@
                         $('a.btn-outline-order, a.btn-outline-delivery, a.btn-outline-pickup, a.btn-outline-backorderorder')
                             .click(function(e){
                                 console.log("click");
-                                $(this).attr("disabled", true);
+                                $(this).prop("disabled", true);
                             });
                     },
                     loadProductionCharts: function(){
