b68d4a552a04467e39c8f62f1414ba79c5cd658d
marxolly <mark@solly.com.au>
2021-12-20 10:38:35 +1100

    contact us setup

---
+++ app/views/contact/contactUs.php
@@ -0,0 +1,13 @@
+<?php
+$address = Form::value('address');
+$address2 = Form::value('address2');
+$suburb = Form::value('suburb');
+$state = Form::value('state');
+$postcode = Form::value('postcode');
+$country = Form::value('country');
+?>
+<div id="page-wrapper">
+    <div id="page_container" class="container-xxl">
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+    </div>
+</div>
---
+++ app/views/layout/contact/footer.php
@@ -0,0 +1,24 @@
+
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/footer_common.php");?>
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/footer_scripts.php");?>
+
+        <script>
+            //the actions for each page
+            var actions = {
+                common: {
+                    init: function(){
+
+                },
+                contact = {
+                    init: function(){
+                        
+                    }
+                }
+            }
+
+            //run the script for the current page
+            actions[config.curPage].init();
+        </script>
+        <?php Database::closeConnection(); ?>
+    </body>
+</html>
