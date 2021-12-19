739ca647aa3835cabea40ff595021ee67acfdb08
marxolly <mark@solly.com.au>
2021-12-20 10:57:51 +1100

    Update contactUs.php

--- app/views/contact/contactUs.php
+++ app/views/contact/contactUs.php
@@ -9,5 +9,13 @@ $country = Form::value('country');
 <div id="page-wrapper">
     <div id="page_container" class="container-xxl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
+        <?php echo Form::displayError('general');?>
+        <div class="row">
+            <div class="col">
+                <form id="contact_us" method="post"  action="/form/procContactUs">
+                </form>
+            </div>
+        </div>
     </div>
 </div>
