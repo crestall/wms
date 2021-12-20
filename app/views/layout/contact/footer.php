b487c841d48951b53d5c5e628e6cd1b89705f071
marxolly <mark@solly.com.au>
2021-12-20 12:00:22 +1100

    Update footer.php

--- app/views/layout/contact/footer.php
+++ app/views/layout/contact/footer.php
@@ -8,10 +8,88 @@
                 common: {
                     init: function(){
 
+                    },
+                    createCKEditors: function(){
+                        //.ckeditorInstance.destroy()
+                        var allTextAreas = document.querySelectorAll('textarea.ckeditor');
+                        var currentCKEditors = document.querySelectorAll('.ck-editor__editable');
+                        for( var j = 0; j < currentCKEditors.length; ++j)
+                        {
+                            currentCKEditors[j].ckeditorInstance.destroy();
+                        }
+                        for (var i = 0; i < allTextAreas.length; ++i)
+                        {
+                            var this_id = allTextAreas[i].id;
+                            console.log("this_id: "+this_id);
+                            ClassicEditor
+                                .create( allTextAreas[i] , {
+                                    toolbar: {
+                                        items: [
+                                            'heading',
+                                            '|',
+                                            'bold',
+                                            'italic',
+                                            'strikethrough',
+                                            'subscript',
+                                            'superscript',
+                                            'underline',
+                                            '|',
+                                            'fontColor',
+                                            'highlight',
+                                            '|',
+                                            'outdent',
+                                            'indent',
+                                            'alignment',
+                                            'insertTable',
+                                            '|',
+                                            'undo',
+                                            'redo'
+                                        ]
+                                    },
+                                    language: 'en',
+                                    image: {
+                                        toolbar: [
+                                            'imageTextAlternative',
+                                            'imageStyle:full',
+                                            'imageStyle:side'
+                                        ]
+                                    },
+                                    table: {
+                                        contentToolbar: [
+                                            'tableColumn',
+                                            'tableRow',
+                                            'mergeTableCells'
+                                        ]
+                                    }
+                                } )
+                                .then( editor => {
+                                    if(window.editor)
+                                    {
+                                        window.editor[this_id] = editor;
+                                    }
+                                    else
+                                    {
+                                        window.editor = {};
+                                        window.editor[this_id] = editor;
+                                    }
+                                    editor.model.document.on( 'change', () => {
+                                        //console.log( 'The Document has changed!' );
+                                        editor.updateSourceElement();
+                                        actions.common.validateTextField(editor.sourceElement);
+                                    } );
+                                } )
+                                .catch( error => {
+                                    console.error( 'Oops, something went wrong!' );
+                                    console.error( 'Please, report the following error on https://github.com/ckeditor/ckeditor5/issues with the build id and the error stack trace:' );
+                                    console.warn( 'Build id: x86d9y47fxh6-q4s9v3hwa0g6' );
+                                    console.error( error );
+                                } );
+                        }
+                    }
                 },
                 contact = {
                     init: function(){
-                        
+                        actions.common.createCKEditors();
                     }
                 }
             }
