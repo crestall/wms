dd8515f61c02819a23674bdfcb82241f8daec95b
marxolly <mark@solly.com.au>
2021-12-22 09:11:33 +1100

    Update containerUnloading.php

--- app/views/dataentry/containerUnloading.php
+++ app/views/dataentry/containerUnloading.php
@@ -13,7 +13,7 @@
                     <div class='form-group row'>
                         <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Client</label>
                         <div class="col-md-4">
-                            <select id="client_id" name="client_id" class="form-control selectpicker" data-live-search="true"><option value="0">--Select One--</option><?php echo $this->controller->client->getSelectClients(Form::value('client_id'));?></select>
+                            <select id="client_id" name="client_id" data-style="btn-outline-secondary" data-live-search="true"><option value="0">--Select One--</option><?php echo $this->controller->client->getSelectClients(Form::value('client_id'));?></select>
                             <?php echo Form::displayError('client_id');?>
                         </div>
                     </div>
@@ -21,7 +21,7 @@
                     <div class='form-group row'>
                         <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Container Size</label>
                         <div class="col-md-4">
-                            <select id="container_size" name="container_size" class="form-control selectpicker">
+                            <select id="container_size" name="container_size" class="form-control selectpicker" data-style="btn-outline-secondary">
                                 <option value="0">--Select One--</option>
                                 <option <?php if(Form::value('container_size') == '20 Foot') echo 'selected';?> >20 Foot</option>
                                 <option <?php if(Form::value('container_size') == '40 Foot') echo 'selected';?>>40 Foot</option>
@@ -32,7 +32,7 @@
                     <div class='form-group row'>
                         <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Type of Load</label>
                         <div class="col-md-4">
-                            <select id="load_type" name="load_type" class="form-control selectpicker">
+                            <select id="load_type" name="load_type" class="form-control selectpicker" data-style="btn-outline-secondary">
                                 <option value="0">--Select One--</option>
                                 <option <?php if(Form::value('load_type') == 'Loose') echo 'selected';?>>Loose</option>
                                 <option <?php if(Form::value('load_type') == 'Palletised') echo 'selected';?>>Palletised</option>
