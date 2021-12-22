4697159a35f76ee864b34597cf912662cb9ea0f4
marxolly <mark@solly.com.au>
2021-12-22 12:47:37 +1100

    Update addClient.php

--- app/views/clients/addClient.php
+++ app/views/clients/addClient.php
@@ -262,35 +262,8 @@ $country = Form::value('country');
                             <h4 class="col-md-8">Container Unloading Charges</h4>
                         </div>
                         <div class="row">
-                            <div class="col-md-4">
-                                <div class="form-group row">
-                                    <label class"col-md-5">40&rsquo; Loose Container</label>
-                                    <div class="col-md-5">
-                                        <div class="input-group">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text"><i class="far fa-dollar-sign"></i></span>
-                                            </div>
-                                            <input type="text" class="form-control" data-rule-number="true" name="40GP_loose" id="oversize_bay" value="<?php echo Form::value('40GP_loose');?>" />
-                                        </div>
-                                        <?php echo Form::displayError('40GP_loose');?>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-4">
-                                <div class="form-group row">
-                                    <label class"col-md-5">20&rsquo; Loose Container</label>
-                                    <div class="col-md-5">
-                                        <div class="input-group">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text"><i class="far fa-dollar-sign"></i></span>
-                                            </div>
-                                            <input type="text" class="form-control" data-rule-number="true" name="20GP_loose" id="oversize_bay" value="<?php echo Form::value('20GP_loose');?>" />
-                                        </div>
-                                        <?php echo Form::displayError('20GP_loose');?>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-4">
+
+                            <div class="col-md-6">
                                 <div class="form-group row">
                                     <label class="col-md-5">Oversize Charge</label>
                                     <div class="col-md-5">
