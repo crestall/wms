d3457a4d518c89205f66333c8d058948c654b104
marxolly <mark@solly.com.au>
2022-01-19 15:06:57 +1100

    Update view_pickups_table.php

--- app/views/layout/page-includes/view_pickups_table.php
+++ app/views/layout/page-includes/view_pickups_table.php
@@ -1,3 +1,5 @@
+<?php
+?>
 <table id="view_pickups_table" class="table-striped table-hover" style="width:98%">
     <thead>
         <tr>
@@ -69,7 +71,7 @@
                 <td><?php echo $d['vehicle_type'];?></td>
                 <td class="middle">
                     <a class="btn btn-block btn-outline-secondary print_docket" href="/deliveries/pickup-detail/pickup=<?php echo $d['id'];?>" target="_blank">View and Print Details</a>
-                    <?php if($d['date_completed'] == 0 && Session::getUserRole() != "client"):?>
+                    <?php if($d['date_fulfilled'] == 0 && Session::getUserRole() != "client"):?>
                         <div class="border-bottom border-secondary border-bottom-dashed my-2"></div>
                         <a class="btn btn-block btn-outline-fsg" role="button" href="/deliveries/manage-pickup/pickup=<?php echo $d['id'];?>">Manage</a>
                     <?php endif;?>
