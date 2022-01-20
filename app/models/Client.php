ff4f79879bec9d6de736b6d5925dada4a914f5e1
marxolly <mark@solly.com.au>
2022-01-20 13:46:49 +1100

    Update Client.php

--- app/models/Client.php
+++ app/models/Client.php
@@ -349,23 +349,5 @@ class Client extends Model{
         $db = Database::openConnection();
         return ( $db->queryValue($this->table, array('id' => $client_id), 'production_client') > 0 );
     }
-
-    public function getClientTruckDeliveryCharges($client_id = 0)
-    {
-        $db = Database::openConnection();
-        return $db->queryRow("SELECT * FROM ".$this->delivery_charges_table." WHERE client_id = $client_id AND vehicle_type = :truck",["truck" => "truck"]);
-    }
-
-    public function getClientStorageCharges($client_id = 0)
-    {
-        $db = Database::openConnection();
-        return $db->queryRow("SELECT * FROM ".$this->storage_charges_table." WHERE client_id = $client_id");
-    }
-
-    public function getClientUteDeliveryCharges($client_id = 0)
-    {
-        $db = Database::openConnection();
-        return $db->queryRow("SELECT * FROM ".$this->delivery_charges_table." WHERE client_id = $client_id AND vehicle_type = :ute",["ute" => "ute"]);
-    }
 }
 ?>
