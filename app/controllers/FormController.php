87bf763569bdf2b61315f846c8b363dac90e649d
marxolly <mark@solly.com.au>
2022-01-20 14:15:45 +1100

    Update FormController.php

--- app/controllers/FormController.php
+++ app/controllers/FormController.php
@@ -6989,7 +6989,7 @@ class FormController extends Controller {
 
     public function procClientAdd()
     {
-        //echo "<pre>",print_r($this->request->data),"</pre>"; die();
+        echo "<pre>",print_r($this->request->data),"</pre>"; die();
         $post_data = array();
         foreach($this->request->data as $field => $value)
         {
