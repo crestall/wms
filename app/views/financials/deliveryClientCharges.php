e38a50f255aa6b5d40a9e53112141cd5f9c29385
marxolly <mark@solly.com.au>
2022-01-25 14:55:36 +1100

    Create deliveryClientCharges.php

---
+++ app/views/financials/deliveryClientCharges.php
@@ -0,0 +1,16 @@
+<div id="page-wrapper">
+    <div id="page_container" class="container-xxl">
+        <input type="hidden" name="client_id" id="client_id" value="<?php echo $client_id;?>" />
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+        <div class="row form-group">
+            <label class="col-md-3">Select a Client</label>
+            <div class="col-md-4">
+                <p><select id="client_selector" class="form-control selectpicker"  data-style="btn-outline-secondary"><option value="0">Select A Client</option><?php echo $this->controller->client->getSelectDeliveryClients($client_id);?></select></p>
+            </div>
+        </div>
+        <?php if($client_id > 0):?>
+            <?php include(Config::get('VIEWS_PATH')."layout/page-includes/between_dates.php");?>
+        <?php endif;?>
+    </div>
+</div>
+<div id="block_message"></div>
