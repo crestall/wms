fb51af71fe04d31a3192deeb98c21f9c184d8038
marxolly <mark@solly.com.au>
2022-01-25 15:02:58 +1100

    Update footer.php

--- app/views/layout/financials/footer.php
+++ app/views/layout/financials/footer.php
@@ -26,16 +26,16 @@
                                 $.blockUI({ message: '<div style="height:120px; padding-top:40px;"><h2>Calculating Charges...</h2></div>' });
                                 window.location.href = url;
                             }
-                            datePicker.betweenDates();
-                            $('button#change_dates').click(function(e){
-                                e.preventDefault();
-                                $.blockUI({ message: '<div style="height:160px; padding-top:40px;"><h1>Calculating Charges...</h1></div>' });
-                                var from = $('#date_from_value').val();
-                                var to = $('#date_to_value').val();
-                                var client_id = $('#client_id').val();
-                                var url = '/financials/delivery-client-charges/client='+client_id+"/from="+from+"/to="+to;
-                                window.location.href = url;
-                            });
+                        });
+                        datePicker.betweenDates();
+                        $('button#change_dates').click(function(e){
+                            e.preventDefault();
+                            $.blockUI({ message: '<div style="height:160px; padding-top:40px;"><h1>Calculating Charges...</h1></div>' });
+                            var from = $('#date_from_value').val();
+                            var to = $('#date_to_value').val();
+                            var client_id = $('#client_id').val();
+                            var url = '/financials/delivery-client-charges/client='+client_id+"/from="+from+"/to="+to;
+                            window.location.href = url;
                         });
                     }
                 }
