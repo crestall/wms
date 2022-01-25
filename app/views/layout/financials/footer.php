af0684c6f91fc9c76fda9f2652f5314b1da03107
marxolly <mark@solly.com.au>
2022-01-25 14:59:44 +1100

    Update footer.php

--- app/views/layout/financials/footer.php
+++ app/views/layout/financials/footer.php
@@ -12,7 +12,21 @@
                 },
                 'delivery-client-charges':{
                     init: function(){
-                        
+                        $('select#client_selector').change(function(e){
+                            if($(this).val() > 0)
+                            {
+                                var from = $('#date_from_value').val();
+                                var to = $('#date_to_value').val();
+                                var client_id = $(this).val();
+                                var url = '/financials/delivery-client-charges/client='+client_id;
+                                if($('#date_from_value').val())
+                                    url += "/from="+$('#date_from_value').val();
+                                if($('#date_to_value').val())
+                                    url += "/from="+$('#date_to_value').val();
+                                $.blockUI({ message: '<div style="height:120px; padding-top:40px;"><h2>CCalculating Charges...</h2></div>' });
+                                window.location.href = url;
+                            }
+                        });
                     }
                 }
             }
