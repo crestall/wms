4cd8c07785d199710207f740e99c5950984304b5
marxolly <mark@solly.com.au>
2022-01-25 13:47:49 +1100

    Update FinancialsController.php

--- app/controllers/FinancialsController.php
+++ app/controllers/FinancialsController.php
@@ -34,147 +34,16 @@ class FinancialsController extends Controller
         parent::displayIndex(get_class());
     }
 
-    public function huntersCheck()
-    {
-
-        //render the page
-        Config::setJsConfig('curPage', "hunters-check");
-        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/financials/", Config::get('VIEWS_PATH') . 'financials/huntersCheck.php',
-            [
-                'page_title'    =>  "Hunters Invoice Checking",
-                'show_table'    =>  false
-            ]
-        );
-    }
-
-    public function directfreightCheck()
-    {
-
-        //render the page
-        Config::setJsConfig('curPage', "hunters-check");
-        $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/financials/", Config::get('VIEWS_PATH') . 'financials/directfreightCheck.php',
-            [
-                'page_title'    =>  "Direct Frieght Invoice Checking",
-                'show_table'    =>  false
-            ]
-        );
-    }
-
-    public function procHuntersUpdate()
-    {
-        $this->procHuntersCheck();
-    }
-
-    public function procHuntersCheck()
-    {
-        //echo "<pre>",print_r($this->request->data),"</pre>"; die();
-        $post_data = array();
-        foreach($this->request->data as $field => $value)
-        {
-            if(!is_array($value))
-            {
-                ${$field} = $value;
-                $post_data[$field] = $value;
-            }
-        }
-        if($_FILES['csv_file']["size"] > 0)
-        {
-            if ($_FILES['csv_file']['error']  === UPLOAD_ERR_OK)
-            {
-                $tmp_name = $_FILES['csv_file']['tmp_name'];
-                $csv_array = array_map('str_getcsv', file($tmp_name));
-                //echo "<pre>",print_r($csv_array),"</pre>"; die();
-            }
-            else
-            {
-            	$error_message = $this->file_upload_error_message($_FILES[$field]['error']);
-                Form::setError('csv_file', $error_message);
-            }
-        }
-        else
-        {
-            Form::setError('csv_file', 'please select a file to upload');
-        }
-        if(Form::$num_errors > 0)		/* Errors exist, have user correct them */
-        {
-            Session::set('value_array', $_POST);
-            Session::set('error_array', Form::getErrorArray());
-            return $this->redirector->to(PUBLIC_ROOT."financials/hunters-check");
-        }
-        else
-        {
-            $skip_first = isset($header_row);
-            Config::setJsConfig('curPage', "hunters-check");
-            $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/financials/", Config::get('VIEWS_PATH') . 'financials/huntersCheck.php',
-                [
-                    'page_title'    => "Hunters Invoice Checking",
-                    'show_table'    => true,
-                    'csv_array'     => $csv_array,
-                    'skip_first'    => $skip_first
-                ]
-            );
-        }
-    }
-
-    public function procdfCheck()
-    {
-        //echo "<pre>",print_r($this->request->data),"</pre>"; die();
-        $post_data = array();
-        foreach($this->request->data as $field => $value)
-        {
-            if(!is_array($value))
-            {
-                ${$field} = $value;
-                $post_data[$field] = $value;
-            }
-        }
-        if($_FILES['csv_file']["size"] > 0)
-        {
-            if ($_FILES['csv_file']['error']  === UPLOAD_ERR_OK)
-            {
-                $tmp_name = $_FILES['csv_file']['tmp_name'];
-                $csv_array = array_map('str_getcsv', file($tmp_name));
-                //echo "<pre>",print_r($csv_array),"</pre>"; die();
-            }
-            else
-            {
-            	$error_message = $this->file_upload_error_message($_FILES[$field]['error']);
-                Form::setError('csv_file', $error_message);
-            }
-        }
-        else
-        {
-            Form::setError('csv_file', 'please select a file to upload');
-        }
-        if(Form::$num_errors > 0)		/* Errors exist, have user correct them */
-        {
-            Session::set('value_array', $_POST);
-            Session::set('error_array', Form::getErrorArray());
-            return $this->redirector->to(PUBLIC_ROOT."financials/directfreight-check");
-        }
-        else
-        {
-            $skip_first = isset($header_row);
-            Config::setJsConfig('curPage', "df-check");
-            $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/financials/", Config::get('VIEWS_PATH') . 'financials/directfreightCheck.php',
-                [
-                    'page_title'    => "Direct Freight Invoice Checking Results",
-                    'show_table'    => true,
-                    'csv_array'     => $csv_array,
-                    'skip_first'    => $skip_first
-                ]
-            );
-        }
-    }
 
     public function isAuthorized(){
+        $action = $this->request->param('action');
         //$role = Session::getUserRole();
         $role = (Session::isAdminUser())? 'admin' : Session::getUserRole();
-        if( isset($role) && ($role === "admin") )
-        {
-            return true;
-        }
-        return false;
+        $resource = "financials";
+        //only for admin
+        Permission::allow('admin', $resource, "*");
+        Permission::allow('super admin', $resource, "*");
+        return Permission::check($role, $resource, $action);
     }
 }
 ?>
