768d598cc0ad3aef4cb33dc38c2b8cc181055387
marxolly <mark@solly.com.au>
2022-01-27 11:01:34 +1100

    Update FinancialsController.php

--- app/controllers/FinancialsController.php
+++ app/controllers/FinancialsController.php
@@ -21,7 +21,7 @@ class FinancialsController extends Controller
         $from = (isset($this->request->params['args']['from']))? $this->request->params['args']['from'] : strtotime('first day of last month 00:00:00');
         $to = (isset($this->request->params['args']['to']))? $this->request->params['args']['to'] : strtotime('first day of this month 00:00:00');
         $charges = $this->client->getClientCharges($client_id, $from, $to);
-        echo "<pre>",print_r($charges),"</pre>";die();
+        //echo "<pre>",print_r($charges),"</pre>";die();
         Config::setJsConfig('curPage', "delivery-client-charges");
         Config::set('curPage', "delivery-client-charges");
         $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/financials/", Config::get('VIEWS_PATH') . 'financials/deliveryClientCharges.php',[
