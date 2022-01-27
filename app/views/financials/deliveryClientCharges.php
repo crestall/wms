700ab632ee7ff80a6966a529839ee9e20c5973a6
marxolly <mark@solly.com.au>
2022-01-27 11:00:11 +1100

    Update deliveryClientCharges.php

--- app/views/financials/deliveryClientCharges.php
+++ app/views/financials/deliveryClientCharges.php
@@ -10,6 +10,18 @@
         </div>
         <?php if($client_id > 0):?>
             <?php include(Config::get('VIEWS_PATH')."layout/page-includes/between_dates.php");?>
+            <?php if(count($charges)):?>
+
+            <?php else:?>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="errorbox">
+                            <h2><i class="fas fa-exclamation-triangle"></i> No Charges to Display</h2>
+                            <p>Nothing happend for this client in the selected date range</p>
+                        </div>
+                    </div>
+                </div>
+            <?php endif;?>
         <?php endif;?>
     </div>
 </div>
