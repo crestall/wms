b240337dc278f9451b1a9b432f210d30b2bb9efe
marxolly <mark@solly.com.au>
2022-01-31 16:00:32 +1100

    Update repalletisingShrinkwrapping.php

--- app/views/dataentry/repalletisingShrinkwrapping.php
+++ app/views/dataentry/repalletisingShrinkwrapping.php
@@ -1,11 +1,6 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-xxl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-        <div class="row">
-            <div class="col-lg-12">
-                <h3>Record Repalletising and Shrinkwrapping Details Here</h3>
-            </div>
-        </div>
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
         <div class="row">
             <div class="col">
