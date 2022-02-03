c6c3a16967ad2547d7b78d7b64e50e15cf2d0e88
marxolly <mark@solly.com.au>
2022-02-03 11:20:01 +1100

    Update deliveryClientCharges.php

--- app/views/financials/deliveryClientCharges.php
+++ app/views/financials/deliveryClientCharges.php
@@ -23,7 +23,7 @@
 
 
             <?php if(count($delivery_charges)):?>
-                <?php echo "<pre>",print_r($delivery_charges),"</pre>";?>
+                <?php //echo "<pre>",print_r($delivery_charges),"</pre>";?>
                 <div id="waiting" class="row">
                     <div class="col-lg-12 text-center">
                         <h2>Drawing Table..</h2>
