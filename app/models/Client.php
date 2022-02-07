e9ea6e7b7e6a984856bdd566bdfc01106d26b075
marxolly <mark@solly.com.au>
2022-02-07 13:57:46 +1100

    Update Client.php

--- app/models/Client.php
+++ app/models/Client.php
@@ -530,8 +530,8 @@ class Client extends Model{
                 (SELECT
                     cd.client_id,
                  	cd.client_name,
-                    SUM(cb.standard) AS standard_bay_days,
-                    SUM(cb.oversize) AS oversize_bay_days,
+                    FORMAT( SUM(cb.standard),0 ) AS standard_bay_days,
+                    FORMAT( SUM(cb.oversize),0 ) AS oversize_bay_days,
                  	FORMAT( SUM(cb.standard) * cc.standard_bay / 7 ,2 ) AS standard_charge,
                  	FORMAT( SUM(cb.oversize) * cc.oversize_bay / 7 ,2 ) AS oversize_charge,
                  	cc.oversize_bay,
