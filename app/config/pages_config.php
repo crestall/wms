cb04cceced51e77ca4a9afdef2dd90b0a38221c4
marxolly <mark@solly.com.au>
2022-02-09 11:40:04 +1100

    Update pages_config.php

--- app/config/pages_config.php
+++ app/config/pages_config.php
@@ -1015,7 +1015,7 @@ $client = array(
         ),
         'view-pickups'   => array(
             'display'   => true,
-            'icon'      => '<div class="fa-3x"><span class="fa-layers fa-fw"><i class="fa-thin fa-flip-horizontal fa-truck-fast"></i><i class="fa-solid fa-binoculars" data-fa-transform="shrink-6 up-6 right-6 rotate-30"></i></span></div>'
+            'icon'      => $fontastic_icons['reports']['view-pickups']
         ),
         'delivery-search' =>  array(
             'display'   => true,
