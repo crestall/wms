8ac6b4fad847fae8c003e93b356f471a8cbc0609
marxolly <mark@solly.com.au>
2022-02-14 09:38:06 +1100

    Update addPickup.php

--- app/views/deliveries/addPickup.php
+++ app/views/deliveries/addPickup.php
@@ -1,9 +1,9 @@
 <?php
-$pickup_address = (empty(Form::value('pickup_address')))? $client['address'] : Form::value('pickup_address');
-$pickup_address2 = (empty(Form::value('pickup_address2')))? $client['address_2'] : Form::value('pickup_address2');
-$pickup_suburb = (empty(Form::value('pickup_suburb')))? $client['suburb'] : Form::value('pickup_suburb');
-$pickup_state = (empty(Form::value('pickup_state')))? $client['state'] : Form::value('pickup_state');
-$pickup_postcode = (empty(Form::value('pickup_postcode')))? $client['postcode'] : Form::value('pickup_postcode');
+$pickup_address = (empty(Form::value('pickup_address')))? (isset($client['address']))?$client['address']: "" : Form::value('pickup_address');
+$pickup_address2 = (empty(Form::value('pickup_address2')))? (isset($client['address_2']))?$client['address_2']:"" : Form::value('pickup_address2');
+$pickup_suburb = (empty(Form::value('pickup_suburb')))? (isset($client['suburb']))?$client['suburb']:"" : Form::value('pickup_suburb');
+$pickup_state = (empty(Form::value('pickup_state')))? (isset($client['state']))?$client['state']:"" : Form::value('pickup_state');
+$pickup_postcode = (empty(Form::value('pickup_postcode')))? (isset($client['postcode']))?$client['postcode']:"" : Form::value('pickup_postcode');
 ?>
 <input type="hidden" name="client_id" id="client_id" value="<?php echo $client_id;?>" />
 <div id="page-wrapper">
