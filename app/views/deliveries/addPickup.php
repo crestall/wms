c808c72dfa4a5369f2d52d8c795d6b7631cb07a4
marxolly <mark@solly.com.au>
2022-02-14 10:00:50 +1100

    Update addPickup.php

--- app/views/deliveries/addPickup.php
+++ app/views/deliveries/addPickup.php
@@ -9,5 +9,14 @@ $pickup_postcode = (empty(Form::value('pickup_postcode')))? (isset($client['post
 <div id="page-wrapper">
     <div id="page_container" class="container-xxl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+        <div class="row form-group">
+            <label class="col-md-3">Select a Client</label>
+            <div class="col-md-4">
+                <p><select id="client_selector" class="form-control selectpicker"  data-style="btn-outline-secondary"><option value="0">Select</option><?php echo $this->controller->client->getSelectDeliveryClients($client_id);?></select></p>
+            </div>
+        </div>
+        <?php if($client_id > 0):?>
+            put the form here
+        <?php endif;?>
     </div>
 </div>
