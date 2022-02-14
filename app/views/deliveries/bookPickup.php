80d35558c6e3868e10333af18aa671f128b8a5d2
marxolly <mark@solly.com.au>
2022-02-14 11:27:12 +1100

    Update bookPickup.php

--- app/views/deliveries/bookPickup.php
+++ app/views/deliveries/bookPickup.php
@@ -1,9 +1,5 @@
 <?php
-$pickup_address = (empty(Form::value('pickup_address')))? $client['address'] : Form::value('pickup_address');
-$pickup_address2 = (empty(Form::value('pickup_address2')))? $client['address_2'] : Form::value('pickup_address2');
-$pickup_suburb = (empty(Form::value('pickup_suburb')))? $client['suburb'] : Form::value('pickup_suburb');
-$pickup_state = (empty(Form::value('pickup_state')))? $client['state'] : Form::value('pickup_state');
-$pickup_postcode = (empty(Form::value('pickup_postcode')))? $client['postcode'] : Form::value('pickup_postcode');
+
 ?>
 <input type="hidden" name="client_id" id="client_id" value="<?php echo $client_id;?>" />
 <div id="page-wrapper">
