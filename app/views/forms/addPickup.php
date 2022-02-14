5a1f8a63b5ddb1961db7b34a4b7a19f9f9f5461d
marxolly <mark@solly.com.au>
2022-02-14 11:29:01 +1100

    Update addPickup.php

--- app/views/forms/addPickup.php
+++ app/views/forms/addPickup.php
@@ -4,7 +4,7 @@ $pickup_address2 = (empty(Form::value('pickup_address2')))? (isset($client['addr
 $pickup_suburb = (empty(Form::value('pickup_suburb')))? (isset($client['suburb']))?$client['suburb']:"" : Form::value('pickup_suburb');
 $pickup_state = (empty(Form::value('pickup_state')))? (isset($client['state']))?$client['state']:"" : Form::value('pickup_state');
 $pickup_postcode = (empty(Form::value('pickup_postcode')))? (isset($client['postcode']))?$client['postcode']:"" : Form::value('pickup_postcode');
-$manually_entered = !Session::isDeliveryClientUser();
+$manually_entered = (!Session::isDeliveryClientUser())? 1:0;
 ?>
 <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
 <form id="book_pickup" method="post" action="/form/procBookAPickup">
