3ec5318a974536152cb5998bc12afbcef87bd521
marxolly <mark@solly.com.au>
2022-02-14 11:23:10 +1100

    Update addPickup.php

--- app/views/forms/addPickup.php
+++ app/views/forms/addPickup.php
@@ -4,6 +4,7 @@ $pickup_address2 = (empty(Form::value('pickup_address2')))? (isset($client['addr
 $pickup_suburb = (empty(Form::value('pickup_suburb')))? (isset($client['suburb']))?$client['suburb']:"" : Form::value('pickup_suburb');
 $pickup_state = (empty(Form::value('pickup_state')))? (isset($client['state']))?$client['state']:"" : Form::value('pickup_state');
 $pickup_postcode = (empty(Form::value('pickup_postcode')))? (isset($client['postcode']))?$client['postcode']:"" : Form::value('pickup_postcode');
+$manually_entered = !Session::isDeliveryClientUser();
 ?>
 <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
 <form id="book_pickup" method="post" action="/form/procBookAPickup">
@@ -66,6 +67,7 @@ $pickup_postcode = (empty(Form::value('pickup_postcode')))? (isset($client['post
                 <div class="card-footer">
                     <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
                     <input type="hidden" name="client_id" value="<?php echo $client_id; ?>" />
+                    <input type="hidden" name="manually_entered" value="<?php echo $manually_entered; ?>" />
                     <div class="col-md-6 offset-6">
                         <button type="submit" class="btn btn-lg btn-outline-secondary" id="submitter" disabled>Book Pickup</button>
                     </div>
