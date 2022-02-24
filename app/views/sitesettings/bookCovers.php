fe93a23bf3847ad83c9577204e495d71b9211d4b
marxolly <mark@solly.com.au>
2022-02-25 09:53:32 +1100

    Update bookCovers.php

--- app/views/sitesettings/bookCovers.php
+++ app/views/sitesettings/bookCovers.php
@@ -9,78 +9,97 @@
         <form id="add_driver"  method="post" enctype="multipart/form-data" action="/form/procBookCoverAdd">
             <div class="row">
                 <div class="col-lg-12">
-                    <h3>Add New Driver</h3>
+                    <h3>Add New Cover</h3>
                 </div>
             </div>
             <div class="form-group row">
                 <label class="col-md-3 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Name</label>
                 <div class="col-md-4">
                     <input type="text" class="form-control required" name="name" id="name" value="<?php echo Form::value('name');?>" />
-                    <span class="inst">Driver names need to be unique</span>
+                    <span class="inst">Names need to be unique</span>
                     <?php echo Form::displayError('name');?>
                 </div>
             </div>
             <div class="form-group row">
-                <label class="col-md-3 col-form-label">Phone</label>
-                <div class="col-md-4">
-                    <input type="text" class="form-control" name="phone" id="phone" value="<?php echo Form::value('phone');?>" />
-                    <?php echo Form::displayError('phone');?>
+                <label class="col-5"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Location</label>
+                <div class="col-7">
+                    <select id="cover_location" name="cover_location" class="form-control selectpicker" data-live-search="true" data-style="btn-outline-secondary"><option value="0">--Select One--</option>
+                        <?php echo $this->controller->location->getSelectLocations(Form::value('cover_location'));?>
+                    </select>
+                    <?php echo Form::displayError('cover_location');?>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-5 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Quantity</label>
+                <div class="col-7">
+                    <input type="text" class="form-control required number" name="qty" id="qty" value="<?php echo Form::value('qty');?>" />
+                    <?php echo Form::displayError('qty');?>
                 </div>
             </div>
             <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
-            <input type="hidden" name="return_url" value="<?php echo $child;?>-settings/drivers" >
             <div class="form-group row">
                 <div class="col-md-4 offset-md-3">
-                    <button type="submit" class="btn btn-outline-secondary">Add Driver</button>
+                    <button type="submit" class="btn btn-outline-secondary">Add Cover</button>
                 </div>
             </div>
         </form>
-        <div class="row">
-            <div class="col-lg-12">
-                <h2>Current Drivers</h2>
+        <div id="waiting" class="row">
+            <div class="col-lg-12 text-center">
+                <h2>Drawing Table..</h2>
+                <p>May take a few moments</p>
+                <img class='loading' src='/images/preloader.gif' alt='loading...' />
             </div>
         </div>
-        <?php if(count($drivers)):?>
-            <?php foreach($drivers as $d):?>
-                <form id="form_<?php echo $d['id'];?>" class="edit_driver border-bottom border-secondary border-bottom-dashed mb-3" action="/form/procDriverEdit" method="post">
-                    <div class="form-group row">
-                        <div class="col-md-3">
-                            <label class="col-form-label">Name</label>
-                            <input type="text" class="form-control required driver_name" name="name" id="name_<?php echo $d['id'];?>" value="<?php echo $d['name'];?>" />
-                            <?php echo Form::displayError("name_{$d['id']}");?>
-                        </div>
-                        <div class="col-md-3">
-                            <label class="col-form-label">Phone</label>
-                            <input type="text" class="form-control" name="phone" id="phone_<?php echo $d['id'];?>" value="<?php echo $d['phone'];?>" />
-                            <?php echo Form::displayError("phone{$d['id']}");?>
-                        </div>
-                        <div class="col-md-1">
-                            <label class="col-form-label" for="active_<?php echo $d['id'];?>">Active</label>
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" id="active_<?php echo $d['id'];?>" name="active" <?php if($d['active'] > 0) echo "checked";?> />
-                                <label class="custom-control-label" for="active_<?php echo $d['id'];?>"></label>
-                            </div>
-                        </div>
-                        <div class="col-md-1">
-                            <label class="col-form-label">&nbsp;</label>
-                            <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
-                            <input type="hidden" name="line_id" value="<?php echo $d['id'];?>" />
-                            <input type="hidden" class="current_driver_name" name="current_name" id="current_name_<?php echo $d['id'];?>" value="<?php echo $d['name'];?>" />
-                            <input type="hidden" name="return_url" value="<?php echo $child;?>-settings/drivers" >
-                            <button type="submit" class="btn btn-sm btn-outline-secondary">Update</button>
-                        </div>
-                    </div>
-                </form>
-            <?php endforeach;?>
-        <?php else:?>
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="errorbox">
-                        <h2><i class="fas fa-exclamation-triangle"></i> No Drivers Listed</h2>
-                        <p>You will need to add some first</p>
+        <div id="table_holder" style="display:none">
+            <div class="row" id="tablefeedback" style="display: none"></div>
+            <?php if(count($covers)):?>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <table width="100%" class="table-striped table-hover" id="view_bookcovers_table">
+                            <thead>
+                                <tr>
+                                    <th>Book Name</th>
+                                    <th>Edit</th>
+                                    <th>Location</th>
+                                    <th>Quantity</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach($covers as $c): ?>
+                                    <tr id="row_<?php echo $c['id'];?>">
+                                        <td><?php echo $c['name'];?></td>
+                                        <td>
+                                            <input type="text" class="form-control required" name="cover_<?php echo $c['id'];?>" id="cover_<?php echo $c['id'];?>" value="<?php echo $c['name'];?>" />
+                                            <input type="hidden" name="current_name_<?php echo $c['id'];?>" id="current_location_<?php echo $c['id'];?>" value="<?php echo $c['name'];?>" />
+                                        </td>
+                                        <td>
+
+                                        </td>
+                                        <td>
+
+                                        </td>
+                                        <td>
+                                            <p>
+                                                <a class="btn btn-outline-secondary update" data-coverid="<?php echo $c['id'];?>">Update Details</a><span class="label label-success" id="updated_<?php echo $c['id'];?>"></span>
+                                            </p>
+                                        </td>
+                                    </tr>
+                                <?php endforeach;?>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
-            </div>
-        <?php endif;?>
+            <?php else:?>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="errorbox">
+                            <h2><i class="fas fa-exclamation-triangle"></i> No Covers Listed</h2>
+                            <p>You will need to add some first</p>
+                        </div>
+                    </div>
+                </div>
+            <?php endif;?>
+        </div>
     </div>
 </div>
