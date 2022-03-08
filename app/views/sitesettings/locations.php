61a9aedfaa7a939a2e19e05fdf2dcc426b917737
marxolly <mark@solly.com.au>
2022-03-08 15:51:28 +1100

    Update locations.php

--- app/views/sitesettings/locations.php
+++ app/views/sitesettings/locations.php
@@ -1,22 +1,28 @@
 <?php
-    if($active == 0)
-    {
-            $link_text = "<a href='/site-settings/locations' class='btn btn-outline-secondary'>View Active Locations</a>";
-    }
-    else
-    {
-            $link_text = "<a href='/site-settings/locations/active=0' class='btn btn-outline-secondary'>View Inactive Locations</a>";
-    }
     $i = 1;
+    $link_text = "";
+    if($site >0)
+    {
+        if($active == 0)
+        {
+                $link_text = "<a href='/site-settings/locations/site=$site' class='btn btn-outline-secondary'>View Active Locations</a>";
+        }
+        else
+        {
+                $link_text = "<a href='/site-settings/locations/site=$site/active=0' class='btn btn-outline-secondary'>View Inactive Locations</a>";
+        }
+    }
 ?>
 <div id="page-wrapper">
     <div id="page_container" class="container-xxl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-        <div class="row">
-            <div class="col-lg-12">
-                <p class="text-right"><?php echo $link_text;?></p>
+        <?php if($site > 0):?>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p class="text-right"><?php echo $link_text;?></p>
+                </div>
             </div>
-        </div>
+        <? endif;?>
         <div class="row">
             <div class="col-lg-12">
                 <h2>Add a New Location</h2>
@@ -52,83 +58,80 @@
                 </div>
             </div>
         </form>
-        <div id="waiting" class="row">
-            <div class="col-lg-12 text-center">
-                <h2>Drawing Table..</h2>
-                <p>May take a few moments</p>
-                <img class='loading' src='/images/preloader.gif' alt='loading...' />
+        <div class="row mb-3">
+            <label class="col-md-3">Select a Site</label>
+            <div class="col-md-4">
+                <select id="site_selector" class="form-control selectpicker" data-style="btn-outline-secondary" data-live-search="true"><option value="0">Select</option><?php echo $this->controller->site->getSelectSites($site);?></select>
             </div>
         </div>
-        <div id="table_holder" style="display:none">
-            <div class="row" id="tablefeedback" style="display: none"></div>
-            <?php if(count($locations)):?>
-                <div class="row">
-                    <div class="col-lg-12">
-                        <table width="100%" class="table-striped table-hover" id="view_locations_table">
-                            <thead>
-                                <tr>
-                                    <th>Location</th>
-                                    <th>Edit</th>
-                                    <th>Multi SKU</th>
-                                    <th>Trays</th>
-                                    <th>Oversize</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php foreach($locations as $l): ?>
-                                    <tr id="row_<?php echo $l['id'];?>">
-                                        <td data-label="Location"><?php echo $l['location'];?></td>
-                                        <td data-label="Edit">
-                                            <input type="text" class="form-control required" name="location_<?php echo $l['id'];?>" id="location_<?php echo $l['id'];?>" value="<?php echo $l['location'];?>" />
-                                            <input type="hidden" name="current_location_<?php echo $l['id'];?>" id="current_location_<?php echo $l['id'];?>" value="<?php echo $l['location'];?>" />
-                                        </td>
-                                        <td data-label="Multi SKU">
-                                            <div class="form-group row custom-control custom-checkbox custom-control-right">
-                                                <input class="custom-control-input" type="checkbox" id="multisku_<?php echo $l['id'];?>" name="multisku_<?php echo $l['id'];?>" <?php if($l['multi_sku'] > 0) echo 'checked';?> />
-                                                <label class="custom-control-label col-md-3" for="multisku_<?php echo $l['id'];?>"></label>
-                                            </div>
-                                        </td>
-                                        <td data-label="Trays">
-                                            <div class="form-group row custom-control custom-checkbox custom-control-right">
-                                                <input class="custom-control-input" type="checkbox" id="trays_<?php echo $l['id'];?>" name="trays_<?php echo $l['id'];?>" <?php if($l['tray'] > 0) echo "checked";?> />
-                                                <label class="custom-control-label col-md-3" for="trays_<?php echo $l['id'];?>"></label>
-                                            </div>
-                                        </td>
-                                        <td data-label="Oversize">
-                                            <div class="form-group row custom-control custom-checkbox custom-control-right">
-                                                <input class="custom-control-input" type="checkbox" id="oversize_<?php echo $l['id'];?>" name="oversize_<?php echo $l['id'];?>" <?php if($l['oversize'] > 0) echo "checked";?> />
-                                                <label class="custom-control-label col-md-3" for="oversize_<?php echo $l['id'];?>"></label>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <p>
-                                                <a class="btn btn-outline-secondary update" data-locationid="<?php echo $l['id'];?>">Update Details</a><span class="label label-success" id="updated_<?php echo $l['id'];?>"></span>
-                                            </p>
-                                            <?php if($active == 1):?>
-                                                <p>
-                                                    <a class="btn btn-outline-danger deactivate" data-locationid="<?php echo $l['id'];?>">Deactivate Location</a>
-                                                </p>
-                                            <?php else:?>
-                                                <p>
-                                                    <a class="btn btn-outline-warning reactivate" data-locationid="<?php echo $l['id'];?>">Reactivate Location</a>
-                                                </p>
-                                            <?php endif;?>
-                                        </td>
+        <?php if($site > 0):?>
+            <div id="waiting" class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Drawing Table..</h2>
+                    <p>May take a few moments</p>
+                    <img class='loading' src='/images/preloader.gif' alt='loading...' />
+                </div>
+            </div>
+            <div id="table_holder" style="display:none">
+                <div class="row" id="tablefeedback" style="display: none"></div>
+                <?php if(count($locations)):?>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <table width="100%" class="table-striped table-hover" id="view_locations_table">
+                                <thead>
+                                    <tr>
+                                        <th>Location</th>
+                                        <th>Edit</th>
+                                        <th>Site</th>
+                                        <th>Multi SKU</th>
+                                        <th></th>
                                     </tr>
-                                <?php endforeach;?>
-                            </tbody>
-
-                        </table>
+                                </thead>
+                                <tbody>
+                                    <?php foreach($locations as $l): ?>
+                                        <tr id="row_<?php echo $l['id'];?>">
+                                            <td data-label="Location"><?php echo $l['location'];?></td>
+                                            <td data-label="Edit">
+                                                <input type="text" class="form-control required" name="location_<?php echo $l['id'];?>" id="location_<?php echo $l['id'];?>" value="<?php echo $l['location'];?>" />
+                                                <input type="hidden" name="current_location_<?php echo $l['id'];?>" id="current_location_<?php echo $l['id'];?>" value="<?php echo $l['location'];?>" />
+                                            </td>
+                                            <td data-label="Multi SKU">
+                                                <div class="form-group row custom-control custom-checkbox custom-control-right">
+                                                    <input class="custom-control-input" type="checkbox" id="multisku_<?php echo $l['id'];?>" name="multisku_<?php echo $l['id'];?>" <?php if($l['multi_sku'] > 0) echo 'checked';?> />
+                                                    <label class="custom-control-label col-md-3" for="multisku_<?php echo $l['id'];?>"></label>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <select name="site_<?php echo $l['id'];?>" class="selectpicker site_id" data-style="btn-outline-secondary btn-sm" data-width="fit" id="site_<?php echo $l['id'];?>"><?php echo $this->controller->site->getSelectSites($l['site_id']) ;?></select></p>
+                                            </td>
+                                            <td>
+                                                <p>
+                                                    <a class="btn btn-outline-secondary update" data-locationid="<?php echo $l['id'];?>">Update Details</a><span class="label label-success" id="updated_<?php echo $l['id'];?>"></span>
+                                                </p>
+                                                <?php if($active == 1):?>
+                                                    <p>
+                                                        <a class="btn btn-outline-danger deactivate" data-locationid="<?php echo $l['id'];?>">Deactivate Location</a>
+                                                    </p>
+                                                <?php else:?>
+                                                    <p>
+                                                        <a class="btn btn-outline-warning reactivate" data-locationid="<?php echo $l['id'];?>">Reactivate Location</a>
+                                                    </p>
+                                                <?php endif;?>
+                                            </td>
+                                        </tr>
+                                    <?php endforeach;?>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
-                </div>
-            <?php else:?>
-                <div class="col-lg-12">
-                    <div class="errorbox">
-                        <p>No locations listed yet</p>
+                <?php else:?>
+                    <div class="col-lg-12">
+                        <div class="errorbox">
+                            <p>No locations listed yet</p>
+                        </div>
                     </div>
-                </div>
-            <?php endif;?>
-        </div>
+                <?php endif;?>
+            </div>
+        <?php endif;?>
     </div>
 </div>
