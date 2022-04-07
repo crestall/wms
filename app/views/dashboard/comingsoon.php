b07a317ec545def8c70572b340a1ebffde9319d6
marxolly <mark@solly.com.au>
2022-04-08 09:08:59 +1000

    Update comingsoon.php

--- app/views/dashboard/comingsoon.php
+++ app/views/dashboard/comingsoon.php
@@ -5,7 +5,7 @@
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
     <div id="page_container" class="container-xxl">
         <div class="row">
-            <div class="col">
+            <div class="col text-center">
                 <img src="/images/comingsoon-2.png" alt="Coming Soon" />
             </div>
         </div>
