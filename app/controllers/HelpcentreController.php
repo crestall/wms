43fbc0c299d593a41a8fca0ce5bb9d8792aa47ed
marxolly <mark@solly.com.au>
2022-04-07 13:58:15 +1000

    Create HelpcentreController.php

---
+++ app/controllers/HelpcentreController.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Help Centre controller
+ *
+
+ * @author     Mark Solly <mark.solly@fsg.com.au>
+ */
+
+class Helpcentre extends Controller
+{
+    /**
+     * show coming soon page
+     *
+     */
+    public function index()
+    {
+        //set the page name for menu display
+        Config::setJsConfig('curPage', 'help-centre-index');
+        parent::displayIndex(get_class());
+    }
+
+}
