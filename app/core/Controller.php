f8bff44ad969d9ac63d4f507ea84b7c889b48b86
marxolly <mark@solly.com.au>
2022-04-07 15:02:25 +1000

    Update Controller.php

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -513,6 +513,7 @@ class Controller {
         array_pop($names);
         //fix the child name
         $new_child = (ucwords(strtolower(implode("",$names))))."Controller";
+        echo "<p>$new_child</p>";
         if(!$new_child || empty($new_child) || !$app::isControllerValid($new_child))
         {
             return (new ErrorsController())->error(404)->send();
