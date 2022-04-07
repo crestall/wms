cbc057d757da7e70963059a940d114b16df2dcb6
marxolly <mark@solly.com.au>
2022-04-08 09:00:45 +1000

    Update comingsoon.php

--- app/views/dashboard/comingsoon.php
+++ app/views/dashboard/comingsoon.php
@@ -3,9 +3,11 @@
 ?>
 <div id="page-wrapper">
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-    <div class="row">
-        <div class="col">
-            <img src="/images/comingsoon-2.png" alt="Coming Soon" />
+    <div id="page_container" class="container-xxl">
+        <div class="row">
+            <div class="col">
+                <img src="/images/comingsoon-2.png" alt="Coming Soon" />
+            </div>
         </div>
     </div>
 </div>
