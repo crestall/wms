922073dac0579bb03fa74445c0c08233da5d14d5
marxolly <mark@solly.com.au>
2022-04-08 14:14:05 +1000

    Create comingsoon.php

---
+++ app/views/help-centre/comingsoon.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+<div id="page-wrapper">
+    <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+    <div id="page_container" class="container-xxl">
+        <div class="row">
+            <div class="col text-center">
+                <img src="/images/comingsoon-2.png" alt="Coming Soon" />
+                <h2>Please Check Back Shortly</h2>
+            </div>
+        </div>
+    </div>
+</div>
