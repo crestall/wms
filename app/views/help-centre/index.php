9102095bd4c0e025bdefb50e86f5e497a69dc4b8
marxolly <mark@solly.com.au>
2022-04-11 12:07:27 +1000

    Update index.php

--- app/views/help-centre/index.php
+++ app/views/help-centre/index.php
@@ -1,16 +1,14 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-xxl">
-        <?php/*
-            echo "<p>isAdminUser: ".Session::isAdminUser()."</p>";
-            echo "<p>isWarehouseUser: ".Session::isWarehouseUser()."</p>";
-            echo "<p>isProductionUser: ".Session::isProductionUser()."</p>";
-            echo "<p>isDeliveryClientUser: ".Session::isDeliveryClientUser()."</p>";
-            echo "<p>role: ".Session::getUserRole()."</p>";
-        */?>
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <div id="help-centre-top" class="row">
             <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2  col-sm-10 offset-sm-1">
-                <input type="search" class="form-control" id="help-centre-serach" placeholder="Search for help"><i class='fa-light fa-magnifying-glass'></i>
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                       <i class='fa-light fa-magnifying-glass'></i>
+                    </div>
+                    <input type="search" class="form-control" id="help-centre-serach" placeholder="Search for help">
+                </div>
             </div>
         </div>
     </div>
