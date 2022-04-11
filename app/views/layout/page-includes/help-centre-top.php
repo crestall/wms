1ae0f63e97b52502942d250c26ca3bfc58ffc4a7
marxolly <mark@solly.com.au>
2022-04-11 12:41:55 +1000

    Create help-centre-top.php

---
+++ app/views/layout/page-includes/help-centre-top.php
@@ -0,0 +1,6 @@
+        <div id="help-centre-top" class="row">
+            <div id="help-centre-searchbar" class="col-lg-6 offset-lg-3 col-md-8 offset-md-2  col-sm-10 offset-sm-1">
+                <i class='fa-light fa-magnifying-glass'></i>
+                <input type="search" class="form-control" id="help-centre-search" placeholder="Search for help">
+            </div>
+        </div>
