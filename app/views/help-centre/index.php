470c6418411577cb2480d6c09240285dc0973546
marxolly <mark@solly.com.au>
2022-04-11 12:08:48 +1000

    Update index.php

--- app/views/help-centre/index.php
+++ app/views/help-centre/index.php
@@ -5,7 +5,7 @@
             <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2  col-sm-10 offset-sm-1">
                 <div class="input-group">
                     <div class="input-group-prepend">
-                       <i class='fa-light fa-magnifying-glass'></i>
+                       <span class="input-group-text"><i class='fa-light fa-magnifying-glass'></i></span>
                     </div>
                     <input type="search" class="form-control" id="help-centre-serach" placeholder="Search for help">
                 </div>
