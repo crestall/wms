693b5a577d2a653f040591e60bb1779326a4b57d
marxolly <mark@solly.com.au>
2022-04-11 11:03:05 +1000

    Update index.php

--- app/views/help-centre/index.php
+++ app/views/help-centre/index.php
@@ -1,12 +1,12 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-xxl">
-        <?php
+        <?php/*
             echo "<p>isAdminUser: ".Session::isAdminUser()."</p>";
             echo "<p>isWarehouseUser: ".Session::isWarehouseUser()."</p>";
             echo "<p>isProductionUser: ".Session::isProductionUser()."</p>";
             echo "<p>isDeliveryClientUser: ".Session::isDeliveryClientUser()."</p>";
             echo "<p>role: ".Session::getUserRole()."</p>";
-        ?>
+        */?>
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
     </div>
 </div>
