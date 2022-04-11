0b3862eaf2cfe366845b495ddbda4c03ddba48be
marxolly <mark@solly.com.au>
2022-04-11 11:39:05 +1000

    Update index.php

--- app/views/help-centre/index.php
+++ app/views/help-centre/index.php
@@ -10,7 +10,7 @@
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <div id="help-centre-top" class="row">
             <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2  col-sm-10 offset-sm-1">
-                <input type="search" class="form-control" id="help-centre-serach" placeholder="<i class='fa-light fa-magnifying-glass'></i> Search for help">
+                <input type="search" class="form-control" id="help-centre-serach" placeholder="Search for help"><i class='fa-light fa-magnifying-glass position-absolute'></i> 
             </div>
         </div>
     </div>
