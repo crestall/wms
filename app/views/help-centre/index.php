d3f988be302d21e6bb83c70e32dfb3e5f77e3c77
marxolly <mark@solly.com.au>
2022-04-11 12:17:28 +1000

    Update index.php

--- app/views/help-centre/index.php
+++ app/views/help-centre/index.php
@@ -2,15 +2,9 @@
     <div id="page_container" class="container-xxl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <div id="help-centre-top" class="row">
-            <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2  col-sm-10 offset-sm-1">
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                       <button class="btn btn-outline-secondary border-left-0 border" type="button">
-                           <i class='fa-light fa-magnifying-glass'></i>
-                       </button>
-                    </div>
-                    <input type="search" class="form-control" id="help-centre-serach" placeholder="Search for help">
-                </div>
+            <div id="help-centre-searchbar" class="col-lg-6 offset-lg-3 col-md-8 offset-md-2  col-sm-10 offset-sm-1">
+                <i class='fa-light fa-magnifying-glass'></i>
+                <input type="search" class="form-control" id="help-centre-search" placeholder="Search for help">
             </div>
         </div>
     </div>
