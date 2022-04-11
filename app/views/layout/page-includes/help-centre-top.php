c6f4077e26b28b90e895ecb1d78045aa30aeeba6
marxolly <mark@solly.com.au>
2022-04-11 14:10:00 +1000

    Update help-centre-top.php

--- app/views/layout/page-includes/help-centre-top.php
+++ app/views/layout/page-includes/help-centre-top.php
@@ -1,4 +1,4 @@
-        <div id="help-centre-top" class="row">
+        <div id="help-centre-top" class="row mt-2 mb-3 p-2 border border-secondary rounded bg-fsg">
             <div id="help-centre-searchbar" class="col-lg-6 offset-lg-3 col-md-8 offset-md-2  col-sm-10 offset-sm-1">
                 <i class='fa-light fa-magnifying-glass'></i>
                 <input type="search" class="form-control" id="help-centre-search" placeholder="Search for help">
