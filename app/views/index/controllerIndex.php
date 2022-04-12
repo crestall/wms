6cd6f5dd3ea9adab1e69b9c17e83a3859168e6dd
marxolly <mark@solly.com.au>
2022-04-13 09:41:14 +1000

    Update controllerIndex.php

--- app/views/index/controllerIndex.php
+++ app/views/index/controllerIndex.php
@@ -6,7 +6,7 @@ ksort($sections);
 $page_title = $sections['default-icon']['icon']." ".$page_title;
 $role = Session::getUserRole();
 $resource = strtolower(str_replace(" ", "", $page_name));
-//echo "<pre>",print_r($sections),"</pre>";
+echo "SECTION<pre>",print_r($sections),"</pre>";
 //echo "<p>Current Resource: $resource</p>";
 //echo "<p>Checking Role: $role</p>";
 //echo "<pre>",print_r(Permission::$perms),"</pre>";
