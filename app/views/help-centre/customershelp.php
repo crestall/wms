d4c8f7d12e83b5f5d14ac8c90347254f7d98fdf0
marxolly <mark@solly.com.au>
2022-04-13 12:06:36 +1000

    Update customershelp.php

--- app/views/help-centre/customershelp.php
+++ app/views/help-centre/customershelp.php
@@ -1,5 +1,6 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-xxl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+        <?php echo "<pre>",print_r($pages['customer']),"</pre>";?>
     </div>
 </div>
