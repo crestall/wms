6e969cb965bff034a33a356a224df967199d5c20
marxolly <mark@solly.com.au>
2022-05-04 09:36:20 +1000

    Update recordCollection.php

--- app/views/itemscollections/recordCollection.php
+++ app/views/itemscollections/recordCollection.php
@@ -17,80 +17,92 @@ $state = empty(Form::value('state'))? $fsg_address['state'] : Form::value('state
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php"); ?>
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
         <form id="record-item-collection" method="post" action="/form/procRecordItemCollection">
-            <div id="record-item-collection-holder" class="mt-2 mb-3 p-2 border border-secondary rounded bg-fsg">
+            <div class="mt-2 mb-3 p-2 border border-secondary rounded bg-fsg">
                 <h3 class="text-center">Collection Details</h3>
-                <div class="m-2 p-2 border border-secondary rounded bg-light">
-                    <div class="form-group row">
-                        <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Client</label>
-                        <div class="col-md-4">
-                            <select id="client_id" name="client_id" class="form-control selectpicker" data-style="btn-outline-secondary" data-live-search="true" required><option value="0">--Select One--</option><?php echo $this->controller->client->getSelectPPClients($client_id);?></select>
-                            <?php echo Form::displayError('client_id');?>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="m-2 p-2 border border-secondary rounded bg-light">
+                            <h4 class="text-center">Client</h4>
+                            <div class="form-group row">
+                                <label class="col-md-5"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Client</label>
+                                <div class="col-md-7">
+                                    <select id="client_id" name="client_id" class="form-control selectpicker" data-style="btn-outline-secondary" data-live-search="true" required><option value="0">--Select One--</option><?php echo $this->controller->client->getSelectPPClients($client_id);?></select>
+                                    <?php echo Form::displayError('client_id');?>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-md-5 col-form-label">Number of Pallets</label>
+                                <div class="col-md-7">
+                                    <input type="text" class="form-control number counter" name="pallets" id="pallets" value="<?php echo Form::value('pallets');?>" placeholder="A value for at least one of these is required" />
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-md-5 col-form-label">Number of Cartons</label>
+                                <div class="col-md-7">
+                                    <input type="text" class="form-control number counter" name="cartons" id="cartons" value="<?php echo Form::value('cartons');?>" placeholder="A value for at least one of these is required" />
+                                    <?php echo Form::displayError('counter');?>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 col-form-label">Number of Pallets</label>
-                        <div class="col-md-4">
-                            <input type="text" class="form-control number counter" name="pallets" id="pallets" value="<?php echo Form::value('pallets');?>" placeholder="A value for at least one of these is required" />
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 col-form-label">Number of Cartons</label>
-                        <div class="col-md-4">
-                            <input type="text" class="form-control number counter" name="cartons" id="cartons" value="<?php echo Form::value('cartons');?>" placeholder="A value for at least one of these is required" />
-                            <?php echo Form::displayError('counter');?>
+                    <div class="col-md-6">
+                        <div class="m-2 p-2 border border-secondary rounded bg-light">
+                            <h4 class="text-center">Courier</h4>
                         </div>
                     </div>
                 </div>
+                <div class="m-2 p-2 border border-secondary rounded bg-light">
+                </div>
             </div>
-            <div id="record-item-collection-holder" class="mt-2 mb-3 p-2 border border-secondary rounded bg-fsg">
+            <div class="mt-2 mb-3 p-2 border border-secondary rounded bg-fsg">
                 <h3 class="text-center">Collection Addresses</h3>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="m-2 p-2 border border-secondary rounded bg-light">
-                                <h4 class="text-center">Pickup address</h4>
-                                <div class="form-group row">
-                                    <label class="col-md-5 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Address Line 1</label>
-                                    <div class="col-md-7">
-                                        <input type="text" class="form-control required" name="puaddress" id="puaddress" value="<?php echo $puaddress;?>" />
-                                        <?php echo Form::displayError('puaddress');?>
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label class="col-md-5 col-form-label">Address Line 2</label>
-                                    <div class="col-md-7">
-                                        <input type="text" class="form-control" name="puaddress2" id="puaddress2" value="<?php echo $puaddress2;?>" />
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label class="col-md-5 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Suburb/Town</label>
-                                    <div class="col-md-7">
-                                        <input type="text" class="form-control required" name="pusuburb" id="pusuburb" value="<?php echo $pusuburb;?>" />
-                                        <?php echo Form::displayError('pusuburb');?>
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label class="col-md-5 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Postcode</label>
-                                    <div class="col-md-7">
-                                        <input type="text" class="form-control required" name="pupostcode" id="pupostcode" value="<?php echo $pupostcode;?>" />
-                                        <?php echo Form::displayError('pupostcode');?>
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label class="col-md-5"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> State</label>
-                                    <div class="col-md-7">
-                                        <input type="text" class="form-control required" name="pustate" id="pustate" value="<?php echo $pustate;?>" />
-                                        <span class="inst">Use VIC, NSW, QLD, ACT, TAS, WA, SA, NT only</span>
-                                        <?php echo Form::displayError('pustate');?>
-                                    </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="m-2 p-2 border border-secondary rounded bg-light">
+                            <h4 class="text-center">Pickup address</h4>
+                            <div class="form-group row">
+                                <label class="col-md-5 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Address Line 1</label>
+                                <div class="col-md-7">
+                                    <input type="text" class="form-control required" name="puaddress" id="puaddress" value="<?php echo $puaddress;?>" />
+                                    <?php echo Form::displayError('puaddress');?>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="m-2 p-2 border border-secondary rounded bg-light">
-                                <h4 class="text-center">Delivery address</h4>
+                            <div class="form-group row">
+                                <label class="col-md-5 col-form-label">Address Line 2</label>
+                                <div class="col-md-7">
+                                    <input type="text" class="form-control" name="puaddress2" id="puaddress2" value="<?php echo $puaddress2;?>" />
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-md-5 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Suburb/Town</label>
+                                <div class="col-md-7">
+                                    <input type="text" class="form-control required" name="pusuburb" id="pusuburb" value="<?php echo $pusuburb;?>" />
+                                    <?php echo Form::displayError('pusuburb');?>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-md-5 col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Postcode</label>
+                                <div class="col-md-7">
+                                    <input type="text" class="form-control required" name="pupostcode" id="pupostcode" value="<?php echo $pupostcode;?>" />
+                                    <?php echo Form::displayError('pupostcode');?>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-md-5"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> State</label>
+                                <div class="col-md-7">
+                                    <input type="text" class="form-control required" name="pustate" id="pustate" value="<?php echo $pustate;?>" />
+                                    <span class="inst">Use VIC, NSW, QLD, ACT, TAS, WA, SA, NT only</span>
+                                    <?php echo Form::displayError('pustate');?>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <div class="col-md-6">
+                        <div class="m-2 p-2 border border-secondary rounded bg-light">
+                            <h4 class="text-center">Delivery address</h4>
+                        </div>
+                    </div>
+                </div>
             </div>
         </form>
 
