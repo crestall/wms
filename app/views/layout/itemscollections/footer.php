f698bddff00f70e4baff2f73ae31acb1400e05eb
marxolly <mark@solly.com.au>
2022-05-04 14:18:48 +1000

    Update footer.php

--- app/views/layout/itemscollections/footer.php
+++ app/views/layout/itemscollections/footer.php
@@ -19,6 +19,7 @@
                         autoCompleter.suburbAutoComplete($('#suburb'));
                         autoCompleter.suburbAutoComplete($('#pusuburb'), "pu");
                         $('form#record-item-collection').submit(function(){
+                            console.log('submit');
                             if($(this).valid())
                                 $.blockUI({ message: '<div style="height:140px; padding-top:20px;"><h2>Processing form...</h2></div>' });
                             else
