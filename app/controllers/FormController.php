440224b6dbe861ba53e2f14ca3eab091aae29cd3
marxolly <mark@solly.com.au>
2022-05-04 11:37:55 +1000

    Update FormController.php

--- app/controllers/FormController.php
+++ app/controllers/FormController.php
@@ -123,6 +123,7 @@ class FormController extends Controller {
             'procProductionJobDeliveryUpdate',
             'procProfileUpdate',
             'procQualityControl',
+            'procRecordItemCollection,'
             'procRecordPickup',
             'procReeceDepartmentCheck',
             'procReeceDepartmentUpload',
@@ -156,6 +157,28 @@ class FormController extends Controller {
         ];
         $this->Security->config("form", [ 'fields' => ['csrf_token']]);
         $this->Security->requirePost($actions);
+    }
+
+    public function procRecordItemCollection()
+    {
+        //echo "<pre>",print_r($this->request->data),"</pre>"; die();
+        foreach($this->request->data as $field => $value)
+        {
+            if(!is_array($value))
+            {
+                ${$field} = $value;
+                $post_data[$field] = $value;
+            }
+            else
+            {
+                foreach($value as $key => $avalue)
+                {
+                    $post_data[$field][$key] = $avalue;
+                    ${$field}[$key] = $avalue;
+                }
+            }
+        }
+        echo "<pre>",print_r($post_data),"</pre>";
     }
 
     public function procRepalletiseShrinkwrap()
