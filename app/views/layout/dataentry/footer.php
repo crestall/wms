df2a820a7153db35a96c0cd5bc9eb6b6debd0a6c
marxolly <mark@solly.com.au>
2022-05-06 08:52:28 +1000

    move item collections back to data entry

--- app/views/layout/dataentry/footer.php
+++ app/views/layout/dataentry/footer.php
@@ -44,6 +44,22 @@
                             }
                         });
                     }
+                },
+                'items-collection': {
+                    init: function(){
+                        //console.log("Record Collection");
+                        autoCompleter.addressAutoComplete($('#address'));
+                        autoCompleter.addressAutoComplete($('#puaddress'), "pu");
+                        autoCompleter.suburbAutoComplete($('#suburb'));
+                        autoCompleter.suburbAutoComplete($('#pusuburb'), "pu");
+                        $('select#client_id, select#pustate, select#state, #suburb, #pusuburb, #postcode, #pupostcode, #address, #puaddress').change(function(e){
+                            $(this).valid();
+                        });
+                        $('form#record-item-collection').submit(function(){
+                            console.log('submit');
+                            if($(this).valid())
+                                $.blockUI({ message: '<div style="height:140px; padding-top:20px;"><h2>Processing form...</h2></div>' });
+                        });
                 }
             }
             //run the script for the current page
