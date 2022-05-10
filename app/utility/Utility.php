6a4702f9a5199feaa52b6a7923c31b853f54af83
marxolly <mark@solly.com.au>
2022-05-11 09:06:25 +1000

    Update Utility.php

--- app/utility/Utility.php
+++ app/utility/Utility.php
@@ -44,6 +44,8 @@ class Utility{
             }
             //$ret .= $l['site']."<br>";
             $ret .= $l['location']." (".$l['onhand'].")";
+            if(!empty($l['size']) && $l['size'] === "oversize")
+                $ret .= '<i class="fa-solid fa-asterisk text-danger"></i>';
             //$ret .= "<br>".$ind.":".count($locations);
             if(!empty($l['allocated']))
                 $ret .= "<br><span style='margin-left:7px'>".$l['allocated']." allocated</span>";
