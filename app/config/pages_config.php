153bce3d4be35f93e438521f86e3d779fd8249cc
marxolly <mark@solly.com.au>
2022-05-30 09:37:52 +1000

    Update pages_config.php

--- app/config/pages_config.php
+++ app/config/pages_config.php
@@ -1198,7 +1198,7 @@ $client = array(
 );
 
 //merge and tidy page arrays
-$prod['courier-functions'] = $prod_sales_admin['courier-functions'] = $admin['courier-functions'];
+$prod['courier-functions'] = $prod_sales['courier-functions'] = $prod_sales_admin['courier-functions'] = $admin['courier-functions'];
 $padmin['jobs'] = array_merge($padmin['jobs'], $prod['jobs']);
 $padmin['customers'] = array_merge($padmin['customers'], $prod['customers']);
 $padmin['finishers'] = array_merge($padmin['finishers'], $prod['finishers']);
