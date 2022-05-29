1a2b6c32e4ee69e52f598691e229eb8934a1e356
marxolly <mark@solly.com.au>
2022-05-30 09:05:11 +1000

    Create CourierfunctionsController.php

---
+++ app/controllers/CourierfunctionsController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Courier Functions controller
+ *
+ * Get Quotes and bokk extraneous courier jobs
+
+ * @author     Mark Solly <mark.solly@fsg.com.au>
+ */
+
+class CourierFunctionsController extends Controller
+{
+    public function beforeAction()
+    {
+        parent::beforeAction();
+    }
+
+    public function index()
+    {
+        //set the page name for menu display
+        Config::setJsConfig('curPage', 'courier-functions-index');
+        parent::displayIndex(get_class());
+    }
+
+    public function isAuthorized(){
+        $role = Session::getUserRole();
+        $action = $this->request->param('action');
+        $resource = "courierfunctions";
+        // only for all users
+        Permission::allow(['*'], $resource, ['*']);
+        // some super admin restrictions
+
+        //echo "<pre>",print_r(Permission::$perms),"</pre>"; die();
+        return Permission::check($role, $resource, $action);
+    }
+}//end class
+?>
