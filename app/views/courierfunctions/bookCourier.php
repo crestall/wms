74da872693613f9eef129ad69e28212d7ffb6d37
marxolly <mark@solly.com.au>
2022-06-06 11:20:06 +1000

    Create bookCourier.php

---
+++ app/views/courierfunctions/bookCourier.php
@@ -0,0 +1,9 @@
+<?php
+
+?>
+<div id="page-wrapper">
+    <div id="page_container" class="container-xxl">
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+        <div id="feedback_holder" style="display:none;"></div>
+    </div>
+</div>
