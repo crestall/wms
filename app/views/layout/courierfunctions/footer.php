d4f90d6c3930f7e35eba5e77459f06791b002b17
marxolly <mark@solly.com.au>
2022-06-07 10:55:15 +1000

    book courier javascript

--- app/views/layout/courierfunctions/footer.php
+++ app/views/layout/courierfunctions/footer.php
@@ -21,7 +21,21 @@
                 },
                 'book-courier': {
                     init: function(){
-                        
+                            $("a.add-package").click(function(e){
+                                e.preventDefault();
+                                var package_count = $("div#packages_holder div.apackage").length;
+                                //console.log('packages: '+package_count);
+                                var data = {
+                                    i: package_count
+                                }
+                                $.post('/ajaxfunctions/addQuotePackage', data, function(d){
+                                    $('div#packages_holder').append(d.html);
+                                });
+                            });
+                            $("a#remove-all-packages").click(function(e){
+                                e.preventDefault();
+                                $('div#packages_holder div.apackage').not(':first').remove();
+                            });
                     }
                 }
             }
