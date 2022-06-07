f51072c8b729375aff6fa0bdca5540324b2003ad
marxolly <mark@solly.com.au>
2022-06-07 11:00:48 +1000

    Update footer.php

--- app/views/layout/courierfunctions/footer.php
+++ app/views/layout/courierfunctions/footer.php
@@ -21,21 +21,26 @@
                 },
                 'book-courier': {
                     init: function(){
-                            $("a.add-package").click(function(e){
-                                e.preventDefault();
-                                var package_count = $("div#packages_holder div.apackage").length;
-                                //console.log('packages: '+package_count);
-                                var data = {
-                                    i: package_count
-                                }
-                                $.post('/ajaxfunctions/addQuotePackage', data, function(d){
-                                    $('div#packages_holder').append(d.html);
-                                });
+                        autoCompleter.addressAutoComplete($('#address'));
+                        autoCompleter.suburbAutoComplete($('#suburb'));
+                        $('select#state, #postcode').change(function(e){
+                            $(this).valid();
+                        });
+                        $("a.add-package").click(function(e){
+                            e.preventDefault();
+                            var package_count = $("div#packages_holder div.apackage").length;
+                            //console.log('packages: '+package_count);
+                            var data = {
+                                i: package_count
+                            }
+                            $.post('/ajaxfunctions/addQuotePackage', data, function(d){
+                                $('div#packages_holder').append(d.html);
                             });
-                            $("a#remove-all-packages").click(function(e){
-                                e.preventDefault();
-                                $('div#packages_holder div.apackage').not(':first').remove();
-                            });
+                        });
+                        $("a#remove-all-packages").click(function(e){
+                            e.preventDefault();
+                            $('div#packages_holder div.apackage').not(':first').remove();
+                        });
                     }
                 }
             }
