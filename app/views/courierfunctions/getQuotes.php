52e64d2a3ffe73e35d727f49f5a3e033d4bd5e56
marxolly <mark@solly.com.au>
2022-06-08 10:47:54 +1000

    Update getQuotes.php

--- app/views/courierfunctions/getQuotes.php
+++ app/views/courierfunctions/getQuotes.php
@@ -67,7 +67,7 @@ $postcode = Form::value('postcode');
                     <div class="form-group row">
                         <div class="col-md-4 offset-md-3">
                             <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
-                            <p class="text-sm-center">
+                            <p class="text-left text-sm-center">
                                 <button type="submit" class="btn btn-outline-fsg" id="submitter">Get Prices</button>
                             </p>
 
