f61e09f86b09bfc6a7d0eea40ffa2db3e3a81133
marxolly <mark@solly.com.au>
2022-06-16 14:30:11 +1000

    Create salesianBulksOz.php

---
+++ app/views/adminOnly/salesianBulksOz.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<div id="page-wrapper">
+    <div id="page_container" class="container-xxl">
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php"); ?>
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Importing Result</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <?php //echo "<pre>",print_r($result),"</pre>";?>
+            </div>
+        </div>
+    </div>
+</div>
