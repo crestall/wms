3767c8af7189f0915bd787d68c07bebd6ae98ed0
marxolly <mark@solly.com.au>
2022-06-16 14:57:19 +1000

    Update salesianBulksOz.php

--- app/views/adminOnly/salesianBulksOz.php
+++ app/views/adminOnly/salesianBulksOz.php
@@ -8,12 +8,12 @@
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php"); ?>
         <div class="row">
             <div class="col-md-12">
-                <h2>Importing Result</h2>
+                <h2>Importing Shipments</h2>
             </div>
         </div>
         <div class="row">
             <div class="col-md-12">
-                <?php //echo "<pre>",print_r($result),"</pre>";?>
+                <?php echo "<pre>",print_r($sments),"</pre>";?>
             </div>
         </div>
     </div>
