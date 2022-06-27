3718a178ecbb3468feb61d4ed6ec9a3706251c05
marxolly <mark@solly.com.au>
2022-06-27 11:53:34 +1000

    df setup for ind posts

--- app/core/CourierSelector.php
+++ app/core/CourierSelector.php
@@ -217,7 +217,7 @@
         }
         $db = Database::openConnection();
         $df_details = $this->controller->directfreight->getDetails($this->order_details, $this->items);
-        //echo "<pre>",print_r($df_details),"</pre>"; die();
+        echo "<pre>",print_r($df_details),"</pre>"; die();
         $response = $this->controller->directfreight->createConsignment($df_details);
         //echo "<pre>",print_r($response),"</pre>"; die();
 
