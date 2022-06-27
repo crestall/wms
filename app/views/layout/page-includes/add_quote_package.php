e0663f3fafd4c78877846c3b35399a89f8896901
marxolly <mark@solly.com.au>
2022-06-27 11:33:17 +1000

    Update add_quote_package.php

--- app/views/layout/page-includes/add_quote_package.php
+++ app/views/layout/page-includes/add_quote_package.php
@@ -33,7 +33,7 @@ $i = (isset($i))? $i : 0;
         <label class="col-md-1 mb-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Weight</label>
         <div class="col-md-3 mb-3">
             <div class="input-group">
-                <input type="text" class="form-control required" data-rule-positive-number="true" name="items[<?php echo $i;?>][weight]" value="<?php echo Form::value('carton_weight');?>" />
+                <input type="text" class="form-control required" data-rule-positiveNumber="true" name="items[<?php echo $i;?>][weight]" value="<?php echo Form::value('carton_weight');?>" />
                 <div class="input-group-append">
                     <span class="input-group-text">kg</span>
                 </div>
