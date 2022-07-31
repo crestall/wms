b24a3df9ce50cec34bbc433b2e002f71c9239a98
marxolly <mark@solly.com.au>
2022-08-01 09:06:25 +1000

    Update pages_config.php

--- app/config/pages_config.php
+++ app/config/pages_config.php
@@ -919,6 +919,10 @@ $admin = array(
             'display'   => true,
             'icon'      => "<div class='fa-3x'><i class='fal fa-question-square'></i></div>"
         ),
+        'chocolate_import'   => array(
+            'display'   => true,
+            'icon'      => "<div class='fa-3x'><i class='fal fa-question-square'></i></div>"
+        ),
         'inventory-comparing'   => array(
             'display'   => true,
             'icon'      => "<div class='fa-3x'><i class='fal fa-warehouse'></i></div>"
