e07e63e29a63b758f30072733b1dbe8fa073a592
marxolly <mark@solly.com.au>
2022-09-01 16:26:23 +1000

    Update adjustDelivery.php

--- app/views/deliveries/adjustDelivery.php
+++ app/views/deliveries/adjustDelivery.php
@@ -35,10 +35,10 @@ $items = explode("~",$delivery['items']);
                                 list($item_id, $item_name, $item_sku, $item_qty, $location_id, $line_id) = explode("|",$i);?>
                                 <div class="form-group row">
                                     <label class="col-11 offset-1 col-form-label">Locations With <?php echo $item_qty;?> of <?php echo $item_name." (".$item_sku.")";?></label>
-                                    <div class="col-9 col-md-10">
+                                    <div class="col-9 col-lg-10">
                                         <select  name="allocation[<?php echo $line_id;?>]" class="form-control selectpicker" data-live-search="true" data-style="btn-outline-secondary"  required><option value="">--Select One--</option><?php echo $this->controller->location->getSelectLocationsForDeliveryItem($item_id, $item_qty, $location_id);?></select>
                                     </div>
-                                    <div class="col-3 col-md-2">
+                                    <div class="col-3 col-lg-2">
                                         <div class="pretty p-icon p-smooth">
                                             <input type="checkbox" id="remove_<?php echo $line_id;?>" name="allocation[<?php echo $line_id;?>][remove]" />
                                             <div class="state p-danger-o">
