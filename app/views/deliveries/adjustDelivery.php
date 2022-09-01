807239976514b6d4f04a6968f9b9394b05f6e550
marxolly <mark@solly.com.au>
2022-09-01 10:37:06 +1000

    Create adjustDelivery.php

---
+++ app/views/deliveries/adjustDelivery.php
@@ -0,0 +1,9 @@
+<?php
+
+?>
+<div id="page-wrapper">
+    <div id="page_container" class="container-xxl">
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+        <?php echo "Delivery<pre>",print_r($delivery),"</pre>";?>
+    </div>
+</div>
