535da51342c9434a94650ec886392dbe82b8134e
marxolly <mark@solly.com.au>
2022-09-08 09:31:29 +1000

    Update footer.php

--- app/views/layout/deliveries/footer.php
+++ app/views/layout/deliveries/footer.php
@@ -168,7 +168,11 @@
                             //console.log(selector);
                             //console.log("Selector ID: "+ selector.attr('id'));
                             if(this.checked)
+                            {
                                 selector.prop('disabled', true);
+                                console.log("its checked") 
+                            }
+
                             else
                                 selector.prop('disabled', false);
 
