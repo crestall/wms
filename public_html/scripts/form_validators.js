8d1de48bc69874021e7c1a937ef2e603a8ff66de
marxolly <mark@solly.com.au>
2022-09-07 15:29:13 +1000

    Update form_validators.js

--- public_html/scripts/form_validators.js
+++ public_html/scripts/form_validators.js
@@ -145,7 +145,7 @@ $(document).ready(function() {
     $("form#adjust-delivery-items").validate({
         rules: {
             'allocation[]': {
-                notNone: function(element){ console.log(element);return true;/*$("#boxed_item").is(":checked")*/; }
+                notNone: function(element){ console.log('element: '+element);return true;/*$("#boxed_item").is(":checked")*/; }
             }
         },
         messages: {
