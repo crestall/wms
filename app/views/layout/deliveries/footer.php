b6c74a2c65f2d299ff80186b34cf0e6743c2040c
marxolly <mark@solly.com.au>
2022-09-13 11:15:39 +1000

    Update footer.php

--- app/views/layout/deliveries/footer.php
+++ app/views/layout/deliveries/footer.php
@@ -147,7 +147,7 @@
                                 });
                             }
                             $('.close_box').each(function(index, element){
-                                console.log('This is a close box');
+                                console.log($(this));
                                 $(this).click(function(ev){
                                     console.log('clicked close box');
                                     var $this_box = $(this).closest('div.item_holder');
