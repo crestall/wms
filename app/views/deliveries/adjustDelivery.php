35b76ce10d4f4219246e50e371a3cb2867a10d86
marxolly <mark@solly.com.au>
2022-09-13 10:42:18 +1000

    Update adjustDelivery.php

--- app/views/deliveries/adjustDelivery.php
+++ app/views/deliveries/adjustDelivery.php
@@ -6,11 +6,11 @@ $items = explode("~",$delivery['items']);
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <?php //echo "Delivery<pre>",print_r($delivery),"</pre>";?>
         <div class="row">
-            <div class="col-4 text-right font-weight-bold">
-                Delivery:
+            <div class="col-4 text-right">
+                <h4>Delivery:</h4>
             </div>
             <div class="col-8">
-                <?php echo $delivery['delivery_number'];?>
+                <p><?php echo $delivery['delivery_number'];?> </p>
             </div>
         </div>
         <div class="row">
