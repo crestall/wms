d15886e95fcff40150cf4945e4ea0f86277ee3a9
marxolly <mark@solly.com.au>
2022-09-15 11:25:51 +1000

    Update add_quote_package.php

--- app/views/layout/page-includes/add_quote_package.php
+++ app/views/layout/page-includes/add_quote_package.php
@@ -1,6 +1,10 @@
 <?php
 $i = (isset($i))? $i : 0;
 $w = (isset($w))? $w : "";
+$l = (isset($l))? $l : "";
+$h = (isset($h))? $h : "";
+$cw = (isset($cw))? $cw : "";
+$cc = (isset($cc))? $cc : 1;
 ?>
 <div class="p-3 light-grey mb-3 apackage">
     <div class="form-group row">
@@ -16,7 +20,7 @@ $w = (isset($w))? $w : "";
         <label class="col-md-1 mb-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Length</label>
         <div class="col-md-3 mb-3">
             <div class="input-group">
-                <input type="text" class="form-control required number" data-rule-positiveWholeNumber="true" name="items[<?php echo $i;?>][length]" value="" />
+                <input type="text" class="form-control required number" data-rule-positiveWholeNumber="true" name="items[<?php echo $i;?>][length]" value="<?php echo $l;?>" />
                 <div class="input-group-append">
                     <span class="input-group-text">cm</span>
                 </div>
@@ -25,7 +29,7 @@ $w = (isset($w))? $w : "";
         <label class="col-md-1 mb-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Height</label>
         <div class="col-md-3 mb-3">
             <div class="input-group">
-                <input type="text" class="form-control required number" data-rule-positiveWholeNumber="true" name="items[<?php echo $i;?>][height]" value="" />
+                <input type="text" class="form-control required number" data-rule-positiveWholeNumber="true" name="items[<?php echo $i;?>][height]" value="<?php echo $h;?>" />
                 <div class="input-group-append">
                     <span class="input-group-text">cm</span>
                 </div>
@@ -34,7 +38,7 @@ $w = (isset($w))? $w : "";
         <label class="col-md-1 mb-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Weight</label>
         <div class="col-md-3 mb-3">
             <div class="input-group">
-                <input type="text" class="form-control required" data-rule-positiveNumber="true" name="items[<?php echo $i;?>][weight]" value="<?php echo Form::value('carton_weight');?>" />
+                <input type="text" class="form-control required" data-rule-positiveNumber="true" name="items[<?php echo $i;?>][weight]" value="<?php echo $cw?>" />
                 <div class="input-group-append">
                     <span class="input-group-text">kg</span>
                 </div>
@@ -42,7 +46,7 @@ $w = (isset($w))? $w : "";
         </div>
         <label class="col-md-1 mb-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Count</label>
         <div class="col-md-3 mb-3">
-            <input type="text" class="form-control required digits" data-rule-positiveWholeNumber="true" name="items[<?php echo $i;?>][count]" value="1" />
+            <input type="text" class="form-control required digits" data-rule-positiveWholeNumber="true" name="items[<?php echo $i;?>][count]" value="<?php echo $cc?>" />
         </div>
         <div class="col-md-4 mb-3">
             <div class="custom-control custom-checkbox col-sm-2">
