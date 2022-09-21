ea66948a99e16fdf39062e72817e03ab1085d19f
marxolly <mark@solly.com.au>
2022-09-21 10:37:51 +1000

    Update shipping_quotes.php

--- app/views/orders/shipping_quotes.php
+++ app/views/orders/shipping_quotes.php
@@ -60,7 +60,7 @@
             <div class="card-body">
                 <?php echo "<pre>",print_r($df_response),"</pre>"; //die();?>
                 <?php if($df_response['ResponseCode'] == 300):
-                    $fuel_surcharge = 1 + Utility::getDFFuelLevee($charge['FuelLevy']);
+                    $fuel_surcharge = 1 + Utility::getDFFuelLevee($df_response['FuelLevy']);
                     $surcharges = Utility::getDFSurcharges($df_response['df_items']);
                     $surcharges = number_format($surcharges * 1.1 * $fuel_surcharge, 2);
                     $df_charge = number_format($df_response['TotalFreightCharge'] * 1.1 * $fuel_surcharge, 2);
