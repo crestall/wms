23c312afe8f7087d9b2ace84cf3558e3b909fb0f
marxolly <mark@solly.com.au>
2022-09-21 14:41:18 +1000

    Update FormController.php

--- app/controllers/FormController.php
+++ app/controllers/FormController.php
@@ -206,7 +206,7 @@ class FormController extends Controller {
             //echo "ALL GOOD<pre>",print_r($post_data),"</pre>"; die();
             //Create the Direct Freight Consignment
             $deliver_to = (!empty($company_name))? $company_name.": ".$deliver_to:$deliver_to;
-            $details = [
+            $details[] = [
                 "ConsignmentList" => [
                     "ConsignmentId"         => Utility::randomNumber(10),
                     "ReceiverDetails"       => [
@@ -224,7 +224,7 @@ class FormController extends Controller {
             {
                 $rate_type = (isset($it['pallet']))? "PALLET" : "ITEM";
                 $package_description = (isset($it['pallet']))? "Plain Pallet" : "Carton of Goods";
-                $details['ConsignmentList']["ConsignmentLineItems"][] = [
+                $details[0]['ConsignmentList']["ConsignmentLineItems"][] = [
                     "RateType"              => $rate_type,
                     "PackageDescription"    => $package_description,
                     "Items"                 => $it['count'],
@@ -234,13 +234,13 @@ class FormController extends Controller {
                     "Height"                => $it['height']
                 ];
             }
-            if(!empty($tracking_email)) $details['ConsignmentList']['ReceiverDetails'][0]['ReceiverContactEmail'] = $tracking_email;
-            if(!empty($contact_phone)) $details['ConsignmentList']['ReceiverDetails'][0]['ReceiverContactMobile'] = $contact_phone;
-            if(!empty($FSG_reference)) $details['ConsignmentList']['CustomerReference'] = $FSG_reference;
+            if(!empty($tracking_email)) $details[0]['ConsignmentList']['ReceiverDetails'][0]['ReceiverContactEmail'] = $tracking_email;
+            if(!empty($contact_phone)) $details[0]['ConsignmentList']['ReceiverDetails'][0]['ReceiverContactMobile'] = $contact_phone;
+            if(!empty($FSG_reference)) $details[0]['ConsignmentList']['CustomerReference'] = $FSG_reference;
             //create the consignment
             $final_result = [];
             $ConsignmentList[] = $details;
-            echo "DETAILS<pre>",print_r($ConsignmentList),"</pre>";//die();
+            echo "DETAILS<pre>",print_r($details),"</pre>";die();
             $con_result = $this->directfreight->createConsignment($details);
             if($con_result['ResponseCode'] != 300)
             {
