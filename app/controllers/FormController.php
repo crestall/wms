838bbc37b7af8ea937dd86d63f5c287bc09940be
marxolly <mark@solly.com.au>
2022-09-21 15:03:17 +1000

    Update FormController.php

--- app/controllers/FormController.php
+++ app/controllers/FormController.php
@@ -250,7 +250,7 @@ class FormController extends Controller {
             else
             {
                 $consignment = $con_result['ConsignmentList'][0];
-                echo "<p>Consignment: $consingnment</p>";
+                echo "<p>Consignment: $consignment</p>";
                 if($consignment['ResponseCode'] != 200)
                 {
                     //Form::setError('general', $consignment['ResponseMessage']);
