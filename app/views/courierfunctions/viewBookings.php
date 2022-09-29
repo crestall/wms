0284056be21a5d2df03495caf64bb7e8b97c6b01
marxolly <mark@solly.com.au>
2022-09-29 13:46:46 +1000

    Update viewBookings.php

--- app/views/courierfunctions/viewBookings.php
+++ app/views/courierfunctions/viewBookings.php
@@ -7,5 +7,23 @@
         <div class="row">
             <?php echo "<pre>",print_r($bookings),"</pre>";?>
         </div>
+        <?php if(count($bookings)):?>
+            <div id="waiting" class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Drawing Table..</h2>
+                    <p>May take a few moments</p>
+                    <img class='loading' src='/images/preloader.gif' alt='loading...' />
+                </div>
+            </div>
+        <?php else:?>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="errorbox">
+                        <h2><i class="fas fa-exclamation-triangle"></i> No Bookings Listed</h2>
+                        <p>You may need to remove some filters</p>
+                    </div>
+                </div>
+            </div>
+        <?php endif;?>
     </div>
 </div>
