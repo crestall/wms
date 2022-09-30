35c57f8d9525bd8c6cc2f5165cc829e6ac1865e3
marxolly <mark@solly.com.au>
2022-09-30 11:48:27 +1000

    Update viewBookings.php

--- app/views/courierfunctions/viewBookings.php
+++ app/views/courierfunctions/viewBookings.php
@@ -5,7 +5,7 @@
     <div id="page_container" class="container-xxl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <div class="row">
-            <?php echo "<pre>",print_r($bookings),"</pre>";?>
+            <?php //echo "<pre>",print_r($bookings),"</pre>";?>
         </div>
         <?php if(count($bookings)):?>
             <div id="waiting" class="row">
