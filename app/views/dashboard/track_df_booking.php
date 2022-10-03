bd71f4c8833140c67270af43e49d497a7401c1c0
marxolly <mark@solly.com.au>
2022-10-03 14:27:18 +1100

    Update track_df_booking.php

--- app/views/dashboard/track_df_booking.php
+++ app/views/dashboard/track_df_booking.php
@@ -1,4 +1,46 @@
 <div class="page-wrapper">
-    <div class="row" id="feedback_holder" style="display:none"></div>
     <?php echo "<pre>",print_r($tr),"</pre>";?>
+    <div class="p-3 pb-0 mb-2 rounded-top form-section-holder">
+        <div class="row">
+            <div class="col">
+                <h3>Tracking Details</h3>
+            </div>
+        </div>
+        <?php if($tr['ResponseCode'] != 300):?>
+            <div class="p-3 error-box mb-3">
+                <div class="row">
+                    <div class="col">
+                        <h4>There Has Been An error</h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <?php echo $tr['ResponseMessage'];?>
+                    </div>
+                </div>
+            </div>
+        <?php else:
+            $atr = $tr['TrackingResults'];
+            if($atr['ResponseCode'] != 300):?>
+                <div class="p-3 error-box mb-3">
+                    <div class="row">
+                        <div class="col">
+                            <h4>There Has Been An error</h4>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <?php echo $atr['ResponseCode'].": ".$atr['ResponseMessage'];?>
+                        </div>
+                    </div>
+                </div>
+            <?php else:
+                foreach($atr['ConsignmentTrackingDetails'] as $ctd):?>
+                    <div class="p-3 light-grey mb-3">
+                        <?php echo "<pre>",print_r($ctd),"</pre>";?>
+                    </div>
+                <?php endforeach;?>
+            <?php endif;?>
+        <?php endif;?>
+    </div>
 </div>
