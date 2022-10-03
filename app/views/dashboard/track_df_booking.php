101e1153ef48b2e6dc57cc2fbdfa89fa169c62d1
marxolly <mark@solly.com.au>
2022-10-03 14:30:39 +1100

    Update track_df_booking.php

--- app/views/dashboard/track_df_booking.php
+++ app/views/dashboard/track_df_booking.php
@@ -1,5 +1,5 @@
 <div class="page-wrapper">
-    <?php echo "<pre>",print_r($tr),"</pre>"; die();?>
+    <?php //echo "<pre>",print_r($tr),"</pre>"; die();?>
     <div class="p-3 pb-0 mb-2 rounded-top form-section-holder">
         <div class="row">
             <div class="col">
@@ -20,7 +20,7 @@
                 </div>
             </div>
         <?php else:
-            $atr = $tr['TrackingResults'];
+            $atr = $tr['TrackingResults'][0];
             if($atr['ResponseCode'] != 300):?>
                 <div class="p-3 error-box mb-3">
                     <div class="row">
