f513369fae3d9cc32fbb29b4ccd998043d39d03e
marxolly <mark@solly.com.au>
2022-10-21 12:12:27 +1100

    Update footer.php

--- app/views/layout/deliveries/footer.php
+++ app/views/layout/deliveries/footer.php
@@ -200,7 +200,7 @@
                             $('input#item_searcher').rules( "remove" )
                             $('input#item_searcher').rules("add",{
                                 required: function(){
-                                    console.log( "Not Checked boxes: "+ $('input.remove_location:not(":checked")').length );return $('input.remove_location:checked').length  != 0 ; },
+                                    console.log( "Not Checked boxes: "+ $('input.remove_location:not(":checked")').length );return $('input.remove_location:not(":checked")').length   == 0 ; },
                                 messages: {
                                     required: "At least one item must be chosen for delivery"
                                 }
