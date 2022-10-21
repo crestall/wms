9dda4b477192bbbbdec04e8dd79337249bde45e7
marxolly <mark@solly.com.au>
2022-10-21 11:04:10 +1100

    Update footer.php

--- app/views/layout/deliveries/footer.php
+++ app/views/layout/deliveries/footer.php
@@ -140,6 +140,12 @@
                                         }
                                     });
                                 });
+                                $('input#item_searcher').rules("add",{
+                                        required: function(){ return $("input.remove_allocation:not(':checked')").length = 0 ; },
+                                        messages: {
+                                            required: "At least one item must be chosen for delivery"
+                                        }
+                                });
                             }
                             else
                             {
