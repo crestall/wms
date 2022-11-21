625a4285c379a8d5c39a355f104c0d9f73d917f9
marxolly <mark@solly.com.au>
2022-11-21 16:26:10 +1100

    Update AjaxfunctionsController.php

--- app/controllers/AjaxfunctionsController.php
+++ app/controllers/AjaxfunctionsController.php
@@ -1510,7 +1510,7 @@ class ajaxfunctionsController extends Controller
             //echo "<pre>",print_r(json_encode($df_details)),"</pre>"; //die();
             $df_r = $this->directfreight->getQuote($df_details);
             $df_response = json_decode($df_r,true);
-            echo "<pre>",var_dump($df_response),"</pre>"; die();
+            //echo "<pre>",var_dump($df_response),"</pre>"; die();
             if($df_response['ResponseCode'] == 300)
             {
                 $surcharges = Utility::getDFSurcharges($df_details['ConsignmentList'][0]['ConsignmentLineItems']);
