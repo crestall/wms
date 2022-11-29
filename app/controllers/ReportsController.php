00e47f469972df3ff87af7ce233b41ebe9d01a8a
marxolly <mark@solly.com.au>
2022-11-30 10:34:39 +1100

    Update ReportsController.php

--- app/controllers/ReportsController.php
+++ app/controllers/ReportsController.php
@@ -335,6 +335,7 @@ class ReportsController extends Controller
 
     public function dispatchReport()
     {
+        die(Session::getUserRole());
         if(Session::getUserRole() == "client" || Session::getUserRole() == "freedom_warehouse")
         {
             return $this->clientDispatchReport();
