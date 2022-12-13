6740b1d2e667a9d3b17989b6f4d75c9b3d6cd9cf
marxolly <mark@solly.com.au>
2022-12-14 10:43:33 +1100

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,5 @@
-<?php  
-/* Maintenece */
+<?php
+/* Maintenance */
 
 /* Only deliver over https */
 if(!((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443))
