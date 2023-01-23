d01b6d6674ca9cbdc5d92b500d6f6dcf6dce9dea
marxolly <mark@solly.com.au>
2023-01-23 11:31:40 +1100

    Update Eparcel.php

--- app/core/Eparcel.php
+++ app/core/Eparcel.php
@@ -130,6 +130,7 @@
         //echo $data_string; //die();
         $url = eParcel::API_SCHEME . eParcel::API_HOST . eParcel::API_BASE_URL . $s_action;
         echo "<p>URL: ".$url."</p>";;
+        echo "Authorization: Basic ". base64_encode($this->API_KEY . ":" . $this->API_PWD);
         echo "<p>Account ".$this->ACCOUNT_NO."</p>";  die();
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
