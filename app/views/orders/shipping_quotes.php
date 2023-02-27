54218654c45db18fb360d9c79acde7fd9cb02b23
marxolly <mark@solly.com.au>
2023-02-28 10:25:39 +1100

    Update shipping_quotes.php

--- app/views/orders/shipping_quotes.php
+++ app/views/orders/shipping_quotes.php
@@ -1,8 +1,37 @@
 <?php
-//echo "<pre>",print_r($eparcel_response),"</pre>";die();
+//echo "<pre>",print_r($express_response),"</pre>";die();
+$eparcel_express_charge = $eparcel_charge = 0;
+if(!isset($express_response['errors']))
+{
+        $eparcel_express_charge = "$".number_format($express_response['shipments'][0]['shipment_summary']['total_cost_ex_gst'],2);
+
+}
+else
+{
+    ?>
+    <div class="row mb-3">
+        <div class="col-12 errorbox">
+            <h2>There has been an eParcel API error</h2>
+            <p>
+                CODE: <?php echo $eparcel_response['errors'][0]['code']?>
+            </p>
+            <p>
+                NAME: <?php echo $eparcel_response['errors'][0]['name']?>
+            </p>
+            <p>
+                MESSAGE: <?php echo $eparcel_response['errors'][0]['message']?>
+            </p>
+        </div>
+    </div>
+    <?php
+    die();
+}
 if(!isset($eparcel_response['errors']))
 {
-    /*foreach($eparcel_response['items'][0]['prices'] as $pt)
+    if($eparcel_response['shipments'][0]['items'][0]['product_id'] == '3D85')
+        $eparcel_charge = "$".number_format($eparcel_response['shipments'][0]['shipment_summary']['total_cost_ex_gst'],2);
+
+    /*foreach($eparcel_response['shipments'][0]['shipment_summary'] as $pt)
     {
         if($pt['product_id'] == '3D85') //parcelpost
             $eparcel_charge = "$".number_format($pt['calculated_price_ex_gst'],2);
