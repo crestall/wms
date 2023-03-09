0fc22b0fa3f15b48b9351a6f9700163d2863621d
marxolly <mark@solly.com.au>
2023-03-09 11:06:31 +1100

    Update AdminonlyController.php

--- app/controllers/AdminonlyController.php
+++ app/controllers/AdminonlyController.php
@@ -22,7 +22,7 @@ class AdminOnlyController extends Controller
         parent::displayIndex(get_class());
     }
 
-    public function salesianBulkMailOz()
+    public function addSalesianCustomers()
     {
         $sments = array();
         $line = 1;
