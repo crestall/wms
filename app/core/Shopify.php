668416a7f9d75135a661a2daeee470f84b3dbed6
marxolly <mark@solly.com.au>
2023-03-27 16:14:05 +1100

    Update Shopify.php

--- app/core/Shopify.php
+++ app/core/Shopify.php
@@ -307,9 +307,9 @@ class Shopify{
         $this->output .= "==============================================Filtering for already sent============================================".PHP_EOL;
         foreach($collected_orders as $co)
         {
-            if(isset($co['source_name']) && $co['source_name'] == "shopify_draft_order")
-               $this->output .= "Removing ".$co['id']." cos it is a draft".PHP_EOL;
-            elseif(strpos($co['tags'], "sent_to_fsg") === false)
+            //if(isset($co['source_name']) && $co['source_name'] == "shopify_draft_order")
+               //$this->output .= "Removing ".$co['id']." cos it is a draft".PHP_EOL;
+            if(strpos($co['tags'], "sent_to_fsg") === false)
                 $filtered_orders[] = $co;
             else
                 $this->output .= "Removing ".$co['id']." cos we already have it".PHP_EOL;
